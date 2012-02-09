0c82a2f09fa4f21fd7d58c0f26a2a01605e2ec00
dg7 <dg7@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2012-02-09 15:30:00 +0000

    Aggiunta la guida rapida in italiano che appare nei menù printipali dei vari moduli. Questa documentazione fa riferimento a video esterni (su http://www.youtube.com) e ad altra documentazione contenuta nell'opera «a2», disponibile a partire da http://appunti2.net, ovvero da http://informaticalibera.net.

---
+++ modules/contab/help/italian/docume_contab_help.php
@@ -0,0 +1,94 @@
+<p>Men&ugrave; <strong>Contabilit&agrave;</strong>. Qui si raccolgono le
+funzioni relative alla gestine della contabilit&agrave; generale,
+incluse le stampe periodiche. Va osservato per&ograve; che i clienti e i
+fornitori sono accessibili da altri men&ugrave;. Inoltre, la stampa dei
+bilanci e le operazioni di chiusura e riapertura dei conti si trovano
+invece nel men&ugrave; <strong>Fine anno</strong>.</p>
+
+<p>Per accedere agli elenchi delle scritture, dei conti e delle causali
+gi&agrave; esistenti, basta selezionare la voce principale relativa:
+<strong>Prima nota</strong>, <strong>Piano dei conti</strong>,
+<strong>Causali contabili</strong>, tralasciando le voci di men&ugrave;
+ulteriori che appaiono mentre il puntatore del mouse vi si sposta
+sopra.</p>
+
+<ul>
+<li>
+
+    <p><strong>Prima nota</strong></p>
+
+    <p>Da qui &egrave; possibile gestire le scritture contabili della
+    contabilit&agrave; generale. Per inserire un nuovo movimento si deve
+    scegliere la voce <strong>Nuovo movimento contabile</strong>, mentre
+    per accedere alle scritture esistenti, si deve selezionare la voce
+    principale di questo men&ugrave;.</p>
+
+    <p>Esempi: <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=_ursC5We6nY">registrazione con
+    causale libera</a>, <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=XYdFTPwQZFE">registrazione di
+    un acquisto con una causale gi&agrave; predisposta</a>, <a
+    title="guarda il video"
+    href="http://www.youtube.com/watch?v=FKNz1l0gWLw">registrazione di
+    un acquisto con una causale predisposta, ma da modificare</a>.</p>
+
+</li>
+<li>
+
+    <p><strong>Piano dei conti</strong></p>
+
+    <p>Da qui &egrave; possibile gestire il piano dei conti, stampare il
+    partitario ed eventualmente stampare il piano dei conti evidenziando
+    l'associazione alle voci di bilancio.</p>
+
+    <p>Esempio: <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=IXyrXjLTO_Q">stampa del
+    partitario di un cliente</a>.</p>
+
+</li>
+<li>
+
+    <p><strong>Causali contabili</strong></p>
+
+    <p>Da qui &egrave; possibile gestire le causali usate nelle
+    scritture contabili. Le causali predefinite, riconoscibili
+    perch&eacute; sono scritte con un testo tutto maiuscolo, non vanno
+    modificate, essendo utilizzate automaticamente dalla procedura;
+    tuttavia, si possono creare altre causali, in base alla propria
+    convenienza.</p>
+
+</li>
+<li>
+
+    <p><strong>Registri IVA</strong></p>
+
+    <p>Da qui &egrave; possibile ottenere la stampa dei registri IVA, o
+    «registri delle fatture».</p>
+
+    <p>Esempi: <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=lNHfnjJx-tk">stampa del
+    registro IVA degli acquisti in prova</a>, <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=Cxn2zZ3jlG8">stampa del
+    registro IVA delle vendite in prova</a>.</p>
+
+</li>
+<li>
+
+    <p><strong>Liquidazione IVA</strong></p>
+
+    <p>Da qui &egrave; possibile ottenere la stampa della liquidazione IVA.</p>
+
+</li>
+<li>
+
+    <p><strong>Libro giornale</strong></p>
+
+    <p>Da qui &egrave; possibile ottenere la stampa del giornale di
+    contabilit&agrave; generale.</p>
+
+    <p>Esempio: <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=4q3PdZiBAbg">stampa del
+    giornale di contabilit&agrave; generale in prova</a>.</p>
+
+</li>
+</ul>
---
+++ modules/magazz/help/italian/docume_magazz_help.php
@@ -0,0 +1,139 @@
+<p>Men&ugrave; <strong>Magazzino</strong>. Qui si raccolgono le
+funzioni relative alla gestine della contabilit&agrave; di magazzino,
+incluse le stampe periodiche.</p>
+
+<p>Per accedere agli elenchi degli articoli, delle categorie
+merceologiche, dei movimenti e delle causali, basta selezionare la voce
+principale relativa: <strong>Articoli</strong>, <strong>Cat.
+merceologiche</strong>, <strong>Movimenti</strong>,
+<strong>Causali</strong>, tralasciando le voci di men&ugrave; ulteriori
+che appaiono mentre il puntatore del mouse vi si sposta sopra.</p>
+
+<ul>
+<li>
+
+    <p><strong>Articoli</strong></p>
+
+    <p>Da qui &egrave; possibile intervenire nell'elenco degli articoli
+    di magazzino. Per inserire un nuovo articolo si deve scegliere la voce
+    <strong>Nuovo articolo di magazzino</strong>, mentre per accedere agli
+    articoli gi&agrave; esistenti, si deve selezionare la voce principale di
+    questo men&ugrave;.</p>
+
+</li>
+<li>
+
+    <p><strong>Cat. merceologiche</strong></p>
+
+    <p>Gli articoli di magazzino sono raggruppati in categorie, a cui
+    si accede da questa voce di men&ugrave;. Se non si intende suddividere
+    gli articoli in gruppi, &egrave; comunque necessario definire almeno
+    una categoria generica.</p>
+
+    <p>Per inserire una nuova categoria si deve scegliere la voce
+    <menuitem>Nuova categoria merceologica</menuitem>, mentre per accedere
+    alle categorie gi&agrave; esistenti, si deve selezionare la voce
+    principale di questo men&ugrave;.</p>
+
+</li>
+<li>
+
+    <p><strong>Movimenti</strong></p>
+
+    <p>Da qui &egrave; possibile intervenire nei movimenti di magazzino
+    (scritture di carico e scarico). Per inserire un nuovo movimento si
+    deve scegliere la voce <strong>Nuovo movimento di
+    magazzino</strong>, mentre per accedere ai movimenti gi&agrave;
+    esistenti, si deve selezionare la voce principale di questo
+    men&ugrave;.</p>
+
+    <p>Dai movimenti inseriti &egrave; possibile ottenere la stampa del
+    giornale di magazzino attraverso la voce <menuitem>Stampa giornale
+    di magazzino</menuitem>, mentre per ottenere una stampa dettagliata,
+    articolo per articolo, si deve scegliere la voce <menuitem>Stampa
+    schedari articoli di magazzino.</p>
+
+    <!-- <p>In molti casi, i movimenti di magazzino possono essere generati
+    automaticamente, a seguito dell'inserimento di documenti che ne
+    rappresentano implicitamente il movimento (per esempio nel caso dell'emissione
+    dei documenti di trasporto o di fatture). In condizioni normali, la produzione
+    di questi movimenti automatici avviene senza alcuna richiesta esplicita; tuttavia,
+    &egrave; possibile configurare Gazie in modo che non lo faccia: in questa
+    evenienza particolare, si pu&ograve; rendere utile la voce
+    <strong>Genera movimenti di magazzino</strong> per ottenere queste
+    registrazioni.</p> -->
+
+    <p>Esempi: <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=qnypB8B8nH4">carico per
+    acquisto</a>, <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=Wm9Ix2IboiI">scarico per
+    vendita</a>, <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=GIpPRKImO34">carico per
+    acquisto</a>, <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=fyvXMJuCl-g">scarico per
+    vendita</a>, <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=kyV8HJ6cDAE">scarico per
+    vendita</a>, <a title="guarda il video"
+    href="http://www.youtube.com/watch?v=vc5kDenlgHo">stampa del giornale di magazzino</a>.</p>
+
+</li>
+<li>
+
+    <p><strong>Causali</strong></p>
+
+    <p>Da qui &egrave; possibile gestire le causali usate per descrivere
+    i movimenti di magazzino. Le causali predefinite, riconoscibili
+    perch&eacute; sono scritte con un testo tutto maiuscolo, non vanno
+    modificate, essendo utilizzate automaticamente dalla procedura;
+    tuttavia, si possono creare altre causali, in base alla propria
+    convenienza.</p>
+
+    <p>Per inserire una nuova cusale si deve scegliere la voce
+    <menuitem>Nuova causale di magazzino</menuitem>, mentre per accedere
+    alle causali gi&agrave; esistenti, si deve selezionare la voce
+    principale di questo men&ugrave;.</p>
+
+</li>
+<li>
+
+    <p><strong>Listini</strong></p>
+
+    <p>Da qui si ottiene la stampa del listino di vendita, dove viene
+    comunque richiesto di selezionare quale listino evidenziare. Sono
+    disponibili quattro prezzi di vendita alternativi, ma eventualmente,
+    se l'informazione risulta disponibile, &egrave; possibile anche
+    stampare il prezzo dell'ultimo acquisto.</p>
+
+</li>
+<li>
+
+    <p><strong>Modifica prezzi</strong></p>
+
+    <p>Quando si deve procedere alla modifica sistematica dei prezzi di
+    listino di vendita degli articoli, attraverso questa funzione è
+    possibile attribuire un incremento fisso o percentuale, rispetto al
+    prezzo unitario annotato in precedenza. Si osservi che se il
+    cambiamento riguarda uno solo o pochi articoli, conviene intervenire
+    per questo tipo di modifica direttamente nell'elenco degli
+    articoli.</p>
+
+</li>
+<li>
+
+    <p><strong>Catalogo</strong></p>
+
+    <p>Da qui &egrave; possibile ottenere la stampa di un catalogo degli
+    articoli di magazzino, scegliendo uno dei prezzi di listino di vendita
+    disponibili.</p>
+
+</li>
+<li>
+
+    <p><strong>Statistiche</strong></p>
+
+    <p>Da qui &egrave; possibile ottenere un grafico con l'entit&agrave;
+    degli scarichi per vendita, o dei carichi per acquisto, suddiviso
+    per articolo.</p>
+
+</li>
+</ul>
