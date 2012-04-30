e2d386a07853a46cc83185dbfb2afe46bfe0886e
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2012-04-30 20:17:46 +0000

    pr indicare a mysql il charset utilizzato per il backup

--- modules/inform/backup.php
+++ modules/inform/backup.php
@@ -158,22 +158,21 @@ if ($form['do_backup'] != 1)
     echo "-- originaria e di selezionarla prima di procedere con il recupero\n";
     echo "-- delle tabelle.\n";
     echo "--\n";
-    if ($form['create_database'] == 1)
-      {
+    if ($form['create_database'] == 1) {
         echo "CREATE DATABASE IF NOT EXISTS $Database;\n";
-      }
-    else
-      {
+    } else {
         echo "-- CREATE DATABASE IF NOT EXISTS $Database;\n";
-      }
-    if ($form['use_database'] == 1)
-      {
+    }
+    if ($form['use_database'] == 1) {
         echo "USE $Database;\n";
-      }
-    else
-      {
+    } else {
         echo "-- USE $Database;\n";
-      }
+    }
+    if ($form['text_encoding'] == 0) {
+        echo "SET NAMES utf8;\n";
+    } else {
+        echo "SET NAMES latin1;\n";
+    }
     echo "\n";
     echo "\n";
     echo "\n";
