fe99a49bbc3f8d04d51664491c3e6808b8414412
dg7 <dg7@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2012-08-25 11:01:24 +0000

    Mi ero dimenticato un punto e virgola... :-|

--- config/config/gconfig.php
+++ config/config/gconfig.php
@@ -72,7 +72,7 @@ $table_prefix = "gaz";
 // Utente proposto inizialmente per l'accesso a Gazie. Se non si vuole
 // suggerire alcunché, è sufficiente assegnare la stringa vuota.
 //
-$default_user = "amministratore"
+$default_user = "amministratore";
 //
 // Fuso orario, per la rappresentazione corretta delle date, indipendentemente
 // dalla collocazione del server HTTP+PHP. MA NON FUNZIONA, perché MySQL aggiorna
