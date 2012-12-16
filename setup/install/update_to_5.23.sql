e1120d32b4bcafaddef08e6deb9c0597c2e5a261
jaures <jaures@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2012-12-16 21:38:13 +0000

    Rimosso commento inutile

--- setup/install/update_to_5.23.sql
+++ setup/install/update_to_5.23.sql
@@ -1,6 +1,5 @@
 UPDATE `gaz_config` SET `cvalue` = '81' WHERE `id` =2;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
--- DELETE FROM `gaz_XXXcompany_config` WHERE var='smtp_auth';
 INSERT INTO `gaz_XXXcompany_config` SELECT MAX(id)+1, 'Mailer (mail,smtp,sendmail,qmail)', 'mailer', 'smtp'  FROM `gaz_XXXcompany_config`;
 INSERT INTO `gaz_XXXcompany_config` SELECT MAX(id)+1, 'SMTP Port (25,587,465)', 'smtp_port', '25'  FROM `gaz_XXXcompany_config`;
 INSERT INTO `gaz_XXXcompany_config` SELECT MAX(id)+1, 'SMTP Secure (tls,ssl)', 'smtp_secure', ''  FROM `gaz_XXXcompany_config`;
