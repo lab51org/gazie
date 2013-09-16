b1cb8224a5750472c019efa2410397cd4f98e726
zanella69 <zanella69@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2013-09-16 09:08:44 +0000

    Aggiunta ricerca per cliente

--- modules/vendit/report_doctra.php
+++ modules/vendit/report_doctra.php
@@ -34,12 +34,22 @@ if (isset($_GET['auxil'])) {
 }
 $where = " (tipdoc = 'FAD' or tipdoc like 'DD_') and seziva = '$auxil'";
 $documento ='';
+$cliente='';
 if (isset($_GET['numdoc'])) {
    if ($_GET['numdoc'] > 0) {
       $documento = $_GET['numdoc'];
-      $auxil = $_GET['auxil']."&numdoc=".$documento;
       $where = " (tipdoc = 'FAD' or tipdoc like 'DD_') and seziva = '$auxil' and numdoc = '$documento'";
       $passo = 1;
+      $auxil = $_GET['auxil']."&numdoc=".$documento;
+   }
+}
+if (isset($_GET['cliente'])) {
+   if ($_GET['cliente'] <> '') {
+      $cliente = $_GET['cliente'];
+      $where = " (tipdoc = 'FAD' or tipdoc like 'DD_') and seziva = '$auxil' and descri like '%$cliente%'";
+      $passo = 50;
+      $auxil = $_GET['auxil']."&cliente=".$cliente;
+	  unset($documento);
    }
 }
 if (isset($_GET['all'])) {
@@ -47,6 +57,8 @@ if (isset($_GET['all'])) {
    $auxil = $_GET['auxil']."&all=yes";
    $passo = 100000;
    $where = " (tipdoc = 'FAD' or tipdoc like 'DD_') and seziva = '$auxil'";
+   unset($documento);
+   unset($cliente);
 }
 
 $titolo="Documenti di trasporto";
@@ -118,21 +130,25 @@ for ($sez = 1; $sez <= 3; $sez++) {
 <?php
 if (!isset($_GET['field']) or ($_GET['field'] == 2) or(empty($_GET['field'])))
         $orderby = "datemi desc, numdoc desc";
-$recordnav = new recordnav($gTables['tesdoc'], $where, $limit, $passo);
+$recordnav = new recordnav($gTables['tesdoc'].' LEFT JOIN '.$gTables['clfoco'].' on '.$gTables['tesdoc'].'.clfoco = '.$gTables['clfoco'].'.codice', $where, $limit, $passo);
 $recordnav -> output();
 ?>
 <table class="Tlarge">
-<tr>
-<td colspan="2" class="FacetFieldCaptionTD">Numero:
-<input type="text" name="numdoc" value="<?php if ($documento > 0) print $documento; ?>" maxlength="6" size="3" tabindex="1" class="FacetInput">
-</td>
-<td>
-<input type="submit" name="search" value="Cerca" tabindex="1" onClick="javascript:document.report.all.value=1;">
-</td>
-<td>
-<input type="submit" name="all" value="Mostra tutti" onClick="javascript:document.report.all.value=1;">
-</td>
-</tr>
+  <tr>
+     <td class="FacetFieldCaptionTD">Numero:
+       <input type="text" name="numdoc" value="<?php if ($documento > 0) print $documento; ?>" maxlength="6" size="3" tabindex="1" class="FacetInput">
+     </td>
+     <td colspan="2" class="FacetFieldCaptionTD">Cliente:
+       <input type="text" name="cliente" value="<?php if ($cliente <> '') print $cliente; ?>" maxlength="40" size="30" tabindex="2" class="FacetInput">
+     </td>
+     <td>
+       <input type="submit" name="search" value="Cerca" tabindex="1" onClick="javascript:document.report.all.value=1;">
+     </td>
+     <td colspan="4">
+       <input type="submit" name="all" value="Mostra tutti" onClick="javascript:document.report.all.value=1;">
+     </td>
+ </tr>
+
 <tr>
 <?php
 // creo l'array (header => campi) per l'ordinamento dei record
@@ -152,7 +168,7 @@ $linkHeaders -> output();
 ?>
 </tr>
 <?php
-$rs_ultimo_documento = gaz_dbi_dyn_query("*", $gTables['tesdoc'], $where,"datemi desc, numdoc desc",0,1);
+$rs_ultimo_documento = gaz_dbi_dyn_query("*", $gTables['tesdoc'].' LEFT JOIN '.$gTables['clfoco'].' on '.$gTables['tesdoc'].'.clfoco = '.$gTables['clfoco'].'.codice', $where,"datemi desc, numdoc desc",0,1);
 $ultimo_documento = gaz_dbi_fetch_array($rs_ultimo_documento);
 if ($ultimo_documento)
     $ultimoddt = $ultimo_documento['numdoc'];
@@ -275,7 +291,7 @@ while ($r = gaz_dbi_fetch_array($result)) {
     }
 }
 ?>
-</form>
 </table>
+</form>
 </body>
 </html>
