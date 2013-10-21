336ad89f992477867797e7aaeaffea8233f860c2
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2013-10-21 07:50:02 +0000

    aggiunto controllo mancanza conti di riferimento

--- modules/finann/error_rigmoc.php
+++ modules/finann/error_rigmoc.php
@@ -25,9 +25,7 @@
 require("../../library/include/datlib.inc.php");
 
 $admin_aziend=checkAdmin();
-$where = "codcon not like '%000000' group by ".$gTables['rigmoc'].".id_tes";
-$orderby = "1";
-$titolo = "Controllo sbilancio movimenti contabili";
+$titolo = "Controllo movimenti contabili";
 
 require("../../library/include/header.php");
 $script_transl=HeadMain();
@@ -36,18 +34,32 @@ echo '<div align="center" class="FacetFormHeaderFont">Controllo sbilancio movime
 <table class="Tlarge">
 <form method="POST">
 <?php
-$result = gaz_dbi_dyn_query ($gTables['tesmov'].".id_tes,descri,sum(import*(darave='D')) as dare,sum(import*(darave='A')) as avere", $gTables['rigmoc']." left join ".$gTables['tesmov']." on ".$gTables['rigmoc'].".id_tes = ".$gTables['tesmov'].".id_tes ", $where, $orderby);
+$where = "codcon not like '%000000' group by ".$gTables['rigmoc'].".id_tes";
+$result = gaz_dbi_dyn_query ($gTables['tesmov'].".id_tes, ".$gTables['rigmoc'].".codcon as accnum,descri,sum(import*(darave='D')) as dare,sum(import*(darave='A')) as avere", $gTables['rigmoc']." left join ".$gTables['tesmov']." on ".$gTables['rigmoc'].".id_tes = ".$gTables['tesmov'].".id_tes ", $where, 1);
 $message = '<tr><th class="FacetFieldCaptionTD">Numero ID</th><th class="FacetFieldCaptionTD">Descrizione </th><th class="FacetFieldCaptionTD">DARE </th><th class="FacetFieldCaptionTD">AVERE </th><th class="FacetFieldCaptionTD">SBILANCIO</th></tr>
-         <tr><td class="FacetDataTDred" align="left" colspan="5">I seguenti movimenti presentano degli errori di sbilancio DARE/AVERE quindi sono da modificare cliccando sul numero di ID :</td></tr>';
+         <tr><td class="FacetDataTDred" align="left" colspan="5">I seguenti movimenti contabili presentano degli errori e quindi sono da modificare cliccando sul numero di ID :</td></tr>';
 while ($a_row = gaz_dbi_fetch_array($result)) {
       if ($a_row['dare'] != $a_row['avere']){
-         if ($message !=  "") {
+       if ($message != "") {
             echo $message;
             $message = "";
-         }
+       }                         
          echo "<tr><td class=\"FacetDataTD\" align=\"center\"><a href=\"../contab/admin_movcon.php?Update&id_tes=".$a_row["id_tes"]."\" title=\"Modifica il movimento\" >".$a_row["id_tes"]."</a></td><td class=\"FacetDataTD\">".$a_row["descri"]."</td><td class=\"FacetDataTD\" align=\"right\">".$a_row["dare"]."</td><td class=\"FacetDataTD\" align=\"right\">".$a_row["avere"]."</td><td class=\"FacetDataTDred\" align=\"right\">".gaz_format_number($a_row["dare"]-$a_row["avere"])."</td></tr>\n";
       }
 }
+
+$where = "codcon < 1";
+$result = gaz_dbi_dyn_query ($gTables['tesmov'].".id_tes, ".$gTables['rigmoc'].".codcon as accnum, id_rig,descri ", $gTables['rigmoc']." left join ".$gTables['tesmov']." on ".$gTables['rigmoc'].".id_tes = ".$gTables['tesmov'].".id_tes ", $where, 1);
+while ($a_row = gaz_dbi_fetch_array($result)) {
+      if ($a_row['accnum'] < 100000000 ){
+       if ($message != "") {
+            echo $message;
+            $message = "";
+       }                         
+         echo "<tr><td class=\"FacetDataTD\" align=\"center\"><a href=\"../contab/admin_movcon.php?Update&id_tes=".$a_row["id_tes"]."\" title=\"Modifica il movimento\" >".$a_row["id_tes"]."</a></td><td class=\"FacetDataTD\">".$a_row["descri"]."</td><td class=\"FacetDataTDred\" colspan=\"3\" align=\"center\">Il rigo ".$a_row["id_rig"]." non ha il conto di riferimento</td></tr>\n";
+      }
+}
+
 if ($message !=  "") {
    echo "<tr><td class=\"FacetFormHeaderFont\" align=\"center\" colspan=\"5\">Il controllo effettuato non ha evidanziato movimenti con sbilanci DARE/AVERE !</td></tr>\n";
 }
