e7607da06ae803c4eab5aa00166c4ed117d828f2
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2013-12-09 13:33:50 +0000

    GAzie 5.25 ready

--- config/config/gconfig.php
+++ config/config/gconfig.php
@@ -26,7 +26,7 @@ if (isset($_SERVER['SCRIPT_FILENAME']) && (str_replace('\\','/',__FILE__) == $_S
     exit('Accesso diretto non consentito') ;
 }
 //versione software
-$versSw = '5.24';
+$versSw = '5.25';
 
 //nome DBMS usato per la libreria specifica (MySQL=mysql.lib, SQLite=sqlite.lib, ecc)
 //per il momento disponibile solo la libreria mysql.lib
