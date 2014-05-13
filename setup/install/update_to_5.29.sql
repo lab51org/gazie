847c954c2f743064a5f241742872e44604e6f8ce
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-05-13 15:09:33 +0000

    aggiunto il regime fiscale in configurazione azienda

--- setup/install/update_to_5.29.sql
+++ setup/install/update_to_5.29.sql
@@ -1,4 +1,6 @@
 UPDATE `gaz_config` SET `cvalue` = '85' WHERE `id` =2;
 ALTER TABLE `gaz_anagra` ADD `fe_cod_univoco` VARCHAR( 6 ) NOT NULL AFTER `pariva` ;
+ALTER TABLE `gaz_aziend` ADD `fiscal_reg` VARCHAR( 4 ) NOT NULL AFTER `regime` ;
+UPDATE `gaz_aziend` SET `fiscal_reg`= 'RF01' WHERE 1;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query seguenti su tutte le aziende dell'installazione)
