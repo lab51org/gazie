607e7be42fac0c1a19a6e18ff887c7ceadc40d7b
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-05-20 07:30:23 +0000

    per permettere l'aggiunta di tipi di righi diversi (es. con tag CUP,CIG, ecc per la fattura elettronica della pubblica amministrazione)

--- setup/install/update_to_5.29.sql
+++ setup/install/update_to_5.29.sql
@@ -3,4 +3,5 @@ ALTER TABLE `gaz_anagra` ADD `fe_cod_univoco` VARCHAR( 6 ) NOT NULL AFTER `pariv
 ALTER TABLE `gaz_aziend` ADD `fiscal_reg` VARCHAR( 4 ) NOT NULL AFTER `regime` ;
 UPDATE `gaz_aziend` SET `fiscal_reg`= 'RF01' WHERE 1;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+ALTER TABLE `gaz_XXXrigdoc` CHANGE `tiprig` `tiprig` INT( 2 ) NOT NULL DEFAULT '0';
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query seguenti su tutte le aziende dell'installazione)
