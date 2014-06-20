37374ea58f05060f1a82ac240054aa7fc294d722
gvalentino <gvalentino@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-06-20 15:20:59 +0000

    lavori per poter gestire fae_flux

--- modules/vendit/check_fae_sdi.php
+++ modules/vendit/check_fae_sdi.php
@@ -4,7 +4,7 @@ require_once('../../library/php-imap/ImapMailbox.php');
 require("../../library/include/datlib.inc.php");
 
 $admin_aziend=checkAdmin();
-gaz_set_time_limit (0);
+set_time_limit(3600);
 global $gTables;
 // IMAP  
 $cemail = gaz_dbi_get_row($gTables['company_config'],'var','cemail');
@@ -38,7 +38,7 @@ $domDoc = new DOMDocument;
 
 foreach($mailsIds as $mailId) {
   $mail = $mailbox->getMail($mailId);
-  
+    
     $aaa= $mail->getAttachments();
     $ccc = array_values($aaa);  
     $bbb = $ccc[0];
@@ -54,6 +54,11 @@ foreach($mailsIds as $mailId) {
 	
 	$result = $xpath->query("//DataOraRicezione")->item(0);
     $data_ora_ricezione = $result->textContent;
+
+
+   $result = $xpath->query("//MessageId")->item(0);
+   $message_id = $result->textContent;    
+    
 	
 	$result = $xpath->query("//ListaErrori/Errore/Descrizione")->item(0);
 
@@ -64,21 +69,33 @@ foreach($mailsIds as $mailId) {
     
 	
   
-    print $idsidi . " " . $nome_file . " " . $errore;
+
+  
+    $status=""; 
+    if (strpos($bbb->name, '_MC_') >0) {
+       $status = "Mancata consegna"; 
+    } elseif (strpos($bbb->name, '_NS_') >0) {
+       $status = "Notifica di scarto";
+    } elseif (strpos($bbb->name, '_RC_') >0) {
+       $status = "Consegnata";   
+    }   
   
    $valori=array('filename_ori'=>$nome_file,
-                 'id_tes_ref'=>11,
+         'id_tes_ref'=>11,
 				 'exec_date'=>$data_ora_ricezione,
 				 'filename_son'=>'',
 				 'id_SDI'=>$idsidi,
+         'filename_ret'=>$bbb->name,
+         'mail_id'=>$message_id,
 				 'data'=>'',
-				 'status'=>'',
-				 'descri'=>'');
+				 'status'=>$status,
+				 'descri'=>$errore);
     
     fae_fluxInsert($valori);
+    print $idsidi . " " . $nome_file . " " . $status ."<br/>";
   
 }
 
-gaz_set_time_limit (30);
+
 
 ?>
