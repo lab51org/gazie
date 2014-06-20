4922d33bb14b4e111e2ddcf1a0f26d0e9f65708d
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-06-20 13:34:34 +0000

    aggiunte colonne id_mail e filename_ret

--- setup/install/update_to_5.30.sql
+++ setup/install/update_to_5.30.sql
@@ -2,10 +2,6 @@ UPDATE `gaz_config` SET `cvalue` = '86' WHERE `id` =2;
 ALTER TABLE `gaz_aziend` ADD `virtual_stamp_auth_prot` VARCHAR( 14 ) NOT NULL AFTER `round_bol` ,
 ADD `virtual_stamp_auth_date` DATE NOT NULL AFTER `virtual_stamp_auth_prot`, ADD `causale_pagam_770` VARCHAR( 1 ) NOT NULL AFTER `virtual_stamp_auth_date`; 
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
-CREATE TABLE IF NOT EXISTS `gaz_XXXfae_flux` (  `id` int(11) NOT NULL AUTO_INCREMENT,  `filename_ori` varchar(30) NOT NULL,  `id_tes_ref` int(9) NOT NULL DEFAULT '0',  `exec_date` datetime NOT NULL,  `filename_son` varchar(30) NOT NULL,  `id_SDI` int(20) NOT NULL DEFAULT '0',  `data` blob NOT NULL,  `status` varchar(10) NOT NULL,  `descri` varchar(255) NOT NULL DEFAULT '',  PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-INSERT INTO `gaz_XXXcompany_config` (`id`, `description`, `var`, `val`) VALUES 
-(15, 'Casella di posta', 'cemail', ''), 
-(16, 'Password', 'cpassword', ''), 
-(17, 'FIltro casella di posta', 'cfiltro', ''), 
-(18, 'Configurazione pop imap', 'cpopimap', '');
+CREATE TABLE IF NOT EXISTS `gaz_XXXfae_flux` (  `id` int(11) NOT NULL AUTO_INCREMENT,  `filename_ori` varchar(30) NOT NULL,  `id_tes_ref` int(9) NOT NULL DEFAULT '0',  `exec_date` datetime NOT NULL,  `filename_son` varchar(30) NOT NULL,  `id_SDI` int(20) NOT NULL DEFAULT '0',  `filename_ret` varchar(30) NOT NULL,  `mail_id` varchar(50) NOT NULL,  `data` blob NOT NULL,  `status` varchar(10) NOT NULL,  `descri` varchar(255) NOT NULL DEFAULT '',  PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+INSERT INTO `gaz_XXXcompany_config` (`id`, `description`, `var`, `val`) VALUES (15, 'Casella di posta', 'cemail', ''), (16, 'Password', 'cpassword', ''),(17, 'FIltro casella di posta', 'cfiltro', ''), (18, 'Configurazione pop imap', 'cpopimap', '');
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query seguenti su tutte le aziende dell'installazione)
