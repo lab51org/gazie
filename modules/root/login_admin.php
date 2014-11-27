23aa198e350da452bab8d28f6b24abb0001adf5e
andreabia <andreabia@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-11-27 20:20:18 +0000

    Modificata la pagina di login in stile bootstrap

--- modules/root/login_admin.php
+++ modules/root/login_admin.php
@@ -124,18 +124,22 @@ if ((isset($_SESSION['Abilit']) and isset($_SESSION["Login"])) and ($_SESSION['A
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="author" content="Antonio De Vincentiis http://www.devincentiis.it">
-<link rel="stylesheet" type="text/css" href="../../library/style/default.css">
+
+<link rel="stylesheet" href="../../library/bootstrap/css/bootstrap.min.css" >
 <link rel="shortcut icon" href="../../library/images/favicon.ico">
+<link rel="stylesheet" type="text/css" href="../../library/style/default.css">
 <title>Gazie - Login</title>
 <noscript>
 <h1><center>
 ATTENTION!!! <br />Yours browser it is not qualified to execute code Javascript, in order to use GAZIE is indispensable to change such formulation!<BR />
 ATTENZIONE!!!<br />Il tuo browser non &egrave; abilitato ad eseguire codice Javascript, per usare GAZIE &egrave; indispensabile cambiare tale impostazione!<br />
 </h1></noscript>
+
 <script language="JavaScript1.2" src="../../js/cookies/cookies.js"></script>
 <script language="JavaScript" src="../../js/md5/md5.js"></script>
-<script language="JavaScript" src="../../js/jquery/jquery-1.3.2.min.js"></script>
+<script language="JavaScript" src="../../library/bootstrap/js/jquery.min.js"></script>
 <script language="JavaScript" src="../../js/jquery/capslock.js"></script>
+
 <script type="text/javascript">
       $(document).ready(function() {
 
@@ -149,43 +153,85 @@ ATTENZIONE!!!<br />Il tuo browser non &egrave; abilitato ad eseguire codice Java
         $("#ppass").focus();
 
       });
+	  
+	  function showPassword() {
+    
+    var key_attr = $('#Password').attr('type');
+    
+    if(key_attr != 'text') {
+        
+        $('.checkbox').addClass('show');
+        $('#Password').attr('type', 'text');
+        
+    } else {
+        
+        $('.checkbox').removeClass('show');
+        $('#Password').attr('type', 'password');
+        
+    }
+    
+}
 </script>
 </head>
 <body background="../../library/images/sfondo.png">
 <form method="post" onsubmit="document.forms[0].Password.value=hex_hmac_md5(document.forms[0].Password.value, GetCookie('<?php echo session_name(); ?>'));" action="<?php echo "login_admin.php?tp=".$tp; ?> ">
 <input type="hidden" name="tp" value="<?php echo $tp; ?>" />
-<div align="center" class="FacetFormHeaderFont" >Authentication</div>
-<table align="center" border="0" cellpadding="3" cellspacing="1" class="FacetFormTABLE">
-<!-- BEGIN Error -->
-    <?php
-    if (!$message == "") {
-        print '<tr><td colspan="3" class="FacetDataTDred">'.$message.'</td></tr>';
-    }
-    ?>
-<!-- END Error -->
-<tr><td class="FacetDataTD" rowspan="2"><img src="../../library/images/gazie.gif" width="51"></td>
-<td class="FacetFieldCaptionTD">User </td>
-<td class="FacetDataTD"><input type="text" name="Login" value="<?php if (isset($_POST['Login'])) {echo $form['Login'];} else {if (isset($usr)) {echo $usr;}}; ?>" maxlength="30" size="20" class="FacetInput"> </td>
-</tr>
-<tr>
-<td class="FacetFieldCaptionTD">Password </td>
-<td class="FacetDataTD"><input type="password" name="Password" value="" maxlength="20" size="20" class="FacetInput" id="ppass"><div class="FacetDataTDred" id="pmsg"></div></td>
-</tr>
-<?php
-    if ($newpass == true) {
-        echo "<tr><td class=\"FacetDataTD\"></td><td class=\"FacetFieldCaptionTD\">$script_transl[5]</td>";
-        echo "<td class=\"FacetDataTD\"><input type=\"password\" name=\"Nuovapass\" value=\"\" maxlength=\"20\" size=\"20\" class=\"FacetInput\"> </td><tr>\n";
-        echo "<tr><td class=\"FacetDataTD\"></td><td class=\"FacetFieldCaptionTD\">$script_transl[6]</td>";
-        echo "<td class=\"FacetDataTD\"><input type=\"password\" name=\"Confepass\" value=\"\" maxlength=\"20\" size=\"20\" class=\"FacetInput\"> </td><tr>\n";
-    }
-?>
-  <tr>
-    <td colspan="3" align="center">
-      <!-- BEGIN Button DoLogin --><input name="actionflag" type="submit" value="Login" ><!-- END Button DoLogin -->
-    </td>
-  </tr></table>
+<div class="container">    
+    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+        <div class="panel panel-info" >
+            <div class="panel-heading panel-gazie">
+                <div class="panel-title"><img width="5%" src="../../library/images/gazie.gif" /> Accesso al sistema</div>
+                <div style="color: red; float:right; font-size: 100%; position: relative; top:-10px"></div>
+            </div>
+
+            <div style="padding-top:30px" class="panel-body" >
+			<?php
+			if (!$message == "") {
+				echo '<div id="login-alert" class="alert alert-danger col-sm-12">';
+				print $message;
+				echo '</div>';
+			}
+			?>
+			<p><h4>Benvenuto in GAzie</h4>GAzie è il sistema di Gestione della tua Azienda.
+			Con GAzie è possibile tenere traccia dei clienti, mantenerne il conto
+			e la loro storia, preparare preventivi e fatture, gestire i 
+			costi, e molto, molto altro.</p>
+			<p>Inserisci il nome utente e la password che ti sono stati assegnati per iniziare :
+			</p><br/>
+                    <div style="margin-bottom: 25px" class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                        <input id="login-username" type="text" class="form-control" name="Login" value="<?php if (isset($_POST['Login'])) {echo $form['Login'];} else {if (isset($usr)) {echo $usr;}}; ?>" placeholder="Inserisci il Nome Utente">
+                                    </div>
+                                
+                    <div style="margin-bottom: 25px" class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                        <input id="login-password" type="password" class="form-control" name="Password" placeholder="Inserisci la Password">
+                                    </div>
+					<?php
+					if ($newpass == true) {
+					?>
+					Nuova Password
+					<div style="margin-bottom: 25px" class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+						<input id="login-password" type="password" class="form-control" name="Nuovapass" placeholder="Inserisci Nuova Password">
+					</div>
+
+					Conferma Password			
+					<div style="margin-bottom: 25px" class="input-group">	
+						<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+						<input id="login-password" type="password" class="form-control" name="Confepass" placeholder="Reinserisci la Password">
+					</div>
+					<?php
+					}
+					?>
+                    <div style="margin-top:10px" class="form-group">
+                        <div class="col-sm-12 controls">
+                            <input style="float:right;" class="btn btn-success" name="actionflag" type="submit" value="Login" >
+                        </div>
+                    </div>
+                </div>                     
+            </div>  
+        </div>
 </form>
-<!-- END Record Login -->
 </body>
 </html>
-
