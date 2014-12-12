54c392466a562eb77e798c32f5ff6b4a0559c6ab
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-12-12 08:52:06 +0000

    tolte le due icone di prova dal database

--- setup/install/update_to_5.31.sql
+++ setup/install/update_to_5.31.sql
@@ -1,6 +1,4 @@
 UPDATE `gaz_config` SET `cvalue` = '87' WHERE `id` =2;
 UPDATE `gaz_admin` SET `style` = 'default.css' WHERE 1;
-UPDATE `gaz_menu_module` SET `icon` = 'admin_bank_account.png' WHERE `gaz_menu_module`.`id` = 38;
-UPDATE `gaz_menu_script` SET `icon` = 'admin_contract.png' WHERE `gaz_menu_script`.`id` =55;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query seguenti su tutte le aziende dell'installazione)
