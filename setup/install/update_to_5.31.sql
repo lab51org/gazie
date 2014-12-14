3b842711bb4b683f5cb88261bfb1231c0447eb20
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-12-14 20:30:50 +0000

    aggiunti campi per la gestione dei bolli sui documenti senza iva applicata

--- setup/install/update_to_5.31.sql
+++ setup/install/update_to_5.31.sql
@@ -1,4 +1,7 @@
 UPDATE `gaz_config` SET `cvalue` = '87' WHERE `id` =2;
 UPDATE `gaz_admin` SET `style` = 'default.css' WHERE 1;
+ALTER TABLE `gaz_aziend` ADD `ricbol_limit` DECIMAL(5,2) NOT NULL AFTER `acciva`;
+UPDATE `gaz_aziend` SET `ricbol_limit` = 77.47 WHERE 1; 
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+ALTER TABLE `gaz_XXXtesdoc` ADD `ricbol` DECIMAL(5,2) NOT NULL AFTER `stamp`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query seguenti su tutte le aziende dell'installazione)
