ac9f29110b36796e74982ef540b9bd6463da6699
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-12-15 10:00:56 +0000

    bolli anche sugli ordini/preventivi

--- setup/install/update_to_5.31.sql
+++ setup/install/update_to_5.31.sql
@@ -4,4 +4,5 @@ ALTER TABLE `gaz_aziend` ADD `ricbol_limit` DECIMAL(5,2) NOT NULL AFTER `acciva`
 UPDATE `gaz_aziend` SET `ricbol_limit` = 77.47 WHERE 1; 
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXtesdoc` ADD `ricbol` DECIMAL(5,2) NOT NULL AFTER `stamp`;
+ALTER TABLE `gaz_XXXtesbro` ADD `ricbol` DECIMAL(5,2) NOT NULL AFTER `stamp`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query seguenti su tutte le aziende dell'installazione)
