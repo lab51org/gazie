e386f7cb1959f6dd3d779a65983099f4fa870dfb
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-12-16 07:13:09 +0000

    eliminate funzioni non più utilizzate

--- modules/config/lib.data.php
+++ modules/config/lib.data.php
@@ -1,41 +1,4 @@
 <?php
-function aziendInsert ($codice, $newValue)
-{
-    $table = 'aziend';
-    $columns = array('ragso1', 'ragso2', 'image' ,'sedleg', 'legrap', 'sexper', 'datnas',
-                    'luonas', 'pronas','indspe', 'capspe', 'citspe', 'prospe', 'country',
-                    'telefo', 'fax', 'codfis', 'pariva', 'rea', 'e_mail', 'cod_ateco',
-                    'regime', 'vat_susp', 'decimal_quantity', 'decimal_price', 'stock_eval_method',
-                    'mascli', 'masfor', 'masban', 'cassa_', 'ivaacq', 'ivaven', 'iva_susp', 'ivacor',
-                    'ivaera', 'impven', 'imptra', 'impimb', 'impspe', 'impvar', 'boleff', 'omaggi',
-                    'impacq', 'cost_tra' ,'cost_imb' ,'cost_var' ,'latitude', 'longitude',
-                    'coriba', 'cotrat', 'cocamb', 'c_ritenute', 'ritenuta', 'upgrie', 'upggio',
-                    'upginv', 'upgve1', 'upgve2', 'upgve3', 'upgac1', 'upgac2', 'upgac3', 'upgco1',
-                    'upgco2', 'upgco3', 'acciva', 'ricbol', 'perbol', 'ivabol', 'round_bol', 'sperib',
-                    'desez1', 'desez2', 'desez3', 'fatimm', 'artsea', 'colore', 'conmag', 'ivam_t',
-                    'interessi', 'alliva', 'adminid');
-    $newValue['adminid'] = $_SESSION['Login'];
-    tableInsert($table, $columns, $newValue);
-}
-
-function aziendUpdate ($codice, $newValue)
-{
-   $table = 'aziend';
-   $columns = array('ragso1', 'ragso2', 'image' ,'sedleg', 'legrap', 'sexper', 'datnas',
-                    'luonas', 'pronas','indspe', 'capspe', 'citspe', 'prospe', 'country',
-                    'telefo', 'fax', 'codfis', 'pariva', 'rea', 'e_mail', 'cod_ateco',
-                    'regime', 'vat_susp', 'decimal_quantity', 'decimal_price', 'stock_eval_method',
-                    'mascli', 'masfor', 'masban', 'cassa_', 'ivaacq', 'ivaven', 'ivacor',
-                    'ivaera', 'impven', 'imptra', 'impimb', 'impspe', 'impvar', 'boleff', 'omaggi',
-                    'impacq', 'cost_tra' ,'cost_imb' ,'cost_var' ,'latitude', 'longitude',
-                    'coriba', 'cotrat', 'cocamb', 'c_ritenute', 'ritenuta', 'upgrie', 'upggio',
-                    'upginv', 'upgve1', 'upgve2', 'upgve3', 'upgac1', 'upgac2', 'upgac3', 'upgco1',
-                    'upgco2', 'upgco3', 'acciva', 'ricbol', 'perbol', 'ivabol', 'round_bol', 'sperib',
-                    'desez1', 'desez2', 'desez3', 'fatimm', 'artsea', 'colore', 'conmag', 'ivam_t',
-                    'interessi', 'alliva', 'adminid');
-    $newValue['adminid'] = $_SESSION['Login'];
-    tableUpdate($table, $columns, $codice, $newValue);
-}
 
 function aliivaInsert ($newValue)
 {
