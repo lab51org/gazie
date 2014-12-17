2d720a3c60bb0024189e5a27927463a0e0c232dc
angelo983 <angelo983@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-12-17 10:42:19 +0000

    Calcolo dell'imponibile oltre taxstamp_limit per il suggerimento dell'importo del bollo. Cambiamento collocamento grafico dei campi relativi al bollo

--- modules/vendit/admin_docven.php
+++ modules/vendit/admin_docven.php
@@ -577,10 +577,10 @@ if ((isset($_POST['Insert'])) or (isset($_POST['Update']))) {   //se non e' il p
            $form['speban'] = $admin_aziend['sperib'];
            $form['numrat'] = $pagame['numrat'];
            $form['stamp'] = 0;
-		   if ($pagame['tippag'] == 'T') { 
-				$form['stamp']=$admin_aziend['perbol'];
-		   }	
-		   $form['round_stamp'] = $admin_aziend['round_bol'];
+           if ($pagame['tippag'] == 'T') {
+              $form['stamp']=$admin_aziend['perbol'];
+           }
+           $form['round_stamp'] = $admin_aziend['round_bol'];
     } elseif ($pagame['tippag'] == 'R') {
            $form['speban'] = 0.00;
            $form['numrat'] = 1;
@@ -1478,10 +1478,10 @@ foreach ($form['rows'] as $k => $v) {
         case "0":
         echo "<tr>";
         if ( file_exists ( "../../data/files/fotoart/".$v["codart"].".gif" ) ) {
-			$boxover = "title=\"cssbody=[FacetInput] cssheader=[FacetButton] header=[".$v['annota']."] body=[<center><img width='50%' height='50%' src='../../data/files/fotoart/".$v["codart"].".gif'>] fade=[on] fadespeed=[0.03] \"";		
-		} else {
-			$boxover = "title=\"cssbody=[FacetInput] cssheader=[FacetButton] header=[".$v['annota']."] body=[<center><img src='../root/view.php?table=artico&value=".$v['codart']."'>] fade=[on] fadespeed=[0.03] \"";			
-		}
+           $boxover = "title=\"cssbody=[FacetInput] cssheader=[FacetButton] header=[".$v['annota']."] body=[<center><img width='50%' height='50%' src='../../data/files/fotoart/".$v["codart"].".gif'>] fade=[on] fadespeed=[0.03] \"";
+        } else {
+           $boxover = "title=\"cssbody=[FacetInput] cssheader=[FacetButton] header=[".$v['annota']."] body=[<center><img src='../root/view.php?table=artico&value=".$v['codart']."'>] fade=[on] fadespeed=[0.03] \"";
+        }
         if ($v['pesosp'] != 0){
             $boxpeso = "title=\"cssbody=[FacetInput] cssheader=[FacetButton] header=[peso = ".gaz_format_number($v['quanti'] * $v['pesosp'])."]  fade=[on] fadespeed=[0.03] \"";
         } else {
@@ -1665,8 +1665,6 @@ if ($form['tipdoc']=='DDT' || $form['template']=='FatturaImmediata' || $form['ti
     echo "<td class=\"FacetDataTD\"><input type=\"text\" value=\"".$form['units']."\" name=\"units\" maxlength=\"6\" size=\"4\" ></td>\n";
     echo "<td align=\"right\" class=\"FacetFieldCaptionTD\">$script_transl[55]</td>\n";
     echo "<td class=\"FacetDataTD\"><input type=\"text\" value=\"".$form['volume']."\" name=\"volume\" maxlength=\"9\" size=\"4\" ></td>\n";
-    echo "<td class=\"FacetFieldCaptionTD\">".$script_transl['taxstamp']."<input type=\"text\" value=\"".$form['taxstamp']."\" name=\"taxstamp\" maxlength=\"6\" size=\"4\" > ".$script_transl['virtual_taxstamp'];
-    $gForm->variousSelect('virtual_taxstamp',$script_transl['virtual_taxstamp_value'],$form['virtual_taxstamp']);
     echo "</td>";
     echo "</tr>";
 } else {
@@ -1685,8 +1683,6 @@ if ($form['tipdoc']=='DDT' || $form['template']=='FatturaImmediata' || $form['ti
     echo "<input type=\"hidden\" value=\"".$form['gross_weight']."\" name=\"gross_weight\">\n";
     echo "<input type=\"hidden\" value=\"".$form['units']."\" name=\"units\">\n";
     echo "<input type=\"hidden\" value=\"".$form['volume']."\" name=\"volume\">\n";
-    echo "<td class=\"FacetFieldCaptionTD\">".$script_transl['taxstamp']."<input type=\"text\" value=\"".$form['taxstamp']."\" name=\"taxstamp\" maxlength=\"6\" size=\"4\" > ".$script_transl['virtual_taxstamp'];
-    $gForm->variousSelect('virtual_taxstamp',$script_transl['virtual_taxstamp_value'],$form['virtual_taxstamp']);
     echo "</td>";
     echo "</tr>";
     echo "<tr>\n";
@@ -1694,15 +1690,22 @@ if ($form['tipdoc']=='DDT' || $form['template']=='FatturaImmediata' || $form['ti
 
 echo "<tr><td class=\"FacetFieldCaptionTD\" align=\"right\">$script_transl[32]</td>
           <td class=\"FacetFieldCaptionTD\" align=\"right\">$script_transl[33]</td>
+          <td class=\"FacetFieldCaptionTD\" align=\"right\">".$script_transl['taxstamp']."</td>
           <td class=\"FacetFieldCaptionTD\" align=\"right\">$script_transl[34]</td>
           <td class=\"FacetFieldCaptionTD\" align=\"right\">% $script_transl[24] <input type=\"text\" name=\"sconto\" value=\"".$form["sconto"]."\" maxlength=\"6\" size=\"1\" onchange=\"this.form.submit()\"></td>
           <td class=\"FacetFieldCaptionTD\" align=\"right\">$script_transl[32]</td>
           <td class=\"FacetFieldCaptionTD\" align=\"right\">$script_transl[19]</td>
-          <td class=\"FacetFieldCaptionTD\" align=\"right\">".$script_transl['stamp']."</td>
           <td class=\"FacetFieldCaptionTD\" align=\"right\">$script_transl[36] ".$admin_aziend['symbol']."</td>\n";
 $somma_spese = $form['traspo'] + $form['speban']*$form['numrat'] + $form['spevar'];
 $last=count($decalc_castle);
 $acc_val=$somma_spese;
+
+$imp_iva_esente = 0.00;
+
+$epsilon = 0.000001;    // Massima differenza tra 2 float
+                        // http://www.php.net/manual/en/language.types.float.php
+                        // http://stackoverflow.com/questions/3148937/compare-floats-in-php
+
 foreach ($castle as $k=> $v) {
       $vat = gaz_dbi_get_row($gTables['aliiva'],"codice",$k);
       if (isset($decalc_castle[$k])) {
@@ -1718,6 +1721,9 @@ foreach ($castle as $k=> $v) {
         }
       }
       $ivacast = round($v*$vat['aliquo'])/100;
+      if (abs($ivacast) < $epsilon) {
+        $imp_iva_esente+= $v;
+      }
       $totivafat += $ivacast;
       if ($next_row > 0) {
         echo "<tr><td align=\"right\">".gaz_format_number($v)."</td><td align=\"right\">".$vat['descri']." ".gaz_format_number($ivacast)."</td>\n";
@@ -1731,11 +1737,23 @@ if ($next_row > 0) {
         } else {
           $stamp = 0;
         }
+        if ($admin_aziend['virtual_taxstamp'] != 0 && (!isset($_POST['taxstamp']) || abs(floatval($form['taxstamp']))<$epsilon) && !isset($_GET['Update'])) {
+           //se la quota parte imponibile esente iva supera il valore massimo 'max_imp_iva_esente_bollo' viene aggiunto il costo della imposta di bollo
+           if ($imp_iva_esente > $admin_aziend['taxstamp_limit']) {
+              $form['taxstamp'] = $admin_aziend['taxstamp'];
+           }
+        }
+        echo "<td align=\"right\">";
+        echo $script_transl['virtual_taxstamp'];
+        $gForm->variousSelect('virtual_taxstamp',$script_transl['virtual_taxstamp_value'],$form['virtual_taxstamp']);
+        echo " - ".$script_transl['taxstamp']." <input type=\"text\" value=\"".number_format($form['taxstamp'],2,'.','')."\" name=\"taxstamp\" maxlength=\"6\" size=\"4\" style=\"text-align: right;\" >";
+        echo "<br />";
+        echo $script_transl['stamp']." <input type=\"text\" value=\"".gaz_format_number($stamp)."\" name=\"taxstamp\" maxlength=\"6\" size=\"4\" disabled=\"disabled\" style=\"text-align: right;\" >";
+        echo "</td>";
         echo "<td align=\"right\">".gaz_format_number($totimp_body)."</td>
               <td align=\"right\">".gaz_format_number(($totimp_body-$totimpfat+$somma_spese),2, '.', '')."</td>
               <td align=\"right\">".gaz_format_number($totimpfat)."</td>
               <td align=\"right\">".gaz_format_number($totivafat)."</td>
-              <td align=\"right\">".gaz_format_number($stamp)."</td>
               <td align=\"right\" style=\"font-weight:bold;\">".gaz_format_number($totimpfat+$totivafat+$stamp)."</td>\n";
         echo '<td colspan ="2" class="FacetFieldCaptionTD" align="center"><input name="ins" id="preventDuplicate" onClick="chkSubmit();" onClick="chkSubmit();" type="submit" value="'.strtoupper($script_transl[$toDo]).'!"></td></tr>';
         if ($rit > 0) {
