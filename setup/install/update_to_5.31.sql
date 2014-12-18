d0adbf1110430cb9f261605205544af0121f4567
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2014-12-18 22:26:18 +0000

    modificati i template fattura_semplice e fattura_immediata pertanto forzo l'utilizzo di quelli di default di GAzie al fine di evitare errori, in particolare si potrebbero avere malfunzionamenti sui bolli e sui valori degli addebiti di trasporto specie se si stampano fatture differite con più di 1 DdT. Quindi sarebbe bene rivedere i template personalizzati prima di usarli.

--- setup/install/update_to_5.31.sql
+++ setup/install/update_to_5.31.sql
@@ -4,6 +4,7 @@ ALTER TABLE `gaz_aziend` ADD `taxstamp_limit` DECIMAL(5,2) NOT NULL AFTER `acciv
 UPDATE `gaz_aziend` SET `taxstamp_limit` = 77.47 WHERE 1; 
 ALTER TABLE `gaz_aziend` ADD `virtual_taxstamp` TINYINT(1) NOT NULL AFTER `round_bol`;
 UPDATE `gaz_aziend` SET `virtual_taxstamp` = 1 WHERE 1; 
+UPDATE `gaz_aziend` SET `template` = '' WHERE 1; 
 ALTER TABLE `gaz_aziend` CHANGE `alliva` `preeminent_vat` INT(2) NOT NULL DEFAULT '0';
 ALTER TABLE `gaz_aziend` DROP `iva_susp`;
 ALTER TABLE `gaz_aziend` CHANGE `ricbol` `taxstamp` DECIMAL(5,2) NOT NULL;
