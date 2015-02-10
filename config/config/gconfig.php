ad72da09ed8fe7923dc7d2e8ae3045facf3e8cd1
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2015-02-10 08:12:21 +0000

    GAzie 6.1 ready

--- config/config/gconfig.php
+++ config/config/gconfig.php
@@ -26,7 +26,7 @@ if (isset($_SERVER['SCRIPT_FILENAME']) && (str_replace('\\','/',__FILE__) == $_S
     exit('Accesso diretto non consentito') ;
 }
 //versione software
-$versSw = '6.0';
+$versSw = '6.1';
 
 //nome DBMS usato per la libreria specifica (MySQL=mysql.lib, SQLite=sqlite.lib, ecc)
 //per il momento disponibile solo la libreria mysql.lib
