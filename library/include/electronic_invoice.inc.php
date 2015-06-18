7604a4e1546b761e0afce76b143dc357db8f454b
ldmax <ldmax@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2015-06-18 15:41:35 +0000

    correzione calcolo dell'importo totale

--- library/include/electronic_invoice.inc.php
+++ library/include/electronic_invoice.inc.php
@@ -833,8 +833,8 @@ function create_XML_invoice($testata, $gTables, $rows='rigdoc', $dest=false)
 
 // ----- CALCOLO TOTALI E RATE DEL PAGAMENTO
     $totpag = $XMLvars->totimpfat+$XMLvars->totriport+$XMLvars->totivafat-$XMLvars->tot_ritenute-$XMLvars->ivasplitpay;
-// ----- INSERITO variabile che calcola il totale della fattura al lordo della RDA
-    $totpar = $XMLvars->totimpfat+$XMLvars->totriport+$XMLvars->totivafat-$XMLvars->ivasplitpay;
+// ----- INSERITO variabile che calcola il totale della fattura al lordo della RDA e dell'IVA
+    $totpar = $XMLvars->totimpfat+$XMLvars->totriport+$XMLvars->totivafat;
 	
 	if ($XMLvars->virtual_taxstamp != 3) {
 	   $totpag = $totpag + $XMLvars->impbol;
