9bd703bc0ec4ddcf2b50ee2ea0619a9836b8fd8e
faithfulsoft <faithfulsoft@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2015-11-11 17:46:01 +0000

    tooltip, bootstrap, glyphicon "documento"

--- modules/magazz/report_artico.php
+++ modules/magazz/report_artico.php
@@ -28,23 +28,24 @@ if ($admin_aziend['decimal_quantity']>4){
 	$admin_aziend['decimal_quantity']=4;
 }
 
-function getLastDoc($item_code)
-   {
-      global $gTables;
-      $rs=false;
-      $rs_last_doc = gaz_dbi_dyn_query("*", $gTables['files'], " item_ref ='".$item_code."'",'id_doc DESC',0,1);
-      $last_doc = gaz_dbi_fetch_array($rs_last_doc);
-      // se e' il primo documento dell'anno, resetto il contatore
-      if ($last_doc) {
-         $rs=$last_doc;
-      }
-      return $rs;
-   }
+function getLastDoc($item_code) {
+	global $gTables;
+	$rs          = false;
+	$rs_last_doc = gaz_dbi_dyn_query("*", $gTables['files'], " item_ref ='".$item_code."'",'id_doc DESC',0,1);
+	$last_doc    = gaz_dbi_fetch_array($rs_last_doc);
+	// se e' il primo documento dell'anno, resetto il contatore
+	if ($last_doc) {
+		$rs = $last_doc;
+	}
+	return $rs;
+}
 
 $search_field_Array = array('C'=>array('codice','Codice'), 'D'=>array('descri','Descrizione'),'B'=>array('barcode','Codice a barre'));
 //
 require("../../library/include/header.php");
+
 $script_transl=HeadMain();
+
 if (isset($_GET['auxil'])) {
    $auxil = $_GET['auxil'];
 }
@@ -64,56 +65,63 @@ if (!isset($_GET['auxil'])) {
 }
 ?>
 <div align="center" class="FacetFormHeaderFont">Articoli</div>
+<?php
+
+$recordnav   = new recordnav($gTables['artico'], $where, $limit, $passo);
+$recordnav   -> output();
+?>
 <form method="GET">
-<table class="Tlarge">
-<tr>
-<td class="FacetFieldCaptionTD" colspan="2"><?php echo $search_field_Array[$admin_aziend['artsea']][1]; ?>:
-<input type="text" name="auxil" value="<?php if ($auxil != "&all=yes") echo $auxil; ?>" maxlength="15" size="15" tabindex="1" class="FacetInput">
-<input type="submit" name="search" value="Cerca" tabindex="1" onClick="javascript:document.report.all.value=1;">
-</td>
-<td></td>
-<td>
-<input type="submit" name="all" value="Mostra tutti" onClick="javascript:document.report.all.value=1;">
-</td>
-</tr>
+    <table class="TlargeTlarge table table-striped table-bordered table-condensed table-responsive">
+        <thead>
+            <tr>
+                <th class="FacetFieldCaptionTD" colspan="2"><?php echo $search_field_Array[$admin_aziend['artsea']][1]; ?>:
+                    <input type="text" name="auxil" value="<?php if ($auxil != "&all=yes") echo $auxil; ?>" maxlength="15" size="15" tabindex="1" class="FacetInput" />
+                    <input type="submit" name="search" value="Cerca" tabindex="1" onClick="javascript:document.report.all.value=1;" />
+                </th>
+                <th></th>
+                <th>
+                    <input type="submit" name="all" value="Mostra tutti" onClick="javascript:document.report.all.value=1;" />
+                </th>
+            </tr>
+        </thead>
+        <tbody>
 <?php
 $result = gaz_dbi_dyn_query ('*', $gTables['artico'], $where, $orderby, $limit, $passo);
 // creo l'array (header => campi) per l'ordinamento dei record
-$headers_artico = array  (
-              "Codice" => "codice",
-              "Descrizione" => "descri",
-              "Doc." => "",
-              "Categoria<br>merceologica" => "catmer",
-              "U.M." => "unimis",
-              "Prezzo 1" => "preve1",
-              "Prezzo<br>acquisto" => "preacq",
-              "Giacenza" => "");
+$headers_artico = array ("Codice"                 => "codice",
+						 "Descrizione"            => "descri",
+						 "Doc."                   => "",
+						 "Categoria merceologica" => "catmer",
+						 "U.M."                   => "unimis",
+						 "Prezzo 1"               => "preve1",
+						 "Prezzo acquisto"        => "preacq",
+						 "Giacenza"               => "");
 if ($admin_aziend['conmag']>0) {
-   $headers_artico = array_merge($headers_artico,array(
-              "Visualizza<br>e/o stampa"=>'',
-              "Barcode" => "barcode",
-              "Duplica" => "",
-              "Cancella" => ""
-              ));
+   $headers_artico = array_merge($headers_artico,array("Visualizza e/o stampa" => '',
+													   "Barcode"               => "barcode",
+													   "Duplica"               => "",
+													   "Cancella"              => ""));
 } else {
-   $headers_artico = array_merge ( $headers_artico,array(
-              "Barcode" => "barcode",
-              "Duplica" => "",
-              "Cancella" => ""
-              ));
+   $headers_artico = array_merge($headers_artico,array("Barcode"  => "barcode",
+													   "Duplica"  => "",
+													   "Cancella" => ""));
 }
 
 $linkHeaders = new linkHeaders($headers_artico);
+$gForm       = new magazzForm();
+
+echo '<tr>';
 $linkHeaders -> output();
-$recordnav = new recordnav($gTables['artico'], $where, $limit, $passo);
-$recordnav -> output();
-$gForm = new magazzForm();
+echo '</tr>';
 while ($r = gaz_dbi_fetch_array($result)) {
+	
        gaz_set_time_limit (30);
-       $lastdoc=getLastDoc($r["codice"]);
-       $mv=$gForm->getStockValue(false,$r['codice']); 
-       $magval=array_pop($mv);
-       $image_src = '';
+       $lastdoc = getLastDoc($r["codice"]);
+       $mv      = $gForm->getStockValue(false,$r['codice']); 
+       $magval  = array_pop($mv);
+	   /** ENRICO FEDELE */
+		/* Eliminiamo boxover */
+       /*$image_src = '';
        if((!empty($r["image"]) || (file_exists("../../data/files/fotoart/".$r["codice"].".gif" )))){
 		if ( !empty( $r["image"] ) ) {
 			$image_src = '<img border="1px" height="20" src="../root/view.php?table=artico&value='.$r['codice'].'" />';
@@ -126,44 +134,73 @@ while ($r = gaz_dbi_fetch_array($result)) {
 		}
        } else {
             $boxover = "title=\"cssbody=[FacetInput] cssheader=[FacetButton] header=[".$r['annota']."]  fade=[on] fadespeed=[0.03] \"";
-       }
+       }*/
+	   /** ENRICO FEDELE */
        $iva = gaz_dbi_get_row($gTables['aliiva'],"codice",$r["aliiva"]);
-       echo "<tr>";
+       echo '<tr>
+	   			<td class="FacetDataTD">
+					<a class="btn btn-xs btn-block btn-success " href="admin_artico.php?codice='.$r["codice"].'&amp;Update">
+						'.$r["codice"].'
+					</a>
+				</td>';
        ?>
+       <!--
 			<td class="FacetDataTD" style="min-width:80px">
 				<div>		
 					<div style="float:left;vertical-align: middle;">
-						<a class="btn btn-xs btn-block btn-default " href="admin_artico.php?codice=<?php echo $r["codice"]; ?>&Update">
-						<?php echo $r["codice"]; ?>
+						<a class="btn btn-xs btn-block btn-default " href="admin_artico.php?codice=<?php /*echo $r["codice"]; */ ?>&Update">
+						<?php /* echo $r["codice"]; */ ?>
 						</a>
 					</div>
 				</div>
 			</td>
+        -->
 		 <?php
-       echo "<td class=\"FacetDataTD\" $boxover>".$r["descri"]." </td>";
-       echo "<td class=\"FacetDataTD\" align=\"center\" title=\"\">";
+       echo '	<td class="FacetDataTD">
+	   				<span class="gazie-tooltip" data-type="product-thumb" data-id="'.$r["codice"].'" data-title="'.$r["descri"].'">'.$r["descri"].'</span>
+				</td>
+	   			<td class="FacetDataTD" align="center" title="">';
        if ($lastdoc){
-         echo "<a href=\"../root/retrieve.php?id_doc=".$lastdoc["id_doc"]."\"><img src=\"../../library/images/doc.png\" title=\"Ultimo certificato e/o documentazione disponibile\" border=\"0\"></a>";
+         echo '		<a href="../root/retrieve.php?id_doc='.$lastdoc["id_doc"].'">
+		 				<i class="glyphicon glyphicon-file" title="Ultimo certificato e/o documentazione disponibile"></i>
+					</a>';
        }
-       echo "</td>\n";
-       echo "<td class=\"FacetDataTD\" align=\"center\">".$r["catmer"]." </td>";
-       echo "<td class=\"FacetDataTD\" align=\"center\">".$r["unimis"]." </td>";
-       echo "<td class=\"FacetDataTD\" align=\"right\">".number_format($r["preve1"],$admin_aziend['decimal_price'],',','.')." </td>";
-       echo "<td class=\"FacetDataTD\" align=\"right\">".number_format($r["preacq"],$admin_aziend['decimal_price'],',','.')." </td>";
-       echo "<td class=\"FacetDataTD\" align=\"right\" title=\"".$admin_aziend['symbol']." ".$magval['v_g']."\">".number_format($magval['q_g'],$admin_aziend['decimal_quantity'],',','.')." </td>";
+       echo '	</td>
+	   			<td class="FacetDataTD" align="center">'.$r["catmer"].' </td>
+				<td class="FacetDataTD" align="center">'.$r["unimis"].' </td>
+				<td class="FacetDataTD" align="right">'.number_format($r["preve1"],$admin_aziend['decimal_price'],',','.').' </td>
+				<td class="FacetDataTD" align="right">'.number_format($r["preacq"],$admin_aziend['decimal_price'],',','.').' </td>
+				<td class="FacetDataTD" align="right" title="'.$admin_aziend['symbol'].' '.$magval['v_g'].'">
+					'.number_format($magval['q_g'],$admin_aziend['decimal_quantity'],',','.').'
+				</td>';
        if ($admin_aziend['conmag']>0) {
-          echo "<td class=\"FacetDataTD\" align=\"center\" title=\"Visualizza e/o stampa la scheda di magazzino\">
-               <a class=\"btn btn-xs btn-default\" href=\"../magazz/select_schart.php?di=0101".date('Y')."&df=".date('dmY')."&id=".$r['codice']."\">
-               <i class=\"glyphicon glyphicon-check\"></i><i class=\"glyphicon glyphicon-print\"></i></a></td>";
+          echo '	<td class="FacetDataTD" align="center" title="Visualizza e/o stampa la scheda di magazzino">
+		  				<a class="btn btn-xs btn-default" href="../magazz/select_schart.php?di=0101'.date('Y').'&df='.date('dmY').'&id='.$r['codice'].'">
+							<i class="glyphicon glyphicon-check"></i><i class="glyphicon glyphicon-print"></i>
+						</a>
+					</td>';
        }
-       echo "<td class=\"FacetDataTD\" align=\"center\" title=\"Stampa Codici a Barre\"><a class=\"btn btn-xs btn-default\" href=\"stampa_barcode.php?code=".$r["codice"]."\"><i class=\"glyphicon glyphicon-barcode\"></i></a></td>"; //<img src=\"../../library/images/barcode.png\" border=\"0\"><br />".$r['barcode']."
-       echo "<td class=\"FacetDataTD\" align=\"center\" title=\"Duplica articolo in (".$r["codice"]."_2)\"><a class=\"btn btn-xs btn-default\" href=\"clone_artico.php?codice=".$r["codice"]."\"><i class=\"glyphicon glyphicon-export\"></i></a></td>";
-       echo "<td class=\"FacetDataTD\" align=\"center\"><a class=\"btn btn-xs btn-default btn-elimina\" href=\"delete_artico.php?codice=".$r["codice"]."\"><i class=\"glyphicon glyphicon-remove\"></i></a></td>";
-       echo "</tr>";
+       echo '	<td class="FacetDataTD" align="center" title="Stampa Codici a Barre">
+	   				<a class="btn btn-xs btn-default" href="stampa_barcode.php?code='.$r["codice"].'">
+						<i class="glyphicon glyphicon-barcode"></i>
+					</a>
+				</td>
+				<td class="FacetDataTD" align="center" title="Duplica articolo in ('.$r["codice"].'_2)">
+					<a class="btn btn-xs btn-default" href="clone_artico.php?codice='.$r["codice"].'">
+						<i class="glyphicon glyphicon-export"></i>
+					</a>
+				</td>
+				<td class="FacetDataTD" align="center">
+					<a class="btn btn-xs btn-default btn-elimina" href="delete_artico.php?codice='.$r["codice"].'">
+						<i class="glyphicon glyphicon-remove"></i>
+					</a>
+				</td>
+			</tr>';
 }
 ?>
-</form>
-</table>
-</body>
+            </tbody>
+        </table>
+    </form>
+    </body>
 </html>
-<script src="../../js/boxover/boxover.js"></script>
+<!--<script src="../../js/boxover/boxover.js"></script>-->
