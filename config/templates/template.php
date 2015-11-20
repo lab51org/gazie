7a7ca4a195d034f3f6f588b144726457b8cc2eef
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2015-11-20 16:18:23 +0000

    ripristinati i templates originali, chiunque può crearsene di personalizzati clonando e rinominando la cartella ad es. in "templates.custom"

--- config/templates/template.php
+++ config/templates/template.php
@@ -35,7 +35,6 @@ class Template extends TCPDF
         $this->aliiva = $docVars->gTables['aliiva'];
         $this->tesdoc = $docVars->tesdoc;
         $this->testat = $docVars->testat;
-        $this->caumag = $docVars->caumag;
         $this->pagame = $docVars->pagame;
         $this->banapp = $docVars->banapp;
         $this->banacc = $docVars->banacc;
