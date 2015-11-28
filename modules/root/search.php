a324baa8ded1ca31a8bfafaf03813a7148b93834
faithfulsoft <faithfulsoft@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2015-11-28 23:44:15 +0000

    Mi sono permesso di apportare alcune correzioni alla ricerca per autocompletamento. In particolare, ho modificato un pò la query al database in modo che la ricerca del testo digitato venga effettuata contemporaneamente su tutti i campi che ci interessano ma presi singolarmente, anzichè sul concat. Questo ci consente di essere più efficaci e di poter sfruttare meglio il carattere jolly %. In sostanza se si scrive stringa% oppure %stringa, vegono cercati (singolarmente) tutti i campi che iniziano o finiscono con "stringa", mentre con il concat il match veniva fatto sulla concatenazione di tutti i campi. Es: si vuole cercare "zaino per notebook" (prodotto nel db di esempio di gazie): Metodo attuale (match su campi singoli): si digita za%, e viene fuori "NOTETRACK - Zaino per notebook - 1000000000085", perchè il nome del prodotto inizia per "za" Metodo concat: si digita za%, non viene fuori nulla, perchè il risultato della concatenazione inizia per NOTETRACK

--- modules/root/search.php
+++ modules/root/search.php
@@ -42,7 +42,6 @@ if(isset($_GET['term'])) {	//	Evitiamo errori se lo script viene chiamato dirett
 	require("../../library/include/datlib.inc.php");
 	$admin_aziend = checkAdmin();
 	$return_arr   = array();
-
 	$term         = filter_var(substr($_GET['term'],0,20),FILTER_SANITIZE_MAGIC_QUOTES);
 	$term         = gaz_dbi_real_escape_string($term);
 	 
@@ -54,7 +53,7 @@ if(isset($_GET['term'])) {	//	Evitiamo errori se lo script viene chiamato dirett
 	 
 	// SECURITY HOLE ***************************************************************
 	// allow space, any unicode letter and digit, underscore, dash, slash, percent, dot,
-	if(preg_match("/[^\040\pL\pN_-\/\%\.]/u", $term)) {
+	if(preg_match("/[^\040\pL\pN\%\/\._-]/u", $term)) {
 	  print $json_invalid;
 	  exit;
 	}
@@ -65,21 +64,21 @@ if(isset($_GET['term'])) {	//	Evitiamo errori se lo script viene chiamato dirett
 
 	$parts = explode(' ', $term);
 
-	//$result = gaz_dbi_dyn_query("id,ragso1,citspe",$gTables['anagra'],"ragso1 LIKE '%".$term."%'",'ragso1');
 	/** ENRICO FEDELE */
-	/* tolto citspe perchè pare inutilizzato, associato nome 'label' a ragso1 per poter usare fetch_assoc dopo */
-	
 	switch($opt) {
 		case 'product':
-			foreach($parts as $id => $part) {
-				$like[] = "CONCAT(codice,descri,barcode) LIKE '%".$part."%'";
-			}
-			$like = implode(" AND ", $like);	//	creo la porzione di query per il like
+			$fields = array("codice","descri","barcode");	//	Sono i campi sui quali effettuare la ricerca
+			foreach($fields as $id1 => $field) {			//	preparo i diversi campi per il like, questo funziona meglio del concat
+				foreach($parts as $id => $part) {			//	(inteso come stringa sulla quale fare il like) perchè è più flessibile con i caratteri jolly
+					$like[] = like_prepare($field, $part);	//	Altrimenti se si cerca za%, il like viene fatto su tutto il concat, e se il codice prodotto
+				}											//	non inizia per za il risultato è nullo, così invece se cerco za%, viene fuori anche un prodotto il
+			}												//  cui nome (o descrizione) inizia per za ma il cui codice può anche essere TPQ 
+			$like   = implode(" OR ", $like);				//	creo la porzione di query per il like, con OR perchè cerco in campi differenti
 			$result = gaz_dbi_dyn_query("codice AS id, CONCAT(codice,' - ',descri,' - ',barcode) AS label, codice AS value", $gTables['artico'], $like, "catmer, codice");
 			break;
 		case 'location':
 			foreach($parts as $id => $part) {
-				$like[] = $gTables['municipalities'].".name LIKE '%".$part."%'";
+				$like[] = like_prepare($gTables['municipalities'].".name", $part);
 			}
 			$like = implode(" AND ", $like);	//	creo la porzione di query per il like
 			$result = gaz_dbi_dyn_query("UPPER(".$gTables['municipalities'].".name) AS value,
@@ -98,32 +97,42 @@ if(isset($_GET['term'])) {	//	Evitiamo errori se lo script viene chiamato dirett
 							$like,$gTables['municipalities'].".name ASC");
 			break;
 		default:
-			foreach($parts as $id => $part) {
-				$like[] = "CONCAT(ragso1,ragso2) LIKE '%".$part."%'";
-			}
-			$like = implode(" AND ", $like);	//	creo la porzione di query per il like
-		$result = gaz_dbi_dyn_query("id, CONCAT(ragso1,' ',ragso2) AS label, ragso1 AS value",$gTables['anagra'],$like,'ragso1');
+			$fields = array("ragso1","ragso2");				//	Sono i campi sui quali effettuare la ricerca
+			foreach($fields as $id1 => $field) {			//	preparo i diversi campi per il like, questo funziona meglio del concat
+				foreach($parts as $id => $part) {			//	(inteso come stringa sulla quale fare il like) perchè è più flessibile con i caratteri jolly
+					$like[] = like_prepare($field, $part);	//	Altrimenti se si cerca za%, il like viene fatto su tutto il concat, e se il codice prodotto
+				}											//	non inizia per za il risultato è nullo, così invece se cerco za%, viene fuori anche un prodotto il
+			}												//  cui nome (o descrizione) inizia per za ma il cui codice può anche essere TPQ 
+			$like   = implode(" OR ", $like);				//	creo la porzione di query per il like, con OR perchè cerco in campi differenti
+			$result = gaz_dbi_dyn_query("id, CONCAT(ragso1,' ',ragso2) AS label, ragso1 AS value",$gTables['anagra'],$like,'ragso1');
 	}
 	while($row = gaz_dbi_fetch_assoc($result)) {
-		/* l'array è già associativo, questo codice non serve
-		$r['id']=$row['id'];
-		$r['label']=$row['ragso1'];
-		$r['value']=$row['ragso1'];*/
-		//	meglio usare $return_arr[], si risparmia overhead
-		//	array_push($return_arr,$r);
 		$return_arr[] = $row;
 	}
-	$return_arr = apply_highlight($return_arr, $parts);
+	if($term!='%%') {	//	E' indispensabile, altrimenti si possono generare warning che non fanno funzionare l'autocompletamento
+		$return_arr = apply_highlight($return_arr, str_replace("%", '', $parts));
+	}
 	echo json_encode($return_arr);
 } else {
   return;
 }
 
-
-
-
-
-
+/** ENRICO FEDELE */
+/**
+ * prepara la porzione di like per la query
+ * se l'utente non ha inserito
+ *
+ * @param string $dbfield: campo del db sul quale fare la like
+ * @param string $txtsearch: testo da cercare nel campo
+ * @return array or false
+ */
+function like_prepare($dbfield, $txtsearch) {
+	if(mb_stripos($txtsearch, "%")===false) {	//	L'utente non ha inserito il carattere jolly
+		return $dbfield." LIKE '%".$txtsearch."%'";
+	} else {	//	L'utente sta usanto il carattere jolly %, quindi non devo inserirlo nella query
+		return $dbfield." LIKE '".$txtsearch."'";
+	}
+}
 
 /** ENRICO FEDELE */
 /* Codice preso da
@@ -140,25 +149,23 @@ if(isset($_GET['term'])) {	//	Evitiamo errori se lo script viene chiamato dirett
  * @return array or false
  */
 function mb_stripos_all($haystack, $needle) {
- 
-  $s = 0;
-  $i = 0;
- 
-  while(is_integer($i)) {
- 
-    $i = mb_stripos($haystack, $needle, $s);
- 
-    if(is_integer($i)) {
-      $aStrPos[] = $i;
-      $s = $i + mb_strlen($needle);
-    }
-  }
- 
-  if(isset($aStrPos)) {
-    return $aStrPos;
-  } else {
-    return false;
-  }
+	$s = 0;
+	$i = 0;
+	
+	while(is_integer($i)) {
+		$i = mb_stripos($haystack, $needle, $s);
+		
+		if(is_integer($i)) {
+			$aStrPos[] = $i;
+			$s         = $i + mb_strlen($needle);
+		}
+	}
+	
+	if(isset($aStrPos)) {
+		return $aStrPos;
+	} else {
+		return false;
+	}
 }
  
 /**
@@ -169,66 +176,54 @@ function mb_stripos_all($haystack, $needle) {
  * @return array
  */
 function apply_highlight($a_json, $parts) {
- 
-  $p = count($parts);
-  $rows = count($a_json);
- 
-  for($row = 0; $row < $rows; $row++) {
- 
-    $label = $a_json[$row]["label"];
-    $a_label_match = array();
- 
-    for($i = 0; $i < $p; $i++) {
- 
-      $part_len = mb_strlen($parts[$i]);
-      $a_match_start = mb_stripos_all($label, $parts[$i]);
- 
-      foreach($a_match_start as $part_pos) {
- 
-        $overlap = false;
-        foreach($a_label_match as $pos => $len) {
-          if($part_pos - $pos >= 0 && $part_pos - $pos < $len) {
-            $overlap = true;
-            break;
-          }
-        }
-        if(!$overlap) {
-          $a_label_match[$part_pos] = $part_len;
-        }
- 
-      }
- 
-    }
- 
-    if(count($a_label_match) > 0) {
-      ksort($a_label_match);
- 
-      $label_highlight = '';
-      $start = 0;
-      $label_len = mb_strlen($label);
- 
-      foreach($a_label_match as $pos => $len) {
-        if($pos - $start > 0) {
-          $no_highlight = mb_substr($label, $start, $pos - $start);
-          $label_highlight .= $no_highlight;
-        }
-        $highlight = '<span class="hl_results">' . mb_substr($label, $pos, $len) . '</span>';
-        $label_highlight .= $highlight;
-        $start = $pos + $len;
-      }
- 
-      if($label_len - $start > 0) {
-        $no_highlight = mb_substr($label, $start);
-        $label_highlight .= $no_highlight;
-      }
- 
-      $a_json[$row]["label"] = $label_highlight;
-    }
- 
-  }
- 
-  return $a_json;
- 
+	$p    = count($parts);
+	$rows = count($a_json);
+	
+	for($row = 0; $row < $rows; $row++) {
+		$label         = $a_json[$row]["label"];
+		$a_label_match = array();
+	
+		for($i = 0; $i < $p; $i++) {
+			$part_len      = mb_strlen($parts[$i]);
+			$a_match_start = mb_stripos_all($label, $parts[$i]);
+	
+			foreach($a_match_start as $part_pos) {	
+				$overlap = false;
+				foreach($a_label_match as $pos => $len) {
+					if($part_pos - $pos >= 0 && $part_pos - $pos < $len) {
+						$overlap = true;
+						break;
+					}
+				}
+				if(!$overlap) {
+					$a_label_match[$part_pos] = $part_len;
+				}
+			}
+		}
+		if(count($a_label_match) > 0) {
+			ksort($a_label_match);
+			
+			$label_highlight = '';
+			$start           = 0;
+			$label_len       = mb_strlen($label);
+	
+			foreach($a_label_match as $pos => $len) {
+				if($pos - $start > 0) {
+					$no_highlight     = mb_substr($label, $start, $pos - $start);
+					$label_highlight .= $no_highlight;
+				}
+				$highlight        = '<span class="hl_results">' . mb_substr($label, $pos, $len) . '</span>';
+				$label_highlight .= $highlight;
+				$start            = $pos + $len;
+			}
+			if($label_len - $start > 0) {
+				$no_highlight     = mb_substr($label, $start);
+				$label_highlight .= $no_highlight;
+			}
+			$a_json[$row]["label"] = $label_highlight;
+		}
+	}
+	return $a_json;
 }
 ?>
 
