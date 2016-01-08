4c5bfbe83cf884c68565583fe842bce3d2ef2451
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-01-08 08:17:24 +0000

    per ripristinare il giusto valore del campo ddt_type sui ddt modificati successivamente alla fatturazione (FAD)

--- setup/install/update_to_6.6.sql
+++ setup/install/update_to_6.6.sql
@@ -7,4 +7,5 @@ INSERT INTO `gaz_menu_module` SELECT MAX(id)+1, 3, 'report_agenti_forn.php', '',
 INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT MAX(id) FROM `gaz_menu_module`), 'admin_agenti_forn.php?Insert', '', '', 14, '', 1  FROM `gaz_menu_script`;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 CREATE TABLE gaz_XXXagenti_forn (`id_agente` int(9) NOT NULL,`id_fornitore` int(9) NOT NULL,`base_percent` decimal(4,2) NOT NULL,`tipo_contratto` tinyint(1) NOT NULL,`adminid` varchar(20) NOT NULL,`last_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+UPDATE `gaz_XXXtesdoc` SET `ddt_type`= 'T' WHERE `ddt_type`='D';
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
