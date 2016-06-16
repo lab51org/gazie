e4253ad863f1787417d8dbaf5ef59ab7d96d7c2f
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-06-16 06:15:55 +0000

    per evitare conflitti nel caso di menù personalizzato

--- setup/install/update_to_7.1.sql
+++ setup/install/update_to_7.1.sql
@@ -4,7 +4,7 @@ ALTER TABLE `gaz_aziend` ADD COLUMN `min_rate_deprec` DECIMAL(4,1) NOT NULL DEFA
 ALTER TABLE `gaz_aziend` ADD COLUMN `super_amm_account` INT(9) NOT NULL DEFAULT '0' AFTER `lost_cost_assets`;
 ALTER TABLE `gaz_aziend` ADD COLUMN `super_amm_rate` DECIMAL(4,1) NOT NULL DEFAULT '40.0' AFTER `super_amm_account`;
 INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT id FROM `gaz_menu_module` WHERE `link`='report_assets.php'), 'depreciation_assets.php', '', '', 7, '', 7  FROM `gaz_menu_script`;
-INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, 63, 'select_debiti_crediti.php', '', '', 7, '', 2  FROM `gaz_menu_script`;
+INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT id FROM `gaz_menu_module` WHERE `link`='select_situazione_contabile.php'), 'select_debiti_crediti.php', '', '', 7, '', 2  FROM `gaz_menu_script`;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXassets` ADD COLUMN `lost_cost_value` DECIMAL(12,2) NOT NULL DEFAULT '0.00' AFTER `no_deduct_cost_rate`;
 ALTER TABLE `gaz_XXXassets` ADD COLUMN `super_ammort` DECIMAL(12,2) NOT NULL DEFAULT '0.00' AFTER `lost_cost_value`;
