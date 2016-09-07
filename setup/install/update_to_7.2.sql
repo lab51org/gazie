fb079aec700e9477cdcfd75457fb8f32843136bf
pepinfor <pepinfor@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-09-07 17:13:00 +0000

    introdotto calcolo margine ricavi di un cliente suddiviso per fornitore
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1343 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.2.sql
+++ setup/install/update_to_7.2.sql
@@ -1,8 +1,9 @@
 UPDATE `gaz_config` SET `cvalue` = '96' WHERE `id` =2;
 INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, 7, 'select_sconti_articoli.php', '', '', 41, '', 7 FROM `gaz_menu_script`;
 INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, 7, 'select_sconti_raggruppamenti.php', '', '', 42, '', 8 FROM `gaz_menu_script`;
-INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, 9, 'select_analisi_fatturato_clienti.php', '', '', 43, '', 5  FROM `gaz_menu_script`;
-INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, 9, 'select_analisi_fatturato_clienti_fornitori.php', '', '', 44, '', 6  FROM `gaz_menu_script`;
+UPDATE `gaz_menu_script` SET `weight`=5 WHERE `weight`=3 and `id_menu`=9
+INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, 9, 'select_analisi_fatturato_clienti.php', '', '', 43, '', 3  FROM `gaz_menu_script`;
+INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, 9, 'select_analisi_fatturato_cliente_fornitore.php', '', '', 44, '', 4  FROM `gaz_menu_script`;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 CREATE TABLE `gaz_XXXsconti_articoli` (`clfoco` int(9),`codart` varchar(15),`sconto` decimal(6,3),`prezzo_netto` decimal(14,5), primary key(`clfoco`,`codart`));
 CREATE TABLE `gaz_XXXsconti_raggruppamenti` (`clfoco` int(9),`ragstat` char(15),`sconto` decimal(6,3), primary key(`clfoco`,`ragstat`));
