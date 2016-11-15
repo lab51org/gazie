4862799a168610a84b2c8c07f9c6a2b71bd212ae
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-11-15 11:23:11 +0000

    Iniziati i lavori per la creazione di un modulo, totalmente integrato ed attivabile in qualsiasi momento, che permetterà l'utilizzo didattico di qualsiasi installazione di GAzie
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1365 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/school/help/italian/docume.php
@@ -0,0 +1,29 @@
+<div class="panel panel-default panel-body">
+    <h3>Men&ugrave; "A scuola"</h3>
+    <p> Questo menù serve esclusivamente nella didattica. </p>
+    <p>
+        Offre ai professori di economia aziendale l'opportunità di separare il database in maniera che i singoli studenti 
+        abbiano la possibilità di lavorare in autonomia pur utilizzando un'unica installazione di GAzie.
+    </p>
+    <p> Gli insegnanti hanno i privilegi per poter accedere agli script di questo modulo e quindi possonno creare, modificare, eliminare sia le classi che gli alunni fino ad un massimo di 1000. 
+        Essi sono di fatto tutti gli utenti dell'installazione di base aventi il livello di abilitazione al massimo, ovvero a 9
+    </p>
+</div>
+
+<ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#teachers">Insegnanti</a></li>
+    <li class=""><a data-toggle="tab" href="#classrooms">Classi</a></li>
+    <li class=""><a data-toggle="tab" href="#students">Alunni</a></li>
+</ul>
+
+<div class="tab-content contenuto-help">
+    <div id="teachers" class="tab-pane fade in active">
+        <p class="help-text">Da qui &egrave; possibile attribuire una descrizione aggiuntiva all'utente-insegnante, che non coincida necessariamente con la descrizione dello stesso sulla installazione di base</p>
+    </div>
+    <div id="classrooms" class="tab-pane fade in">
+        <p class="help-text">Da qui &egrave; possibile gestire le classi</p>
+    </div>
+    <div id="students" class="tab-pane fade in">
+        <p class="help-text">Da qui &egrave; possibile gestire gli alunni, e quindi aggiungere, modificare, rimuovere dalla base dati le tabelle ad essi associate, ovvero quelle aventi tp=gazNNN dove NNN sono numeri naturali che costituisco table_prefix. </p>
+    </div>
+</div>
