78201df31290352885195dc6138063a492ef16ca
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-11-15 08:42:51 +0000

    corretto l'errore di duplicazione nella creazione dei submenù

--- trunk/modules/suppor/menu.creation_data.php
+++ trunk/modules/suppor/menu.creation_data.php
@@ -28,9 +28,14 @@ $menu_data = array('m1' => array('link' => "docume_suppor.php"),
                                         2 => array('link' => "report_period.php", 'weight' => 2),
                                         3 => array('link' => "report_install.php", 'weight' => 3)
                          ),
-                        'm3' => array('m2' => array(1 => array('translate_key' => 1, 'link' => "admin_assist.php", 'weight' => 1),
-                                                    2 => array('translate_key' => 2, 'link' => "admin_period.php", 'weight' => 2),
-                                                    3 => array('translate_key' => 3, 'link' => "admin_install.php", 'weight' => 3)
+                        'm3' => array('m2' => array(1 => array(
+                                                                array('translate_key' => 1, 'link' => "admin_assist.php", 'weight' => 1)
+                                                              ),
+                                                    2 => array(
+                                                                array('translate_key' => 2, 'link' => "admin_period.php", 'weight' => 2)
+                                                              ),
+                                                    3 => array( array('translate_key' => 3, 'link' => "admin_install.php", 'weight' => 3)
+                                                              )
                                                     )
                                      )
                 );
