8808af5a4520cd4afe5fa4cb30e0a644261baa5b
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-12-10 11:28:43 +0000

    stampa libro cespiti ammortizzabili

--- trunk/modules/finann/assets_book.php
+++ trunk/modules/finann/assets_book.php
@@ -32,40 +32,173 @@ if (!ini_get('safe_mode')) { //se me lo posso permettere...
     gaz_set_time_limit(0);
 }
 
+function getAssets($date) {
+    /*  funzione per riprendere dal database tutti i beni ammortizzabili 
+      e proporre una anteprima di ammortamenti */
+    global $gTables, $admin_aziend;
+    $ctrl_fix = 0;
+    // riprendo i righi da assets
+    $from = $gTables['assets'] . ' AS assets ' .
+            'LEFT JOIN ' . $gTables['tesmov'] . ' AS tesmov ON assets.id_movcon=tesmov.id_tes ' .
+            'LEFT JOIN ' . $gTables['clfoco'] . ' AS fornit ON tesmov.clfoco=fornit.codice ';
+    $field = ' assets.*, tesmov.datreg AS dtrtes, tesmov.numdoc AS nudtes, tesmov.datreg AS dtdtes, tesmov.descri AS destes, fornit.descri as desfor';
+    $where = " datreg <= '" . $date . "'";
+    $orderby = "acc_fixed_assets ASC, datreg ASC, type_mov ASC, id ASC";
+    $result = gaz_dbi_dyn_query($field, $from, $where, $orderby);
+    $acc = array();
+    while ($row = gaz_dbi_fetch_array($result)) {
+        // ad ogni cambio di bene creo un array e sulla radice metto tutti i dati che mi servono sulla intestazione del bene stesso
+        if ($ctrl_fix <> $row['acc_fixed_assets']) {
+            // azzero i totali delle colonne
+            // in ordine di data necessariamente il primo rigo dev'essere l'acquisto
+            $acc[$row['acc_fixed_assets']][1] = $row;
+            // prendo il valore della immobilizzazione dal rigo contabile
+            $f = gaz_dbi_get_row($gTables['rigmoc'], 'codcon', $row['acc_fixed_assets'] . ' AND id_tes = ' . $row['id_movcon']);
+            $acc[$row['acc_fixed_assets']][1]['fixed_val'] = $f['import'];
+            $acc[$row['acc_fixed_assets']][1]['found_val'] = 0;
+            $acc[$row['acc_fixed_assets']][1]['cost_val'] = 0;
+            $acc[$row['acc_fixed_assets']][1]['noded_val'] = 0;
+            $acc[$row['acc_fixed_assets']][1]['lost_cost'] = 0; // non è più fiscalmente una quota persa ma da segnalare sul libro
+            // questi sono i totali
+            $acc[$row['acc_fixed_assets']][1]['fixed_tot'] = $f['import'];
+            $acc[$row['acc_fixed_assets']][1]['found_tot'] = 0;
+            $acc[$row['acc_fixed_assets']][1]['cost_tot'] = 0;
+            $acc[$row['acc_fixed_assets']][1]['noded_tot'] = 0;
+            // i subtotali
+            $acc[$row['acc_fixed_assets']][1]['fixed_subtot'] = $f['import'];
+            $acc[$row['acc_fixed_assets']][1]['found_subtot'] = 0;
+            $acc[$row['acc_fixed_assets']][1]['cost_subtot'] = 0;
+            $acc[$row['acc_fixed_assets']][1]['noded_subtot'] = 0;
 
+            // trovo i giorni dall'ultimo ammortamento o acquisto
+            $dateamm = new DateTime($date);
+            $rs_gglast = gaz_dbi_dyn_query("*", $gTables['tesmov'], "caucon = 'AMM'", 'datreg DESC', 0, 1);
+            $r_gglast = gaz_dbi_fetch_array($rs_gglast);
+            if ($r_gglast) {
+                // dall'ultimo ammortamento
+                $datelast = new DateTime($r_gglast['datreg']);
+            } else {
+                // dall'acquisto
+                $datelast = new DateTime($row['dtrtes']);
+            }
+            $ddays = $dateamm->diff($datelast);
+            $acc[$row['acc_fixed_assets']][1]['gglast'] = $ddays->days;
+            // ricavo il gruppo e la specie dalla tabella ammortamenti ministeriali 
+            $xml = simplexml_load_file('../../library/include/ammortamenti_ministeriali.xml') or die("Error: Cannot create object");
+            preg_match("/^([0-9 ]+)([a-zA-Z ]+)$/", $admin_aziend['amm_min'], $m);
+            foreach ($xml->gruppo as $vg) {
+                if ($vg->gn[0] == $m[1]) {
+                    foreach ($vg->specie as $v) {
+                        if ($v->ns[0] == $m[2]) {
+                            $acc[$row['acc_fixed_assets']][1]['ammmin_gruppo'] = $vg->gn[0] . '-' . $vg->gd[0];
+                            $acc[$row['acc_fixed_assets']][1]['ammmin_specie'] = $v->ns[0] . '-' . $v->ds[0];
+                            $acc[$row['acc_fixed_assets']][1]['ammmin_ssd'] = $v->ssd[intval($row['ss_amm_min'])] . ' ';
+                            $acc[$row['acc_fixed_assets']][1]['ammmin_ssrate'] = $v->ssrate[intval($row['ss_amm_min'])] . ' ';
+                        }
+                    }
+                }
+            }
+        } else {
+            //nei movimenti successivi a seconda del tipo di rigo agisco in maniera differente
+            switch ($row['type_mov']) {
+                case '10' : // incremento valore del bene (accessorio/ampliamento/ammodernamento/manutenzione)
+                    break;
+                case '50' : // decremento valore del bene per ammortamento
+                    // prendo il valore del fondo ammortamento dal rigo contabile
+                    $f = gaz_dbi_get_row($gTables['rigmoc'], 'codcon', $row['acc_found_assets'] . "' AND id_tes = '" . $row['id_movcon']);
+                    $row['fixed_val'] = 0;
+                    $row['fixed_subtot'] = $acc[$row['acc_fixed_assets']][1]['fixed_tot'];
+                    $row['found_val'] = $f['import'];
+                    $acc[$row['acc_fixed_assets']][1]['found_tot'] += $f['import'];
+                    $row['found_subtot'] = $acc[$row['acc_fixed_assets']][1]['found_tot'];
+                    // prendo il valore dell'ammortamento dal rigo contabile
+                    $c = gaz_dbi_get_row($gTables['rigmoc'], 'codcon', $row['acc_cost_assets'] . "' AND id_tes = '" . $row['id_movcon']);
+                    $row['cost_val'] = $c['import'];
+                    $acc[$row['acc_fixed_assets']][1]['cost_tot'] += $c['import'];
+                    $row['cost_subtot'] = $acc[$row['acc_fixed_assets']][1]['cost_tot'];
+                    // prendo il valore della quota indeducibile dal rigo contabile
+                    $n = gaz_dbi_get_row($gTables['rigmoc'], 'codcon', $row['acc_no_deduct_cost'] . "' AND id_tes = '" . $row['id_movcon']);
+                    $row['noded_val'] = $n['import'];
+                    $acc[$row['acc_fixed_assets']][1]['noded_tot'] += $n['import'];
+                    $row['noded_subtot'] = $acc[$row['acc_fixed_assets']][1]['noded_tot'];
+                    /* anche se da qualche anno non è più fiscalmente una quota persa si deve segnalare sul libro
+                     */
+                    $row['lost_cost'] = ($acc[$row['acc_fixed_assets']][1]['valamm'] * $row['fixed_subtot'] / 200) - ($c['import'] + $n['import']);
+                    if ($row['lost_cost'] < 0) {
+                        $row['lost_cost'] = 0;
+                    }
+                    // aggiungo all'array del bene
+                    $acc[$row['acc_fixed_assets']][] = $row;
+                    break;
+                case '80' : // alienazione parziale
+                    break;
+                case '90' : // alienazione del bene 
+                    break;
+            }
+        }
+        $ctrl_fix = $row['acc_fixed_assets'];
+    }
+    return $acc;
+}
 
 $luogo_data = $admin_aziend['citspe'] . ", lì " . ucwords(strftime("%d %B %Y", mktime(0, 0, 0, date("m"), date("d"), date("Y"))));
 
 
 require("../../config/templates/report_template.php");
-$title = array('luogo_data' => $luogo_data,
-    'title' => "!!! BOZZA INIZIALE INUTILIZZABILE del Libro dei BENI AMMORTIZZABILI !!!",
-    'hile' => array(array('lun' => 40, 'nam' => 'ID'),
-        array('lun' => 70, 'nam' => 'Descrizione bene'),
-        array('lun' => 70, 'nam' => 'Fornitore'),
-        array('lun' => 40, 'nam' => 'Valore'),
-        array('lun' => 40, 'nam' => '% Ammortamento')
-    )
-);
 
-$pdf = new Report_template();
-$pdf->setVars($admin_aziend, $title);
-$pdf->SetTopMargin(39);
-$pdf->SetFooterMargin(20);
-$pdf->AddPage('L');
-$pdf->SetFont('helvetica', '', 7);
-$result = gaz_dbi_dyn_query('*', $gTables['assets'], '1', 'id DESC');
 
-while ($row = gaz_dbi_fetch_array($result)) {
-    $tesmov = gaz_dbi_get_row($gTables['tesmov'], "id_tes", $row['id_movcon']);
-    $anagrafica = new Anagrafica();
-    $fornitore = $anagrafica->getPartner($tesmov['clfoco']);
-    $pdf->Cell(40, 3, $row['id'], 1);
-    $pdf->Cell(70, 3, $row['descri'], 1);
-    $pdf->Cell(70, 3, $fornitore["descri"], 1);
-    $pdf->Cell(40, 3, gaz_format_number($row["a_value"] * $row["quantity"]), 1, 0, 'R');
-    $pdf->Cell(40, 3, round($row["valamm"], 1), 1, 1, 'R');
+$form['assets'] = getAssets("2018-12-31");
+
+$head = true;
+foreach ($form['assets'] as $ka => $va) {
+    $r = array();
+    // ogni assets ha più righi-movimenti
+    foreach ($va as $k => $v) {
+        if ($head) {
+            $title = array('luogo_data' => $luogo_data,
+                'title' => "Gruppo:" . $v['ammmin_gruppo'] . " Specie:" . $v['ammmin_specie'],
+                'hile' => array(array('lun' => 70, 'nam' => 'Descrizione bene'),
+                    array('lun' => 20, 'nam' => '%'),
+                    array('lun' => 30, 'nam' => 'Immobilizzazione'),
+                    array('lun' => 30, 'nam' => 'Fondo'),
+                    array('lun' => 30, 'nam' => 'Quota deducibile'),
+                    array('lun' => 30, 'nam' => 'Quota non deduc.'),
+                    array('lun' => 30, 'nam' => 'Residuo'),
+                    array('lun' => 30, 'nam' => 'Amm.<50%')
+                )
+            );
+
+            $pdf = new Report_template();
+            $pdf->setVars($admin_aziend, $title);
+            $pdf->SetTopMargin(39);
+            $pdf->SetFooterMargin(20);
+            $pdf->AddPage('L');
+            $pdf->SetFont('helvetica', '', 8);
+            $head = false;
+        }
+        if ($v['type_mov'] == 1) {
+            $pdf->MultiCell(70, 4, $v['descri'] . "\n" . $v["desfor"] . ' Fatt.' . $v["nudtes"] . ' del ' . gaz_format_date($v['dtdtes'], false, true) . "\n" . $v['ammmin_ssd'] . "\n Ammortamento normale = " . $v['ammmin_ssrate'] . '%', 1, 'L', FALSE, 2);
+            $pdf->Ln(-4);
+            $pdf->Cell(70, 4);
+            $pdf->Cell(20, 4, gaz_format_number($v['valamm']), 1, 0, 'C');
+            $pdf->Cell(30, 4, gaz_format_number($v['fixed_val']), 1, 0, 'R');
+            $pdf->Cell(30, 4, '', 1);
+            $pdf->Cell(30, 4, '', 1);
+            $pdf->Cell(30, 4, '', 1);
+            $pdf->Cell(30, 4, gaz_format_number($v['fixed_val']), 1, 0, 'R');
+            $pdf->Cell(30, 4, '', 1, 1);
+        } else {
+            $pdf->Cell(70, 4, gaz_format_date($v['dtdtes']) . ' ' . $v['descri'], 1, 0, 'L', 0, '', 2);
+            $pdf->Cell(20, 4, gaz_format_number($v['valamm']), 1, 0, 'C');
+            $pdf->Cell(30, 4, gaz_format_number($v['fixed_subtot']), 1, 0, 'R');
+            $pdf->Cell(30, 4, gaz_format_number($v['found_subtot']), 1, 0, 'R');
+            $pdf->Cell(30, 4, gaz_format_number($v['cost_val']), 1, 0, 'R');
+            $pdf->Cell(30, 4, gaz_format_number($v['noded_val']), 1, 0, 'R');
+            $pdf->Cell(30, 4, gaz_format_number($v['fixed_subtot'] - $v['found_subtot']), 1, 0, 'R');
+            $pdf->Cell(30, 4, gaz_format_number($v['lost_cost']), 1, 1, 'R');
+        }
+    }
+    $pdf->Ln(4);
 }
-
 $pdf->Output();
 ?>
