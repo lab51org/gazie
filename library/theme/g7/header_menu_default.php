acb2ecfc58b709f1416193d8e1d5cff520499868
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-12-24 17:23:44 +0000

    diversi piccoli errori che impedivano a smartmenu di essere responsivo quando c'erano troppe voci
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1499 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- library/theme/g7/header_menu_default.php
+++ library/theme/g7/header_menu_default.php
@@ -43,9 +43,9 @@ function submenu($menu_data) {
         }
         $submnu = '<a href="' . $mnu['link'] . '">' . $submnu . stripslashes($mnu['name']);
         if (count($mnu) > 5) { //	Esiste un sotto menu
-           echo "\t\t\t\t\t\t\t" . '<li>' . $submnu. "<span class=\"caret\"></span></a>";
-           submenu($mnu);
-           echo "\t\t\t\t\t\t\t</li>\n";
+            echo "\t\t\t\t\t\t\t" . '<li>' . $submnu . "<span class=\"caret\"></span></a>";
+            submenu($mnu);
+            echo "\t\t\t\t\t\t\t</li>\n";
         } else {
             echo "\t\t\t\t\t\t\t<li>" . $submnu . "</a></li>\n";
         }
@@ -61,67 +61,65 @@ function submenu($menu_data) {
 ?>
 
 <!-- Navbar static top per menu multilivello responsive -->
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container-fluid gaz-nav">
-        <div id="l-wrapper" class="navbar-header company-color">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-                <a  href="../../modules/root/admin.php"> <?php echo strtoupper($admin_aziend["ragso1"]); ?>
-                </a>
-        </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-<?php
-                // stampo la prima voce della barra del menù con il dropdown dei moduli 
-                $i = 0;
-                foreach ($menuArray as $menu_modules_val) {
-                    if ($i == 0) { // sul modulo attivo non permetto i submenu in quanto verrano messi sulla barra orizzontale 
-                        echo "\t\t\t\t<li>" . '<a class="dropdown-toggle" data-toggle="dropdown"><img src="' . $menu_modules_val["icon"] . '"/>&nbsp;' . $menu_modules_val['name'] . '<span class="caret"></span></a>';
-                        echo "\n\t\t\t\t\t" . '<ul class="dropdown-menu">' . "\n";
-                    } else {
-                        echo "\t\t\t\t\t";
-                        echo '<li><a href="' . $menu_modules_val['link'] . '"><img src="' . $menu_modules_val["icon"] . '"/>&nbsp;' . $menu_modules_val['name'] . "<span class=\"caret\"></span></a>\n";
-                        submenu($menu_modules_val);
-                        echo "\t\t\t\t\t</li>\n";
-                    }
-                    $i++;
-                }
-                // fine stampa prima voce menu
-?>
-            </ul>
-            </li>
-<?php
+<div class="navbar navbar-default" role="navigation">
+    <div id="l-wrapper" class="navbar-header company-color">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a  href="../../modules/root/admin.php"> <?php echo strtoupper($admin_aziend["ragso1"]); ?>
+        </a>
+    </div>
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <?php
+            // stampo la prima voce della barra del menù con il dropdown dei moduli 
             $i = 0;
-            foreach ($menuArray[0] as $menu) {
-                // stampo nella barra del menù il dropdown del modulo 
-                $icon_lnk = '';
-                if (isset($menu['icon']) && preg_match("/^[A-Za-z0-9!@#$%&()*;:_.'\/\\\\ ]+\.png$/", $menu['icon'])) {
-                    $icon_lnk = '<img src="' . $menu['icon'] . '" />';
-                }
-                if ($i > 4) { // perché ci sono 5 indici prima dei dati veri e propri
-                    if (count($menu) > 5) { // Esiste un sotto menu
-                        echo "\t\t\t" . '<li class="dropdown">'
-                        . '<a href="' . $menu['link'] . '">' . $icon_lnk . ' ' . $menu['name'] . '<span class="caret"></span></a>';
-                    } else {
-                        echo "\t\t\t" . '<li><a class="dropdown" href="'.$menu['link'].'">'.$icon_lnk.''.$menu['name'].'</a>';
-                    }
-                    submenu($menu);
+            foreach ($menuArray as $menu_modules_val) {
+                if ($i == 0) { // sul modulo attivo non permetto i submenu in quanto verrano messi sulla barra orizzontale 
+                    echo "\t\t\t\t<li>" . '<a class="dropdown-toggle" data-toggle="dropdown"><img src="' . $menu_modules_val["icon"] . '"/>&nbsp;' . $menu_modules_val['name'] . '<span class="caret"></span></a>';
+                    echo "\n\t\t\t\t\t" . '<ul class="dropdown-menu">' . "\n";
+                } else {
+                    echo "\t\t\t\t\t";
+                    echo '<li><a href="' . $menu_modules_val['link'] . '"><img src="' . $menu_modules_val["icon"] . '"/>&nbsp;' . $menu_modules_val['name'] . "<span class=\"caret\"></span></a>\n";
+                    submenu($menu_modules_val);
                     echo "\t\t\t\t\t</li>\n";
-                    $livello3 = $menu;
                 }
                 $i++;
             }
-?>
-            <li id="user-position">
-                <a href="../root/logout.php">
-                    <img src="../root/view.php?table=admin&field=Login&value=<?php echo $admin_aziend['Login'] ?>" height="30" title="Logout - <?php echo $admin_aziend['Cognome'] . ' ' . $admin_aziend['Nome']; ?>" >Exit
-                </a>
-            </li>
-            </ul>
-        </div>
-    </div><!-- chiude div container -->
+            // fine stampa prima voce menu
+            ?>
+        </ul>
+        </li>
+        <?php
+        $i = 0;
+        foreach ($menuArray[0] as $menu) {
+            // stampo nella barra del menù il dropdown del modulo 
+            $icon_lnk = '';
+            if (isset($menu['icon']) && preg_match("/^[A-Za-z0-9!@#$%&()*;:_.'\/\\\\ ]+\.png$/", $menu['icon'])) {
+                $icon_lnk = '<img src="' . $menu['icon'] . '" />';
+            }
+            if ($i > 4) { // perché ci sono 5 indici prima dei dati veri e propri
+                if (count($menu) > 5) { // Esiste un sotto menu
+                    echo "\t\t\t" . '<li class="dropdown">'
+                    . '<a href="' . $menu['link'] . '">' . $icon_lnk . ' ' . $menu['name'] . '<span class="caret"></span></a>';
+                } else {
+                    echo "\t\t\t" . '<li><a class="dropdown" href="' . $menu['link'] . '">' . $icon_lnk . '' . $menu['name'] . '</a>';
+                }
+                submenu($menu);
+                echo "\t\t\t\t\t</li>\n";
+                $livello3 = $menu;
+            }
+            $i++;
+        }
+        ?>
+        <li id="user-position">
+            <a href="../root/logout.php">
+                <img src="../root/view.php?table=admin&field=Login&value=<?php echo $admin_aziend['Login'] ?>" height="30" title="Logout - <?php echo $admin_aziend['Cognome'] . ' ' . $admin_aziend['Nome']; ?>" >Exit
+            </a>
+        </li>
+        </ul>
+    </div>
 </div><!-- chiude navbar -->
