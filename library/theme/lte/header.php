bd4596bc4a09fbee60104c2be681f0a4d6e5941d
andreabia <andreabia@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2016-12-26 17:42:44 +0000

    anche i bottoni rapidi vengono filtrati dai permessi sul menu
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1500 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- library/theme/lte/header.php
+++ library/theme/lte/header.php
@@ -1,4 +1,28 @@
-<?php  
+<?php
+/*
+  -------------------------------------------------------------------
+  GAzie - Gestione Azienda
+  Copyright (C) 2004-2016 Antonio De Vincentiis Montesilvano (PE)
+  (http://www.devincentiis.it)
+  <http://gazie.sourceforge.net>
+  -------------------------------------------------------------------
+  Questo programma e` free software;   e` lecito redistribuirlo  e/o
+  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
+  come e` pubblicata dalla Free Software Foundation; o la versione 2
+  della licenza o (a propria scelta) una versione successiva.
+
+  Questo programma  e` distribuito nella speranza  che sia utile, ma
+  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
+  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
+  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
+
+  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
+  Generica GNU insieme a   questo programma; in caso  contrario,  si
+  scriva   alla   Free  Software Foundation, 51 Franklin Street,
+  Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
+  -------------------------------------------------------------------
+ */
+
     if (!strstr($_SERVER["REQUEST_URI"], "login_admin") == "login_admin.php") {
         $_SESSION['lastpage'] = $_SERVER["REQUEST_URI"];
     }
@@ -518,7 +542,8 @@ function HeadMain($idScript = '', $jsArray = '', $alternative_transl = false, $c
                 echo "<li><a href=\"../../modules/root/admin.php\"><i class=\"fa fa-home\"></i></a></li>";
                 //echo "<li><a href=\"../../modules/".$module."/docume_".$module.".php\"><i class=\"fa fa-question\"></i></a></li>";
                 while ($r = gaz_dbi_fetch_array($result2)) {
-                    echo '<li><a href="'.$r["link"].'">'.stripslashes ($transl[$module]["m3"][$r["translate_key"]]["1"]).'</a></li>';
+                    if ( $admin_aziend["Abilit"]>=$r["accesskey"] )
+                        echo '<li><a href="'.$r["link"].'">'.stripslashes ($transl[$module]["m3"][$r["translate_key"]]["1"]).'</a></li>';
                 }
                 echo "</ol>";
             } else {
@@ -535,7 +560,8 @@ function HeadMain($idScript = '', $jsArray = '', $alternative_transl = false, $c
                     echo "<li><a href=\"../../modules/root/admin.php\"><i class=\"fa fa-home\"></i></a></li>";
                     //echo "<li><a href=\"../../modules/".$module."/docume_".$module.".php\"><i class=\"fa fa-help\"></i></a></li>";
                     while ($r = gaz_dbi_fetch_array($result4)) {
-                        echo '<li><a href="'.$r["link"].'">'.stripslashes ($transl[$module]["m3"][$r["translate_key"]]["1"]).'</a></li>';
+                        if ( $admin_aziend["Abilit"]>=$r["accesskey"] )
+                            echo '<li><a href="'.$r["link"].'">'.stripslashes ($transl[$module]["m3"][$r["translate_key"]]["1"]).'</a></li>';
                     }
                     echo "</ol>";
                 }
