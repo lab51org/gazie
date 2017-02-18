7992925dbb81522a366441eeb93a3c7fd017e59e
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2017-02-18 13:26:51 +0000

    corretto errore di mancato recepimento id_paymov_ref sui righi successivi al primon modifica movimento contabile e reso meno stringente il controllo di congruenza con il valore del rigo, adesso segna l'errore sole se le partite superano il rigo contabile e non viceversa, è utile quando si inizia ad usare lo scadenziario partendo da un database vecchio

--- trunk/modules/contab/admin_movcon.php
+++ trunk/modules/contab/admin_movcon.php
@@ -199,9 +199,9 @@ if ((!isset($_POST['Update'])) and ( isset($_GET['Update']))) { //se e' il primo
                 $paymov_tot[$i] = 0.00;
                 foreach ($_POST['paymov'][$i] as $k => $v) {
                     $form['paymov'][$i][$k] = $v;  // qui devo ancora fare il parsing
-                    $paymov_tot[$i] +=$v['amount'];
+                    $paymov_tot[$i] += $v['amount'];
                 }
-                if ($paymov_tot[$i] >= 0.01 && round($paymov_tot[$i], 2) <> $form['importorc'][$i]) {
+                if ($paymov_tot[$i] >= 0.01 && round($paymov_tot[$i], 2) > $form['importorc'][$i]) {
                     $msg .= '15+';
                 }
             } else {
@@ -619,9 +619,9 @@ if ((!isset($_POST['Update'])) and ( isset($_GET['Update']))) { //se e' il primo
                                                 $y_paymov = $form['date_doc_Y'];
                                                 $num_paymov = intval($_POST['protocollo']);
                                             }
-					    if (intval(substr($v['id_tesdoc_ref'],0,4)) <= 2000) {
-                                            	$new_paymov[$j]['id_tesdoc_ref'] = $y_paymov . $form['registroiva'] . $form['sezioneiva'] . str_pad($num_paymov, 9, 0, STR_PAD_LEFT);
-					    }
+                                        }
+                                        if (intval(substr($v['id_tesdoc_ref'], 0, 4)) <= 2000) {
+                                            $new_paymov[$j]['id_tesdoc_ref'] = $y_paymov . $form['registroiva'] . $form['sezioneiva'] . str_pad($num_paymov, 9, 0, STR_PAD_LEFT);
                                         }
                                         if ($form['paymov_op_cl'][$i] == 1) { // apertura partita
                                             $new_paymov[$j]['id_rigmoc_doc'] = $row_con['id_rig'];
@@ -671,9 +671,9 @@ if ((!isset($_POST['Update'])) and ( isset($_GET['Update']))) { //se e' il primo
                                     $y_paymov = $form['date_doc_Y'];
                                     $num_paymov = intval($_POST['protocollo']);
                                 }
-	                        if (intval(substr($v['id_tesdoc_ref'],0,4)) <= 2000) {
-                                   $new_paymov[$j]['id_tesdoc_ref'] = $y_paymov . $form['registroiva'] . $form['sezioneiva'] . str_pad($num_paymov, 9, 0, STR_PAD_LEFT);
-				}
+                                if (intval(substr($v['id_tesdoc_ref'], 0, 4)) <= 2000) {
+                                    $new_paymov[$j]['id_tesdoc_ref'] = $y_paymov . $form['registroiva'] . $form['sezioneiva'] . str_pad($num_paymov, 9, 0, STR_PAD_LEFT);
+                                }
                             }
                             if ($form['paymov_op_cl'][$i] == 1) { // apertura partita
                                 $new_paymov[$j]['id_rigmoc_doc'] = $last_id_rig;
