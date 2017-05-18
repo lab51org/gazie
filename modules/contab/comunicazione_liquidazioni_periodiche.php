36e601afb1768014cdcf14ba1189517052352d21
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2017-05-18 14:59:54 +0000

    preparativi per implementare la generazione del file xml delle comunicazioni liquidazioni periodiche iva IVP
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1673 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/contab/comunicazione_liquidazioni_periodiche.php
@@ -0,0 +1,79 @@
+<?php
+/*
+  --------------------------------------------------------------------------
+  GAzie - Gestione Azienda
+  Copyright (C) 2004-2017 - Antonio De Vincentiis Montesilvano (PE)
+  (http://www.devincentiis.it)
+  <http://gazie.sourceforge.net>
+  --------------------------------------------------------------------------
+  Questo programma e` free software;   e` lecito redistribuirlo  e/o
+  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
+  come e` pubblicata dalla Free Software Foundation; o la versione 2
+  della licenza o (a propria scelta) una versione successiva.
+
+  Questo programma  e` distribuito nella speranza  che sia utile, ma
+  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
+  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
+  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
+
+  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
+  Generica GNU insieme a   questo programma; in caso  contrario,  si
+  scriva   alla   Free  Software Foundation, 51 Franklin Street,
+  Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
+  --------------------------------------------------------------------------
+ */
+require("../../library/include/datlib.inc.php");
+$admin_aziend = checkAdmin();
+$msg = array('err' => array(), 'war' => array());
+require("../../library/include/check.inc.php");
+
+require("../../library/include/header.php");
+$script_transl = HeadMain();
+$gForm = new contabForm();
+if (count($msg['err']) > 0) { // ho un errore
+    $gForm->gazHeadMessage($msg['err'], $script_transl['err'], 'err');
+}
+?>
+
+<form method="POST" name="form" enctype="multipart/form-data">
+    <div class="text-center"><b><?php echo $script_transl['title']; ?></b></div>
+    <div class="panel panel-default gaz-table-form">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                    </div>
+                </div>
+            </div><!-- chiude row  -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                    </div>
+                </div>
+            </div><!-- chiude row  -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                    </div>
+                </div>
+            </div><!-- chiude row  -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <div class="col-sm-8">
+                        </div>
+                    </div>
+                </div>
+            </div><!-- chiude row  -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                    </div>
+                </div>
+            </div><!-- chiude row  -->
+        </div><!-- chiude container  -->
+    </div><!-- chiude panel  -->
+</form>
+<?php
+require("../../library/include/footer.php");
+?>
