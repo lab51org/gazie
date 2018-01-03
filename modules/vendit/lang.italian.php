0871d29366817b5dbb5237832205aa209e6dc43b
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-01-03 10:14:03 +0000

    piccole correzioni
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1788 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/vendit/lang.italian.php
+++ modules/vendit/lang.italian.php
@@ -999,7 +999,7 @@ $strScript = array("admin_client.php" =>
             'La data di emissione precede quella dell\'ultima fattura emessa',
             'aut_pay' => 'Pagamento che prevede l\'incasso automatico',
             'no_bank' => 'ATTENZIONE! MANCA LA BANCA D\'APPOGGIO',
-            'cust_pay' => 'Cliente con DdT con diversi pagamenti',
+            'cust_pay' => 'Cliente con DdT aventi modalità di pagamento diversi, verrà generata una fattura per tipo',
         ),
         'cliente' => 'Cliente: ',
         'date_exe' => 'Data di emissione ',
