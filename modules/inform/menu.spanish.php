000975a051ea231b38c37879fbfe7ade09016302
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-02-10 15:10:01 +0000

    Prime modifiche per la gestione delle commesse
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1816 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/inform/menu.spanish.php
+++ modules/inform/menu.spanish.php
@@ -48,7 +48,8 @@ $transl['inform'] = array('name' => "Herramientas & Info",
         8 => array("Backup su browser", "Backup remoto"),
         9 => array("Backup su server","Backup locale"),
         10 => array("Anagrafiche clienti-fornitori", "Anagrafiche clienti-fornitori"),
-        11 => array("Comuni","Municipalità")
+        11 => array("Comuni","Municipalità"),
+        12 => array("Province","Province")
     )
 );
 ?>
