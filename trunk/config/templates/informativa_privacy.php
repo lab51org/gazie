3dc76d4fe1acca8cd810e7ee637cc4d98952ee19
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-04-20 20:33:39 +0000

    questa, in linea di massima, è la nuova informativa sulla privacy aderente alle prossime disposizioni in materia (GDPR) il prossimo passo sarà quello di farlo generare ad ogni inserimento/modifica dei partner commerciali. Lo stesso dovrà essere fatto per gli utenti del gestionale, si dovranno poter generare le lettere di incarico in automatico. Discorso diverso per il registro (il vecchio DPS): per questo ho intenzione di avvalermi di una interfaccia che permette la modifica e generazione ad ogni 31 di marzo...

--- trunk/config/templates/informativa_privacy.php
+++ trunk/config/templates/informativa_privacy.php
@@ -42,6 +42,8 @@ class InformativaPrivacy extends Template
         $this->cliente4 = $this->docVars->cliente4;
         $this->cliente5 = $this->docVars->cliente5;
         $this->cliente6 = $this->docVars->client['sexper'];
+		$this->luogo = $this->docVars->client['citspe'].' ('.$this->docVars->client['prospe'].')';
+		$this->pec = $this->docVars->azienda['pec'];
         if ($this->docVars->intesta5 == 'F'){
            $this->descriAzienda = 'la sottoscritta';
         } elseif ($this->docVars->intesta5 == 'M'){
@@ -75,34 +77,143 @@ class InformativaPrivacy extends Template
 
     function body()
     {
-    $testo ="<p>".ucfirst($this->descriAzienda)." <b>".$this->intesta1." ".$this->intesta1bis."</b> ".$this->intesta2." ".$this->intesta3." email: ".$this->intesta4." (in seguito <b>“Titolare”</b>),in qualità di titolare del trattamento, La informa ai sensi dell’art. 13 D.Lgs. 30.6.2003 n. 196 (in seguito, <b>“Codice Privacy”</b>) e dell’art. 13 Regolamento UE n. 2016/679 (in seguito, <b>“GDPR”</b>) che i Suoi dati saranno trattati con le modalità e per le finalità seguenti:</p>
+    $testo =
+"<p>".ucfirst($this->descriAzienda)." <b>".$this->intesta1." ".$this->intesta1bis."</b> ".$this->intesta2." ".$this->intesta3." email: ".$this->intesta4." (in seguito <b>“Titolare”</b>),in qualità di titolare del trattamento, La informa ai sensi dell’art. 13 D.Lgs. 30.6.2003 n. 196 (in seguito, <b>“Codice Privacy”</b>) e dell’art. 13 Regolamento UE n. 2016/679 (in seguito, <b>“GDPR”</b>) che i Suoi dati saranno trattati con le modalità e per le finalità seguenti:</p>
 <ol><li><h4><b>Oggetto del Trattamento</b></h4></li>
-a) agli obblighi previsti dalla normativa vigente in materia fiscale;<br>
-b) all'invio ad un istituto di credito dei dati relativi alla fatturazione per la eventuale relativa riscossione a mezzo banca;
- c) alla formazione di un indirizzario per l'invio di eventuali comunicazioni
-3)Nell'ambito dei trattamenti descritti è necessaria la conoscenza e la memorizzazione di informazioni relative ai dati anagrafici e fiscali, e ad ogni altro dato necessario esclusivamente all'espletamento degli obblighi imposti dalla normativa fiscale e amministrativa.
-4)E' obbligo del titolare del trattamento rendere noto agli interessati che l'eventuale non comunicazione di una informazione indicata come obbligatoria ha come conseguenze emergenti l'impossibilità del titolare di adempiere agli obblighi imposti dalla normativa fiscale e amministrativa cui esso sia indirizzato.
-5)Ai sensi del Decreto Leg.vo n. 196/2003, il titolare informa che i dati possono in tutto o in parte, ove necessario e nei limiti della necessità stessa, essere comunicati al nostro consulente fiscale, per l'elaborazione della contabilità e per l'adempimento degli altri obblighi previsti dalla normativa fiscale e amministrativa. Tale comunicazione avverrà comunque con garanzia di tutela dei diritti dell'interessato e con divieto di ulteriore comunicazione o diffusione senza esplicita autorizzazione in proposito.
-6)La Spett.le ".$this->cliente1 .$this->cliente2.
-" può far valere i propri diritti come espressi dall'art. 7 del Decr. Leg.vo n. 196/2003, riportato di seguito, rivolgendosi al titolare del trattamento: ".
-$this->intesta1 . $this->intesta1bis . $this->intesta2 .$this->intesta3 ." email: ". $this->intesta4;
-     $diritti =
-"1. L'interessato ha diritto di ottenere la conferma dell'esistenza o meno di dati personali che lo riguardano, anche se non ancora registrati, e la loro comunicazione in forma intelligibile.
-2. L'interessato ha diritto di ottenere l'indicazione:
- a)dell'origine dei dati personali;
- b)delle finalità e modalità del trattamento;
- c)della logica applicata in caso di trattamento effettuato con l'ausilio di strumenti elettronici;
- d)degli estremi identificativi del titolare, dei responsabili e del rappresentante designato ai sensi dell'articolo 5, comma 2;
- e)dei soggetti o delle categorie di soggetti ai quali i dati personali possono essere comunicati o che possono venirne a conoscenza in qualità di rappresentante designato nel territorio dello Stato, di responsabili o incaricati.
-3. L'interessato ha diritto di ottenere:
- a)l'aggiornamento, la rettificazione ovvero, quando vi ha interesse, l'integrazione dei dati;
- b)la cancellazione, la trasformazione in forma anonima o il blocco dei dati trattati in violazione di legge, compresi quelli di cui non è necessaria la conservazione in relazione agli scopi per i quali i dati sono stati raccolti o successivamente trattati;
- c)l'attestazione che le operazioni di cui alle lettere a) e b) sono state portate a conoscenza, anche per quanto riguarda il loro contenuto, di coloro ai quali i dati sono stati comunicati o diffusi, eccettuato il caso in cui tale adempimento si rivela impossibile o comporta un impiego di mezzi manifestamente sproporzionato rispetto al diritto tutelato.
-4. L'interessato ha diritto di opporsi, in tutto o in parte:
- a)per motivi legittimi al trattamento dei dati personali che lo riguardano, ancorchè pertinenti allo scopo della raccolta;
- b)al trattamento di dati personali che lo riguardano a fini di invio di materiale pubblicitario o di vendita diretta o per il compimento di ricerche di mercato o di comunicazione commerciale.</ol>";
-    $this->SetFont('courier','',8);
-    $this->WriteHTMLCell(184,4,10,70,$testo, 0, 0, 0, true, 'J');
+Il Titolare tratta i dati personali, identificativi quali: nome, cognome, ragione
+sociale, indirizzo, telefono, e-mail, riferimenti bancari e di pagamento (in seguito,
+<b>“dati personali”</b> o anche <b>“dati”</b>) da Lei comunicati in occasione della conclusione di
+contratti per la fornitura di merci e/o servizi e di cui qui riportiamo l'anagrafica: "
+.$this->cliente1 .' '.$this->cliente2.' '.$this->cliente3.' '.$this->cliente4."
+	<li><h4><b>Finalità del trattamento</b></h4></li>
+I Suoi dati personali sono trattati:<br>
+A) senza il Suo consenso espresso (art. 24 lett. a), b), c) Codice Privacy e art. 6 lett.b), e) GDPR), per le seguenti Finalità di Servizio:<ul>
+<li></li>concludere i contratti per i servizi del Titolare;
+<li></li>adempiere agli obblighi precontrattuali, contrattuali e fiscali derivanti da
+rapporti con Lei in essere;
+<li></li>adempiere agli obblighi previsti dalla legge, da un regolamento, dalla
+normativa comunitaria o da un ordine dell’Autorità (come ad esempio in
+materia di antiriciclaggio);
+<li></li>esercitare i diritti del Titolare, ad esempio il diritto di difesa in giudizio;
+</ul><br>
+B) Solo previo Suo specifico e distinto consenso (artt. 23 e 130 Codice Privacy e art. 7
+GDPR), per le seguenti Finalità di Marketing:
+<ul>
+<li></li>inviarLe via e-mail, posta e/o sms e/o contatti telefonici, newsletter,
+comunicazioni commerciali e/o materiale pubblicitario su prodotti o servizi
+offerti dal Titolare e rilevazione del grado di soddisfazione sulla qualità dei
+servizi;
+<li></li>inviarLe via e-mail, posta e/o sms e/o contatti telefonici comunicazioni
+commerciali e/o promozionali inerenti la nostra attività commerciale.
+</ul>
+<br>Le segnaliamo che se siete già nostri clienti, potremo inviarLe comunicazioni
+commerciali relative a servizi e prodotti del Titolare analoghi a quelli di cui ha già
+usufruito, salvo Suo dissenso (art. 130 c. 4 Codice Privacy).	
+<li><h4><b>Modalità di trattamento</b></h4></li>
+Il trattamento dei Suoi dati personali è realizzato per mezzo delle operazioni indicate
+all’art. 4 Codice Privacy e all’art. 4 n. 2) GDPR e precisamente: raccolta,
+registrazione, organizzazione, conservazione, consultazione, elaborazione,
+modificazione, selezione, estrazione, raffronto, utilizzo, interconnessione, blocco,
+comunicazione, cancellazione e distruzione dei dati. I Suoi dati personali sono
+sottoposti a trattamento sia cartaceo che elettronico e/o automatizzato.
+Il Titolare tratterà i dati personali per il tempo necessario per adempiere alle finalità di
+cui sopra e comunque per non oltre 10 anni dalla cessazione del rapporto per le
+Finalità di Servizio e per non oltre 2 anni dalla raccolta dei dati per le Finalità di
+Marketing.
+<li><h4><b>Accesso ai dati</b></h4></li>
+I Suoi dati potranno essere resi accessibili per le finalità di cui all’art. 2.A) e 2.B):
+<ul>
+<li></li>a dipendenti e collaboratori del Titolare nella loro qualità di incaricati e/o
+responsabili interni del trattamento e/o amministratori di sistema;
+<li></li>a società terze o altri soggetti (a titolo indicativo, istituti di credito, studi
+professionali, consulenti, società di assicurazione per la prestazione di servizi
+assicurativi, etc.) che svolgono attività in outsourcing per conto del Titolare,
+nella loro qualità di responsabili esterni del trattamento.
+</ul>
+<li><h4><b>Comunicazione dei dati</b></h4></li>
+Senza la necessità di un espresso consenso (ex art. 24 lett. a), b), d) Codice
+Privacy e art. 6 lett. b) e c) GDPR), il Titolare potrà comunicare i Suoi dati per
+le finalità di cui all’art. 2 ad altri organi della P.A., Autorità
+giudiziarie, a società di assicurazione per la prestazione di servizi assicurativi,
+nonché a quei soggetti ai quali la comunicazione sia obbligatoria per legge
+per l’espletamento delle finalità dette. Detti soggetti tratteranno i dati nella loro
+qualità di autonomi titolari del trattamento.
+I Suoi dati non saranno diffusi.
+<li><h4><b>Trasferimento dati</b></h4></li>
+I dati personali sono conservati su server ubicati in ".$this->luogo." , all’interno dell’Unione
+Europea. 
+<li><h4><b> Natura del conferimento dei dati e conseguenze del rifiuto di rispondere/b></h4></li>
+Il conferimento dei dati per le finalità di cui all’art. 2.A) è obbligatorio. 
+In loro assenza, non potremo garantirLe i Servizi dell’art. 2.A). 
+<br>Il conferimento dei dati per le finalità di cui all’art. 2.B) è invece facoltativo. Può quindi 
+decidere di non conferire alcun dato o di negare successivamente la possibilità di trattare dati 
+già forniti: in tal caso, non potrà ricevere newsletter, comunicazioni commerciali e materiale 
+pubblicitario inerenti ai Servizi offerti.
+<li><h4><b>Diritti dell’interessato</b></h4></li>
+Nella Sua qualità di interessato, ha i diritti di cui all’art. 7 Codice Privacy e
+art. 15 GDPR e precisamente i diritti di:
+<ul>
+<li></li>
+a) ottenere la conferma dell'esistenza o meno di dati personali che La
+riguardano, anche se non ancora registrati, e la loro comunicazione in
+forma intelligibile;
+<li></li>b) ottenere l'indicazione: a) dell'origine dei dati personali; b) delle finalità e
+modalità del trattamento; c) della logica applicata in caso di trattamento
+effettuato con l'ausilio di strumenti elettronici; d) degli estremi
+identificativi del titolare, dei responsabili e del rappresentante designato ai
+sensi dell'art. 5, comma 2 Codice Privacy e art. 3, comma 1, GDPR; e) dei
+soggetti o delle categorie di soggetti ai quali i dati personali possono
+essere comunicati o che possono venirne a conoscenza in qualità di
+rappresentante designato nel territorio dello Stato, di responsabili o
+incaricati;
+<li></li>c) ottenere: a) l'aggiornamento, la rettificazione ovvero, quando vi ha
+interesse, l'integrazione dei dati; b) la cancellazione, la trasformazione in
+forma anonima o il blocco dei dati trattati in violazione di legge, compresi
+quelli di cui non è necessaria la conservazione in relazione agli scopi per i
+quali i dati sono stati raccolti o successivamente trattati;
+<li></li>d) l'attestazione che le operazioni di cui alle lettere a) e b) sono state portate
+a conoscenza, anche per quanto riguarda il loro contenuto, di coloro ai
+quali i dati sono stati comunicati o diffusi, eccettuato il caso in cui tale
+adempimento si rivela impossibile o comporta un impiego di mezzi
+manifestamente sproporzionato rispetto al diritto tutelato;
+<li></li>e) opporsi, in tutto o in parte: a) per motivi legittimi al trattamento dei dati
+personali che La riguardano, ancorché pertinenti allo scopo della raccolta;
+b) al trattamento di dati personali che La riguardano a fini di invio di
+materiale pubblicitario o di vendita diretta o per il compimento di ricerche di
+mercato o di comunicazione commerciale, mediante l’uso di sistemi
+automatizzati di chiamata senza l’intervento di un operatore mediante e-mail
+e/o mediante modalità di marketing tradizionali mediante telefono e/o posta
+cartacea. Si fa presente che il diritto di opposizione dell’interessato,
+esposto al precedente punto b), per finalità di marketing diretto mediante
+modalità automatizzate si estende a quelle tradizionali e che comunque
+resta salva la possibilità per l’interessato di esercitare il diritto di
+opposizione anche solo in parte. Pertanto, l’interessato può decidere di
+ricevere solo comunicazioni mediante modalità tradizionali ovvero solo
+comunicazioni automatizzate oppure nessuna delle due tipologie di
+comunicazione.
+<li></li>f) Ove applicabili, ha altresì i diritti di cui agli artt. 16-21 GDPR (Diritto di
+rettifica, diritto all’oblio, diritto di limitazione di trattamento, diritto alla
+portabilità dei dati, diritto di opposizione), nonché il diritto di reclamo
+all’Autorità Garante.
+</ul>
+<li><h4><b>Modalità di esercizio dei diritti</b></h4></li>
+Potrà in qualsiasi momento esercitare i diritti inviando:
+<ul>
+<li></li>una raccomandata a.r. a ".$this->intesta1." ".$this->intesta1bis." ".$this->intesta2."
+<li></li>una e-mail all’indirizzo P.E.C.: ".$this->pec."
+</ul>
+<li><h4><b>Titolare, responsabile e incaricati</b></h4></li>
+Il Titolare del trattamento è ".$this->intesta1." ".$this->intesta1bis." con sede
+in ".$this->intesta2.".
+L’elenco aggiornato dei responsabili e degli incaricati al trattamento è custodito
+presso la sede legale del Titolare del trattamento.
+</ol><b>Ai sensi dell’art. 23 del D. Lgs. 196/2003 e degli art. 4,5 e 7 del G.D.P.R. 2016/679, dichiaro di aver preso visione della presente Informativa<br><br>
+In Fede: __________________________
+<br>Dichiaro inoltre di <br> |_| Autorizzare <br> |_|  Non Autorizzare
+<br>il Titolare al trattamento dei miei dati nell’ambito delle finalità e nei modi qui documentati.<br><br>
+In Fede: __________________________";
+    $this->SetFont('courier','',7);
+    $this->WriteHTMLCell(184,4,10,67,$testo, 0, 0, 0, true, 'J');
     }
     function pageFooter()
     {
