303621cb96398967acb097c528d9beb67bc9d01b
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-05-06 09:12:52 +0000

    Cambiate le query per aggiornare il database al wiki della documentazione, prima veniva abilitato solo l'utente "amministratore" ed, inoltre, si rischiava il conflitto con moduli personalizzati
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1891 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_8.0.sql
+++ setup/install/update_to_8.0.sql
@@ -1,6 +1,5 @@
-UPDATE `gaz_config` SET `cvalue` = '110' WHERE `id` =2;
-INSERT INTO `gaz_module` (`name`, `link`, `icon`, `class`, `access`, `weight`) VALUES ('wiki', 'docume_wiki.php', 'wiki.png', '', '0', 17);
-INSERT INTO `gaz_admin_module` (`adminid`, `company_id`, `moduleid`, `access`) VALUES ('amministratore', '1', '17', '3');
+UPDATE `gaz_config` SET `cvalue` = '109' WHERE `id` =2;
+INSERT INTO `gaz_module` (`name`, `link`, `icon`, `class`, `access`, `weight`) VALUES ('wiki', 'docume_wiki.php', 'wiki.png', '', '0', 15);
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
-
+INSERT INTO `gaz_admin_module` (`adminid`, `company_id`, `moduleid`, `access`) SELECT user_name,(CONVERT('XXX',UNSIGNED INTEGER)),(SELECT MAX(id)+1 FROM `gaz_module`),3 FROM `gaz_admin` WHERE 1;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
