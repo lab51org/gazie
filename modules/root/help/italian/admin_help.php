1d1b3234d23b0b94310f8f1f3295fc40117d3d86
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-06-05 13:49:22 +0000

    aggiornati i link del footer della home page
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1940 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/root/help/italian/admin_help.php
+++ modules/root/help/italian/admin_help.php
@@ -1,7 +1,7 @@
-<span>Sui monitor ampi il menù principale si ottiene spostando il puntatore del mouse sopra la voce <strong>Home</strong> in alto a sinistra, su quelli piccoli (smartphone) cliccando sul <strong>bottone</strong> che appare in alto a sinistra.<!-- Si veda
-eventualmente questo breve <a title="guarda il video" href="help/italian/home_menu.ogv">filmato</a>.--></span>
-<span>Per una guida generale introduttiva a GAzie si possono consultare alcuni capitoli tratti da &laquo;<big>a</big>2&raquo; <a href="http://www.archive.org/details/AppuntiDiInformaticaLibera" target="new">Appunti Di Informatica Libera</a>: <a
-title="leggi la guida" href="http://appuntilinux.mirror.garr.it/mirrors/appuntilinux/a2/installazione_e_manutenzione_generale.pdf" target="_new"><em>Installazione e manutenzione generale</em></a>, <a title="leggi la guida" href="http://appuntilinux.mirror.garr.it/mirrors/appuntilinux/a2/manuale_sintetico_delle_funzionalita_principali_di_gazie.pdf" target="_new"><em>Manuale sintetico delle funzionalit&agrave; di Gazie</em></a>, <a title="leggi la guida" href="http://appuntilinux.mirror.garr.it/mirrors/appuntilinux/a2/tabelle_principali_della_base_di_dati_di_gazie.pdf" target="_new">
-<em>Tabelle principali della base di dati di Gazie</em></a>, <a title="leggi la guida" href="http://appuntilinux.mirror.garr.it/mirrors/appuntilinux/a2/esercitazioni_con_gazie.pdf" target="_new">
-<em>Esercitazioni con Gazie</em></a>. Si veda eventualmente anche questa <a title="leggi l'appendice" href="http://appuntilinux.mirror.garr.it/mirrors/appuntilinux/a2/gazie_non_e_un_giocattolo.htm" target="_new">appendice</a>
-se si cerca assistenza.</span>
+<span><div>Sui monitor ampi il menù principale si ottiene spostando il puntatore del mouse sopra la voce <strong>Home</strong> in alto a sinistra, su quelli piccoli (smartphone) cliccando sul <strong>bottone</strong> che appare in alto a sinistra.</div>
+<div>Per una guida generale introduttiva a GAzie si possono consultare alcuni capitoli tratti da &laquo;<big>a</big>2&raquo; <a href="http://www.archive.org/details/AppuntiDiInformaticaLibera" target="new">Appunti Di Informatica Libera</a>:</div><div>
+<ul>
+<li><a title="GAzie: indice generale" href="http://a2.pluto.it/a2/a2155.htm" target="_new"><em>Indice generale</em></a></li>
+<li> <a title="GAzie: manuale" href="http://a2.pluto.it/a2/a2153.htm" target="_new"><em>Manuale sintetico delle funzionalità principali di Gazie</em></a></li>
+<li> <a title="GAzie: video" href="https://www.youtube.com/user/appunti2/search?query=gazie" target="_new"><em>I video ad uso didattico (dic.2011)</em></a></li>
+</ul></div></span>
