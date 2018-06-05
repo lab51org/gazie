92aebcda3f9a1ccbf4fa0d226905c20c93a3207d
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-06-05 07:06:56 +0000

    Aggiunto al menù la voce per l'emissione di scontrini in modalità "prezzi IVA compresa" realizzato da D.Santarella (al momento manca l'array della traduzione)
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@1938 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/vendit/menu.italian.php
+++ modules/vendit/menu.italian.php
@@ -85,6 +85,7 @@ $transl['vendit'] = array('name' => "Vendite",
         46 => array("Visualizza Ordini settimanali del giorno", "Ordini settimanali del giorno"),
         47 => array("Nuovo ordine settimanale del giorno", "Inserisci ordine settimanale del giorno"),
         48 => array("Evadi ordini settimanali del giorno", "Evadi ordini settimanali del giorno"),
+        49 => array("Emetti scontrino prezzi IVA inclusa", "Scontrino prezzi IVA inclusa")
     )
 );
 ?>
--- setup/install/update_to_7.10.sql
+++ setup/install/update_to_7.10.sql
@@ -2,7 +2,6 @@ UPDATE `gaz_config` SET `cvalue` = '109' WHERE `id` =2;
 ALTER TABLE `gaz_module` CHANGE COLUMN `link` `link` VARCHAR(100) NOT NULL DEFAULT '' AFTER `name`;
 ALTER TABLE `gaz_menu_module` CHANGE COLUMN `link` `link` VARCHAR(100) NOT NULL DEFAULT '' AFTER `id_module`;
 ALTER TABLE `gaz_menu_script` CHANGE COLUMN `link` `link` VARCHAR(100) NOT NULL DEFAULT '' AFTER `id_menu`;
-INSERT INTO `gaz_module` (`name`, `link`, `icon`, `class`, `access`, `weight`) VALUES ('wiki', 'docume_wiki.php', 'wiki.png', '', '0', 15);
 DELETE FROM `gaz_menu_script` WHERE  `link` LIKE 'select_an%';
 DELETE FROM `gaz_menu_script` WHERE  `link` LIKE 'select_esportazione%';
 DELETE FROM `gaz_menu_module` WHERE  `link` LIKE '%report_statis.php%';
