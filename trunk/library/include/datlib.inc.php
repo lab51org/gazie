7b3d16603ec81bc95f09b75d8fefedd5048356b5
andreabia <andreabia@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-07-11 06:16:20 +0000

    rimossa funzione che creava problemi

--- trunk/library/include/datlib.inc.php
+++ trunk/library/include/datlib.inc.php
@@ -86,7 +86,6 @@ foreach ($tn as $v) {
 }
 
 // controllo se è abilitata la funzionalità per il calcolo totale alternativo
-$enable_quanti2 = gaz_dbi_get_row($gTables['company_config'], 'var', 'calc_quanti2');
-$enable_quanti2 = $enable_quanti2["val"];
-
+/*$enable_quanti = gaz_dbi_get_row($gTables['company_config'], 'var', 'calc_quanti2');
+$enable_quanti2 = $enable_quanti["val"];*/
 ?>
