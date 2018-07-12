d3b040ec593325618b9f61e136361fc464bdee34
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-07-12 14:59:07 +0000

    Applicato il ridimensionamento automatico immagini introdotto da Antonio Germani anche ad admin_artico.php del modulo magazzino, incrementata la dimensione massima in pixel
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2010 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/admin_artico.php
+++ modules/camp/admin_artico.php
@@ -26,7 +26,9 @@ require("../../library/include/datlib.inc.php");
 $admin_aziend = checkAdmin();
 $msg = array('err' => array(), 'war' => array());
 $modal_ok_insert = false;
-$today=	strtotime(date("Y-m-d H:i:s",time()));$presente="";$largeimg="";
+$today=	strtotime(date("Y-m-d H:i:s",time()));
+presente="";
+$largeimg=0;
 /** ENRICO FEDELE */
 /* Inizializzo per aprire in finestra modale */
 $modal = false;
@@ -116,26 +118,27 @@ if (isset($_POST['Insert']) || isset($_POST['Update'])) {   //se non e' il primo
             if ($_FILES['userfile']['size'] > 65530){
 				 //$msg['err'][] = 'filsiz';
 				 //Antonio Germani anziche segnalare errore ridimensiono l'immagine
-							$maxDim = 80;
-							$file_name = $_FILES['userfile']['tmp_name'];
-							list($width, $height, $type, $attr) = getimagesize( $file_name );
-							if ( $width > $maxDim || $height > $maxDim ) {
-								$target_filename = $file_name;
-								$ratio = $width/$height;
-									if( $ratio > 1) {
-										$new_width = $maxDim;
-										$new_height = $maxDim/$ratio;
-									} else {
-											$new_width = $maxDim*$ratio;
-											$new_height = $maxDim;
-										}
-							$src = imagecreatefromstring( file_get_contents( $file_name ) );
-							$dst = imagecreatetruecolor( $new_width, $new_height );
-							imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
-							imagedestroy( $src );
-							imagepng( $dst, $target_filename); // adjust format as needed
-							imagedestroy( $dst );
-							}
+				$maxDim = 400;
+				$file_name = $_FILES['userfile']['tmp_name'];
+				list($width, $height, $type, $attr) = getimagesize( $file_name );
+				if ( $width > $maxDim || $height > $maxDim ) {
+					$target_filename = $file_name;
+					$ratio = $width/$height;
+						if( $ratio > 1) {
+							$new_width = $maxDim;
+							$new_height = $maxDim/$ratio;
+						} else {
+								$new_width = $maxDim*$ratio;
+								$new_height = $maxDim;
+						}
+					$src = imagecreatefromstring( file_get_contents( $file_name ) );
+					$dst = imagecreatetruecolor( $new_width, $new_height );
+					imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+					imagedestroy( $src );
+					imagepng( $dst, $target_filename); // adjust format as needed
+					imagedestroy( $dst );
+				}
+				$largeimg=1;
 				// fine ridimensionamento immagine
 			}	           
         }
@@ -166,8 +169,11 @@ if (isset($_POST['Insert']) || isset($_POST['Update'])) {   //se non e' il primo
         }
         if (count($msg['err']) == 0) { // nessun errore
             if ($_FILES['userfile']['size'] > 0) { //se c'e' una nuova immagine nel buffer
-             If ($largeimg==0){$form['image'] = file_get_contents($_FILES['userfile']['tmp_name']);}
-			 else {$form['image'] = file_get_contents($target_filename);}
+             if ($largeimg==0){
+				 $form['image'] = file_get_contents($_FILES['userfile']['tmp_name']);
+			 } else {
+				 $form['image'] = file_get_contents($target_filename);
+			 }
             } elseif ($toDo == 'update') { // altrimenti riprendo la vecchia ma solo se è una modifica
                 $oldimage = gaz_dbi_get_row($gTables['artico'], 'codice', $form['ref_code']);
                 $form['image'] = $oldimage['image'];
