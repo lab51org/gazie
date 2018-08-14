c92800fb941853825a056f04cd0f53ebbdeddfa8
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-08-14 10:59:38 +0000

    Acquisizione fattura elettronica da fornitore: il report offre la possibilità di visualizzare le fatture elettroniche acquisite

--- trunk/setup/install/update_to_7.11.sql
+++ trunk/setup/install/update_to_7.11.sql
@@ -183,8 +183,8 @@ UPDATE `gaz_XXXtesdoc` SET `template` = 'FatturaSemplice' WHERE `tipdoc` = 'FNC'
 ALTER TABLE `gaz_XXXrigdoc`	CHANGE COLUMN `descri` `descri` VARCHAR(1000) NOT NULL COMMENT '1000 caratteri per uniformarsi al tracciato della fattura elettronica' AFTER `codart`;
 ALTER TABLE `gaz_XXXrigbro`	CHANGE COLUMN `descri` `descri` VARCHAR(1000) NOT NULL COMMENT '1000 caratteri per uniformarsi al tracciato della fattura elettronica' AFTER `codice_fornitore`;
 ALTER TABLE `gaz_XXXtesdoc`	ADD COLUMN `datreg` DATE NULL DEFAULT NULL COMMENT 'Data in cui si vuole venga registrata in contabilità (prima per le fatture d\'acquisto si usava impropriamente datemi)' AFTER `id_con`;
-UPDATE `gaz_XXXtesdoc` SET `datreg`=`datemi` WHERE `tipdoc` LIKE 'A__';
-UPDATE `gaz_XXXtesdoc` SET `datemi`=FALSE WHERE `tipdoc` LIKE 'A__';
+UPDATE `gaz_XXXtesdoc` SET `datreg`=`datemi` WHERE `tipdoc` LIKE 'AF_';
+UPDATE `gaz_XXXtesdoc` SET `datemi`=FALSE WHERE `tipdoc` LIKE 'AF_';
 UPDATE `gaz_XXXtesdoc` SET `datemi`=FALSE WHERE `datemi` <= '2004-01-27';
 UPDATE `gaz_XXXtesdoc` SET `data_ordine`=FALSE WHERE `data_ordine` <= '2004-01-27';
 UPDATE `gaz_XXXtesdoc` SET `datfat`=FALSE WHERE `datfat` <= '2004-01-27';
