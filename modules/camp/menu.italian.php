dc103f102f2218b071a7db9c0101d3a9d2e37554
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-08-23 06:49:44 +0000

    Registro di campagna: inseriti i nuovi file, ancora vuoti, per il nuovo menù di gestione fitofarmaci.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2096 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/menu.italian.php
+++ modules/camp/menu.italian.php
@@ -28,7 +28,8 @@ $transl['camp'] = array('name' => "Registro di campagna",
                                          2 =>array("Gestione Categorie Merceologiche","Cat.Merceologiche"),
                                          3 =>array("Giornale di Magazzino","Movimenti"),
                                          4 =>array("Causali di Magazzino","Causali"),
-										 5=>array("Gestione Campi di coltivazione","Campi di coltivazione")
+										 5=>array("Gestione Campi di coltivazione","Campi di coltivazione"),
+										 6 =>array("Gestione fitofarmaci","Fitofarmaci")
                                          ),
                           'm3' => array( 1 =>array("Inserisci un nuovo articolo","Nuova merce/servizio"),
                                          2 =>array("Inventario di magazzino","Inventario di magazzino"),
@@ -41,7 +42,9 @@ $transl['camp'] = array('name' => "Registro di campagna",
 										 9 =>array("Inserisci un nuovo campo","Nuovo campo di coltivazione"),
 										 10 =>array("Stampa dichiarazione rame metallo","Dichiarazione rame metallo"),
 										 11 =>array("Update tabella fitofarmaci","Aggiorna tabella fitofarmaci"),
-										 12 =>array("Calcolo costo di una produzione","Costo produzione")
+										 12 =>array("Calcolo costo di una produzione","Costo produzione"),
+										 13 =>array("Gestione avversità","Avversità"),
+										 14 =>array("Gestione coltivazioni","Coltivazioni")
 										 
                                       )
                           );
