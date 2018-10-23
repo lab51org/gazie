3d37b276cd7129180f5ba0cd3732d80a7e9e1747
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-10-23 05:16:17 +0000

    Registro di campagna- mostra lotti: risolto il problema della visualizzazione dei file protetti da htaccess

--- trunk/modules/camp/mostra_lotti.php
+++ trunk/modules/camp/mostra_lotti.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <?php
 /*
   --------------------------------------------------------------------------
@@ -22,6 +22,7 @@
   scriva   alla   Free  Software Foundation, 51 Franklin Street,
   Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
   --------------------------------------------------------------------------
+>>>>>> Antonio Germani -- MOSTRA Lotti  <<<<<<
  */
 
 require("../../library/include/datlib.inc.php");
@@ -29,7 +30,14 @@ $lm = new lotmag;
 $admin_aziend=checkAdmin();
 $codice = filter_input(INPUT_GET, 'codice');
 $lm -> getAvailableLots($codice,0);
-require("../../library/include/header.php");
+require("../../library/include/header.php"); 
+
+if (isset($_POST['close'])){
+	foreach (glob("../../modules/camp/tmp/*") as $fn) {// prima cancello eventuali precedenti file temporanei
+             unlink($fn);
+    } // poi chiudo la finestra e esco
+	echo "<script>window.close();</script>";exit;
+}
 ?>
 
 <body>
@@ -37,58 +45,55 @@ require("../../library/include/header.php");
 <table class="Tlarge table table-striped table-bordered table-condensed table-responsive">
     	<thead>
             <tr class="FacetDataTD">
-                
-                <th align="center" >Lotti disponibili
-                    
+                <th align="center" >Lotti disponibili   
                 </th>
-                <th align="center" >Certificati
-                   
-                </th>
-               
+                <th align="center" >Certificati   
+                </th>              
             </tr>
-			
 			</thead>
-			
-			<?php
-if (count($lm->available) > 0) { 
-                    foreach ($lm->available as $v_lm) {
-                            $img="";
-                            echo '<tr class="FacetDataTD"><td class="FacetFieldCaptionTD">id: '
-                            . $v_lm['id']
-                            . ' - lotto: ' . $v_lm['identifier']
-                            . ' - exp: ' . gaz_format_date($v_lm['expiry'])
-							. ' - disponibilità: ' . gaz_format_quantity($v_lm['rest'], 0, $admin_aziend['decimal_quantity'])
-                            .'</td><td>';
+<?php
+	foreach (glob("../../modules/camp/tmp/*") as $fn) {// prima cancello eventuali precedenti file temporanei
+             unlink($fn);
+    } 
+	
+	if (count($lm->available) > 0) { 
+        foreach ($lm->available as $v_lm) {
+               $img="";
+               echo '<tr class="FacetDataTD"><td class="FacetFieldCaptionTD">id: '
+               . $v_lm['id']
+               . ' - lotto: ' . $v_lm['identifier']
+               . ' - exp: ' . gaz_format_date($v_lm['expiry'])
+				. ' - disponibilità: ' . gaz_format_quantity($v_lm['rest'], 0, $admin_aziend['decimal_quantity'])
+                .'</td><td>';
 							
-							If (file_exists('../../data/files/' . $admin_aziend['company_id'])>0) {		
-								// recupero il filename dal filesystem e lo sposto sul tmp 
-								$dh = opendir('../../data/files/' . $admin_aziend['company_id']);
-								while (false !== ($filename = readdir($dh))) {
-									$fd = pathinfo($filename); 
-									$r = explode('_', $fd['filename']); 
-									if ($r[0] == 'lotmag' && $r[1] == $v_lm['id']) {
-									// riassegno il nome file 
-										$img = $fd['basename'];
-									} 
-								}
-							 
-								if (strlen($img)>0) {
-									echo '
-									<img src="../../data/files/'. $admin_aziend['company_id'] .'/'.$img.'" alt="certificato lotto" width="50" border="1" style="cursor: -moz-zoom-in;" onclick="this.width=500;" ondblclick="this.width=50;" />
-									';
-									echo '
-									<a class="btn btn-xs btn-default btn-elimina" href="../../data/files/'. $admin_aziend['company_id'] .'/'.$img.'" download>
-									<i class="glyphicon glyphicon-download"></i>
-									</a></td>
-								';
-								} else {
+				If (file_exists('../../data/files/' . $admin_aziend['company_id'])>0) {		
+					// recupero il filename 
+					$dh = opendir('../../data/files/' . $admin_aziend['company_id']);
+					while (false !== ($filename = readdir($dh))) {
+						$fd = pathinfo($filename); 
+						$r = explode('_', $fd['filename']); 
+						if ($r[0] == 'lotmag' && $r[1] == $v_lm['id']) {
+							// assegno il nome file a img
+							$img = $fd['basename'];
+							} 
+						}
+						if (strlen($img)>0) {
+							$tmp_file = "../../data/files/".$admin_aziend['company_id']."/".$img;
+							// sposto nella cartella di lettura il relativo file temporaneo            
+							copy($tmp_file, "../../modules/camp/tmp/".$img);
+							echo '<img src="../../modules/camp/tmp/'.$img.'" alt="certificato lotto" width="50" border="1" style="cursor: -moz-zoom-in;" onclick="this.width=500;" ondblclick="this.width=50;" />';
+							echo '<a class="btn btn-xs btn-default btn-elimina" href="../../modules/camp/tmp/'.$img.'" download><i class="glyphicon glyphicon-download"></i></a></td>';
+							} else {
 									echo '<i class="glyphicon glyphicon-eye-close"></i>';
-								}
-							}
-                    } 
-                } else {
-                    echo '<div><button class="btn btn-xs btn-danger" type="image" >Non sono disponibili altri lotti, <br> oppure non è possibile cambiare lotto negli inserimenti multipli</button></div>';
-                }
-?></table>
-</body>			
-
+								} 
+				}
+            } 
+        } else {
+				echo '<div><button class="btn btn-xs btn-danger" type="image" >Non sono disponibili altri lotti, <br> oppure non è possibile cambiare lotto negli inserimenti multipli</button></div>';
+            }
+?>
+	</table>
+	</body>	
+	<form method="post" name="closewindow">  
+	<input type="submit" title="elimina file temporanei e chiudi finestra" name="close" value="X"  style="float:right">
+	</form>	
