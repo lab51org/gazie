2de97ff4ffa36414380bd4152de112561ccc0505
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-11-05 08:23:20 +0000

    Produzioni: avviata l'implementazione della produzione con articolo composto + inserita gestione dei luoghi di produzione Ancora da terminare produzione articolo composto e da fare il debug della gestione luoghi di produzione
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2279 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/orderman/report_luoghi.php
@@ -0,0 +1,141 @@
+<?php
+/*
+ --------------------------------------------------------------------------
+                            GAzie - Gestione Azienda
+    Copyright (C) 2004-2017 - Antonio De Vincentiis Montesilvano (PE)
+         (http://www.devincentiis.it)
+           <http://gazie.sourceforge.net>
+ --------------------------------------------------------------------------
+    Questo programma e` free software;   e` lecito redistribuirlo  e/o
+    modificarlo secondo i  termini della Licenza Pubblica Generica GNU
+    come e` pubblicata dalla Free Software Foundation; o la versione 2
+    della licenza o (a propria scelta) una versione successiva.
+
+    Questo programma  e` distribuito nella speranza  che sia utile, ma
+    SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
+    NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
+    veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
+
+    Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
+    Generica GNU insieme a   questo programma; in caso  contrario,  si
+    scriva   alla   Free  Software Foundation, 51 Franklin Street,
+    Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
+ --------------------------------------------------------------------------
+*/
+// gestione campi o appezzamenti di terreno
+
+require("../../library/include/datlib.inc.php");
+
+
+
+$admin_aziend=checkAdmin();
+$titolo = 'Campi';
+require("../../library/include/header.php");
+$script_transl = HeadMain();
+
+if (isset($_GET['auxil'])) {
+   $auxil = $_GET['auxil'];
+}
+if (isset($_GET['all'])) {
+   $auxil = "&all=yes";
+   $where = "descri like '%'";
+   $passo = 100000;
+} else {
+   if (isset($_GET['auxil'])) {
+      $where = "descri like '".addslashes($_GET['auxil'])."%'";
+   }
+}
+
+if (!isset($_GET['auxil'])) {
+   $auxil = "";
+   $where = "descri like '".addslashes($auxil)."%'";
+}
+/** ENRICO FEDELE */
+/* pulizia del codice, eliminato boxover, aggiunte classi bootstrap alla tabella, convertite immagini in glyphicons */
+?>
+<div align="center" class="FacetFormHeaderFont">Luoghi di produzione aziendali</div>
+<?php
+$recordnav = new recordnav($gTables['campi'], $where, $limit, $passo);
+$recordnav -> output();
+?>
+<form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <table class="Tlarge table table-striped table-bordered table-condensed table-responsive">
+    	<thead>
+            <tr>
+                <td></td>
+                <td class="FacetFieldCaptionTD">Descrizione:
+                    <input type="text" name="auxil" value="<?php if ($auxil != "&all=yes") echo $auxil; ?>" maxlength="6" size="3" tabindex="1" class="FacetInput" />
+                </td>
+                <td>
+                    <input type="submit" name="search" value="Cerca" tabindex="1" onClick="javascript:document.report.all.value=1;" />
+                </td>
+                <td>
+                    <input type="submit" name="all" value="Mostra tutti" onClick="javascript:document.report.all.value=1;" />
+                </td>
+            </tr>
+            <tr>
+<?php
+	$result = gaz_dbi_dyn_query ('*', $gTables['campi'], $where, $orderby, $limit, $passo);
+	// creo l'array (header => campi) per l'ordinamento dei record
+	$headers_campi = array("Codice"      => "codice",
+							"Descrizione" => "descri",
+							"Immagine" => "image",
+							
+							"Note" => "annota",
+							
+							"Mappa di Google" => "web_url",
+							
+							"Cancella"    => ""
+							);
+	$linkHeaders = new linkHeaders($headers_campi);
+	$linkHeaders -> output();
+?>
+        	</tr>
+        </thead></form>
+        <tbody>
+<?php
+
+
+while ($a_row = gaz_dbi_fetch_array($result)) {
+?>		<tr class="FacetDataTD">
+			<td>
+				<a class="btn btn-xs btn-success btn-block" href="admin_luoghi.php?Update&codice=<?php echo $a_row["codice"]; ?>">
+					<i class="glyphicon glyphicon-edit"></i>&nbsp;<?php echo $a_row["codice"];?>
+				</a>
+			</td>
+			<td>
+				<span class="gazie-tooltip" data-type="campi-thumb" data-id="<?php echo $a_row['codice']; ?>" data-title="<?php echo $a_row['annota']; ?>"><?php echo $a_row["descri"]; ?></span>
+			</td>
+								
+			<td align="center"> <img width="100" style="cursor: -moz-zoom-in;" 
+			<?php echo 'src="data:image/jpeg;base64,'.base64_encode( $a_row['image'] ).'"';?>
+			onclick="this.width=500;" ondblclick="this.width=100;" title="<?php echo $a_row["descri"]; ?>" alt="IMG non presente" /></td>
+			
+			<td align="center"><?php echo $a_row["annota"];?></td>
+			
+			<td align="center"><a  href="javascript:;" onclick="window.open('<?php echo($a_row["web_url"])?>', 'titolo', 'width=800, height=400, left=80%, top=80%, resizable, status, scrollbars=1, location');">
+			<i class="glyphicon glyphicon-picture"></i>
+			</a></td>
+			
+			<td align="center">
+				<a class="btn btn-xs btn-default btn-elimina" href="delete_luogo.php?codice=<?php echo $a_row["codice"]; ?>">
+					<i class="glyphicon glyphicon-remove"></i>
+				</a>
+			</td>
+		</tr>
+<?php
+}
+?>
+<tr class=\"FacetFieldCaptionTD\">
+<form method="post" action="stampa_luoghi.php">
+         <td colspan="7" align="right"><input type="submit" name="print" value="<?php echo $script_transl['print'];?>">
+         
+         
+         </td>
+         </tr>
+
+    		</tbody>
+        </table></form>
+    <?php
+require("../../library/include/footer.php");
+?>
