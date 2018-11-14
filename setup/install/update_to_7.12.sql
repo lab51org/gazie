774c138eaeeb84d6d68b8653b8e5bc0b21714662
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-11-14 16:11:15 +0000

    Modulo acquisti: rivisto il menù del modulo acquisti per separare gli i preventivi/offerte dagli ordini ai fornitori
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2331 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.12.sql
+++ setup/install/update_to_7.12.sql
@@ -5,6 +5,12 @@ INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT MIN(id) FROM `gaz_menu_m
 INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='report_doctra.php'), 'report_doccmr.php', '', '', 51, '', 15  FROM `gaz_menu_script`;
 INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='report_doctra.php'), 'emissi_fatdif.php?tipodocumento=CMR', '', '', 53, '', 20  FROM `gaz_menu_script`;
 DELETE FROM `gaz_menu_module` WHERE  `link`='stampa_schcar.php';
+UPDATE `gaz_menu_module` SET `weight`=`weight`*3 WHERE  `id_module`= (SELECT MIN(id) FROM `gaz_module` WHERE `name`='acquis');
+UPDATE `gaz_menu_module` SET `link`='report_broacq.php?flt_tipo=APR' WHERE  `link`='report_broacq.php';;
+INSERT INTO `gaz_menu_module` SELECT  MAX(id)+1,(SELECT MIN(id) FROM `gaz_module` WHERE `name`='acquis'), 'report_broacq.php?flt_tipo=AOR','', '', 10, '', 4 FROM `gaz_menu_module`;
+UPDATE `gaz_menu_script` SET `id_menu`=(SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='report_broacq.php?flt_tipo=AOR') WHERE  `link`='admin_broacq.php?tipdoc=AOR';
+UPDATE `gaz_menu_script` SET `id_menu`=(SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='report_broacq.php?flt_tipo=AOR') WHERE  `link`='prop_ordine.php';
+UPDATE `gaz_menu_script` SET `weight`='10' WHERE  `link`='admin_docacq.php?Insert&tipdoc=AFC';
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXtesmov`	ADD COLUMN `notess` TEXT NULL DEFAULT '' COMMENT 'Note che NON vengono stampate sui registri contabili' AFTER `descri`;
 ALTER TABLE `gaz_XXXrigbro`	ADD COLUMN `id_orderman` INT(9) NULL COMMENT 'Per avere riferimenti uno a molti, e viceversa, con le produzioni (orderman)' AFTER `id_mag`;
