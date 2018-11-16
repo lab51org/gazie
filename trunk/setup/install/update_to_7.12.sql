4a13b2e6c11eb73e3ad263902be2eb81e05bfa22
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-11-16 14:28:37 +0000

    Modifiche alla struttura del database per poter implementare: - la cassa previdenziale (tipo rigo 4) <TipoCassa> sulle fatture elettroniche - l'invio dei preventivi-ordini a mail diverse da quelle registrate sulle anagrafiche e tenerne traccia

--- trunk/setup/install/update_to_7.12.sql
+++ trunk/setup/install/update_to_7.12.sql
@@ -11,9 +11,11 @@ INSERT INTO `gaz_menu_module` SELECT  MAX(id)+1,(SELECT MIN(id) FROM `gaz_module
 UPDATE `gaz_menu_script` SET `id_menu`=(SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='report_broacq.php?flt_tipo=AOR') WHERE  `link`='admin_broacq.php?tipdoc=AOR';
 UPDATE `gaz_menu_script` SET `id_menu`=(SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='report_broacq.php?flt_tipo=AOR') WHERE  `link`='prop_ordine.php';
 UPDATE `gaz_menu_script` SET `weight`='10' WHERE  `link`='admin_docacq.php?Insert&tipdoc=AFC';
+ALTER TABLE `gaz_aziend` ADD COLUMN `fae_tipo_cassa` VARCHAR(4) NOT NULL COMMENT 'eventualmente con uno dei valori dell\'elemento <TipoCassa> della fattura elettronica TC01,TC02,ecc' AFTER `amm_min`;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXtesmov`	ADD COLUMN `notess` TEXT NULL DEFAULT '' COMMENT 'Note che NON vengono stampate sui registri contabili' AFTER `descri`;
 ALTER TABLE `gaz_XXXrigbro`	ADD COLUMN `id_orderman` INT(9) NULL COMMENT 'Per avere riferimenti uno a molti, e viceversa, con le produzioni (orderman)' AFTER `id_mag`;
 ALTER TABLE `gaz_XXXrigbro`	ADD INDEX `id_orderman` (`id_orderman`);
 ALTER TABLE `gaz_XXXrigbro`	CHANGE COLUMN `status` `status` VARCHAR(100) NOT NULL DEFAULT '' AFTER `id_orderman`;
+ALTER TABLE `gaz_XXXtesbro`	ADD COLUMN `email` VARCHAR(50) NULL DEFAULT '' COMMENT 'Utilizzato per inviare i documenti ad un indirizzo diverso da quello in anagrafica' AFTER `template`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
