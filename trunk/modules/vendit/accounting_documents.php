67aa6dc8218f3a61569514e1cb67d0e330118181
danelsan <danelsan@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-12-10 13:57:32 +0000

    Corretto errore select rows generato con MariaDB maggiore di 10.2.4

--- trunk/modules/vendit/accounting_documents.php
+++ trunk/modules/vendit/accounting_documents.php
@@ -146,10 +146,10 @@ function getDocumentsAccounts($type = '___', $vat_section = 1, $date = false, $p
             }
         }
         //recupero i dati righi per creare il castelletto
-        $from = $gTables['rigdoc'] . ' AS rows
+        $from = $gTables['rigdoc'] . ' AS rs
                     LEFT JOIN ' . $gTables['aliiva'] . ' AS vat
-                    ON rows.codvat=vat.codice';
-        $rs_rig = gaz_dbi_dyn_query('rows.*,vat.tipiva AS tipiva', $from, "rows.id_tes = " . $tes['id_tes'], "id_tes DESC");
+                    ON rs.codvat=vat.codice';
+        $rs_rig = gaz_dbi_dyn_query('rs.*,vat.tipiva AS tipiva', $from, "rs.id_tes = " . $tes['id_tes'], "id_tes DESC");
         while ($r = gaz_dbi_fetch_array($rs_rig)) {
             if ($r['tiprig'] <= 1  || $r['tiprig'] == 4 || $r['tiprig'] == 90) { // se del tipo normale, forfait, cassa previdenziale, vendita cespite
                 //calcolo importo rigo
