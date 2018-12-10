70695790f3d6f2910288425cb2a0b43a4a0da9e4
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-12-10 06:48:28 +0000

    Sincronizzazione online shop: completata maschera iniziale per scelta fra download o upload.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2425 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/shop-synchronize/synchronize.php
+++ modules/shop-synchronize/synchronize.php
@@ -23,26 +23,65 @@
   --------------------------------------------------------------------------
 */
  
-/*
+/*    >>> LEGGIMI <<<
 ------------------------------------------------------------------------------------------
-	**Antonio Germani**
-	Questo file serve per caricare il file di interfaccia specifica per la sincronizzazione di GAzie con un negozio online
-	Ad esempio, è possibile eseguire un download degli ordini dal sito web oppure aggiornare le
-	quantità degli articoli e ogni altra cosa da Gazie al sito web e viceversa.
-	Sono previsti due file intefaccia uno da mettere nella cartella synchronize di GAzie e l'altro da mettere nella root del sito online
-	Ogni sito web ha bisogno di un codice specifico per interfacciarsi con GAzie.
+	** Antonio Germani - www.lacasettabio.it **
+	Questo codice serve per avviare l'interfaccia specifica per la sincronizzazione di GAzie con un negozio online.
+	Per ogni caso specifico, sono necessari due file intefaccia, uno da mettere nella cartella "shop-synchronize" di GAzie e l'altro da mettere nella root del negozio online.
+	I file interfaccia sono specifici per ciascun CMS e/o componente utilizzato dal negozio online.
+	La cartella synchronize di GAzie potrà contenere tutti i file interfaccia che a mano a mano verranno crati dagli sviluppatori.
+	L'utente deve scegliere quali interfacce utilizzare (sulla base delle caratteristiche del suo negozio online) e scrivere i relativi nomi dei file nella sottostante "Impostazione".
+	-Caso download: L'interfaccia presente nella root del negozio online elabora i dati del database del negozio e crea un file xml. In GAzie, la seconda interfaccia elabora il file xml scrivendone i dati sul database di GAzie.
+	-Caso Upload: .... da realizzare!
 ------------------------------------------------------------------------------------------
 */
+
+// IMPOSTAZIONE NECESSARIA: Impostare qui i nomi dei file interfaccia da utilizzare presenti nella cartella "shop_synchronize" di GAzie 
+
+$file_download = "ordini_joomla_hikashop.php";
+$file_upload = "prodotti_joomla_hikashop.php";
+
+// fine impostazione
+
+
 require("../../library/include/datlib.inc.php");
 require("../../library/include/header.php");
 
-
-// inserire qui sotto il nome del file interfaccia presente nella cartella synchronize di GAzie
-include "ordini_joomla_hikashop.php";
-
-
-
-echo "<br> pippo";die;
-header('Location: ' . $_SERVER['HTTP_REFERER']);
-exit;
+if (!isset($_POST['ritorno'])) {
+    $_POST['ritorno'] = $_SERVER['HTTP_REFERER'];
+}
+if (isset($_POST['Return'])) { 
+        header("Location: " . $_POST['ritorno']);
+        exit;
+    }
+if (isset ($_POST['download'])) {
+	if (file_exists($file_download)) {
+		include $file_download;
+	} else {
+		header("Location: " . $_POST['ritorno']);
+        exit;
+	}
+} elseif (isset ($_POST['upload'])) {
+	
+	if (file_exists($file_upload)){
+		include $file_upload;
+	} else {
+		header("Location: " . $_POST['ritorno']);
+        exit;
+		}
+} else {
+	?>
+	<form method="POST" name="chouse" enctype="multipart/form-data">
+	<input type="hidden" name="ritorno" value="<?php echo $_POST['ritorno'];?>" >	
+	<table class="table table-striped" style="margin: 0 auto; max-width: 40%; margin-top:100px;">
+	<tr><td>
+	<input type="submit" name="download"  onClick="chkSubmit();" value="Scarica ordini">
+	</td><td style="text-align: right;" >
+	<input type="submit" name="upload"  onClick="chkSubmit();" value="Aggiorna prodotti">
+	</td></tr>
+	</table>
+	</div>
+	</form>
+	<?php
+}
 ?>
