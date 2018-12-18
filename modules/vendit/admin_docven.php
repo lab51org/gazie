bda5d5b439b17f47bf8d780df471cb4c4acbcb31
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-12-18 16:46:04 +0000

    Admin_docven: completato inserimento veloce articoli con pistola lettrice di codici a barre. Si ricorda che la pistola deve essere programmata per inviare un CR13 dopo la lettura.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2455 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/vendit/admin_docven.php
+++ modules/vendit/admin_docven.php
@@ -1553,6 +1553,8 @@ if ((isset($_POST['Insert'])) or ( isset($_POST['Update']))) {   //se non e' il 
         $form['mintra'] = date("i");
     }
 } elseif (!isset($_POST['Insert'])) { //se e' il primo accesso per INSERT
+	$form['in_barcode']="";
+	$form['ok_barcode']="";
     $form['tipdoc'] = '';
     $form['id_doc_ritorno'] = 0;
     if (isset($_GET['tipdoc'])) {
@@ -1987,12 +1989,14 @@ $select_artico->output(substr($form['cosear'], 0, 20));
 echo '&nbsp;<a href="#" id="addmodal" href="#myModal" data-toggle="modal" data-target="#edit-modal" class="btn btn-xs btn-default"><i class="glyphicon glyphicon-export"></i> ' . $script_transl['add_article'] . '</a>';
 
 // Antonio Germani - input ricerca con pistola lettore codice a barre 
-				
+					?>
+					</td><td class="FacetColumnTD" style="width:10%;">
+					<?php
 					if ($form['ok_barcode']!="ok"){
 						?>
-						<div class="col-sm-6 col-md-1 col-lg-1">
+						<div class="col-sm-12 col-md-1 col-lg-1">
 							<div class="form-group text-center">
-								<button type="submit"  class="btn btn-default btn-sm col-sm-6" name="button_ok_barcode" title="inserisci con pistola Barcode"> 
+								<button type="submit"  name="button_ok_barcode" title="inserisci con pistola Barcode"> 
                                 <span class="glyphicon glyphicon-barcode"></span>
 								</button>
 							</div>
@@ -2005,33 +2009,32 @@ echo '&nbsp;<a href="#" id="addmodal" href="#myModal" data-toggle="modal" data-t
 					if ($form['ok_barcode']=="ok"){
 						if ($form['in_barcode']==""){
 						?>
-						<div class="col-sm-4 col-md-2 col-lg-2">
-							<div class="form-group">
-								<label for="item" class="col-sm-3 control-label"><?php echo "Barcode"; ?></label>
-								<input  class="col-sm-8" type="text" value="<?php echo $form['in_barcode']; ?>" name="in_barcode" onkeypress="submitOnEnter(this, event);" />
-								<button type="submit"  class="btn btn-default btn-sm col-sm-1" name="no_barcode" title="Togli con pistola Barcode"> 
+						
+								<label for="item" ><?php echo "Barcode"; ?></label>
+								<input  type="text" value="<?php echo $form['in_barcode']; ?>" name="in_barcode" onkeypress="submitOnEnter(this, event);" />
+								<button type="submit"  name="no_barcode" title="Togli con pistola Barcode"> 
                                 <span class="glyphicon glyphicon-remove"></span>
-							</div>
-						</div>
+							
+						
 						<?php
 						} elseif ($form['in_barcode']=="NOT FOUND") {
 							$form['in_barcode']="";
 							?>
-							<div class="col-sm-4 col-md-2 col-lg-2">
-								<div class="form-group">
-									<label for="item" class="col-sm-3 control-label"><?php echo "Barcode"; ?></label>
-									<input style="border: 1px solid red;" class="col-sm-8" type="text" value="<?php echo $form['in_barcode']; ?>" name="in_barcode" onkeypress="submitOnEnter(this, event);" />
-									<button type="submit"  class="btn btn-default btn-sm col-sm-1" name="no_barcode" title="Togli con pistola Barcode"> 
+							
+									<label for="item" ><?php echo "Barcode"; ?></label>
+									<input style="border: 1px solid red;"  type="text" value="<?php echo $form['in_barcode']; ?>" name="in_barcode" onkeypress="submitOnEnter(this, event);" />
+									<button type="submit"  name="no_barcode" title="Togli con pistola Barcode"> 
 									<span class="glyphicon glyphicon-remove"></span>
-								</div>
-							</div>
+								
 							<?php
 						}
 					}
+					?>
 					
+					<?php
 					// Antonio Germani - fine ricerca con pistola lettore codice a barre -->
 
-echo "</td><td class=\"FacetColumnTD\">$script_transl[16]: <input type=\"text\" value=\"" . $form['in_quanti'] . "\" maxlength=\"11\" size=\"7\" name=\"in_quanti\" tabindex=\"5\" accesskey=\"q\">\n";
+echo "</td><td class=\"FacetColumnTD\">$script_transl[16]: <input type=\"text\" value=\"" . $form['in_quanti'] . "\" maxlength=\11\" size=\"7\" name=\"in_quanti\" tabindex=\"1\" accesskey=\"q\">\n";
 echo '  </td>
 		<td class="FacetColumnTD" align="right">
 			<button type="submit" class="btn btn-default btn-sm" name="in_submit" title="' . $script_transl['submit'] . $script_transl['thisrow'] . '" tabindex="6"><i class="glyphicon glyphicon-ok"></i></button>
@@ -2803,6 +2806,15 @@ echo '</table>';
 		}
 	}
 </script>
+<?php
+if ($form['ok_barcode']=="ok"){
+	?>
+	<script type="text/javascript">
+	if (this.document.docven.in_barcode.value == '') this.document.docven.in_barcode.focus();
+	</script>
+	<?php
+}
+?>
 <script language="JavaScript">
 var last_focus_value;
 var last_focus;
