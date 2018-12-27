144208c7762b811b7ae81a15dbddf7e9118c6986
angelo983 <angelo983@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-12-27 10:41:45 +0000

    Libreria con la funzione per l'invio di fatture elettroniche tramite il servizio FatturaCAT

---
+++ trunk/library/catsrl/InviaXml.php
@@ -0,0 +1,36 @@
+<?php
+function SendFatturaElettronica_CATsrl($fatturaxml)
+{
+	$CA_FILE = 'CA_Agenzia_delle_Entrate.pem';
+	$CATSRL_ENDPOINT = 'https://fatture.catsrl.it/gazie/RiceviXml.php';
+
+	// initialise the curl request
+	$request = curl_init();
+
+	// send a file
+	curl_setopt($request, CURLOPT_CAINFO, dirname(__FILE__).'/'.$CA_FILE);
+	curl_setopt($request, CURLOPT_POST, true);
+	curl_setopt(
+		$request,
+		CURLOPT_POSTFIELDS,
+		array(
+		  'file_contents' => curl_file_create(realpath('./'.$fatturaxml))
+		)
+	);
+	/**/curl_setopt($request, CURLOPT_SSL_VERIFYHOST, false); // DISATTIVARE AL PROSSIMO RILASCIO CERTIFICATO
+	curl_setopt($request, CURLOPT_URL, $CATSRL_ENDPOINT);
+
+	// output the response
+	curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($request);
+	echo($result);
+	echo(curl_error($request));
+
+	// close the session
+	curl_close($request);
+}
+
+if (!empty($_REQUEST['fatturaxml'])) {
+	SendFatturaElettronica_CATsrl($_REQUEST['fatturaxml']);
+}
+?>
