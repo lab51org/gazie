d7007463620f763c7a31ba87831b258c24ba9c88
andreabia <andreabia@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2018-12-29 13:57:23 +0000

    aggiunta la stampa del totale nel ddt e rigo (tipo conai) in fondo al documento. aggiungo i file di configurazione per l'utilizzo di composer per l'aggiornamento e il mantenimento delle librerie, potrebbe essere utile per avere in fase di sviluppo un sorgente più snello e avere sempre aggiornate le librerie aggiuntive. https://getcomposer.org/
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2519 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.14.sql
+++ setup/install/update_to_7.14.sql
@@ -1,4 +1,5 @@
 UPDATE `gaz_config` SET `cvalue` = '114' WHERE `id` =2;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 UPDATE `gaz_XXXtesdoc` SET `fattura_elettronica_zip_package`='FAE_ZIP_NOGENERATED' WHERE YEAR(`datemi`)< 2018 AND `tipdoc` LIKE 'VCO' AND `numfat` > 0;
+INSERT INTO `gaz_XXXcompany_config` (`description`,`var`,`val`) VALUES ('Eventuale ultimo rigo descrittivo su ddt (multiriga)','descriptive_last_ddt','');
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
