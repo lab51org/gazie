15db0ba9dd021975572cd76b1e7681719d534db7
andreabia <andreabia@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-01-09 22:33:37 +0000

    la numerazione dei ddt non era progressiva se c'erano dei cmr evasi

--- trunk/modules/vendit/admin_docven.php
+++ trunk/modules/vendit/admin_docven.php
@@ -697,12 +697,12 @@ if ((isset($_POST['Insert'])) or ( isset($_POST['Update']))) {   //se non e' il 
                     case "DDT":
                     case "DDV": // conto visione
                     case "DDY": // triangolazione
-                        $sql_documento = "YEAR(datemi) = " . $form['annemi'] . " AND( tipdoc like 'DD_' or tipdoc = 'FAD') and seziva = $sezione";
+                        $sql_documento = "YEAR(datemi) = " . $form['annemi'] . " AND ( tipdoc like 'DD_' or (tipdoc = 'FAD' && ddt_type = 'T')) and seziva = $sezione";
                         $where = "numdoc DESC";
                         $sql_protocollo = " 0";
                         break;
                     case "CMR":
-                        $sql_documento = "YEAR(datemi) = " . $form['annemi'] . " AND( tipdoc like 'CMR' or tipdoc = 'FAC') and seziva = $sezione";
+                        $sql_documento = "YEAR(datemi) = " . $form['annemi'] . " AND ( tipdoc like 'CMR' or (tipdoc = 'FAD' && ddt_type = 'R')) and seziva = $sezione";
                         $where = "numdoc DESC";
                         $sql_protocollo = " 0";
                         break;
