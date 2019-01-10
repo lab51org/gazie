876a1dd92dc500dcad1c3c7d48874e48c926ebb2
angelo983 <angelo983@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-01-10 11:25:17 +0000

    OPS! Versione 7.17

--- trunk/config/config/gconfig.php
+++ trunk/config/config/gconfig.php
@@ -45,7 +45,7 @@ DELLA CARTELLA "gazie" COMPRESO IL PRESENTE "gconfig.php"
     exit('Accesso diretto non consentito');
 }
 //versione software
-define('GAZIE_VERSION', '7.16');
+define('GAZIE_VERSION', '7.17');
 
 if (file_exists(dirname(__FILE__) . '/gconfig.myconf.php')) {
 	include_once(dirname(__FILE__) . '/gconfig.myconf.php');
