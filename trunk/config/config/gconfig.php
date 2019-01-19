9b9686bda17a8884b159f7531c540d55fefad5fe
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-01-19 10:58:22 +0000

    Quando utilizzo il tracciamento dell'IP dianamico: in caso di mancata connessione a internet limito il tempo di timeout del socket a 3 secondi, altrimenti si deve aspettare troppo per loggarsi dalla LAN.

--- trunk/config/config/gconfig.php
+++ trunk/config/config/gconfig.php
@@ -45,7 +45,7 @@ DELLA CARTELLA "gazie" COMPRESO IL PRESENTE "gconfig.php"
     exit('Accesso diretto non consentito');
 }
 //versione software
-define('GAZIE_VERSION', '7.18');
+define('GAZIE_VERSION', '7.19');
 
 if (file_exists(dirname(__FILE__) . '/gconfig.myconf.php')) {
 	include_once(dirname(__FILE__) . '/gconfig.myconf.php');
