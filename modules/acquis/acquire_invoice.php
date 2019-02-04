908cca9bc38ae75b6148c93fdbda05c892b13fb2
angelo983 <angelo983@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-02-04 11:27:05 +0000

    Trovato altro carattere EOT da togliere nei file firmati
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2806 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/acquis/acquire_invoice.php
+++ modules/acquis/acquire_invoice.php
@@ -62,6 +62,7 @@ function removeSignature($string, $filename) {
 	// elimino le sequenze di caratteri aggiunti dalla firma (ancora da testare approfonditamente)
 	$string = preg_replace ('/[\x{0004}]{1}[\x{0082}]{1}[\x{0001}\x{0002}\x{0003}\x{0004}]{1}[\s\S]{1}/i', '', $string);
 	$string = preg_replace ('/[\x{0004}]{1}[\x{0081}]{1}[\s\S]{1}/i', '', $string);
+	$string = preg_replace ('/[\x{0004}]{1}[\s\S]{1}/i', '', $string);
 	$string = preg_replace ('/[\x{0004}]{1}[A-Za-z]{1}/i', '', $string); // per eliminare tag finale   
 	return $string;
 }
