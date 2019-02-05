e596beabe635db3e1e6e9f0c0bdc3376798cb6f1
danelsan <danelsan@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-02-05 14:21:19 +0000

    Configurazione GAzie richiamata dalla classe GAzie\Config

--- trunk/library/include/classes/GAzie/Config.php
+++ trunk/library/include/classes/GAzie/Config.php
@@ -36,25 +36,14 @@ class Config {
 	public function __construct() {
                 global $gTables;
 		$this->data = [];
-		if( defined('PATH_ROOT') )
-			$this->set('path_root', PATH_ROOT);
-		$this->set('directories', [
-			$this->getPathRoot()."/admin.php", 
-			$this->getPathRoot()."/composer.json", 
-			$this->getPathRoot()."/composer.lock.php", 
-			$this->getPathRoot()."/config.php", 
-			$this->getPathRoot()."/doc", 
-			$this->getPathRoot()."/.htaccess", 
-			$this->getPathRoot()."/INDEX.html", 
-			$this->getPathRoot()."/index.php", 
-			$this->getPathRoot()."/js", 
-			$this->getPathRoot()."/language", 
-			$this->getPathRoot()."/library", 
-			$this->getPathRoot()."/modules", 
-			$this->getPathRoot()."/README.html", 
-			$this->getPathRoot()."/setup", 
-			$this->getPathRoot()."/SETUP.html", 
-		]);
+		if (! defined('PATH_ROOT') )
+			die ('Error setting path root');
+	       
+		$this->set('path_root', PATH_ROOT);
+		$this->setDirectories( PATH_ROOT );
+		$this->setConfigFile("../../config/config/gconfig.php");
+		
+		$tmp = json_decode($json_data,TRUE);
                 $data = gaz_dbi_get_row($gTables['company_config'], 'var', 'syncronize_oc');
                 if ( ! $data ) {
                         $json_data = json_encode(array(
@@ -66,11 +55,10 @@ class Config {
                 } else {
                         $json_data = $data['val'];
                 }
-                $tmp = json_decode($json_data,TRUE);
+		$tmp = json_decode($json_data,TRUE);
                 $this->data['username'] = $tmp['user'];
                 $this->data['password'] = $tmp['pass'];
-                $this->data['url'] = $tmp['url'];
-
+		$this->data['url'] = $tmp['url'];
 	}
 
 	public function set($key,$value) {  
@@ -80,6 +68,105 @@ class Config {
 	public function get($key) {
 		return $this->data[$key];
 	}
+
+	private function setDirectories($path_root)  {
+		$this->set('directories', [
+                        $path_root."/admin.php",
+                        $path_root."/composer.json",
+                        $path_root."/composer.lock.php",
+                        $path_root."/config.php",
+                        $path_root."/doc",
+                        $path_root."/.htaccess",
+                        $path_root."/INDEX.html",
+                        $path_root."/index.php",
+                        $path_root."/js",
+                        $path_root."/language",
+                        $path_root."/library",
+                        $path_root."/modules",
+                        $path_root."/README.html",
+                        $path_root."/setup",
+                        $path_root."/SETUP.html",
+                ]);
+
+	}
+
+	public function getMonth() {
+		return [ "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio","Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+	}
+	
+	/**
+	 * 
+	 */
+	private function setConfigFile($file) {
+		if ( ! is_file($file ) )
+			die ('Configuration file '.$file.' not found');
+		include_once($file);
+		if ( isset($_SESSION['table_prefix'] ) ) {
+			$table_prefix = substr($_SESSION['table_prefix'], 0, 12);
+		} else {
+    			$table_prefix = filter_var(substr($table_prefix, 0, 12), FILTER_SANITIZE_MAGIC_QUOTES);
+		}
+		if ( ! defined('GAZIE_VERSION') ) 
+			die('Version of GAzie not defined. Control configuration file!');
+		$this->set('GAZIE_VERSION',defined('GAZIE_VERSION') ? GAZIE_VERSION: '');
+		$this->set('database', [
+			'host'		=>	Host,
+			'dbname'	=>	Database,
+			'user'		=>	User,
+			'password'	=>	Password,
+			'port'		=>	Port,
+			'table_prefix'	=>	defined('table_prefix') ? table_prefix : '',
+		]);
+		$this->set('default_user', defined('default_user') ? default_user : '');
+		$this->set('timezone', defined('Timezone') ? Timezone : '');
+		$this->set('email_footer', defined('EMAIL_FOOTER') ? EMAIL_FOOTER : '');
+		$this->set('disable_set_time_limit', defined('disable_set_time_limit') ? disable_set_time_limit : false);
+		$this->set('gazie_locale', defined('gazie_locale') ? gazie_locale : '');
+		$this->set('PER_PAGE', defined('PER_PAGE') ? PER_PAGE : 30);
+		$this->set('DATA_DIR', defined('DATA_DIR') ? DATA_DIR : PATH_ROOT . '/data/');
+		$this->set('K_PATH_CACHE', defined('K_PATCH_CACHE') ? K_PATH_CACHE : PATH_ROOT . '/library/tcpdf/cache/');
+		$this->set('_SESSION_NAME', defined('_SESSION_NAME') ? _SESSION_NAME : 'gazie');
+		$this->set('SET_DYNAMIC_IP', defined('SET_DYNAMIC_IP') ? SET_DYNAMIC_IP : '');
+		$this->set('update_URI_files', defined('update_URI_files') ? update_URI_files : 'https://sourceforge.net/projects/gazie');
+		$this->set('debug_active', defined('debug_active') ? debug_active : FALSE);
+		$this->set('modifica_fatture_ddt', defined('modifica_fatture_ddt') ? modifica_fatture_ddt : FALSE);
+
+
+	}
+
+
+	public function getTabelleComuni() {
+		return [
+			'admin', 
+			'admin_config', 
+			'admin_module', 
+			'anagra', 
+			'aziend', 
+			'classroom', 
+			'config',
+			'country', 
+			'currencies', 
+			'currency_history', 
+			'destina', 
+			'camp_avversita', 
+			'camp_colture',
+			'camp_fitofarmaci', 
+			'camp_uso_fitofarmaci',
+			'languages', 
+			'menu_module', 
+			'menu_script', 
+			'menu_usage',
+			'module', 
+			'municipalities', 
+			'provinces', 
+			'regions', 
+			'staff_absence_type', 
+			'staff_work_type', 
+			'students',
+			'breadcrumb'
+		];
+	}
+
 
 	public function getPathRoot() {
 		return $this->data['path_root'];
