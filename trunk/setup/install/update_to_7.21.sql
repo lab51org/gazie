95445eedd5ebb1b8979aa764e795b55459decd5a
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-02-06 18:36:51 +0000

    pro rata spostato su script

--- trunk/setup/install/update_to_7.21.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `gaz_config` SET `cvalue` = '118' WHERE `id` =2;
--- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
-INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`, `ref`) VALUES ('Percentuale di detrazione sugli acquisti (PRO RATA)', 'pro_rata', '0', '');
--- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
