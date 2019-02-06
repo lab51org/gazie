f769e45cc10bac201f57a2288a3802aa1c8e5edc
dcrescenti <dcrescenti@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-02-06 22:05:11 +0000

    Risolti bug su r2818 - Personalizzazione finestra a run-time. Prevenuto accesso diretto a script e controllato file IER inesistente.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2825 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- library/IER/IERincludeExcludeRows.php
+++ library/IER/IERincludeExcludeRows.php
@@ -1,6 +1,22 @@
  <?php
 //Credit: m_zolfo
 
+/*if (isset($_SERVER['SCRIPT_FILENAME']) && (str_replace('\\', '/', __FILE__) == $_SERVER['SCRIPT_FILENAME'])) {
+    exit('Accesso diretto non consentito');
+}*/
+
+// prevent direct access
+
+$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND
+        strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+if (!$isAjax) {
+    $user_error = 'Access denied - not an AJAX request...';
+    trigger_error($user_error, E_USER_ERROR);
+}
+
+if($_POST["fn"] == "read" && !file_exists($_POST["filename"]))
+  return;
+
 if($_POST["fn"] == "read" && $_POST["filename"] !="")
 {
 	$myfile = fopen($_POST["filename"], "r") or die(""); //echo "";return;
