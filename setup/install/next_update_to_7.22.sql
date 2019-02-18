bff538ec9cc739255166ac112239142ba506e8c8
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-02-18 20:58:46 +0000

    Preparazione database per accogliere la data di riferimento per la liquidazione IVA e i nuovi templates 2xA5 affiancati di ricevute fiscali e DdT
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2910 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/next_update_to_7.22.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `gaz_config` SET `cvalue` = '118' WHERE `id` =2;
--- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
-INSERT INTO `gaz_NNNcompany_config` (`description`, `var`, `val`) VALUES ('Stampa Documenti di Trasporto in modalità 2xA5 (affiancati)', 'ddt_A5', '0');
--- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
