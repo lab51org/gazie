6ad05873d408eb3875092e7cf28a9d5ff3ded65d
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-02-26 14:09:26 +0000

    Inizio gestione versamenti IVA ad erario

--- trunk/setup/install/update_to_7.22.sql
+++ trunk/setup/install/update_to_7.22.sql
@@ -5,4 +5,7 @@ INSERT INTO `gaz_XXXcompany_config` (`description`, `var`, `val`) VALUES ('Stamp
 INSERT INTO `gaz_XXXcompany_config` (`description`, `var`, `val`) VALUES ('Stampa Ricevute su moduli prenumerati es.\'buffetti\' per Buffetti art.8205L2000', 'received_template', '');
 ALTER TABLE `gaz_XXXtesmov` ADD COLUMN `datliq` DATE DEFAULT '2004-01-27' COMMENT 'Data di riferimento del periodo della liquidazione periodica dell\'IVA, non può precedere la data del documento (datdoc)' AFTER `datreg`;
 UPDATE `gaz_XXXtesmov` SET `datliq` = `datreg` WHERE 1;
+INSERT INTO `gaz_XXXcaucon` (`codice`,`descri`,`insdoc`,`regiva`,`operat`,`pay_schedule`) VALUES ('VEI','VERSAMENTO IVA AD ERARIO',1,9,0,0);
+INSERT INTO `gaz_XXXcaucon_rows` (`caucon_cod`, `clfoco_ref`, `type_imp`, `dare_avere`, `n_order`) VALUES ('VEI',597000000,'','A',1),
+('VEI',215000005,'','D',2);
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
