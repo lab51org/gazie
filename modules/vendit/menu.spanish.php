f9e6311de3676aab9a4f46c2a57385bea26c0d97
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-03-04 15:04:14 +0000

    Modificata lista clienti, aggiunta possibilità di ordinarla per regioni-province
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@2989 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/vendit/menu.spanish.php
+++ modules/vendit/menu.spanish.php
@@ -94,7 +94,9 @@ $transl['vendit'] = array('name' => "Ventas",
         50 => array("Emetti CMR", "Emetti CMR"),
         51 => array("Lista CMR", "Lista CMR"),
         52 => array("Impacchetta fatture elettroniche", "Impacchetta fatture elettroniche"),
-        53 => array("Genera fatture differite da CMR", "Fatturazione CMR")
+        53 => array("Genera fatture differite da CMR", "Fatturazione CMR"),
+        54 => array("Importa XML fattura di vendita", "Importa XML fattura di vendita"),
+        55 => array("Lista Clienti per zone", "Lista Clienti per zone")
     )
 );
 ?>
--- modules/vendit/print_anagrafe.php
+++ modules/vendit/print_anagrafe.php
@@ -26,54 +26,59 @@
 require("../../library/include/datlib.inc.php");
 
 $admin_aziend = checkAdmin();
-if (!ini_get('safe_mode')) { //se me lo posso permettere...
-   ini_set('memory_limit', '128M');
-   gaz_set_time_limit(0);
-}
-//if (!isset($_GET['id_agente'])) {
-//   header("Location: " . $_SERVER['HTTP_REFERER']);
-//   exit;
-//}
-require("../../config/templates/report_template.php");
-$form['id_agente'] = (isset($_GET['id_agente']) ? $_GET['id_agente'] : '');
-$form['clifor'] = (isset($_GET['clifor']) ? $_GET['clifor'] : '');
+
+
+$luogo_data = $admin_aziend['citspe'] . ", lì " . ucwords(strftime("%d %B %Y", mktime(0, 0, 0, date("m"), date("d"), date("Y"))));
+
+$form['id_agente'] = (isset($_GET['id_agente']) ? intval($_GET['id_agente']) : '');
+$form['clifor'] = (isset($_GET['clifor']) ? substr($_GET['clifor'],-1) : '');
+$orderby="ragioneSociale";
 if (empty($form['id_agente']) && empty($form['clifor'])) { // mancano i dati per la selezione
    alert("Niente da stampare");
    tornaPaginaPrecedente();
 } elseif (!empty($form['id_agente'])) {  // vogliamo la stampa dei clienti di un agente
    $where = "id_agente=" . $form['id_agente'] . " and clfoco.codice like '" . $admin_aziend['mascli'] . "%'";
    $titolo = "CLIENTI DELL'AGENTE: " . queryNomeAgente($form['id_agente'], $gTables);
+} elseif (isset($_GET['order'])&&$_GET['order']=='ZONE') {  // vogliamo la stampa dei clienti in ordine di zona
+   $mastro = ($form['clifor'] == 'C' ? $admin_aziend['mascli'] : $admin_aziend['masfor']);
+   $where = "clfoco.codice LIKE '$mastro%'";
+   $titolo = "CLIENTI IN ORDINE DI ZONA ";
+   $orderby="regions.id, provinces.id";
 } else {   // vogliamo la stampa dell'anagrafica
    $mastro = ($form['clifor'] == 'C' ? $admin_aziend['mascli'] : $admin_aziend['masfor']);
    $where = "clfoco.codice like '$mastro%'";
    $titolo = ($form['clifor'] == 'C' ? 'Elenco Clienti' : 'Elenco Fornitori');
 }
-$luogo_data = $admin_aziend['citspe'] . ", lì " . ucwords(strftime("%d %B %Y", mktime(0, 0, 0, date("m"), date("d"), date("Y"))));
 
-$title = array('luogo_data' => $luogo_data,
-    'title' => $titolo,
-    'hile' => array(/* array('lun' => 45, 'nam' => 'Cliente'), */
-        array('lun' => 60, 'nam' => 'Ragione Sociale'),
-        array('lun' => 60, 'nam' => 'Sede Legale'),
-        array('lun' => 40, 'nam' => 'Telefono'),
-        array('lun' => 40, 'nam' => 'Email'),
-    )
-);
-$pdf = new Report_template();
-$pdf->setVars($admin_aziend, $title);
-$pdf->setFooterMargin(22);
-$pdf->setTopMargin(43);
-$pdf->SetLeftMargin(5);
-$pdf->SetFillColor(160, 255, 220);
-$pdf->AddPage();
-//$config = new Config;
-$rs = gaz_dbi_dyn_query("concat(ragso1,space(1),ragso2) as ragioneSociale, concat (indspe, space(1), citspe, ' (',prospe,')') as sede, concat(telefo, space(1), cell, space(1), fax) as telefono, e_mail", $gTables['clfoco'] . " clfoco LEFT JOIN " . $gTables['anagra'] . " anagra ON anagra.id = clfoco.id_anagra ", $where, "ragioneSociale");
+require("../../config/templates/report_template.php");
+$title = array('luogo_data'=>$luogo_data,
+               'title'=>$titolo,
+               'hile'=>array(        
+					array('lun' => 70, 'nam' => 'Ragione Sociale'),
+					array('lun' => 20, 'nam' => 'Partitia IVA'),
+					array('lun' => 90, 'nam' => 'Indirizzo'),
+					array('lun' => 50, 'nam' => 'Telefono'),
+					array('lun' => 40, 'nam' => 'Email'),
+				)
+              );
+$pdf = new Report_template('L','mm','A4',true,'UTF-8',false,true);
+$pdf->setVars($admin_aziend,$title);
+$pdf->SetTopMargin(39);
+$pdf->SetFooterMargin(20);
+$config = new Config;
+$pdf->AddPage('L',$config->getValue('page_format'));
+$pdf->SetFont('helvetica','',9);
+
+$rs = gaz_dbi_dyn_query("concat(ragso1,space(1),ragso2) as ragioneSociale, concat (indspe, space(1), citspe, ' (',prospe,')') as sede, concat(telefo, space(1), cell, space(1), fax) as telefono, e_mail, pariva", $gTables['clfoco'] . " clfoco 
+LEFT JOIN " . $gTables['anagra'] . " anagra ON anagra.id = clfoco.id_anagra 
+LEFT JOIN " . $gTables['provinces'] . " provinces ON anagra.prospe = provinces.abbreviation 
+LEFT JOIN " . $gTables['regions'] . " regions ON provinces.id_region = regions.id", $where, $orderby);
 
 while ($cliente = gaz_dbi_fetch_array($rs)) {
-   $pdf->SetFont('helvetica', '', 10);
-   $pdf->Cell(60, 0, $cliente['ragioneSociale'], 1, 0, 'L', false, '', 1);
-   $pdf->Cell(60, 0, $cliente['sede'], 1, 0, 'L', false, '', 1);
-   $pdf->Cell(40, 0, $cliente["telefono"], 1, 0, 'L', false, '', 1);
+   $pdf->Cell(70, 0, $cliente['ragioneSociale'], 1, 0, 'L', false, '', 1);
+   $pdf->Cell(20, 0, $cliente['pariva'], 1, 0, 'L', false, '', 1);
+   $pdf->Cell(90, 0, $cliente['sede'], 1, 0, 'L', false, '', 1);
+   $pdf->Cell(50, 0, $cliente["telefono"], 1, 0, 'L', false, '', 1);
    $pdf->Cell(40, 0, $cliente["e_mail"], 1, 1, 'L', false, '', 1);
 }
 $pdf->Output();
