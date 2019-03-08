15804481a532d80c78bd84c84e86f843743ee1c3
danelsan <danelsan@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-03-08 10:49:33 +0000

    Abilitato error_reporting(E_ALL) se è attivo il debug_active

--- trunk/library/include/datlib.inc.php
+++ trunk/library/include/datlib.inc.php
@@ -26,7 +26,10 @@
 require("../../config/config/gconfig.php");
 require('../../library/include/' . $NomeDB . '.lib.php');
 require("../../library/include/function.inc.php");
-if ( $debug_active ) require ("../../library/kint/build/kint.php");
+if ( $debug_active ) {
+	error_reporting(E_ALL);
+	require ("../../library/kint/build/kint.php");
+}
 
 if (isset($_SESSION['table_prefix'])) {
     $table_prefix = substr($_SESSION['table_prefix'], 0, 12);
