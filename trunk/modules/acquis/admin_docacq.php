392526da3d9e06a78153bc569dc6f34ce91d7e2b
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-03-08 11:52:55 +0000

    admin_docacq.php: corretto il controllo dei movimenti con numero uguale. Adesso, se il documento è di tipo R__ non controlla più il numfat, bensì il numdoc.

--- trunk/modules/acquis/admin_docacq.php
+++ trunk/modules/acquis/admin_docacq.php
@@ -371,14 +371,25 @@ if ((isset($_POST['Insert'])) or ( isset($_POST['Update']))) {   //se non e' il 
 					}
                 }
 				if (!empty($form["clfoco"])) {
-					if (!preg_match("/^id_([0-9]+)$/", $form['clfoco'], $match)) {
-						//controllo se ci sono altri documenti con lo stesso numero fornitore
-						$rs_stesso_numero = gaz_dbi_dyn_query("*", $gTables['tesdoc'], "YEAR(datemi) = " .substr($datemi,0,4) . " and tipdoc like '" . substr($form['tipdoc'], 0, 1) . "%' and clfoco = " . $form['clfoco'] . " and numfat = '" . $form['numfat'] . "'", "protoc desc, datfat desc, datemi desc", 0, 1);
-						$stesso_numero = gaz_dbi_fetch_array($rs_stesso_numero);
-						if ($stesso_numero) {
-							$msg['err'][] = "samedoc";
+					if (substr($form['tipdoc'], 0, 1)=="R"){
+						if (!preg_match("/^id_([0-9]+)$/", $form['clfoco'], $match)) {
+							//controllo se ci sono altri documenti "R__" con lo stesso numero fornitore
+							$rs_stesso_numero = gaz_dbi_dyn_query("*", $gTables['tesdoc'], "YEAR(datemi) = " .substr($datemi,0,4) . " and tipdoc like '" . substr($form['tipdoc'], 0, 1) . "%' and clfoco = " . $form['clfoco'] . " and numdoc = '" . $form['numdoc'] . "'", "protoc desc, datfat desc, datemi desc", 0, 1);
+							$stesso_numero = gaz_dbi_fetch_array($rs_stesso_numero);
+							if ($stesso_numero) {
+								$msg['err'][] = "samedoc"; echo $form['tipdoc'];die;
+							}
 						}
-					}
+					} else {				
+						if (!preg_match("/^id_([0-9]+)$/", $form['clfoco'], $match)) {
+							//controllo se ci sono altri documenti con lo stesso numero fornitore
+							$rs_stesso_numero = gaz_dbi_dyn_query("*", $gTables['tesdoc'], "YEAR(datemi) = " .substr($datemi,0,4) . " and tipdoc like '" . substr($form['tipdoc'], 0, 1) . "%' and clfoco = " . $form['clfoco'] . " and numfat = '" . $form['numfat'] . "'", "protoc desc, datfat desc, datemi desc", 0, 1);
+							$stesso_numero = gaz_dbi_fetch_array($rs_stesso_numero);
+							if ($stesso_numero) {
+								$msg['err'][] = "samedoc"; echo $form['tipdoc'];die;
+							}
+						}
+					}			
 				}
             }
         }
