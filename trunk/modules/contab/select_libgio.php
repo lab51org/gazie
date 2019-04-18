0e827c8288eaef069d19d59676b4a1d8cd424ec3
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-04-18 13:37:21 +0000

    A causa dell'eccessiva lentezza sono stato costretto a riscrivere completamente la stampa del Libro giornale (contab/stampa_libgio.php). Ho aggiunto l'indice mancate sulla data di registrazione della tabella dei movimenti contabili, riscritto la query, ed utilizzato la libreria tFPDF molto più veloce di TCPDF (anche se carente di funzioni). Adesso anche le aziende con 100.000 righi contabili/anno e un server non performante riesce a generare il pdf di oltre 1600 pagine in tempi umani senza far andare lo stesso in timeout.

--- trunk/modules/contab/select_libgio.php
+++ trunk/modules/contab/select_libgio.php
@@ -26,14 +26,7 @@ require("../../library/include/datlib.inc.php");
 $admin_aziend=checkAdmin();
 $msg = "";
 
-if (!isset($_GET['valdar'])) {  //al primo accesso
-    $upggio = gaz_dbi_get_row($gTables['company_data'],'var','upggio');
-    $form['pagini'] = $upggio['data']+1;
-    //$form['pagini'] = $admin_aziend["upggio"]+1;
-    $form['stadef'] = false;
-    $form['copert'] = false;
-    $form['valdar'] = 0;
-    $form['valave'] = 0;
+if (!isset($_GET['date_ini_D'])) {  //al primo accesso
     $form['date_ini_D'] = 1;
     $form['date_ini_M'] = 1;
     $form['date_ini_Y'] =  date("Y");
@@ -41,19 +34,6 @@ if (!isset($_GET['valdar'])) {  //al primo accesso
     $form['date_fin_M'] =  date("m");
     $form['date_fin_Y'] =  date("Y");
 } else {
-    $form['pagini'] = intval($_GET['pagini']);
-    if (isset($_GET['stadef'])){
-       $form['stadef']=intval($_GET['stadef']);
-    } else {
-       $form['stadef']='';
-    }
-    if (isset($_GET['copert'])){
-       $form['copert']=intval($_GET['copert']);
-    } else {
-       $form['copert']='';
-    }
-    $form['valdar'] = number_format($_GET['valdar'],2,'.','');
-    $form['valave'] = number_format($_GET['valave'],2,'.','');
     $form['date_ini_D'] = intval($_GET['date_ini_D']);
     $form['date_ini_M'] = intval($_GET['date_ini_M']);
     $form['date_ini_Y'] = intval($_GET['date_ini_Y']);
@@ -72,23 +52,11 @@ $datainizio = date("Ymd",$utsini);
 $datafine = date("Ymd",$utsfin);
 if ($utsini > $utsfin) $msg .="2+";
 if (isset($_GET['stampa']) && $msg == "") {
-        if ($form['copert']==1) {
-           $form['copert'] = "&copert";
-        } else {
-           $form['copert']="";
-        }
-        if ($form['stadef']==1) {
-           $form['stadef'] = "&stadef";
-        } else {
-           $form['stadef']="";
-        }
         //Mando in stampa i movimenti contabili selezionati
-        $locazione = "Location: stampa_libgio.php?pagini=".$form['pagini']."&regini=".date("d-m-Y",$utsini)."&regfin=".date("d-m-Y",$utsfin)."&valdar=".$form['valdar']."&valave=".$form['valave'].$form['copert'];
-        //print $locazione;
+        $locazione = "Location: stampa_libgio.php?regini=".date("d-m-Y",$utsini)."&regfin=".date("d-m-Y",$utsfin);
         header($locazione);
         exit;
 }
-
 if (isset($_GET['Return'])) {
         header("Location:docume_contab.php");
         exit;
@@ -115,7 +83,7 @@ function setDate(name) {
 }
 </script>
 ";
-echo "<form method=\"GET\" name=\"select\">\n";
+echo "<form method=\"GET\">\n";
 $gForm = new contabForm();
 echo "<div align=\"center\" class=\"FacetFormHeaderFont\">".$script_transl['title'];
 echo "</div>\n";
@@ -124,27 +92,13 @@ if (!empty($msg)) {
     echo '<tr><td colspan="4" class="FacetDataTDred">'.$gForm->outputErrors($msg,$script_transl['errors'])."</td></tr>\n";
 }
 echo "<tr>\n";
-echo "<td class=\"FacetFieldCaptionTD\">".$script_transl['stadef']."</td><td class=\"FacetDataTD\">\n";
-$gForm->selCheckbox('stadef',$form['stadef'],$script_transl['stadef_title']);
-echo "</td>\n";
-echo "\t<td class=\"FacetFieldCaptionTD\">".$script_transl['pagini']."</td>\n";
-echo "\t<td class=\"FacetDataTD\"><input type=\"text\" name=\"pagini\" value=\"".$form['pagini']."\" maxlength=\"5\" size=\"5\" /></td>\n";
-echo "</td>\n";
-echo "</tr>\n";
-echo "<tr>\n";
-echo "\t<td class=\"FacetFieldCaptionTD\">".$script_transl['valdar']."</td>\n";
-echo "\t<td class=\"FacetDataTD\"><input type=\"text\" name=\"valdar\" value=\"".$form['valdar']."\" maxlength=\"15\" size=\"15\" /></td>\n";
-echo "\t<td class=\"FacetFieldCaptionTD\">".$script_transl['valave']."</td>\n";
-echo "\t<td class=\"FacetDataTD\"><input type=\"text\" name=\"valave\" value=\"".$form['valave']."\" maxlength=\"15\" size=\"15\" /></td>\n";
-echo "</tr>\n";
-echo "<tr>\n";
 echo "<td class=\"FacetFieldCaptionTD\">".$script_transl['date_ini']."</td><td colspan=\"3\" class=\"FacetDataTD\">\n";
-$gForm->CalendarPopup('date_ini',$form['date_ini_D'],$form['date_ini_M'],$form['date_ini_Y'],'FacetSelect',1);
+$gForm->CalendarPopup('date_ini',$form['date_ini_D'],$form['date_ini_M'],$form['date_ini_Y'],'FacetSelect','date_ini');
 echo "</td>\n";
 echo "</tr>\n";
 echo "<tr>\n";
 echo "<td class=\"FacetFieldCaptionTD\">".$script_transl['date_fin']."</td><td colspan=\"3\" class=\"FacetDataTD\">\n";
-$gForm->CalendarPopup('date_fin',$form['date_fin_D'],$form['date_fin_M'],$form['date_fin_Y'],'FacetSelect',1);
+$gForm->CalendarPopup('date_fin',$form['date_fin_D'],$form['date_fin_M'],$form['date_fin_Y'],'FacetSelect','date_fin');
 echo "</td>\n";
 echo "</tr>\n";
 $result = gaz_dbi_dyn_query ("darave,datreg, SUM(import) AS import, COUNT(*) AS nrow",$gTables['rigmoc']." LEFT JOIN ".$gTables['tesmov']." ON (".$gTables['rigmoc'].".id_tes = ".$gTables['tesmov'].".id_tes) ","datreg BETWEEN ".$datainizio." AND ".$datafine." GROUP BY darave");
@@ -160,9 +114,10 @@ while ($rs = gaz_dbi_fetch_array($result)){
 }
 echo "<tr><td class=\"FacetFieldCaptionTD\">".$script_transl['nrow']."</td><td class=\"FacetDataTD\" colspan=\"3\">".$nr." &nbsp;</td></tr>";
 echo "<tr><td class=\"FacetFieldCaptionTD\">".$script_transl['tot_d']."</td><td class=\"FacetDataTD\">".gaz_format_number($dare)."</td><td class=\"FacetFieldCaptionTD\">".$script_transl['tot_a']."</td><td class=\"FacetDataTD\">".gaz_format_number($avere)." &nbsp;</td></tr>";
-echo "<tr><td class=\"FacetFieldCaptionTD\"> &nbsp;</td><td align=\"right\" class=\"FacetFooterTD\" colspan=4><input type=\"submit\" name=\"Return\" value=\"Indietro\"><input type=\"submit\" name=\"stampa\" value=\"".$script_transl['print']." !\" > ".$script_transl['cover']." <input type=\"checkbox\"  name=\"copert\" value=1></td></tr>";
+echo "<tr><td class=\"FacetFieldCaptionTD\"> &nbsp;</td><td align=\"right\" class=\"FacetFooterTD\" colspan=4><input type=\"submit\" name=\"Return\" value=\"Indietro\"><input type=\"submit\" name=\"stampa\" value=\"".$script_transl['print']." !\" ></td></tr>";
 ?>
 </table>
+<input type="hidden" name="hidden_req" />
 </form>
 <?php
 require("../../library/include/footer.php");
