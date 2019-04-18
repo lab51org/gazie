6089adf8464c81558931095e281e113ebc31ce05
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-04-18 13:37:21 +0000

    A causa dell'eccessiva lentezza sono stato costretto a riscrivere completamente la stampa del Libro giornale (contab/stampa_libgio.php). Ho aggiunto l'indice mancate sulla data di registrazione della tabella dei movimenti contabili, riscritto la query, ed utilizzato la libreria tFPDF molto più veloce di TCPDF (anche se carente di funzioni). Adesso anche le aziende con 100.000 righi contabili/anno e un server non performante riesce a generare il pdf di oltre 1600 pagine in tempi umani senza far andare lo stesso in timeout.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3077 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/contab/stampa_libgio.php
+++ modules/contab/stampa_libgio.php
@@ -22,11 +22,19 @@
     Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
  --------------------------------------------------------------------------
 */
+function get_render_time($prev) {
+    list($usec, $sec) = explode(" ", microtime());
+    $this_time = ((float) $usec + (float) $sec);
+    return round($this_time - $prev, 8);
+}
+
+define('ROWS_PERPAGE',60);
+
 require("../../library/include/datlib.inc.php");
 $admin_aziend=checkAdmin();
 if (!ini_get('safe_mode')){ //se me lo posso permettere...
-    ini_set('memory_limit','128M');
-    gaz_set_time_limit (0);
+    ini_set('memory_limit','512M');
+    gaz_set_time_limit (240);
 }
 
 if (!isset($_GET['regini']) or !isset($_GET['regfin'])) {
@@ -41,117 +49,130 @@ $giofin = substr($_GET['regfin'],0,2);
 $mesfin = substr($_GET['regfin'],3,2);
 $annfin = substr($_GET['regfin'],6,4);
 $utsfin= mktime(0,0,0,$mesfin,$giofin,$annfin);
-$datainizio = date("Ymd",$utsini);
-$datafine = date("Ymd",$utsfin);
-$title = 'Libro Giornale dal '.date("d-m-Y",$utsini).' al '.date("d-m-Y",$utsfin);
+$datainizio = date("Y-m-d",$utsini);
+$datafine = date("Y-m-d",$utsfin);
+$admin_aziend['title'] = 'Libro Giornale dal '.date("d-m-Y",$utsini).' al '.date("d-m-Y",$utsfin);
+/*list ($usec, $sec) = explode(' ', microtime());
+$render_time = ((float) $usec + (float) $sec);
+$render_time_before = $render_time;*/
+
 //recupero tutti i movimenti contabili insieme alle relative testate...
-$result = mergeTable($gTables['rigmoc'],"*",$gTables['tesmov'],"*","id_tes","datreg between $datainizio and $datafine  ORDER BY datreg, ".$gTables['rigmoc'].".id_tes,id_rig,codcon");
-
-
-$cover_descri = 'Libro Giornale dell\'anno '.date("Y",$utsfin);
-$tot_avere=number_format($_GET['valave'],2, '.', '');
-$tot_dare=number_format($_GET['valdar'],2, '.', '');
-
-$topCarry = array(array('lenght' => 150,'name'=>'da riporto : ','frame' => 'B','fill'=>0,'font'=>8),
-                  array('lenght' => 20,'name'=>'','frame' => 1,'fill'=>0),
-                  array('lenght' => 20,'name'=>'','frame' => 1,'fill'=>0));
-$botCarry = array(array('lenght' => 150,'name'=>'a riporto : ','frame' => 'T','fill'=>0,'font'=>8),
-                  array('lenght' => 20,'name'=>'','frame' => 1,'fill'=>1),
-                  array('lenght' => 20,'name'=>'','frame' => 1,'fill'=>1));
-$top = array(array('lenght' => 10,'name'=>'Progr.','frame' => 1,'fill'=>1,'font'=>8),
-             array('lenght' => 18,'name'=>'Data Reg.','frame' => 1,'fill'=>1),
-             array('lenght' => 60,'name'=>'Descrizione Operazione / Num. e Data Doc.','frame' => 1,'fill'=>1),
-             array('lenght' => 16,'name'=>'N.Conto','frame' => 1,'fill'=>1),
-             array('lenght' => 46,'name'=>'Nome Conto','frame' => 1,'fill'=>1),
-             array('lenght' => 20,'name'=>'Dare','frame' => 1,'fill'=>1),
-             array('lenght' => 20,'name'=>'Avere','frame' => 1,'fill'=>1));
-
-require("../../config/templates/standard_template.php");
-$pdf = new Standard_template('P','mm','A4',true,'UTF-8',false,true);
-$n_page = intval($_GET['pagini']);
-if (isset($_GET['copert'])) {
-   $n_page--;
+$where = "`datreg` >= '".$datainizio." 00:00:00' AND `datreg` <= '". $datafine." 23:59:59'";
+$rs_count = gaz_dbi_dyn_query('COUNT(*) AS nr', 
+							  $gTables['tesmov'] . ' AS tm ' .
+							  'INNER JOIN ' . $gTables['rigmoc'] . ' AS rm ON tm.id_tes=rm.id_tes ', 
+							  $where);
+$pagetot=ceil(gaz_dbi_fetch_assoc($rs_count)['nr']/ROWS_PERPAGE); //numero di pagine necessarie
+$field = "tm.id_tes, tm.descri, DATE_FORMAT(tm.datreg,'%d-%m-%Y') AS dr, tm.seziva, tm.protoc, tm.numdoc, DATE_FORMAT(tm.datdoc,'%d-%m-%Y') AS dd,  rm.import*(rm.darave='A') AS avere, rm.import*(rm.darave='D') AS dare, rm.codcon, SUBSTR(cf.descri,1,40) AS cfdes";
+$from = $gTables['tesmov'] . ' AS tm ' .
+        'INNER JOIN ' . $gTables['rigmoc'] . ' AS rm ON tm.id_tes=rm.id_tes '.
+        'INNER JOIN ' . $gTables['clfoco'] . ' AS cf ON rm.codcon=cf.codice ';
+$orderby = "`datreg`,`id_tes`,`id_rig`";
+$p=1;
+$r=1;
+$i=1;
+$result = gaz_dbi_dyn_query($field, $from, $where, $orderby);
+$a[0]=$pagetot;
+$rip=array();
+$rid=0.00;
+$ria=0.00;
+while ($mov = gaz_dbi_fetch_assoc($result)) {
+	if ($r>ROWS_PERPAGE){
+		$rip[$p]['dare']=$rid;
+		$rip[$p]['avere']=$ria;
+		$r=1;
+		$p++;
+	}
+	$rid+=$mov['dare'];
+	$ria+=$mov['avere'];
+	$mov['pagerow']=$r;
+	$a[1][$p][$i]=$mov;
+	$r++;
+	$i++;
 }
-$pdf->setVars($admin_aziend,$title,0,array('ini_page'=>$n_page,'year'=>'Pagina '.$annfin));
-if (isset($_GET['copert'])) {
-   $pdf->setCover($cover_descri);
-   $pdf->AddPage();
-}
-$pdf->setTopBar($top);
-if (($tot_avere+$tot_dare)> 0.01){
-    $topCarry[1]['name']= gaz_format_number($tot_dare);
-    $topCarry[2]['name']= gaz_format_number($tot_avere);
-    $pdf->setTopCarryBar($topCarry);
-}
-$pdf->AddPage();
-$pdf->setFooterMargin(21);
-$pdf->setTopMargin(44);
-$pdf->SetFont('helvetica','',7);
-$ctrlmopre = 0;
-$numrig=1;
-
-$anagrafica = new Anagrafica();
-while ($mov = gaz_dbi_fetch_array($result)) {
-    $giomov = substr($mov['datreg'],8,2);
-    $mesmov = substr($mov['datreg'],5,2);
-    $annmov = substr($mov['datreg'],0,4);
-    $giodoc = substr($mov['datdoc'],8,2);
-    $mesdoc = substr($mov['datdoc'],5,2);
-    $anndoc = substr($mov['datdoc'],0,4);
-    $clfoco = $anagrafica->getPartner($mov["codcon"]);
-    $utsmov= mktime(0,0,0,$mesmov,$giomov,$annmov);
-    $datamov = date("d-m-Y",$utsmov);
-    $datadoc = $giodoc.'-'.$mesdoc.'-'.$anndoc;
-
-    if($mov["darave"] == 'D') {
-        $dare = gaz_format_number($mov["import"]);
-        $tot_dare += $mov["import"];
-        $avere = "";
-    } else {
-        $avere = gaz_format_number($mov["import"]);
-        $tot_avere += $mov["import"];
-        $dare = "";
+$rip[$p]['dare']=$rid;
+$rip[$p]['avere']=$ria;
+/*$qt=round(get_render_time($render_time),1);
+echo  $qt;*/
+require('../../library/tFPDF/mem_image.php');
+class GL_template extends PDF_MemImage {
+    function SetVars($admin_aziend) {
+        $this->ad_az = $admin_aziend;
+        $this->intesta1 = $admin_aziend['ragso1'] . ' ' . $admin_aziend['ragso2'];
+        $this->intesta2 = $admin_aziend['indspe'] . ' ' . sprintf("%05d", $admin_aziend['capspe']) . ' ' . $admin_aziend['citspe'] . ' (' . $admin_aziend['prospe'] . ')';
+        $this->intesta3 = ' C.F.:' . $admin_aziend['codfis'] . ' P.I.:' . $admin_aziend['pariva'];
+	}
+	function Header() {
+		$this->MemImage($this->ad_az['image'],10,10,0,12);
+		$this->SetFont('dejavusans','B',8);
+		$this->SetX(40);
+        $this->Cell(85,4,$this->intesta1,0,0,'C');
+        $this->Cell(75,4,$this->ad_az['title'],0,1,'R');
+		$this->SetFont('DejaVu','',7);
+		$this->SetX(40);
+        $this->Cell(85,4,$this->intesta2,0,2,'C');
+        $this->Cell(85,4,$this->intesta3,0,0,'C');
+	}
+    function Footer() {
+        $this->MultiCell(190,4,$this->intesta1.' '.$this->intesta2.' '.$this->intesta3,0,'C');
     }
-
-    if ($mov["id_tes"] != $ctrlmopre) {
-        $pdf->Cell(10,4,$numrig,'LTR',0,'R');
-        $pdf->Cell(18,4,$datamov,1,0,'L');
-        $pdf->Cell(60,4,$mov['descri'],1,0,'L',0,'',1);
-        $pdf->Cell(16,4,$mov['codcon'],'LT',0,'C');
-        $pdf->Cell(46,4,$clfoco['descri'],'LT',0,'L',0,'',1);
-        $pdf->Cell(20,4,$dare,'LT',0,'R');
-        $pdf->Cell(20,4,$avere,'LRT',1,'R');
-        if(!empty($mov["numdoc"])) {
-            $pdf->Cell(28);
-            $pdf->Cell(60,4,"n.".$mov['numdoc']."/".$mov['seziva']." del ".$datadoc,1,0,'L');
-            $pdf->SetX(10);
-        }
-    } else {
-        $pdf->Cell(10,4,$numrig,'LR',0,'R');
-        $pdf->Cell(78,4,'',0,0,'L');
-        $pdf->Cell(16,4,$mov['codcon'],'L',0,'C');
-        $pdf->Cell(46,4,$clfoco['descri'],'L',0,'L',0,'',1);
-        $pdf->Cell(20,4,$dare,'L',0,'R');
-        $pdf->Cell(20,4,$avere,'LR',1,'R');
-    }
-
-    $topCarry[1]['name']= gaz_format_number($tot_dare);
-    $botCarry[1]['name']= gaz_format_number($tot_dare);
-    $topCarry[2]['name']= gaz_format_number($tot_avere);
-    $botCarry[2]['name']= gaz_format_number($tot_avere);
-    $pdf->setTopCarryBar($topCarry);
-    $pdf->setBotCarryBar($botCarry);
-
-    $ctrlmopre = $mov["id_tes"];
-    $numrig ++;
+	
 }
-$pdf->setBotCarryBar('');
-$pdf->Cell(150,4,'TOTALI : ',1,0,'L');
-$pdf->Cell(20,4,gaz_format_number($tot_dare),1,0,'R',1);
-$pdf->Cell(20,4,gaz_format_number($tot_avere),1,0,'R',1);
-if (isset($_GET['stadef'])) {
-    gaz_dbi_put_row($gTables['company_data'],'var','upggio','data',$pdf->getGroupPageNo() + $n_page - 1 );
-    //gaz_dbi_put_row($gTables['aziend'],"codice",$admin_aziend['codice'],'upggio', $pdf->getGroupPageNo()+$n_page-1);
+$pdf = new GL_template();
+// Add a Unicode font (uses UTF-8)
+$pdf->AddFont('DejaVu','','DejaVuSansCondensed.ttf',true);
+$pdf->AddFont('dejavusans','B','DejaVuSans-Bold.ttf', true);
+$pdf->SetVars($admin_aziend);
+$pdf->SetFillColor(hexdec(substr($pdf->ad_az['colore'], 0, 2)), hexdec(substr($pdf->ad_az['colore'], 2, 2)), hexdec(substr($pdf->ad_az['colore'], 4, 2)));
+$ci=0;
+foreach($a[1] as $k1=>$v1){
+	$pdf->AddPage();
+	$pdf->Cell(75,4,'Pagina '.$k1.' di '.$a[0],0,1,'R');
+    $pdf->Cell(10,4,'Rigo',1,0,'R',1);
+    $pdf->Cell(78,4,'Descrizione movimento',1,0,'L',1);
+    $pdf->Cell(16,4,'Cod. conto',1,0,'C',1);
+    $pdf->Cell(46,4,'Descrizione conto',1,0,'L',1);
+    $pdf->Cell(20,4,'Dare',1,0,'R',1);
+    $pdf->Cell(20,4,'Avere',1,1,'R',1);
+	if ($k1>1){
+		$pdf->Cell(104,4,'','B');
+        $pdf->Cell(46,4,'--> da riporto ','B',0,'R');
+        $pdf->Cell(20,4,number_format($rip[$k1-1]['dare'],2,',',''),1,0,'R',1);
+        $pdf->Cell(20,4,number_format($rip[$k1-1]['avere'],2,',',''),1,1,'R',1);
+	} else {
+		//$pdf->Cell(88,4,'Pagina '.$k1.' di '.$a[0],1,1);
+	}
+	foreach($v1 as $k2=>$v2){
+		$v2['dare']=($v2['dare']>0)?number_format($v2['dare'],2,',',''):'';
+		$v2['avere']=($v2['avere']>0)?number_format($v2['avere'],2,',',''):'';
+		if ($ci!=$v2['id_tes']){
+			$ds=wordwrap($v2['descri'],50,"XZX");
+			$dx=explode("XZX",$ds);
+			$dsx=$v2['dr'].' '.$dx[0];
+			$b='T';
+		}else{
+			if(isset($dx[1])){$dsx=$dx[1];}else{$dsx='';}
+			if(!empty($v2["numdoc"])&&$b=='T'){$dsx.= " n.".$v2['numdoc']."/".$v2['seziva']." del ".$v2['dd'];}
+			$b='';
+		}
+        $pdf->Cell(10,4,$k2,1,0,'R');
+        $pdf->Cell(78,4,$dsx,$b);
+        $pdf->Cell(16,4,$v2['codcon'],'LT',0,'C');
+        $pdf->Cell(46,4,$v2['cfdes'],'LT',0,'L');
+        $pdf->Cell(20,4,$v2['dare'],'LT',0,'R');
+        $pdf->Cell(20,4,$v2['avere'],'LRT',1,'R');
+/*		echo '<BR/>Rigo '.$k2.'<br/>';
+		print_r($v2);
+		echo '<BR/><HR>';*/
+	    $ci=$v2['id_tes'];
+	}
+	$pdf->Cell(50,4,'Pagina '.$k1.' di '.$a[0],'T');
+	$pdf->Cell(100,4,'a riporto --> ','T',0,'R');
+    $pdf->Cell(20,4,number_format($rip[$k1]['dare'],2,',',''),1,0,'R',1);
+    $pdf->Cell(20,4,number_format($rip[$k1]['avere'],2,',',''),1,1,'R',1);
+	//echo '<BR/>riporti: D='.$rip[$k1]['dare'].' A='.$rip[$k1]['avere'].'<BR/>Pagina '.$k1.' di '.$a[0].'<HR>';
 }
-$pdf->Output($title.'.pdf');
+$pdf->Output();
+/*$qt=round(get_render_time($render_time),1);
+echo  $qt;*/
 ?>
