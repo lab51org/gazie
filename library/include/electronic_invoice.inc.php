6966388d5ea874894372335b7887d89ae7e49926
angelo983 <angelo983@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-05-10 17:48:42 +0000

    Possibilità di caricare la fattura firmata da Report fatture elettroniche prima dell'invio per le fatture caricate singolarmente
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3114 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- library/include/electronic_invoice.inc.php
+++ library/include/electronic_invoice.inc.php
@@ -1324,7 +1324,7 @@ function create_XML_invoice($testata, $gTables, $rows = 'rigdoc', $dest = false,
             'filename_ret' => '',
             'mail_id' => 0,
             'data' => '',
-            'flux_status' => '#',
+            'flux_status' => (strlen($cod_destinatario)==6) ? '##' : '#',
             'progr_ret' => '000',
             'flux_descri' => '');
 			fae_fluxInsert($valori);
