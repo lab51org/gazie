90c947886ba214d273a511505aae1d8f20878a2c
marcorm <marcorm@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-07-07 15:22:36 +0000

    corregge regressione su report_movmag.php
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3137 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- library/include/function.inc.php
+++ library/include/function.inc.php
@@ -2121,10 +2121,10 @@ class TableSorter {
 
     function __construct($table, $passo, $default_order, $default_search=[], $group_by=[]) {
         $this->passo = $passo;
-	$this->group_by = $group_by ? "GROUP BY " . join(", ", $group_by) : "";
+        $this->group_by = $group_by ? "GROUP BY " . join(", ", $group_by) : "";
         $this->default_search = $default_search;
         $this->parse_search_request();
-        $this->count = gaz_dbi_record_count($table, join(" ", [$this->where, $this->group_by]));
+        $this->count = gaz_dbi_record_count($table, trim("$this->where $this->group_by"));
         $this->set_pagination();
         $this->default_order = $default_order;
         $this->parse_order_request();
