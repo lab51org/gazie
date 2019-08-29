1b773f69e487923adaf7e0df886e4b1d5037455a
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-08-29 23:45:40 +0000

    Aggiunte le immagini thumb e i widgets dei click
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3191 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/root/dash_numclick_widget.php
@@ -0,0 +1,47 @@
+<div class="collapse navbar-collapse">
+	<div class="panel panel-info" >
+    <!-- per adesso lo faccio collassare in caso di small device anche se si potrebbe fare uno switch in verticale -->
+    <?php
+    $result = gaz_dbi_dyn_query("*", $gTables['menu_usage'], ' company_id="' . $form['company_id'] . '" AND adminid="' . $admin_aziend['user_name'] . '" ', ' click DESC, last_use DESC', 0, 8);
+    if (gaz_dbi_num_rows($result) > 0) {
+        while ($r = gaz_dbi_fetch_array($result)) {
+            $rref = explode('-', $r['transl_ref']);
+            switch ($rref[1]) {
+                case 'm1':
+                    require '../' . $rref[0] . '/menu.' . $admin_aziend['lang'] . '.php';
+                    $rref_name = $transl[$rref[0]]['title'];
+                    break;
+                case 'm2':
+                    require '../' . $rref[0] . '/menu.' . $admin_aziend['lang'] . '.php';
+                    $rref_name = $transl[$rref[0]]['m2'][$rref[2]][0];
+                    break;
+                case 'm3':
+                    require '../' . $rref[0] . '/menu.' . $admin_aziend['lang'] . '.php';
+                    $rref_name = $transl[$rref[0]]['m3'][$rref[2]][0];
+                    break;
+                case 'sc':
+                    require '../' . $rref[0] . '/lang.' . $admin_aziend['lang'] . '.php';
+                    $rref_name = $strScript[$rref[2]][$rref[3]];
+                    break;
+                default:
+                    $rref_name = 'Nome script non trovato';
+                    break;
+            }
+            ?>
+            <div class="row">
+                <div class="col-sm-12">
+                    <a href="<?php
+                    if ($r["link"] != "")
+                        echo '../../modules' . $r["link"];
+                    else
+                        echo "&nbsp;";
+                    ?>" type="button" class="btn btn-default btn-full" style="background-color: #<?php echo $r["color"]; ?>; font-size: 85%; text-align: left;">
+                        <span ><?php echo $r["click"] . ' click - <b>' . $rref_name . '</b>'; ?></span></a>
+                </div>
+            </div>
+            <?php
+        }
+    }
+    ?>
+	</div>
+</div>
--- modules/root/help/italian/admin_help.php
+++
@@ -1,7 +0,0 @@
-<span><div>Sui monitor ampi il menù principale si ottiene spostando il puntatore del mouse sopra la voce <strong>Home</strong> in alto a sinistra, su quelli piccoli (smartphone) cliccando sul <strong>bottone</strong> che appare in alto a sinistra.</div>
-<div>Per una guida generale introduttiva a GAzie si possono consultare alcuni capitoli tratti da &laquo;<big>a</big>2&raquo; <a href="http://www.archive.org/details/AppuntiDiInformaticaLibera" target="new">Appunti Di Informatica Libera</a>:</div><div>
-<ul>
-<li><a title="GAzie: indice generale" href="http://a2.pluto.it/a2/a2155.htm" target="_new"><em>Indice generale</em></a></li>
-<li> <a title="GAzie: manuale" href="http://a2.pluto.it/a2/a2153.htm" target="_new"><em>Manuale sintetico delle funzionalità principali di Gazie</em></a></li>
-<li> <a title="GAzie: video" href="https://www.youtube.com/user/appunti2/search?query=gazie" target="_new"><em>I video ad uso didattico (dic.2011)</em></a></li>
-</ul></div></span>
