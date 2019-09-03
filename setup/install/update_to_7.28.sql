765a2d51cae16864bfe257758557bd222a45233d
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-09-03 16:07:08 +0000

    Aggiunti sul database i record per ricordare gli ultimi mesi stampati dei vari registri IVA
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3206 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ setup/install/update_to_7.28.sql
@@ -0,0 +1,30 @@
+UPDATE `gaz_config` SET `cvalue` = '123' WHERE `id` =2;
+-- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 1', 'umeco1', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 2', 'umeco2', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 3', 'umeco3', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 4', 'umeco4', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 5', 'umeco5', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 6', 'umeco6', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 7', 'umeco7', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 8', 'umeco8', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro dei Corrispettivi della sezione IVA 9', 'umeco9', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 1', 'umeve1', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 2', 'umeve2', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 3', 'umeve3', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 4', 'umeve4', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 5', 'umeve5', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 6', 'umeve6', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 7', 'umeve7', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 8', 'umeve8', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di vendita della sezione IVA 9', 'umeve9', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 1', 'umeac1', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 2', 'umeac2', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 3', 'umeac3', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 4', 'umeac4', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 5', 'umeac5', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 6', 'umeac6', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 7', 'umeac7', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 8', 'umeac8', '0');
+INSERT INTO `gaz_XXXcompany_data` (`description`, `var`, `data`) VALUES ('Ultimo mese stampato del Registro delle Fatture di acquito della sezione IVA 9', 'umeac9', '0');
+-- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
