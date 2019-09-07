208d56bae121357dbdb8b42c1e9dc1eeaf06ca27
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-09-07 22:47:46 +0000

    Adesso le favicon vengono generate al volo, messe direttamente inline e legate al contenuto logo aziendale, pertanto cambiano al cambiare dell'azienda attiva
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3216 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/root/views/_header.php
+++ modules/root/views/_header.php
@@ -5,19 +5,38 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="Antonio De Vincentiis http://www.devincentiis.it">
 		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-title" content="GAzie - Gestione AZIEndale">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-        <link rel="stylesheet" href="../../library/bootstrap/css/bootstrap.min.css" >
+<?php
+$rs_az = gaz_dbi_dyn_query('*', $gTables['aziend'], '1', 'codice DESC');
+if (gaz_dbi_num_rows($rs_az) > 1) {
+?>
+		<meta name="apple-mobile-web-app-title" content="GAzie - Gestione AZIEndale">
         <link rel="shortcut icon" href="../../library/images/favicon.ico">
-        <link rel="stylesheet" type="text/css" href="../../library/theme/g7/scheletons/default.css">
-        <link rel="stylesheet" type="text/css" href="../../library/theme/g7/skins/default.css">
 		<link rel="apple-touch-icon" href="../../library/images/apple-icon-114x114-precomposed.png">
 		<link rel="apple-touch-startup-image" href="../../library/images/apple-icon-114x114-precomposed.png">		
-		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="../../library/images/apple-icon-57x57-precomposed.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../../library/images/apple-icon-72x72-precomposed.png" />
 		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../../library/images/apple-icon-114x114-precomposed.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../../library/images/apple-icon-144x144-precomposed.png" />
         <title>GAzie - Login</title>
+<?php	
+} else {
+	$az = gaz_dbi_fetch_array($rs_az);
+?>
+		<meta name="apple-mobile-web-app-title" content="<?php echo $az['ragso1'];?>">
+		<?php
+			$ico=base64_encode(file_get_contents('../../data/files/'.$az['codice'].'/favicon.ico'));
+			$ico114=base64_encode(file_get_contents('../../data/files/'.$az['codice'].'/logo_114x114.png'));
+		?>
+        <link rel="icon" href="data:image/x-icon;base64,<?php echo $ico?>"  type="image/x-icon" />
+		<link rel="icon" sizes="114x114" href="data:image/x-icon;base64,<?php echo $ico114?>"  type="image/x-icon" />
+		<link rel="apple-touch-icon" href="data:image/x-icon;base64,<?php echo $ico114?>"  type="image/x-icon">
+		<link rel="apple-touch-startup-image" href="data:image/x-icon;base64,<?php echo $ico114?>"  type="image/x-icon">		
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="data:image/x-icon;base64,<?php echo $ico114?>"  type="image/x-icon" />
+
+<?php	
+}
+?>
+        <link rel="stylesheet" href="../../library/bootstrap/css/bootstrap.min.css" >
+        <link rel="stylesheet" type="text/css" href="../../library/theme/g7/scheletons/default.css">
+        <link rel="stylesheet" type="text/css" href="../../library/theme/g7/skins/default.css">
 
 </head>
 <body>
