f11a49e31682c4fc723c5621fe98d8906a1dde7b
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-09-10 06:27:59 +0000

    Registro di campagna: nel PDF per la stampa del quaderno di campagna adesso viene evidenziato se il campo di coltivazione è in ZVN
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3226 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/stampa_giomag.php
+++ modules/camp/stampa_giomag.php
@@ -104,61 +104,46 @@ $config = new Config;
 $pdf->AddPage('L',$config->getValue('page_format'));
 $pdf->SetFont('helvetica','',9);
 if (sizeof($result) > 0) {
+	
   while (list($key, $row) = each($result)) {
-	  if ($row['mostra_qdc']==1){ //escludi se è stato selezionato di non mostrare nel Q.d.c.
-      $datadoc = substr($row['datdoc'],8,2).'-'.substr($row['datdoc'],5,2).'-'.substr($row['datdoc'],0,4);
-      $datareg = substr($row['datreg'],8,2).'-'.substr($row['datreg'],5,2).'-'.substr($row['datreg'],0,4);
-      $movQuanti = $row['quanti']*$row['operat'];
-      $pdf->Cell(17,6,$datadoc,1,0,'C');
-      $pdf->Cell(35,6,$row['descri'],1, 0, 'l', 0, '', 1);
-	  $pdf->Cell(30,6,$row['desdoc'],1, 0, 'l', 0, '', 1);
-	  $pdf->Cell(12,6,substr($row['campo_coltivazione'],0,3),1);
-// Antonio Germani carico la tabella campi
-$res = gaz_dbi_dyn_query ('*', $gTables['campi']);
-// fine carico tabella campi			
-	// Antonio Germani Inserisco superficie e coltura		
-            
-			$colonna="0";
-	while($b_row = $res->fetch_assoc()) { 
-	if ($row['campo_coltivazione']==$b_row["codice"]) { 
-	$pdf->Cell(10,6,str_replace('.', ',',$b_row["ricarico"]),1);
-	$res4 = gaz_dbi_get_row($gTables['camp_colture'], 'id_colt', $row['id_colture']);
-	 $pdf->Cell(38,6,substr($res4["nome_colt"],0,40),1);
-	 $colonna="1";
-		} 
-	}
-	 if ($colonna<1) {
-		$pdf->Cell(10,6,"",1);
-		$pdf->Cell(38,6,"",1);
-	 }
-// fine inserisco superficie, coltura	  
+	if ($row['mostra_qdc']==1){ //escludi se è stato selezionato di non mostrare nel Q.d.c.
+		$res = gaz_dbi_get_row ($gTables['campi'], 'codice', $row['campo_coltivazione']);// Antonio Germani carico il campo
+		$datadoc = substr($row['datdoc'],8,2).'-'.substr($row['datdoc'],5,2).'-'.substr($row['datdoc'],0,4);
+		$datareg = substr($row['datreg'],8,2).'-'.substr($row['datreg'],5,2).'-'.substr($row['datreg'],0,4);
+		$movQuanti = $row['quanti']*$row['operat'];
+		$pdf->Cell(17,6,$datadoc,1,0,'C');
+		$pdf->Cell(35,6,$row['descri'],1, 0, 'l', 0, '', 1);
+		$pdf->Cell(30,6,$row['desdoc'],1, 0, 'l', 0, '', 1);
+		if ($res['zona_vulnerabile']==0){
+			$pdf->Cell(12,6,substr($row['campo_coltivazione'],0,5),1);
+		} else {
+			$pdf->Cell(12,6,substr($row['campo_coltivazione']." ZVN",0,5),1);
+		}
+		// Antonio Germani Inserisco superficie e coltura		
+      	$pdf->Cell(10,6,str_replace('.', ',',$res['ricarico']),1);
+		$res4 = gaz_dbi_get_row($gTables['camp_colture'], 'id_colt', $res['id_colture']);
+		$pdf->Cell(38,6,substr($res4["nome_colt"],0,40),1);
+		// fine inserisco superficie, coltura	  
 	  	  
-      $pdf->Cell(69,6,$row['artico'].' - '.$row['desart'], 1, 0, 'l', 0, '', 1);
-	  If ($row['classif_amb']==0){$pdf->Cell(6,6,"Nc",1);}
-	  If ($row['classif_amb']==1){$pdf->Cell(6,6,"Xi",1);}
-	  If ($row['classif_amb']==2){$pdf->Cell(6,6,"Xn",1);}
-	  If ($row['classif_amb']==3){$pdf->Cell(6,6,"T",1);}
-	  If ($row['classif_amb']==4){$pdf->Cell(6,6,"T+",1);}
-	  If ($row['classif_amb']==5){$pdf->Cell(6,6,"Pa",1);}
-	  $pdf->Cell(8,6,$row['unimis'],1,0,'C');
-      $pdf->Cell(12,6,gaz_format_quantity($row["quanti"],1,$admin_aziend['decimal_quantity']),1);
-      $res3 = gaz_dbi_get_row($gTables['camp_avversita'], 'id_avv', $row['id_avversita']);
-      $pdf->Cell(30,6,$res3['nome_avv'],1, 0, 'l', 0, '', 1);
-/* Antonio Germani - trasformo nome utente login in cognome e nome e lo stampo */	  
-$res2 = gaz_dbi_dyn_query ('*', $gTables['admin']);	
- $colonna="0";
-	while($c_row = $res2->fetch_assoc()) { 
-	if ($row['adminid']==$c_row["user_name"]) { 
-	$pdf->Cell(18,6,$c_row['user_lastname']." ".$c_row['user_firstname'],1, 1, 'l', 0, '', 1);
-	 $colonna="1";
-		} 
-	}
-	 if ($colonna<1) {
-		$pdf->Cell(18,6,"",1, 1, 'l', 0, '', 1);
-	} 
-/* Antonio Germani FINE trasformo nome utente login in cognome e nome */	  
+		$pdf->Cell(69,6,$row['artico'].' - '.$row['desart'], 1, 0, 'l', 0, '', 1);
+		If ($row['classif_amb']==0){$pdf->Cell(6,6,"Nc",1);}
+		If ($row['classif_amb']==1){$pdf->Cell(6,6,"Xi",1);}
+		If ($row['classif_amb']==2){$pdf->Cell(6,6,"Xn",1);}
+		If ($row['classif_amb']==3){$pdf->Cell(6,6,"T",1);}
+		If ($row['classif_amb']==4){$pdf->Cell(6,6,"T+",1);}
+		If ($row['classif_amb']==5){$pdf->Cell(6,6,"Pa",1);}
+		$pdf->Cell(8,6,$row['unimis'],1,0,'C');
+		$pdf->Cell(12,6,gaz_format_quantity($row["quanti"],1,$admin_aziend['decimal_quantity']),1);
+		$res3 = gaz_dbi_get_row($gTables['camp_avversita'], 'id_avv', $row['id_avversita']);
+		$pdf->Cell(30,6,$res3['nome_avv'],1, 0, 'l', 0, '', 1);
+		
+		/* Antonio Germani - trasformo nome utente login in cognome e nome e lo stampo */	  
+		$res2 = gaz_dbi_get_row ($gTables['admin'], 'user_name', $row['adminid'] );	
+		$pdf->Cell(18,6,$res2['user_lastname']." ".$res2['user_firstname'],1, 1, 'l', 0, '', 1);
+		$colonna="1";
+		/* Antonio Germani FINE trasformo nome utente login in cognome e nome */	  
       
-	  } 
+	} 
   }
 }
 $pdf->Output();
