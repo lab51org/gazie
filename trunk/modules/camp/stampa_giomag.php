b52b2f5cf073dad8540b0be814cf68cdb1c53091
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-09-27 07:37:22 +0000

    Stampa quaderno di campagna: se non c'è il campo di coltivazione, adesso viene inserito un trattino al posto dello zero.

--- trunk/modules/camp/stampa_giomag.php
+++ trunk/modules/camp/stampa_giomag.php
@@ -104,6 +104,9 @@ if (sizeof($result) > 0) {
 	while (list($key, $row) = each($result)) {
 		if ($row['mostra_qdc']==1){ //escludi se è stato selezionato di non mostrare nel Q.d.c.
 			$res = gaz_dbi_get_row ($gTables['campi'], 'codice', $row['campo_coltivazione']);// Antonio Germani carico il campo
+			if (!isset($res)){
+				$row['campo_coltivazione']="-";
+			}
 			$datadoc = substr($row['datdoc'],8,2).'-'.substr($row['datdoc'],5,2).'-'.substr($row['datdoc'],0,4);
 			$datareg = substr($row['datreg'],8,2).'-'.substr($row['datreg'],5,2).'-'.substr($row['datreg'],0,4);
 			$movQuanti = $row['quanti']*$row['operat'];
