823e01b8a274047e745d3c6bf0a01b77a1d3b813
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-10-14 11:20:57 +0000

    Recepite modifiche alla struttura del database e del menù per accogliere la gestione del SIAN del registro di campagna.Alcuni indici sulle colonne della tabella gaz_001company_data per renderla più performante in caso di utilizzo intensivo da parte di moduli personalizzati.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3326 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/menu.creation_data.php
+++ modules/camp/menu.creation_data.php
@@ -61,7 +61,7 @@ $menu_data = array( 'm1'=>array('link'=>"docume_camp.php"),
                                                     ),
 											7=>array(
                                                     array('translate_key'=>17,'link'=>"rec_stocc.php",'weight'=>1),
-													array('translate_key'=>18,'link'=>"stabilim.php",'weight'=>2)
+													array('translate_key'=>18,'link'=>"stabilim.php",'weight'=>5)
                                                     )						
                                             )
                                )
