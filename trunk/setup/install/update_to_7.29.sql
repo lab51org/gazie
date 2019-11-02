c5ac4d1716ed488a77830dc30ecd7b8cccf60722
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-11-02 12:01:34 +0000

    Recepita nuova struttura database per SIAN (Antonio Germani)

--- trunk/setup/install/update_to_7.29.sql
+++ trunk/setup/install/update_to_7.29.sql
@@ -2,33 +2,21 @@ UPDATE `gaz_config` SET `cvalue` = '124' WHERE `id` =2;
 ALTER TABLE `gaz_anagra` ADD `id_SIAN` INT(10) NULL DEFAULT NULL COMMENT 'identificativo stabilimento assegnato dal SIAN' AFTER `fatt_email`;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 CREATE TABLE IF NOT EXISTS `gaz_XXXcamp_recip_stocc` (
-  `cod_silos` varchar(10) CHARACTER SET utf8 NOT NULL COMMENT 'Codice recipiente di stoccaggio olio. Deve essere identico a quello registrato al SIAN',
+  `cod_silos` varchar(10) NOT NULL COMMENT 'Codice recipiente di stoccaggio olio. Deve essere identico a quello registrato al SIAN',
   `capacita` decimal(13,3) NOT NULL COMMENT 'La capacità in kg di olio del recipiente',
   `affitto` int(1) NOT NULL COMMENT '0=di proprietà 1=in affitto',
-  `dop_igp` int(1) NOT NULL COMMENT '0=non classificato 1=Recipiente destinato a olio DOP o IGP'
+  `dop_igp` int(1) NOT NULL COMMENT '0=non classificato 1=Recipiente destinato a olio DOP o IGP',
+  PRIMARY KEY (`cod_silos`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `gaz_XXXcamp_mov_sian` (
-  `or_macro_prima` int(2) NOT NULL COMMENT 'Codice origine macroarea',
-  `or_macro_dopo` int(2) NOT NULL COMMENT 'Codice origine macroarea a fine operazione',
-  `or_spec_prima` varchar(80) CHARACTER SET utf8 NOT NULL COMMENT 'Origine specifica',
-  `or_spec_dopo` varchar(80) CHARACTER SET utf8 NOT NULL COMMENT 'Origine specifica a fine operazione',
+  `id_mov_sian` int(9) NOT NULL AUTO_INCREMENT,
+  `id_mov_sian_rif` int(9) NOT NULL COMMENT 'Serve in caso di produzione per connettere insieme il prodotto con i suoi componenti',
   `id_movmag` int(9) NOT NULL COMMENT 'Movimento magazzino connesso',
-  `sprem_freddo_prima` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag prima spremitura a freddo',
-  `sprem_freddo_dopo` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag prima spremitura a freddo a fine operazione',
-  `estr_freddo_prima` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag estratto a freddo',
-  `estr_feddo_dopo` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag estratto a freddo a fine operazione',
-  `bio_prima` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag da agricoltura biologica',
-  `bio_dopo` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag da agricoltura biologica a fine operazione',
-  `conver_prima` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag in conversione',
-  `conver_dopo` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag in conversione a fine operazione',
-  `etichett_prima` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag non etichettato',
-  `etichett_dopo` varchar(1) CHARACTER SET utf8 NOT NULL COMMENT 'Flag non etichettato a fine operazione',
-  `capac_conf` decimal(13,3) NOT NULL COMMENT 'Capacità in litri confezione',
-  `categ_olio` int(2) NOT NULL COMMENT 'Codice categoria olio',
-  `categ_olio_dopo` int(2) NOT NULL COMMENT 'Codice categoria olio a fine operazione',
-  `recip_stocc` varchar(10) CHARACTER SET utf8 NOT NULL COMMENT 'Identificativo recipiente di stoccaggio. Deve essere identico a quello inserito nel SIAN',
-  `cod_operazione` varchar(10) CHARACTER SET utf8 NOT NULL COMMENT 'Codice dell''operazione esguita',
-  `stabil_dep` int(10) NOT NULL COMMENT 'Identificativo dello stabilimento o deposito assegnato dal SIAN'
+  `recip_stocc_destin` varchar(10) NOT NULL COMMENT 'ID recipiente stoccaggio di destinazione',
+  `recip_stocc` varchar(10) NOT NULL COMMENT 'Identificativo recipiente di stoccaggio. Deve essere identico a quello inserito nel SIAN',
+  `cod_operazione` varchar(10) NOT NULL COMMENT 'Codice dell''operazione esguita',
+  `stabil_dep` int(10) NOT NULL COMMENT 'Identificativo dello stabilimento o deposito assegnato dal SIAN',
+  PRIMARY KEY (`id_mov_sian`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 ALTER TABLE `gaz_XXXcampi`
 	ADD COLUMN `indirizzo` VARCHAR(50) NULL AFTER `image`,
@@ -38,14 +26,16 @@ ALTER TABLE `gaz_XXXcampi`
 ALTER TABLE `gaz_XXXartico`
 	ADD COLUMN `SIAN` INT(1) NOT NULL DEFAULT '0' COMMENT '0 non movimenta, 1 movimenta il SIAN come olio, 2 movimenta il SIAN come olive' AFTER `tempo_sospensione`;
 CREATE TABLE IF NOT EXISTS `gaz_XXXcamp_artico` (
+  `id_campartico` int(9) NOT NULL AUTO_INCREMENT,
   `codice` varchar(15) NOT NULL COMMENT 'Codice articolo uguale alla tabella artico',
   `or_macro` int(2) NOT NULL COMMENT 'macroarea di origine',
   `or_spec` varchar(80) NOT NULL COMMENT 'origine specifica',
   `estrazione` int(1) NOT NULL COMMENT 'Flag estrazione: 0=non specificata 1=prima spremitura a freddo 2=estratto a freddo',
   `biologico` int(1) NOT NULL COMMENT 'Flag agricoltura: 0=convenzionale 1=biologica 2=in conversione',
   `etichetta` int(1) NOT NULL COMMENT 'Flag etichettatura: 0=non etichettato 1=etichettato',
-  `confezione` decimal(9,3) NOT NULL COMMENT 'Capacità singola confezione. 0=sfuso',
-  `categoria` int(2) NOT NULL COMMENT 'Categoria olio come codificato dal SIAN'
+  `confezione` decimal(9,3) NOT NULL COMMENT 'CapacitĂ  singola confezione. 0=sfuso',
+  `categoria` int(2) NOT NULL COMMENT 'Categoria olio come codificato dal SIAN',
+  PRIMARY KEY (`id_campartico`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 INSERT INTO `gaz_XXXcompany_config` SELECT MAX(id)+1, 'Identificativo dello stabilimento o deposito del SIAN', 'id_sian', '0' FROM `gaz_XXXcompany_config`;
 ALTER TABLE `gaz_XXXsyncronize_oc`	CHANGE COLUMN `date_update` `date_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `date_created`;
