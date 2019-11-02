c3bb06fac03ae7c5eeb54d422681bc653104180d
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-11-02 06:39:32 +0000

    File SIAN: nella situazione silos adesso si visualizza la quantità contenuta. Inserita anche una barra grafica come indicatore.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3375 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/create_sian.php
+++ modules/camp/create_sian.php
@@ -377,7 +377,7 @@ $namefile=substr($namefile,0,-4)
 				<div class="form-group">
 					<label for="cod_silos" class="col-sm-4 control-label"><?php echo "Accedi al portale dell'olio del SIAN: "; ?></label>
 					<p><a  class="btn btn-info btn-md" href="javascript:;" onclick="window.open('<?php echo"https://www.sian.it/SSLicqrfportaleolio/start.do";?>', 'titolo', 'menubar=no, toolbar=no, width=800, height=400, left=80%, top=80%, resizable, status, scrollbars=1, location');">
-					<img src="../../modules/camp/logo_sian.jpg" alt="Logo portale SIAN" title="Vai al portale dell'olio del SIAN" style="max-width:100%">
+					<img src="../../modules/camp/media/logo_sian.jpg" alt="Logo portale SIAN" title="Vai al portale dell'olio del SIAN" style="max-width:100%">
 					</a></p>
 				</div>
 			</div>
