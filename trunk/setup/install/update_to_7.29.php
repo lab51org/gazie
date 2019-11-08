6d964e9afbf74c9a9f6d81e66e71be21bf0dacc1
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-11-08 09:41:42 +0000

    Aggiornamento di versione: se mancano creo molti indici su diverse tabelle, alcuni proprio nuovi altri frutto di un errore (passato) nella generazione di nuove aziende. In più copio le icone/favicon dalla cartella di default a quella relativa ad ogni  azienda. Per sostituire il maialino con quelle aziendali le si potranno rigenerare semplicemente rifacendo l'upload del logo aziendale dalla configurazione azienda

--- trunk/setup/install/update_to_7.29.php
+++ trunk/setup/install/update_to_7.29.php
@@ -22,24 +22,51 @@ if ($camp_mod){
 $result = gaz_dbi_dyn_query("*", $table_prefix.'_aziend', 1);
 while ($row = gaz_dbi_fetch_array($result)) {
 	$aziend_codice = sprintf("%03s", $row["codice"]);
-	// inizio controlli presenza di indici altrimenti li creo perché senza di essi le query ricorsive sarebbero troppo lente in caso di tabelle con molti righi
-	$idx=array(0=>array('ref'=>'company_data','var'=>'company_data','description'=>'company_data','tipdoc'=>'tesdoc','seziva'=>'tesdoc','protoc'=>'tesdoc')); // indicizzo, in particolare le colonne per il report delle fatture
+	// inizio controlli presenza di indici, ne ricontrollo molti in quanto in passato un bug in fase di creazione nuova azienda li perdevacompromettendo gravemente la performance di molte query 
+	$idx=array(0=>array('ref'=>'company_data','var'=>'company_data','description'=>'company_data','tipdoc(1)'=>'tesdoc','sezivaXXX1'=>'tesdoc','protoc'=>'tesdoc','id_agenteXXX1'=>'agenti','id_agenteXXX2'=>'agenti_forn','id_fornitore'=>'agenti_forn','codiceXXX1'=>'aliiva','codiceXXX2'=>'caucon','caucon_cod'=>'caucon_rows','codiceXXX3'=>'caumag','codiceXXX4'=>'clfoco','codiceXXX5'=>'imball','id_rigbro'=>'orderman','codiceXXX6'=>'pagame','codiceXXX7'=>'portos','codiceXXX8'=>'spediz','id_staff'=>'staff_worked_hours','sezivaXXX2'=>'tesbro','datemi'=>'tesbro')); 
 	foreach($idx as $vi){
 		foreach($vi as $k=>$v){
+			$matches=false;
+			$retK=preg_match('/([a-zA-Z_]+)XXX[0-9]{1,2}/',$k,$matches); // sulle colonne con lo stesso nome dovrò accodare (e poi strippare) 'XXXN' per evitare di la ridefinizione della stessa 
+			$k=($matches)?$matches[1]:$k;
 			$rk=gaz_dbi_query("SHOW KEYS FROM ". $table_prefix . "_" . $aziend_codice.$v." WHERE 1");
 			$ex=false;	
 			while ($vk = gaz_dbi_fetch_array($rk)) {
-				if ($vk['Column_name']==$k&&$k!='tipdoc'){
+				if ($vk['Column_name']==$k){
 					$ex=true;
 				}
 			}
 			if (!$ex){
-				$lk=($k=='tipdoc')?'(1)':'';
-				gaz_dbi_query("ALTER TABLE ". $table_prefix . "_" . $aziend_codice.$v." ADD INDEX `idx_".$k.$lk."` (`".$k."`".$lk.") USING BTREE");		
-				echo "<p>Ho creato l'index su <b>".$k."</b> della tabella ". $table_prefix . "_" . $aziend_codice.$v." perché non esisteva</p>";
+				$matches = null;
+				$retV=preg_match('/([a-zA-Z_]+)\\(([0-9]+)\\)/',$k,$matches); // quando definisco un indice di lunghezza inferiore a quella della colonna stessa
+				$nam_idx=$k;
+				$nam_col=$k;
+				$idx_len='';
+				if ($matches){
+				  $nam_idx=$matches[1].$matches[2];
+				  $nam_col=$matches[1];
+				  $idx_len='('.$matches[2].')';
+				}
+				gaz_dbi_query("ALTER TABLE ". $table_prefix . "_" . $aziend_codice.$v." ADD INDEX `idx_".$nam_idx."` (`".$nam_col."`".$idx_len.") USING BTREE");					
+				echo "<p>Ho creato l'index '".$nam_idx."' su <b>".$nam_col."</b> della tabella ". $table_prefix . "_" . $aziend_codice.$v." perché non esisteva</p>";
 			}
 		}
 	}
 	// fine controlli - creazioni indici mancanti
+	
+	// sposto il set delle icone di default su ogni azienda della installazione
+	$dst="../../data/files/" . $row['codice'];
+	$src="../../library/images/default";
+	$dir = opendir($src);
+	while(false !== ( $file = readdir($dir)) ) { 
+		if (( $file != '.' ) && ( $file != '..' )) { 
+			if ( is_dir($src . '/' . $file) ) { 
+				recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+			} else { 
+				copy($src . '/' . $file,$dst . '/' . $file); 
+			} 
+		} 
+	} 
+	closedir($dir); 
 }
 ?>
