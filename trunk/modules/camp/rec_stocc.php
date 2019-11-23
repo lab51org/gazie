45f37ab37a560ac2f34b5cf0bceb422968ec9d0c
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-11-23 05:39:44 +0000

    File SIAN migliorata e corretta la funzione di calcolo giacenze per singolo recipiente/silos

--- trunk/modules/camp/rec_stocc.php
+++ trunk/modules/camp/rec_stocc.php
@@ -59,20 +59,19 @@ function getCont($codsil){
 	$passo=2000000;
 	$where="recip_stocc = '".$codsil."'";
 	$what=	$gTables['movmag'].".operat, ".$gTables['movmag'].".quanti, ".$gTables['movmag'].".id_orderman, ".
-			$gTables['camp_mov_sian'].".* ";
+			$gTables['camp_mov_sian'].".*, ".$gTables['camp_artico'].".confezione ";
 	$groupby= "";
-	$table=$gTables['camp_mov_sian']." LEFT JOIN ".$gTables['movmag']." ON (".$gTables['movmag'].".id_mov = ".$gTables['camp_mov_sian'].".id_movmag)";
+	$table=$gTables['camp_mov_sian']." LEFT JOIN ".$gTables['movmag']." ON ".$gTables['movmag'].".id_mov = ".$gTables['camp_mov_sian'].".id_movmag
+										LEFT JOIN ".$gTables['camp_artico']." ON ".$gTables['camp_artico'].".codice = ".$gTables['movmag'].".artico
+	";
 	$ressilos=gaz_dbi_dyn_query ($what,$table,$where,$orderby,$limit,$passo,$groupby);
-	
 	while ($r = gaz_dbi_fetch_array($ressilos)) {
-		if ($r['id_orderman']>0){
-			if ($r['operat']==-1){
-				$content=$content-$r['quanti'];
-			}
-		} else {
+		if ($r['confezione']==0){
 			$content=$content+($r['quanti']*$r['operat']);
-		}
+		} 
 	}
+	$content=number_format ($content,3);
+	
 	return $content ;
 }
 
