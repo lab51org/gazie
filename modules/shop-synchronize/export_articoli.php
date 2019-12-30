030b70ea37d3d693dd8145f2dbedaa0c0e1f0930
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2019-12-30 08:40:40 +0000

    Shop-synchronize: completato l'ampliamento. Adesso è possibile importare articoli dall'e-commerce a GAzie! Poi, da GAzie è possibile aggiornare i prodotti dell'e-commerce non solo nella quantità disponibile ma anche nella descrizione e nel prezzo (la descrizione andrebbe migliorata usando quella estesa con l'editor e, inoltre, in GAzie andrebbe anche inserita una immagine di migliore qualità; ma questo implica una modifica a GAzie stesso... vedremo con Antonio).
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3471 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/shop-synchronize/export_articoli.php
@@ -0,0 +1,337 @@
+<?php
+/* ------------------------------------------------------------------------
+  INTERFACCIA esporta articoli da GAzie a e-commerce
+  ------------------------------------------------------------------------
+  @Author    Antonio Germani
+  @Website   http://www.lacasettabio.it
+  @Copyright Copyright (C) 2018 - 2019 Antonio Germani All Rights Reserved.
+  versione 1.0
+  ------------------------------------------------------------------------ */
+require("../../library/include/datlib.inc.php");
+require ("../../modules/magazz/lib.function.php");
+$admin_aziend = checkAdmin();
+$gForm = new magazzForm;
+$resserver = gaz_dbi_get_row($gTables['company_config'], "var", "server");
+$ftp_host= $resserver['val'];
+$resuser = gaz_dbi_get_row($gTables['company_config'], "var", "user");
+$ftp_user = $resuser['val'];
+$respass = gaz_dbi_get_row($gTables['company_config'], "var", "pass");
+$ftp_pass= $respass['val'];
+$test = gaz_dbi_query("SHOW COLUMNS FROM `" . $gTables['admin'] . "` LIKE 'enterprise_id'");
+$exists = (gaz_dbi_num_rows($test)) ? TRUE : FALSE;
+if ($exists) {
+    $c_e = 'enterprise_id';
+} else {
+    $c_e = 'company_id';
+}
+$admin_aziend = gaz_dbi_get_row($gTables['admin'] . ' LEFT JOIN ' . $gTables['aziend'] . ' ON ' . $gTables['admin'] . '.' . $c_e . '= ' . $gTables['aziend'] . '.codice', "user_name", $_SESSION["user_name"]);
+$path = gaz_dbi_get_row($gTables['company_config'], 'var', 'path');
+$urlinterf = $path['val']."articoli-gazie.php";// nome del file interfaccia presente nella root dell'e-commerce. Per evitare intrusioni indesiderate Il file dovrà gestire anche una password. Per comodità viene usata la stessa FTP.
+// il percorso per raggiungere questo file va impostato in configurazione avanzata azienda alla voce "Website root directory
+
+//ob_flush();
+//flush();
+//ob_start();
+
+if (!isset($_POST['ritorno'])) {
+    $_POST['ritorno'] = $_SERVER['HTTP_REFERER'];
+}
+if (isset($_POST['Return'])) { 
+        header("Location: " . $_POST['ritorno']);
+        exit;
+    }
+ 
+if (isset($_POST['conferma'])) { // se confermato
+	// imposto la connessione al server
+	$conn_id = ftp_connect($ftp_host);
+	
+	// effettuo login con user e pass
+	$mylogin = ftp_login($conn_id, $ftp_user, $ftp_pass);
+	
+	// controllo se la connessione è OK...
+	if ((!$conn_id) or (!$mylogin)){ 
+		
+		?>
+		<script>
+		alert("<?php echo "Errore: connessione FTP a " . $ftp_host . " non riuscita!"; ?>");
+		location.replace("<?php echo $_POST['ritorno']; ?>");
+		</script>
+		<?php
+	} else {
+		?>
+		<div class="alert alert-success text-center" >
+		<strong>ok</strong> Connessione FTP riuscita.
+		</div>
+		<?php
+	}
+		// creo il file xml
+	$xml_output = '<?xml version="1.0" encoding="ISO-8859-1"?>
+	<GAzieDocuments AppVersion="1" Creator="Antonio Germani 2018-2019" CreatorUrl="https://www.lacasettabio.it">';
+	$xml_output .= "\n<Products>\n";
+	for ($ord=0 ; $ord<=$_POST['num_products']; $ord++){// ciclo gli articoli e creo il file xml
+		if (isset($_POST['download'.$ord])){ // se selezionato	
+			if (intval($_POST['barcode'.$ord])==0) {
+				$_POST['barcode'.$ord]="NULL";
+			}
+			$xml_output .= "\t<Product>\n";
+			$xml_output .= "\t<Code>".$_POST['codice'.$ord]."</Code>\n";
+			$xml_output .= "\t<BarCode>".$_POST['barcode'.$ord]."</BarCode>\n";
+			if ($_GET['qta']!==""){
+				$xml_output .= "\t<AvailableQty>".$_POST['quanti'.$ord]."</AvailableQty>\n";
+			}
+			if ($_GET['prezzo']!=="" AND $_POST['web_price'.$ord]>0){
+				$xml_output .= "\t<WebPrice>".$_POST['web_price'.$ord]."</WebPrice>\n";
+			}
+			if ($_GET['descri']!=="" AND strlen($_POST['descri'.$ord])>0){
+				$xml_output .= "\t<Description>".$_POST['descri'.$ord]."</Description>\n";
+			}
+			$xml_output .= "\t</Product>\n";
+		}
+	 }
+	$xml_output .="\n</Products>\n</GAzieDocuments>";
+	$xmlFile = "prodotti.xml";
+	$xmlHandle = fopen($xmlFile, "w");
+	fwrite($xmlHandle, $xml_output);
+	fclose($xmlHandle);
+	//turn passive mode on
+	ftp_pasv($conn_id, true);
+	// upload file xml
+	if (ftp_put($conn_id, "public_html/easyfatt/prodotti.xml", $xmlFile, FTP_ASCII)){
+		?>
+		<div class="alert alert-success text-center" >
+		<strong>ok</strong> il file xml è stato trasferito al sito web.
+		</div>
+		<?php
+	} else{
+		// chiudo la connessione FTP 
+		ftp_quit($conn_id);
+		header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=4");
+		exit;
+	}
+
+	$access=base64_encode($ftp_pass);
+
+	// avvio il file di interfaccia presente nel sito web remoto
+	$headers = @get_headers($urlinterf.'?access='.$access);
+	if ( intval(substr($headers[0], 9, 3))==200){ // controllo se il file esiste o mi dà accesso
+		$file = fopen ($urlinterf.'?access='.$access, "r");
+		if (!$file) {
+			// chiudo la connessione FTP 
+			ftp_quit($conn_id);
+			header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=3");
+			exit;
+			
+		} else {
+			// chiudo la connessione FTP 
+			ftp_quit($conn_id);
+			?>
+			<div class="alert alert-success text-center" >
+			<strong>ok</strong> Aggiornamento prodotti riuscito.
+			</div>
+			
+			<?php
+			
+		}
+	} else { // IL FILE INTERFACCIA NON ESISTE > ESCO
+		// chiudo la connessione FTP 
+		ftp_quit($conn_id);
+		header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=2");
+		exit;
+	}
+
+	// chiudo la connessione FTP 
+	ftp_quit($conn_id);
+	header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=1");
+    exit;
+} else {
+	require('../../library/include/header.php');
+	$script_transl = HeadMain();
+}
+
+if (!isset($_GET['success'])){	
+// Apro il form per la selezione degli articoli
+		?>
+		<script>
+    function selectCheckbox() {
+        var inputs = document.getElementsByTagName('input');
+        var checkboxes = [];
+        for (var i = 0; i < inputs.length; i++){
+            var input = inputs[i];
+            if (input.getAttribute('type') == 'checkbox'){
+                checkboxes.push(input);
+            }
+        } 
+        return checkboxes;
+    }    
+    function check(checks){
+      var checkboxes = selectCheckbox();
+      for(var i=0; i < checkboxes.length; i++){
+        checkboxes[i].checked = checks.checked;
+      }
+    }    
+</script>
+		<form method="POST" name="download" enctype="multipart/form-data">
+			<input type="hidden" name="ritorno" value="<?php echo $_POST['ritorno'];?>" >
+			<div class="container-fluid" style="max-width:90%;">
+				<div class="row bg-primary" >
+					<div class="col-sm-12" align="center"><h4>Esportazione di articoli da GAzie</h4>
+						<p align="justify">Gli articoli selezionati verranno aggiornati nell'e-commerce se esistenti, altrimenti verranno ignorati. </p>
+					</div>
+				</div>
+				<div class="row bg-info">
+					<div class="col-sm-4">
+						<input type="submit" name="Return"  value="Indietro">
+					</div>
+					<div class="col-sm-4" style="background-color:lightgreen;">
+						<?php echo "E-commerce sincronizzato: " . $ftp_host;?>
+					</div>
+					<div class="col-sm-4" align="right">
+						<!-- Trigger the modal with a button -->
+						<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#downloader">Aggiorna i prodotti nell'e-commerce</button>
+						<!-- Modal content-->
+						<div id="downloader" class="modal fade" role="dialog">    
+							<div class="modal-dialog modal-content">
+								<div class="modal-header" align="left">
+									<button type="button" class="close" data-dismiss="modal">&times;</button>
+									<h4 class="modal-title">ATTENZIONE!</h4>
+								</div>
+								<div class="modal-body">
+									<p>Stai per aggiornare definitivamente i prodotti nell'e-commerce. <br>Questa operazione &egrave irreversibile. <br>Sei sicuro di volerlo fare?</p>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Annulla</button>
+									<input type="submit" class="btn btn-danger pull-right" name="conferma"  value="Aggiorna l'e-commerce">
+								</div>
+							</div>
+						</div>
+					</div>						
+				</div>
+				<div class="row bg-info" style="border-bottom: 1px solid;">
+					<div class="col-sm-2">
+						<h4>Progressivo</h4>
+					</div>
+					<div class="col-sm-2">
+						<h4>Codice</h4>
+					</div>
+					<div class="col-sm-5">	
+						<h4>Nome</h4>
+					</div>
+					<div class="col-sm-2" align="right">	
+						<h4>Seleziona</h4>
+					</div>
+					<div class="col-sm-1">	
+						TUTTI <input type="checkbox" onClick="check(this)">
+					</div>
+				</div>
+				<?php
+				// carico gli articoli presenti in GAzie
+				$artico = gaz_dbi_query ('SELECT codice, barcode, web_price, descri FROM '.$gTables['artico'].' WHERE web_public = \'1\' and good_or_service <> \'1\' ORDER BY codice');
+				$n=0;
+				while ($item = gaz_dbi_fetch_array($artico)){ // li ciclo
+					$avqty = 0;
+					$ordinatic = $gForm->get_magazz_ordinati($item['codice'], "VOR");
+					$mv = $gForm->getStockValue(false, $item['codice']);
+					$magval = array_pop($mv);
+					$avqty=$magval['q_g']-$ordinatic;
+					if ($avqty<0 or $avqty==""){
+						$avqty="0";
+					}
+					?>
+					<div class="row bg-success" style="border-bottom: 1px solid;">
+							<div class="col-sm-2">
+								<?php echo $n;?>
+							</div>
+							<div class="col-sm-2">
+								<?php echo $item['codice'];
+								echo '<input type="hidden" name="codice'. $n .'" value="'. $item['codice'] . '">';
+								?>
+							</div>
+							<div class="col-sm-6">
+								<?php echo $item['descri'];
+								echo '<input type="hidden" name="descri'. $n .'" value="'. $item['descri'] . '">';
+								?>
+							</div>
+							<div class="col-sm-1">
+								<?php //echo '<input type="hidden" name="body_text'. $n .'" value="'. preg_replace('/[\x00-\x1f]/','',htmlspecialchars($item['description'])) . '">';
+								echo '<input type="hidden" name="quanti'. $n .'" value="'. $avqty .'">';
+								echo '<input type="hidden" name="web_price'. $n .'" value="'. $item['web_price'] .'">';
+								echo '<input type="hidden" name="barcode'. $n .'" value="'. $item['barcode'] .'">';
+								//echo '<input type="hidden" name="imgurl'. $n .'" value="'. $item['???'] .'">';
+								?>
+							</div>
+							<div class="col-sm-1" align="right">
+								<input type="checkbox" name="download<?php echo $n; ?>" value="download">
+								<input type="hidden" name="num_products" value="<?php echo $n; ?>">
+							</div>
+			
+					</div>
+				<?php
+				$n++;
+				}
+				?>
+				<div class="row bg-info">
+					<div class="col-sm-4">
+						<input type="submit" name="Return"  value="Indietro">
+					</div>
+					<div class="col-sm-4" style="background-color:lightgreen;">
+						<?php echo "E-commerce sincronizzato: " . $ftp_host;?>
+					</div>
+					<div class="col-sm-4" align="right">
+						<!-- Trigger the modal with a button -->
+						<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#downloader">Aggiorna i prodotti nell'e-commerce</button>
+						<!-- Modal content-->
+						<div id="downloader" class="modal fade" role="dialog">    
+							<div class="modal-dialog modal-content">
+								<div class="modal-header" align="left">
+									<button type="button" class="close" data-dismiss="modal">&times;</button>
+									<h4 class="modal-title">ATTENZIONE!</h4>
+								</div>
+								<div class="modal-body">
+									<p>Stai per aggiornare definitivamente i prodotti nell'e-commerce. <br>Questa operazione &egrave irreversibile. <br>Sei sicuro di volerlo fare?</p>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Annulla</button>
+									<input type="submit" class="btn btn-danger pull-right" name="conferma"  value="Aggiorna l'e-commerce">
+								</div>
+							</div>
+						</div>
+					</div>						
+				</div>
+				
+				
+			</div> <!-- container fluid -->
+		</form>
+
+<?php
+} elseif ($_GET['success']==1){
+	?>
+	<div class="alert alert-success alert-dismissible">
+		<a href="../../modules/shop-synchronize/synchronize.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		<strong>Fatto!</strong> Operazione conclusa con successo.
+	</div>
+<?php
+} elseif ($_GET['success']==2){
+	?>
+	<div class="alert alert-danger alert-dismissible">
+		<a href="../../modules/shop-synchronize/synchronize.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		<strong>ERRORE!</strong> Manca il file di interfaccia nell'e-commerce!.
+	</div>
+<?php
+} elseif ($_GET['success']==3){
+	?>
+	<div class="alert alert-danger alert-dismissible">
+		<a href="../../modules/shop-synchronize/synchronize.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		<strong>ERRORE!</strong> Il file di interfaccia nell'e-commerce non parte!.
+	</div>
+<?php
+} elseif ($_GET['success']==4){
+	?>
+	<div class="alert alert-danger alert-dismissible">
+		<a href="../../modules/shop-synchronize/synchronize.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		<strong>ERRORE!</strong> L'upload del file xml non è riuscito!.
+	</div>
+<?php
+}
+require("../../library/include/footer.php");
+?>
+                            
---
+++ modules/shop-synchronize/import_articoli.php
@@ -0,0 +1,306 @@
+<?php
+/* ------------------------------------------------------------------------
+  INTERFACCIA Download articoli da online-shop a GAzie
+  ------------------------------------------------------------------------
+  @Author    Antonio Germani 340-5011912
+  @Website   http://www.lacasettabio.it
+  @Copyright Copyright (C) 2018 - 2019 Antonio Germani All Rights Reserved.
+  versione 1.0
+  ------------------------------------------------------------------------ */
+require("../../library/include/datlib.inc.php");
+?>
+
+<?php
+$admin_aziend = checkAdmin();
+$resserver = gaz_dbi_get_row($gTables['company_config'], "var", "server");
+$ftp_host= $resserver['val'];
+$resuser = gaz_dbi_get_row($gTables['company_config'], "var", "user");
+$ftp_user = $resuser['val'];
+$respass = gaz_dbi_get_row($gTables['company_config'], "var", "pass");
+$ftp_pass= $respass['val'];
+$path = gaz_dbi_get_row($gTables['company_config'], 'var', 'path');
+$urlinterf = $path['val']."dwnlArticoli-gazie.php";//nome del file interfaccia presente nella root del sito Joomla. Per evitare intrusioni indesiderate Il file dovrà gestire anche una password. Per comodità viene usata la stessa FTP.
+// il percorso per raggiungere questo file va impostato in configurazione avanzata azienda alla voce "Website root directory"
+$test = gaz_dbi_query("SHOW COLUMNS FROM `" . $gTables['admin'] . "` LIKE 'enterprise_id'");
+$exists = (gaz_dbi_num_rows($test)) ? TRUE : FALSE;
+
+?><script>
+    function selectCheckbox() {
+        var inputs = document.getElementsByTagName('input');
+        var checkboxes = [];
+        for (var i = 0; i < inputs.length; i++){
+            var input = inputs[i];
+            if (input.getAttribute('type') == 'checkbox'){
+                checkboxes.push(input);
+            }
+        } 
+        return checkboxes;
+    }    
+    function check(checks){
+      var checkboxes = selectCheckbox();
+      for(var i=0; i < checkboxes.length; i++){
+        checkboxes[i].checked = checks.checked;
+      }
+    }    
+    function submit() {
+        var checks = document.getElementsByClassName('check');
+        var str = '';
+        for ( i = 0; i < checks.length; i++) {
+            if ( checks[i].checked === true ) {
+                str += checks[i].value + " ";
+            }
+        }
+        alert(str);
+    }
+</script>
+
+<?php
+if (!isset($_POST['ritorno'])) {
+    $_POST['ritorno'] = $_SERVER['HTTP_REFERER'];
+}
+if (isset($_POST['Return'])) { 
+        header("Location: " . $_POST['ritorno']);
+        exit;
+    }
+
+if ($exists) {
+    $c_e = 'enterprise_id';
+} else {
+    $c_e = 'company_id';
+}
+$admin_aziend = gaz_dbi_get_row($gTables['admin'] . ' LEFT JOIN ' . $gTables['aziend'] . ' ON ' . $gTables['admin'] . '.' . $c_e . '= ' . $gTables['aziend'] . '.codice', "user_name", $_SESSION["user_name"]);
+	
+if (isset($_POST['conferma'])) { // se confermato
+
+    // scrittura articoli su database di GAzie
+	for ($ord=0 ; $ord<=$_POST['num_products']; $ord++){// ciclo gli articoli e scrivo i database
+		if (isset($_POST['download'.$ord])){ // se selezionato
+			$vat = gaz_dbi_get_row($gTables['aliiva'], "aliquo", $_POST['aliquo'.$ord]); // prendo il codice IVA
+			$url = $_POST['imgurl'.$ord];
+			$expl= explode ("/", $_POST['imgurl'.$ord]);
+			$img = '../../data/files/tmp/'.$expl[count($expl)-1]; 
+			// scrivo l'immagine nella cartella temporanea
+			file_put_contents($img, file_get_contents($url));
+			// ridimensiono l'immagine per rientrare nei 64k
+				$maxDim = 190;				
+				list($width, $height, $type, $attr) = getimagesize( $img );
+				if ( $width > $maxDim || $height > $maxDim ) {
+					$target_filename = $img;
+					$ratio = $width/$height;
+					if( $ratio > 1) {
+						$new_width = $maxDim;
+						$new_height = $maxDim/$ratio;
+					} else {
+							$new_width = $maxDim*$ratio;
+							$new_height = $maxDim;
+					}
+					$src = imagecreatefromstring( file_get_contents( $img ) );
+					$dst = imagecreatetruecolor( $new_width, $new_height );
+					imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+					imagedestroy( $src );
+					imagepng( $dst, $target_filename); // adjust format as needed
+					imagedestroy( $dst );
+				} 
+				//Carico l'immagine
+				$immagine= addslashes (file_get_contents($target_filename));
+			
+			$esiste = gaz_dbi_get_row($gTables['artico'], "codice", $_POST['codice'.$ord]);
+			if ($esiste){ // se esiste aggiorno articolo
+				gaz_dbi_query("UPDATE ". $gTables['artico'] . " SET descri = '".addslashes($_POST['descri'.$ord])."', web_price = '".addslashes($_POST['web_price'.$ord])."' , image = '".$immagine."' WHERE codice = '".addslashes($_POST['codice'.$ord])."'");
+			} else { // altrimenti inserisco nuovo articolo
+				gaz_dbi_query("INSERT INTO " . $gTables['artico'] . "(codice,descri,web_price,unimis,image,web_public,depli_public,aliiva) VALUES ('" . addslashes($_POST['codice'.$ord]) . "', '" . addslashes($_POST['descri'.$ord]) . "', '". addslashes($_POST['web_price'.$ord]). "', '".$_POST['unimis'.$ord]."', '".$immagine."', '1', '1', '".$vat['codice']."')");
+			}
+			
+			unlink ($img);// cancello l'immagine della cartella temporanea
+		}
+	}
+		
+	header("Location: " . "../../modules/shop-synchronize/import_articoli.php?success=1");
+    exit;
+} else {
+	require('../../library/include/header.php');
+	$script_transl = HeadMain();
+}
+ 
+// imposto la connessione al server
+$conn_id = ftp_connect($ftp_host);
+
+// effettuo login con user e pass
+$mylogin = ftp_login($conn_id, $ftp_user, $ftp_pass);
+
+// controllo se la connessione è OK...
+if ((!$conn_id) or (!$mylogin))
+{ 
+	?>
+	<script>
+	alert("<?php echo "Errore: connessione FTP a " . $ftp_host . " non riuscita!"; ?>");
+	location.replace("<?php echo $_POST['ritorno']; ?>");
+    </script>
+	<?php
+}
+
+$access=base64_encode($ftp_pass);
+
+if (!isset($_GET['success'])){
+	// avvio il file di interfaccia presente nel sito web remoto
+	$headers = @get_headers($urlinterf.'?access='.$access);
+	if ( intval(substr($headers[0], 9, 3))==200){ // controllo se il file esiste o mi dà accesso
+		$xml=simplexml_load_file($urlinterf.'?access='.$access) ; // lo carico
+		if (!$xml){
+			?>
+			<script>
+			alert("<?php echo "Errore! Il file xml non è stato creato oppure non è possibile accedervi"; ?>");
+			location.replace("<?php echo $_POST['ritorno']; ?>");
+			</script>
+			<?php
+		}
+			
+		// Apro il form per la selezione degli articoli
+		?>
+		<form method="POST" name="download" enctype="multipart/form-data">
+			<input type="hidden" name="ritorno" value="<?php echo $_POST['ritorno'];?>" >
+			<input type="hidden" name="download" value="download" >
+			<div class="container-fluid" style="max-width:90%;">
+				<div class="row bg-primary" >
+					<div class="col-sm-12" align="center"><h4>Importazione di articoli dal negozio online</h4>
+						<p align="justify">Gli articoli selezionati verranno aggiornati o, se inesistenti, verranno creati. </p>
+					</div>
+				</div>
+				<div class="row bg-info">
+					<div class="col-sm-4">
+						<input type="submit" name="Return"  value="Indietro">
+					</div>
+					<div class="col-sm-4" style="background-color:lightgreen;">
+						<?php echo "Connesso a " . $ftp_host;?>
+					</div>
+					<div class="col-sm-4" align="right">
+						<!-- Trigger the modal with a button -->
+						<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#downloader">Carica prodotti in GAzie</button>
+						<!-- Modal content-->
+						<div id="downloader" class="modal fade" role="dialog">    
+							<div class="modal-dialog modal-content">
+								<div class="modal-header" align="left">
+									<button type="button" class="close" data-dismiss="modal">&times;</button>
+									<h4 class="modal-title">ATTENZIONE !</h4>
+								</div>
+								<div class="modal-body">
+									<p>Stai per scaricare definitivamente i prodotti in GAzie. <br>Questa operazione &egrave irreversibile. <br>Sei sicuro di volerlo fare?</p>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Annulla</button>
+									<input type="submit" class="btn btn-danger pull-right" name="conferma"  value="Carica prodotti in GAzie">
+								</div>
+							</div>
+						</div>
+					</div>						
+				</div>
+				<div class="row bg-info" style="border-bottom: 1px solid;">
+					<div class="col-sm-2">
+						<h4>Progressivo</h4>
+					</div>
+					<div class="col-sm-2">
+						<h4>Codice</h4>
+					</div>
+					<div class="col-sm-5">	
+						<h4>Nome</h4>
+					</div>
+					<div class="col-sm-2" align="right">	
+						<h4>Seleziona</h4>
+					</div>
+					<div class="col-sm-1">	
+						TUTTI <input type="checkbox" onClick="check(this)">
+					</div>
+				</div>
+					
+					<?php
+					$n=0;
+					foreach($xml->Documents->children() as $product) { 
+						$nr=0;
+						?>
+						<div class="row bg-success" style="border-bottom: 1px solid;">
+							<div class="col-sm-2">
+								<?php echo $n;?>
+							</div>
+							<div class="col-sm-3">
+								<?php echo $product->Code;
+								echo '<input type="hidden" name="codice'. $n .'" value="'. $product->Code . '">';
+								?>
+							</div>
+							<div class="col-sm-5">
+								<?php echo $product->Name;
+								echo '<input type="hidden" name="descri'. $n .'" value="'. preg_replace('/[\x00-\x1f]/','',htmlspecialchars($product->Name)) . '">';
+								?>
+							</div>
+							<div class="col-sm-1">
+								<?php echo '<input type="hidden" name="body_text'. $n .'" value="'. preg_replace('/[\x00-\x1f]/','',htmlspecialchars($product->Description)) . '">';
+								echo '<input type="hidden" name="quanti'. $n .'" value="'. $product->AvailableQty .'">';
+								echo '<input type="hidden" name="web_price'. $n .'" value="'. $product->Price .'">';
+								echo '<input type="hidden" name="unimis'. $n .'" value="'. $product->Unimis .'">';
+								echo '<input type="hidden" name="aliquo'. $n .'" value="'. $product->ProductVat .'">';
+								echo '<input type="hidden" name="imgurl'. $n .'" value="'. $product->ProductImgUrl .'">';
+								?>
+							</div>
+							<div class="col-sm-1" align="right">
+								<input type="checkbox" name="download<?php echo $n; ?>" value="download">
+								<input type="hidden" name="num_products" value="<?php echo $n; ?>">
+							</div>
+						</div>
+						<?php					
+						$n++;
+					} 
+					?>					
+					<div class="row bg-info">
+						<div class="col-sm-4">
+							<input type="submit" name="Return"  value="Indietro">
+						</div>
+						<div class="col-sm-4" style="background-color:lightgreen;">
+							<?php echo "Connesso a " . $ftp_host;?>
+						</div>
+						<div class="col-sm-4" align="right">
+							<!-- Trigger the modal with a button -->
+							<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#downloader">Carica prodotti in GAzie</button>
+							<!-- Modal content-->
+							<div id="downloader" class="modal fade" role="dialog">    
+								<div class="modal-dialog modal-content">
+									<div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal">&times;</button>
+										<h4 class="modal-title">ATTENZIONE !</h4>
+									</div>
+									<div class="modal-body">
+										<p>Stai per scaricare definitivamente i prodotti in GAzie. <br>Questa operazione &egrave irreversibile. <br>Sei sicuro di volerlo fare?</p>
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Annulla</button>
+										<input type="submit" class="btn btn-danger pull-right" name="conferma"  value="Carica prodotti in GAzie">
+									</div>
+								</div>
+							</div>
+						</div>						
+					</div>				
+			</div>
+		</form>
+		<?php
+	
+	} else { // IL FILE INTERFACCIA NON ESISTE > ESCO
+		ftp_quit($conn_id);
+		?>
+		<script>
+		alert("<?php echo "Errore di connessione al file di interfaccia web = ",intval(substr($headers[0], 9, 3)),"<br> Riprovare fra qualche minuto!"; ?>");
+		location.replace("<?php echo $_POST['ritorno']; ?>");
+		</script>
+		<?php
+		exit;
+	}
+} else {
+	?>
+	<div class="alert alert-success alert-dismissible">
+		<a href="../../modules/shop-synchronize/synchronize.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		<strong>Fatto!</strong> Operazione conclusa con successo.
+	</div>
+<?php
+}
+// chiudo la connessione FTP 
+ftp_quit($conn_id);
+require("../../library/include/footer.php");
+?>
+                            
