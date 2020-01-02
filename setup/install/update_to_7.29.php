16b2fad3257313c288a3df2f30b366e193c97d1c
angelo983 <angelo983@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-01-02 23:38:21 +0000

    Corretto errore che si verificava aggiornando alla 7.29 una installazione con prefix personalizzato
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3479 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.29.php
+++ setup/install/update_to_7.29.php
@@ -11,19 +11,19 @@ if (isset($_SESSION['table_prefix'])) {
 $camp_mod = gaz_dbi_get_row($table_prefix.'_module','name', 'camp');
 if ($camp_mod){
 	// aggiungo una voce al menù_module (2°livello)
-	gaz_dbi_query("INSERT INTO `gaz_menu_module` SELECT MAX(id)+1 , ".$camp_mod['id'].", 'sian.php', '', '', 7, '', 7  FROM `gaz_menu_module`");
+	gaz_dbi_query("INSERT INTO `". $table_prefix . "_menu_module` SELECT MAX(id)+1 , ".$camp_mod['id'].", 'sian.php', '', '', 7, '', 7  FROM `". $table_prefix . "_menu_module`");
 	// aggiungo le nuove voci di menù di 3° livello
-	gaz_dbi_query("INSERT INTO `gaz_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='sian.php'), 'rec_stocc.php', '', '', 17, '', 1  FROM `gaz_menu_script`");
-	gaz_dbi_query("INSERT INTO `gaz_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='sian.php'), 'admin_sian_files.php', '', '', 19, '', 5  FROM `gaz_menu_script`");
-	gaz_dbi_query("INSERT INTO `gaz_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='sian.php'), 'stabilim.php', '', '', 18, '', 10  FROM `gaz_menu_script`");
-	gaz_dbi_query("INSERT INTO `gaz_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='sian.php'), 'camp_anagra.php', '', '', 20, '', 15  FROM `gaz_menu_script`");
+	gaz_dbi_query("INSERT INTO `". $table_prefix . "_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `". $table_prefix . "_menu_module` WHERE `link`='sian.php'), 'rec_stocc.php', '', '', 17, '', 1  FROM `". $table_prefix . "_menu_script`");
+	gaz_dbi_query("INSERT INTO `". $table_prefix . "_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `". $table_prefix . "_menu_module` WHERE `link`='sian.php'), 'admin_sian_files.php', '', '', 19, '', 5  FROM `". $table_prefix . "_menu_script`");
+	gaz_dbi_query("INSERT INTO `". $table_prefix . "_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `". $table_prefix . "_menu_module` WHERE `link`='sian.php'), 'stabilim.php', '', '', 18, '', 10  FROM `". $table_prefix . "_menu_script`");
+	gaz_dbi_query("INSERT INTO `". $table_prefix . "_menu_script` SELECT MAX(id)+1 , (SELECT MIN(id) FROM `". $table_prefix . "_menu_module` WHERE `link`='sian.php'), 'camp_anagra.php', '', '', 20, '', 15  FROM `". $table_prefix . "_menu_script`");
 	echo "<p>Ho modificato il menù del modulo <b>Registro di campagna</b></p>";
 }
 
 $result = gaz_dbi_dyn_query("*", $table_prefix.'_aziend', 1);
 while ($row = gaz_dbi_fetch_array($result)) {
 	$aziend_codice = sprintf("%03s", $row["codice"]);
-	// inizio controlli presenza di indici, ne ricontrollo molti in quanto in passato un bug in fase di creazione nuova azienda li perdevacompromettendo gravemente la performance di molte query 
+	// inizio controlli presenza di indici, ne ricontrollo molti in quanto in passato un bug in fase di creazione nuova azienda li perdeva compromettendo gravemente la performance di molte query 
 	$idx=array(0=>array('ref'=>'company_data','var'=>'company_data','description'=>'company_data','tipdoc(1)'=>'tesdoc','sezivaXXX1'=>'tesdoc','protoc'=>'tesdoc','id_agenteXXX1'=>'agenti','id_agenteXXX2'=>'agenti_forn','id_fornitore'=>'agenti_forn','codiceXXX1'=>'aliiva','codiceXXX2'=>'caucon','caucon_cod'=>'caucon_rows','codiceXXX3'=>'caumag','codiceXXX4'=>'clfoco','codiceXXX5'=>'imball','id_rigbro'=>'orderman','codiceXXX6'=>'pagame','codiceXXX7'=>'portos','codiceXXX8'=>'spediz','id_staff'=>'staff_worked_hours','sezivaXXX2'=>'tesbro','datemi'=>'tesbro')); 
 	foreach($idx as $vi){
 		foreach($vi as $k=>$v){
