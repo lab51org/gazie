21997665d5b9ff71cb9f0f0fdee9dd21cde89368
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-01-04 18:52:03 +0000

    shop-synchronize: ampliate le selezioni su import e export articoli -> da competare
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3496 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/shop-synchronize/synchronize.php
+++ modules/shop-synchronize/synchronize.php
@@ -22,7 +22,6 @@
   Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
   --------------------------------------------------------------------------
 */
- 
 /*    >>> LEGGIMI <<<
 ------------------------------------------------------------------------------------------
 	** Antonio Germani - www.lacasettabio.it **
@@ -32,7 +31,7 @@
 	La cartella synchronize di GAzie potrà contenere tutti i file interfaccia che a mano a mano verranno crati dagli sviluppatori.
 	L'utente deve scegliere quali interfacce utilizzare (sulla base delle caratteristiche del suo negozio online) e scrivere i relativi nomi dei file nella sottostante "Impostazione".
 	-Caso download: L'interfaccia presente nella root del negozio online elabora i dati del database del negozio e crea un file xml. In GAzie, la seconda interfaccia elabora il file xml scrivendone i dati sul database di GAzie.
-	-Caso Upload: L'interfaccia di GAzie crea un file xml che viene letto dall'interfaccia presente nella root del negozio online. Con i dati presenti nel file xml viene scritto il data base di Joomla.
+	-Caso Upload: L'interfaccia di GAzie crea un file xml che viene letto dall'interfaccia presente nella root del negozio online. Con i dati presenti nel file xml viene scritto il data base dell'e-commerce.
 ------------------------------------------------------------------------------------------
 */
 require("../../library/include/datlib.inc.php");
@@ -83,7 +82,7 @@ if (isset ($_POST['download'])) {
 		if (!isset($_POST['scardescrizione'])){
 			$_POST['scardescrizione']="";
 		}
-		header("Location: " . $file_downloader);
+		header("Location: " . $file_downloader."?updpre=".$_POST['updpre']."&upddes=".$_POST['upddes']."&updimm=".$_POST['updimm']."&imppre=".$_POST['imppre']."&impdes=".$_POST['impdes']."&impimm=".$_POST['impimm']);
 		exit;
 	} else {
 		header("Location: " . $_POST['ritorno']);
@@ -106,50 +105,71 @@ if (isset ($_POST['download'])) {
 	<input type="hidden" name="ritorno" value="<?php echo $_POST['ritorno'];?>" >
 	<div class="container-fluid" style="max-width:90%;">
 		<div class="row bg-primary" >
-			<div class="col-sm-12" align="center"><h4>Sincronizzazione di GAzie con sito internet</h4>
+			<div class="col-sm-12" align="center"><h4>Sincronizzazione di GAzie con sito e-commerce</h4>
 				<p align="justify">Scarica ordini: importa ordini dal sito a GAzie</p>
 				<p align="justify">Aggiorna prodotti: aggiorna le quantità disponibili da Gazie al sito</p>
 			</div>
 		</div>
 		<div>
 			<div class="row bg-info">
-				<div class="col-sm-4 active bg-warning" align="center">
+				<div class="col-sm-4  bg-warning" align="center">
 					<input type="submit" class="btn btn-primary" name="Return"  onClick="chkSubmit();" value="Indietro">
 				</div>
-				<div class="col-sm-4 active bg-success" align="center">
+				<div class="col-sm-4  bg-success" align="center">
 					<input type="submit" class="btn btn-primary" name="download"  onClick="chkSubmit();" value="Scarica ordini">
 				</div>
-				<div class="col-sm-4 active bg-warning" align="center">
+				<div class="col-sm-4 bg-warning" align="center">
 					<input type="submit" class="btn btn-primary" name="upload"  onClick="chkSubmit();" value="Aggiorna q.t&agrave; prodotti">
 				</div>
 			</div>
 			<div class="row bg-info">
-				<div class="col-sm-12 active bg-info" align="center">
+				<div class="col-sm-12  bg-info" align="center">
 					<input type="button" name="button1" id="nextbt" rel="more" value="&#9660" onclick="buttonToggle(this,'&#9650','&#9660')">
 				</div>						
 			</div>
 			<div id="more">
-				<div class="row bg-info">
-					<div class="col-sm-6 active bg-warning" align="left" style="font-size: 18;">
-						<input type="checkbox" name="quantita" value="updqty" checked> quantit&agrave &nbsp 
-						<input type="checkbox" name="prezzo" value="updprice"> prezzo &nbsp
-						<input type="checkbox" name="descrizione" value="upddes" > descrizione &nbsp
-						<!-- <input type="checkbox" name="carimg" value="upddes" > immagine &nbsp -->
+				<div class="row bg-warning" style="border-top: 1px solid;">
+					<div class="col-sm-12 bg-warning" align="center" >
+							<h3 class="text-primary">ESPORTAZIONE</h3>
+						</div>
+					<div class="col-sm-6  bg-warning" align="left" style="font-size: 18;">
+						<input type="checkbox" name="quantita" value="updqty" checked> Quantit&agrave &nbsp 
+						<input type="checkbox" name="prezzo" value="updprice"> Prezzo &nbsp
+						<input type="checkbox" name="descrizione" value="upddes" > Descrizione estesa &nbsp
+						<!-- <input type="checkbox" name="carimg" value="updimg" > immagine &nbsp -->
 					</div>
-					<div class="col-sm-6 active bg-warning" align="left">
-						<input type="submit" class="btn btn-danger btn-sm pull-left" name="uploader"  value="Aggiorna prodotti nell'e-commerce">
-					</div>
+					
+						<div class="col-sm-12  bg-warning">
+							<input type="submit" class="btn btn-danger btn-sm pull-right" name="uploader"  value="Aggiorna prodotti nell'e-commerce">
+						</div>
+			
 				</div>
-				<div class="row bg-info">
-					<div class="col-sm-6 active bg-success" align="left" style="font-size: 18;">
-						<!-- <input type="checkbox" name="scarquantita" value="dwldqty"> quantit&agrave &nbsp 
-						<input type="checkbox" name="scarprezzo" value="dwlprice"> prezzo &nbsp
-						<input type="checkbox" name="scardescrizione" value="dwldes" > descrizione &nbsp -->
-					</div>
-					<div class="col-sm-6 active bg-success" align="left">
-						<input type="submit" class="btn btn-danger btn-sm pull-left" name="downloader"  value="Carica prodotti in GAzie">
-					</div>
+				<div class="row bg-success" style="border-top: 1px solid;">
+						<div class="col-sm-12 bg-success" align="center" >
+							<h3 class="text-primary">IMPORTAZIONE</h3>
+						</div>
+						<div class="col-sm-6  bg-success" align="left" style="font-size: 18;">
+							<p> In aggiornamento variare anche:</p>
+							<!-- <input type="checkbox" name="impquantita" value="dwldqty"> quantit&agrave &nbsp -->
+							<input type="checkbox" name="updpre" value="updprice"> Prezzo &nbsp
+							<input type="checkbox" name="upddes" value="upddes" > Descrizione estesa &nbsp
+							<input type="checkbox" name="updimm" value="updimg" > Immagine &nbsp
+						</div>
+						<div class="col-sm-6  bg-success" align="left" style="font-size: 18;">
+							<p> In nuovo inserimento inserire anche:</p>
+							<!-- <input type="checkbox" name="scarquantita" value="dwldqty"> quantit&agrave &nbsp -->
+							<input type="checkbox" name="imppre" value="dwlprice"> Prezzo &nbsp
+							<input type="checkbox" name="impdes" value="dwldes" > Descrizione estesa &nbsp 
+							<input type="checkbox" name="impimm" value="dwlimg" > Immagine &nbsp
+						</div>
+				
+				
+						<div class="col-sm-12  bg-success">
+							<input type="submit" class="btn btn-danger btn-sm pull-right" name="downloader"  value="Carica prodotti in GAzie">
+						</div>
 				</div>
+					
+				
 			</div>
 		</div>
 	</div>		
