b4f12f0eb29253e1c1a3555358c223c926493c87
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-01-26 18:24:32 +0000

    corretti files installazione e aggiornamento DB

--- trunk/setup/install/update_to_7.30.sql
+++ trunk/setup/install/update_to_7.30.sql
@@ -2,7 +2,7 @@ UPDATE `gaz_config` SET `cvalue` = '125' WHERE `id` =2;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXfae_flux` CHANGE COLUMN `id_SDI` `id_SDI` VARCHAR(20) NOT NULL DEFAULT '0';
 INSERT INTO `gaz_XXXcompany_config` (`description`, `var`, `val`) VALUES ('Destinazione testo descrittivo articolo (0=solo su documenti, 1=entrambi, 2=solo su web, 9=nessuno)', 'article_text', '0');
-CREATE TABLE IF NOT EXISTS `gaz_001fornitore_magazzino` (
+CREATE TABLE IF NOT EXISTS `gaz_XXXfornitore_magazzino` (
   `id` int(9) NOT NULL AUTO_INCREMENT,
   `id_anagr` int(9) NOT NULL,
   `codice_fornitore` varchar(50) NOT NULL,
