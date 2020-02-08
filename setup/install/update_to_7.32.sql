9d0a9feb38d1dad238122f8d266bf1696406fb2e
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-02-08 12:27:54 +0000

    tolgo un vecchio tema e i fogli di stile duplicati (base.css)
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3570 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ setup/install/update_to_7.32.sql
@@ -0,0 +1,4 @@
+UPDATE `gaz_config` SET `cvalue` = '127' WHERE `id` =2;
+-- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+UPDATE `gaz_admin` SET `style`='default.css',`skin`='default.css' WHERE 1;
+-- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
