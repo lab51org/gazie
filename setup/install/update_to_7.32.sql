270f7747c94ee99df9b809fdfb5258720b43784e
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-02-11 07:42:40 +0000

    miglioramento della struttura della grid sulla dashboard - maggiore flessibilità
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3578 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.32.sql
+++ setup/install/update_to_7.32.sql
@@ -1,4 +1,5 @@
 UPDATE `gaz_config` SET `cvalue` = '127' WHERE `id` =2;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 UPDATE `gaz_admin` SET `style`='default.css',`skin`='default.css' WHERE 1;
+ALTER TABLE `gaz_breadcrumb` ADD COLUMN `grid_class` VARCHAR(127) NOT NULL DEFAULT '' AFTER `icon`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
