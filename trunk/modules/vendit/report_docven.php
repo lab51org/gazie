218a81d7d523814a782ad12b7991307fe0599ab9
andreabia <andreabia@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-02-16 16:29:52 +0000

    Ripristinata la ricerca dei filtri in report_docven e adattate le altezze dei dashpanel in home

--- trunk/modules/vendit/report_docven.php
+++ trunk/modules/vendit/report_docven.php
@@ -501,7 +501,28 @@ function confirFae(link){
     </div>
 </form>
 
-
+<script>
+ $(document).ready(function(){
+     var selects = $("select");
+     // la funzione gaz_flt_dsp_select usa "All", qui usiamo invece valori vuoti
+     // (in questo modo i campi non usati possono essere esclusi)        
+     $("option", selects).filter(function(){ return this.value == "All"; }).val("");
+     
+     // la stessa funzione imposta onchange="this.form.submit()" sulle select: 
+     // l'azione non lancia un evento "submit" e non può essere intercettata.
+     // per non andare a modificare la funzione rimpiazziamo l'attributo onchange:
+     selects.attr('onchange', null).change(function() { $(this.form).submit(); });
+     
+     // così ora possiamo intercettare tutti i submit e pulire la GET dal superfluo
+     $("form").submit(function() {
+         $(this).find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
+         return true; // ensure form still submits
+     });
+     
+     // Un-disable form fields when page loads, in case they click back after submission
+     $( "form" ).find( ":input" ).prop( "disabled", false );
+ });
+</script>
 
 <?php
 require("../../library/include/footer.php");
