85f1e872b206b6a3bfb3c79d39a0559abc03e3b0
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-02-19 10:57:10 +0000

    file update dava errore su installazioni con più aziende
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3616 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.32.sql
+++ setup/install/update_to_7.32.sql
@@ -1,7 +1,7 @@
 UPDATE `gaz_config` SET `cvalue` = '127' WHERE `id` =2;
--- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 UPDATE `gaz_admin` SET `style`='default.css',`skin`='default.css' WHERE 1;
 ALTER TABLE `gaz_breadcrumb` ADD COLUMN `grid_class` VARCHAR(127) NOT NULL DEFAULT '' AFTER `icon`;
+-- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXtesbro`	ADD COLUMN `ref_ecommerce_id_order` VARCHAR(50) NULL DEFAULT '' COMMENT 'Identificativo ordine attribuito dall\'eventuale ecommerce collegato attraverso API' AFTER `tipdoc`, ADD INDEX `ref_ecommerce_id_order` (`ref_ecommerce_id_order`);
 ALTER TABLE `gaz_XXXartico`	ADD COLUMN `ref_ecommerce_id_product` VARCHAR(50) NULL DEFAULT '' COMMENT 'Codice di riferimento allo stesso articolo in eventuale ecommerce collegato attraverso API' AFTER `codice_fornitore`, ADD INDEX `ref_ecommerce_id_product` (`ref_ecommerce_id_product`);
 ALTER TABLE `gaz_XXXcatmer`	ADD COLUMN `ref_ecommerce_id_category` VARCHAR(50) NULL DEFAULT '' COMMENT 'Codice di riferimento alla stessa categoria articoli in eventuale ecommerce collegato attraverso API' AFTER `web_url`, ADD INDEX `ref_ecommerce_id_category` (`ref_ecommerce_id_category`);
