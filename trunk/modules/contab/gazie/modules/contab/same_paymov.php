cee3e2bfa63d86aadbba0827060e773bfba354c0
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-02-20 13:37:19 +0000

    Modificata query per visualizzare più velocemente il dialog gestione scadenzario in contabilità (Tiziano Bacocco)

--- trunk/modules/contab/gazie/modules/contab/same_paymov.php
+++ trunk/modules/contab/gazie/modules/contab/same_paymov.php
@@ -35,20 +35,15 @@ $admin_aziend=checkAdmin();
 $tesdoc_ref =  filter_var(substr($_GET['id_tesdoc_ref'],0,15),FILTER_SANITIZE_MAGIC_QUOTES);
 $id_exc =  filter_var(intval($_GET['id_exc']),FILTER_SANITIZE_MAGIC_QUOTES);
 $return_arr = array();
-/********************* OLD QUERY*********************
-$sqlquery= "SELECT * FROM ".$gTables['paymov']."
-            LEFT JOIN ".$gTables['rigmoc']." ON ( ".$gTables['rigmoc'].".id_rig = ".$gTables['paymov'].".id_rigmoc_doc OR ".$gTables['rigmoc'].".id_rig = ".$gTables['paymov'].".id_rigmoc_pay ) 
-            LEFT JOIN ".$gTables['tesmov']." ON ".$gTables['tesmov'].".id_tes = ".$gTables['rigmoc'].".id_tes
-            WHERE id_tesdoc_ref='".$tesdoc_ref."' AND ".$gTables['rigmoc'].".id_rig <> $id_exc ORDER BY ".$gTables['tesmov'].".datreg DESC, id_tesdoc_ref DESC, id_rig";
-			
-********************* QUERY BY TIZIANO BACOCCO ********/
-$sqlquery = "SELECT * FROM ".$gTables['paymov']." LEFT JOIN ".$gTables['rigmoc']." ON ( ".$gTables['rigmoc'].".id_rig = ".$gTables['paymov'].".id_rigmoc_doc )
-			LEFT JOIN ".$gTables['tesmov']." ON ".$gTables['tesmov'].".id_tes = ".$gTables['rigmoc'].".id_tes 
-			WHERE id_tesdoc_ref='".$tesdoc_ref."' AND ".$gTables['rigmoc'].".id_rig <> $id_exc 
-			UNION SELECT * FROM ".$gTables['paymov']." LEFT JOIN ".$gTables['rigmoc']." ON ( ".$gTables['rigmoc'].".id_rig = ".$gTables['paymov'].".id_rigmoc_pay  )
-			LEFT JOIN ".$gTables['tesmov']." ON ".$gTables['tesmov'].".id_tes = ".$gTables['rigmoc'].".id_tes 
-			WHERE id_tesdoc_ref='".$tesdoc_ref."' AND ".$gTables['rigmoc'].".id_rig <> $id_exc 
-			ORDER BY datreg DESC, id_tesdoc_ref DESC, id_rig"; 
+            
+            
+$sqlquery = "SELECT * FROM ".$gTables['paymov']." LEFT JOIN ".$gTables['rigmoc']." ON ( ".$gTables['rigmoc'].".id_rig = ".$gTables['paymov'].".id_rigmoc_doc )              LEFT JOIN ".$gTables['tesmov']." ON ".$gTables['tesmov'].".id_tes = ".$gTables['rigmoc'].".id_tes             WHERE id_tesdoc_ref='".$tesdoc_ref."' AND ".$gTables['rigmoc'].".id_rig <> $id_exc 
+
+UNION 
+
+SELECT * FROM ".$gTables['paymov']." LEFT JOIN ".$gTables['rigmoc']." ON ( ".$gTables['rigmoc'].".id_rig = ".$gTables['paymov'].".id_rigmoc_pay  )              LEFT JOIN ".$gTables['tesmov']." ON ".$gTables['tesmov'].".id_tes = ".$gTables['rigmoc'].".id_tes      WHERE id_tesdoc_ref='".$tesdoc_ref."' AND ".$gTables['rigmoc'].".id_rig <> $id_exc        
+
+ORDER BY datreg DESC, id_tesdoc_ref DESC, id_rig";            
 error_log($sqlquery);
 $result = gaz_dbi_query($sqlquery);
 
