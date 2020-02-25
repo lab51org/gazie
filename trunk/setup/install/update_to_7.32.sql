190da8258dfd2f23df157968e2dcf00d80f22bb2
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-02-25 19:42:01 +0000

    aggiunta colonna che mi servirà per stabilire (rapidamente) se posso fare chiamate alle API dell'ecommerce

--- trunk/setup/install/update_to_7.32.sql
+++ trunk/setup/install/update_to_7.32.sql
@@ -1,6 +1,7 @@
 UPDATE `gaz_config` SET `cvalue` = '127' WHERE `id` =2;
 UPDATE `gaz_admin` SET `style`='default.css',`skin`='default.css' WHERE 1;
 ALTER TABLE `gaz_breadcrumb` ADD COLUMN `grid_class` VARCHAR(127) NOT NULL DEFAULT '' AFTER `icon`;
+ALTER TABLE `gaz_aziend` ADD COLUMN `update_ecommerce` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'indico 1 quando c\'è un ecommerce collegato che quindi deve essere aggiornato tramite API ad ogni cambiamento, es: nuovo movimento di magazzino, nuovo cliente, nuovo articolo,nuova categoria merceologica, nuova aliquota IVA e di controllare la presenza di nuovi ordini e/o clienti inseriti in esso' AFTER `web_url`;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXtesbro`	ADD COLUMN `ref_ecommerce_id_order` VARCHAR(50) NULL DEFAULT '' COMMENT 'Identificativo ordine attribuito dall\'eventuale ecommerce collegato attraverso API' AFTER `tipdoc`, ADD INDEX `ref_ecommerce_id_order` (`ref_ecommerce_id_order`);
 ALTER TABLE `gaz_XXXartico`	ADD COLUMN `ref_ecommerce_id_product` VARCHAR(50) NULL DEFAULT '' COMMENT 'Codice di riferimento allo stesso articolo in eventuale ecommerce collegato attraverso API' AFTER `codice_fornitore`, ADD INDEX `ref_ecommerce_id_product` (`ref_ecommerce_id_product`);
