7eb41e6f239e4a31bd7ef9ee27bbe24ffc142ddf
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-03-03 08:05:34 +0000

    Aggiunto file delete.php nel modulo acquisti
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3675 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/acquis/delete.php
@@ -0,0 +1,48 @@
+<?php
+/*
+  --------------------------------------------------------------------------
+  GAzie - Gestione Azienda
+  Copyright (C) 2004-2020 - Antonio De Vincentiis Montesilvano (PE)
+  (http://www.devincentiis.it)
+  <http://gazie.sourceforge.net>
+  --------------------------------------------------------------------------
+  Questo programma e` free software;   e` lecito redistribuirlo  e/o
+  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
+  come e` pubblicata dalla Free Software Foundation; o la versione 2
+  della licenza o (a propria scelta) una versione successiva.
+
+  Questo programma  e` distribuito nella speranza  che sia utile, ma
+  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
+  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
+  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
+
+  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
+  Generica GNU insieme a   questo programma; in caso  contrario,  si
+  scriva   alla   Free  Software Foundation, 51 Franklin Street,
+  Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
+  --------------------------------------------------------------------------
+ */
+// prevent direct access
+$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND
+        strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+if (!$isAjax) {
+    $user_error = 'Access denied - not an AJAX request...';
+    trigger_error($user_error, E_USER_ERROR);
+}
+if ((isset($_POST['type'])&&isset($_POST['ref'])) OR (isset($_POST['type'])&&isset($_POST['id_tes']))) { 
+	require("../../library/include/datlib.inc.php");
+	$admin_aziend = checkAdmin();
+	switch ($_POST['type']) {
+        case "broacq":
+			$i=intval($_POST['id_tes']);
+			//cancello la testata
+			gaz_dbi_del_row($gTables['tesbro'], "id_tes", $i);
+			//... e i righi
+			$rs_righidel = gaz_dbi_dyn_query("*", $gTables['rigbro'], "id_tes = '{$i}'","id_tes desc");
+			while ($a_row = gaz_dbi_fetch_array($rs_righidel)) {
+				gaz_dbi_del_row($gTables['rigbro'], "id_rig", $a_row['id_rig']);
+			}
+		break;
+	}
+}
+?>
