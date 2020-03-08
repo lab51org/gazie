f8b427b5b442f53f049f4f786b0f348bfdee2bfd
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-03-08 08:28:56 +0000

    Non valorizzava id_mag di rigdoc ?!?!

--- trunk/modules/acquis/lib.data.php
+++ trunk/modules/acquis/lib.data.php
@@ -27,7 +27,8 @@ function bodytextInsert ($newValue)
 {
     $table = 'body_text';
     $columns = array('table_name_ref','id_ref','body_text','lang_id');
-    tableInsert($table, $columns, $newValue);
+    $last_id=tableInsert($table, $columns, $newValue);
+	  return $last_id;
 }
 
 function bodytextUpdate ($codice, $newValue)
