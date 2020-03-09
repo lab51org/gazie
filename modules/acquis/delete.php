d3a9b9c8c812d38386ee5e1f6a26c23f1ef94bdc
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-03-09 08:48:02 +0000

    Cancellazione documenti acquisto: corretto problema che non cancellava movimenti di magazzino
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3710 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/acquis/delete.php
+++ modules/acquis/delete.php
@@ -51,13 +51,14 @@ if ((isset($_POST['type'])&&isset($_POST['ref'])) OR (isset($_POST['type'])&&iss
 			gaz_dbi_del_row($gTables['rigmoc'], 'id_tes', $form['id_con']);
 			gaz_dbi_del_row($gTables['rigmoi'], 'id_tes', $form['id_con']);
 			$rs_righidel = gaz_dbi_dyn_query("*", $gTables['rigdoc'], "id_tes = '".$i."'","id_tes desc");
+			print_r($rs_righidel);
 			while ($a_row = gaz_dbi_fetch_array($rs_righidel)) {
 				gaz_dbi_del_row($gTables['rigdoc'], "id_rig", $a_row['id_rig']);
 				if (intval($a_row['id_mag']) > 0){  //se c'� stato un movimento di magazzino lo azzero
-					$upd_mm->uploadMag('DEL',$form['tipdoc'],'','','','','','','','','','',$a_row['id_mag'],$admin_aziend['stock_eval_method']);
-					// se c'è stato, cancello pure il movimento sian 
+					gaz_dbi_del_row($gTables['movmag'], "id_mov", $a_row['id_mag']);					
+					// cancello pure eventuale movimento sian 
 					gaz_dbi_del_row($gTables['camp_mov_sian'], "id_movmag", $a_row['id_mag']);
-				}
+				}				
 			}
 		break;
 		case "pagdeb":
