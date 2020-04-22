962a2c1382fe50dac483903815ead2755f9ac2e8
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-04-22 06:43:25 +0000

    sync.function.php: implementata la function GetOrder($last_id)

--- trunk/modules/shop-synchronize/sync.function.php
+++ trunk/modules/shop-synchronize/sync.function.php
@@ -220,7 +220,153 @@ class APIeCommerce {
 	}
 	function GetOrder($last_id) { 
 		// prendo gli eventuali ordini arrivati assieme ai dati del cliente, se nuovo lo importo (order+customer), 
-		// in $last_id si deve passare l'ultimo ordine già importato al fine di non importare tutto ma solo i nuovi 
+		// in $last_id si deve passare l'ultimo ordine già importato al fine di non importare tutto ma solo i nuovi
+		//Antonio Germani - $last_id non viene usato perché si controlla con una query se l'ordine è già stato importato
+		session_start();
+		global $gTables,$admin_aziend;	
+		$ftp_host = gaz_dbi_get_row($gTables['company_config'], "var", "server")['val'];
+		$ftp_user = gaz_dbi_get_row($gTables['company_config'], "var", "user")['val'];
+		$ftp_pass = gaz_dbi_get_row($gTables['company_config'], "var", "pass")['val'];
+		$urlinterf = gaz_dbi_get_row($gTables['company_config'], 'var', 'path')['val']."ordini-gazie.php";
+		$test = gaz_dbi_query("SHOW COLUMNS FROM `" . $gTables['admin'] . "` LIKE 'enterprise_id'");
+		$exists = (gaz_dbi_num_rows($test)) ? TRUE : FALSE;
+		if ($exists) {
+			$c_e = 'enterprise_id';
+		} else {
+			$c_e = 'company_id';
+		}
+		$admin_aziend = gaz_dbi_get_row($gTables['admin'] . ' LEFT JOIN ' . $gTables['aziend'] . ' ON ' . $gTables['admin'] . '.' . $c_e . '= ' . $gTables['aziend'] . '.codice', "user_name", $_SESSION["user_name"]);
+		// imposto la connessione al server
+		$conn_id = ftp_connect($ftp_host);
+		// effettuo login con user e pass
+		$mylogin = ftp_login($conn_id, $ftp_user, $ftp_pass);
+		if ((!$conn_id) or (!$mylogin)){// controllo se la connessione è OK...
+			// non si connette FALSE
+			$_SESSION['errmsg'] = "Problemi con le impostazioni FTP in configurazione avanzata azienda.";
+			$_SESSION['errref'] = "Impossibile scaricare gli ordini dall'e-commerce";
+		}
+		$access=base64_encode($ftp_pass);
+		// avvio il file di interfaccia presente nel sito web remoto
+		$headers = @get_headers($urlinterf.'?access='.$access);
+		if ( intval(substr($headers[0], 9, 3))==200){ // controllo se il file esiste o mi dà accesso
+			$xml=simplexml_load_file($urlinterf.'?access='.$access) ;
+			if (!$xml){
+				$_SESSION['errmsg'] = "L'e-commerce non crea il file xml";
+				$_SESSION['errref'] = "Impossibile scaricare gli ordini dall'e-commerce";
+			}
+			foreach($xml->Documents->children() as $order) { // ciclo gli ordini
+				if(!gaz_dbi_get_row($gTables['tesbro'], "numdoc", $order->Number)){ // se il numero d'ordine non esiste carico l'ordine in GAzie
+					$query = "SHOW TABLE STATUS LIKE '" . $gTables['tesbro'] . "'";
+					$result = gaz_dbi_query($query);
+					$row = $result->fetch_assoc();
+					$id_tesbro = $row['Auto_increment']; // questo è l'ID che avrà TESBRO: testata documento/ordine
+					$query = "SHOW TABLE STATUS LIKE '" . $gTables['anagra'] . "'";
+					$result = gaz_dbi_query($query);
+					$row = $result->fetch_assoc();
+					$id_anagra = $row['Auto_increment']; // questo è l'ID che avrà ANAGRA: Anagrafica cliente				
+					$anagrafica = new Anagrafica(); 
+					$last = $anagrafica->queryPartners('*', "codice BETWEEN " . $admin_aziend['mascli'] . "000000 AND " . $admin_aziend['mascli'] . "999999", "codice DESC", 0, 1);
+					$codice = substr($last[0]['codice'], 3) + 1;
+					$clfoco = $admin_aziend['mascli'] * 1000000 + $codice;// questo è il codice di CLFOCO da connettere all'anagrafica cliente se il cliente non esiste
+	    			$esiste=0;
+					if (strlen($order->CustomerFiscalCode)>0){ // controllo esistenza cliente per codice fiscale
+						$query = "SELECT * FROM " . $gTables['anagra'] . " WHERE codfis ='" . $order->CustomerFiscalCode . "'";
+						$check = gaz_dbi_query($query);
+						if ($check->num_rows > 0){
+							$esiste=1;
+							$row = $check->fetch_assoc();
+							$cl = gaz_dbi_get_row($gTables['clfoco'], "id_anagra", $row['id']);
+							$clfoco=$cl['codice'];
+						}
+					}
+					if (intval($order->CustomerVatCode)>0){ // controllo esistenza cliente per partita iva
+						$query = "SELECT * FROM " . $gTables['anagra'] . " WHERE pariva ='" . $order->CustomerVatCode . "'";
+						$check = gaz_dbi_query($query);
+						if ($check->num_rows > 0){
+							$esiste=1;
+							$row = $check->fetch_assoc();
+							$cl = gaz_dbi_get_row($gTables['clfoco'], "id_anagra", $row['id']);
+							$clfoco=$cl['codice'];
+						}
+					}
+					 // controllo esistenza cliente per cognome, nome e città
+					$query = "SELECT * FROM " . $gTables['anagra'] . " WHERE ragso1 ='" . addslashes($order->CustomerName) . "' AND ragso2 ='". addslashes($order->CustomerSurname) . "'";
+					$check = gaz_dbi_query($query);
+					while ($row = $check->fetch_assoc()) {
+						if (($check->num_rows > 0) && ($row['citspe']=$order->CustomerCity) && ($row['indspe']=$order->CustomerAddress)){
+							$esiste=1;
+							$cl = gaz_dbi_get_row($gTables['clfoco'], "id_anagra", $row['id']);
+							$clfoco=$cl['codice'];
+						}
+					}
+								
+					If ($esiste==0) { //registro cliente se non esiste
+							if ($order->CustomerCountry=="IT"){ // se la nazione è IT
+								$lang="1";
+							} else {
+								$lang="0";
+							}
+							gaz_dbi_query("INSERT INTO " . $gTables['anagra'] . "(ragso1,ragso2,indspe,capspe,citspe,prospe,country,id_currency,id_language,telefo,codfis,pariva,fe_cod_univoco,e_mail,pec_email) VALUES ('" . addslashes($order->CustomerName) . "', '" . addslashes($order->CustomerSurname) . "', '". addslashes($order->CustomerAddress) ."', '".$order->CustomerPostCode."', '". addslashes($order->CustomerCity) ."', '". $order->CustomerProvince ."', '" . $order->CustomerCountry. "', '1', '".$lang."', '". $order->CustomerTel ."', '". $order->CustomerFiscalCode ."', '" . $order->CustomerVatCode . "', '" . $order->CustomerCodeFattEl . "', '". $order->CustomerEmail . "', '". $order->CustomerPecEmail . "')");
+							
+							gaz_dbi_query("INSERT INTO " . $gTables['clfoco'] . "(codice,id_anagra,descri,destin,speban) VALUES ('". $clfoco . "', '" . $id_anagra . "', '" .addslashes($order->CustomerName)." ".addslashes($order->CustomerSurname) . "', '". $order->CustomerShippingDestin ."', 'S')");
+					}
+					
+					if ($order->TotalDiscount>0){ // se il sito ha mandato uno sconto totale a valore calcolo lo sconto in percentuale da dare ad ogni rigo
+						$lordo=$order->Total+$order->TotalDiscount-$order->CostPaymentAmount-$order->CostShippingAmount;
+						$netto=$lordo-$order->TotalDiscount;
+						$percdisc= 100-(($netto/$lordo)*100);
+					} else {
+						$percdisc="";
+					}
+					
+					if ($order->PricesIncludeVat=="true"){ // se il sito include l'iva la scorporo dalle spese banca e trasporto
+						$CostPaymentAmount=floatval($order->CostPaymentAmount)/ 1.22; // floatval traforma da alfabetico a numerico
+						$CostShippingAmount=floatval($order->CostShippingAmount) / 1.22;
+					} else {
+						$CostPaymentAmount=floatval($order->CostPaymentAmount);
+						$CostShippingAmount=floatval($order->CostShippingAmount);
+					}
+											
+					// registro testata ordine
+					gaz_dbi_query("INSERT INTO " . $gTables['tesbro'] . "(tipdoc,seziva,print_total,datemi,numdoc,datfat,clfoco,pagame,listin,spediz,traspo,speban,caumag,expense_vat,initra,status,adminid) VALUES ('VOW', '1', '1', '" . $order->DateOrder . "', '" .$order->Number . "', '0000-00-00', '". $clfoco . "', '" .$order->PaymentName."', '". $order->PriceListNum . "', '".$order->Carrier."', '". $CostShippingAmount ."', '". $CostPaymentAmount ."', '1', '". $order->CostVatCode."', '" . $order->DateOrder. "', 'ONLINE-SHOP', '" . $admin_aziend['adminid'] . "')");
+				
+					// Gestione righi ordine					
+					foreach($xml->Documents->Document->Rows->children() as $orderrow) { // carico le righe dell'ordine
+						
+						// controllo se esiste l'articolo in GAzie 
+						$ckart = gaz_dbi_get_row($gTables['artico'], "ref_ecommerce_id_product", $orderrow->Id);
+						$codart=$ckart['codice']; // se esiste ne prendo il codice come $codart
+						$descri=$ckart['descri'];// se esiste ne prendo descri come $descri						
+		 
+						if (!$ckart){ // se non esiste creo un nuovo articolo su gazie come servizio in quanto non si sa se deve movimentare il magazzino					
+							if ($orderrow->VatAli<1){ // se il sito non ha mandato l'aliquota IVA dell'articolo ci metto quella che ha mandato come base aziendale riservato alle spese
+								$orderrow->VatCode=$order->CostVatCode;
+								$orderrow->VatAli=$order->CostVatAli;
+							} 
+							$vat = gaz_dbi_get_row($gTables['aliiva'], "aliquo", $orderrow->VatAli, " AND tipiva = 'I'");
+							gaz_dbi_query("INSERT INTO " . $gTables['artico'] . "(codice,descri,ref_ecommerce_id_product,good_or_service,unimis,catmer,preve2,web_price,web_public,aliiva,codcon,adminid) VALUES ('". substr($orderrow->Code,0,15) ."', '". addslashes($orderrow->Description) ."', '". $orderrow->Id ."', '1', '" . $orderrow->MeasureUnit . "', '" .$orderrow->Category . "', '". $orderrow->Price ."', '". $orderrow->Price ."', '1', '".$vat['codice']."', '420000006', '" . $admin_aziend['adminid'] . "')");
+							$codart= substr($orderrow->Code,0,15);// dopo averlo creato ne prendo il codice come $codart
+							$descri= $orderrow->Description; //prendo anche la descrizione
+							$codvat=$vat['aliiva'];
+							$aliiva=$vat['aliquo'];
+						} else {
+							$codvat=gaz_dbi_get_row($gTables['artico'], "codice", $codart)['aliiva'];
+							$aliiva=$orderrow->VatAli;
+						}
+						
+						// salvo rigo su database tabella rigbro 
+						gaz_dbi_query("INSERT INTO " . $gTables['rigbro'] . "(id_tes,codart,descri,unimis,quanti,prelis,sconto,codvat,codric,pervat,status) VALUES ('" . intval($id_tesbro) . "','" . $codart . "','" . addslashes($descri) . "','". $orderrow->MeasureUnit . "','" . $orderrow->Qty . "','" . $orderrow->Price . "', '".$percdisc."', '". $codvat. "', '420000006', '". $aliiva. "', 'ONLINE-SHOP')");
+					}										
+					$id_tesbro++;				
+				}							
+			}							
+		} else { // IL FILE INTERFACCIA NON ESISTE > chiudo la connessione ftp
+			ftp_quit($conn_id);
+			$_SESSION['errmsg'] = "Il file xml degli ordini non si apre";
+			$_SESSION['errref'] = "Codice errore = ".intval(substr($headers[0], 9, 3));
+			if (intval(substr($headers[0], 9, 3))==0) {
+				$_SESSION['errref'] = $_SESSION['errref']." controllare connessione internet";
+			}
+		}		
 	}
 }
-?>
