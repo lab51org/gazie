64ec3852674964b4cbd5e8d7fadfc96e0e06e8ca
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-08-20 10:19:29 +0000

    Aggiunta referenza al bene ammortizzabile che serve se si vuole implementare la gestione delle  manutenzioni interne delle apparecchiature (beni strumentali) gestendo al contempo il magazzino dei pezzi di ricambio.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@3975 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.34.sql
+++ setup/install/update_to_7.34.sql
@@ -25,4 +25,5 @@ UPDATE `gaz_menu_script` SET `link` = 'camp_admin_caumag.php?Insert' WHERE `id_m
 UPDATE `gaz_XXXcaucon_rows` SET `n_order` = '1' WHERE `caucon_cod` = 'AFT' AND `clfoco_ref` = '212000000';
 UPDATE `gaz_XXXcaucon_rows` SET `n_order` = '2' WHERE `caucon_cod` = 'AFT' AND `clfoco_ref` = '330000004';
 UPDATE `gaz_XXXcaucon_rows` SET `n_order` = '3' WHERE `caucon_cod` = 'AFT' AND `clfoco_ref` = '106000001';
+ALTER TABLE `gaz_XXXmovmag`	ADD COLUMN `id_assets` INT(9) NOT NULL COMMENT 'Ref. alla tabella gaz_001assets (riferito a bene ammortizzabile)' AFTER `id_orderman`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione)
