84c24ddf1a20eef0af7cb23befa7b911a6373b5a
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-10-20 12:32:53 +0000

    Aggiungo file per update db prossima versione. Ripeto query per riempire  tipdoc di gaz_XXXmovmag con il valore "MAG" quando vuoto
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4056 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ setup/install/update_to_7.35.sql
@@ -0,0 +1,4 @@
+-- UPDATE `gaz_config` SET `cvalue` = '129' WHERE `id` =2; 
+-- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+UPDATE `gaz_XXXmovmag` SET `tipdoc`='MAG' WHERE `tipdoc` = '';
+-- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
