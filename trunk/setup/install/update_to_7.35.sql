7ae9e260b506232c65d215d6608a20684f456fb7
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-10-23 07:18:51 +0000

    Piccolo bug  in inserimento cognome-nome dipendente e aggiunta riferimenti e indici sulla tabella ore lavorate

--- trunk/setup/install/update_to_7.35.sql
+++ trunk/setup/install/update_to_7.35.sql
@@ -1,4 +1,6 @@
 -- UPDATE `gaz_config` SET `cvalue` = '129' WHERE `id` =2; 
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 UPDATE `gaz_XXXmovmag` SET `tipdoc`='MAG' WHERE `tipdoc` = '';
+ALTER TABLE `gaz_XXXstaff_skills` CHANGE COLUMN `skill_value` `skill_data` VARCHAR(100) NULL DEFAULT NULL AFTER `variable_name`, ADD COLUMN `skill_description` VARCHAR(100) NULL DEFAULT NULL AFTER `skill_data`, ADD COLUMN `skill_cost` DECIMAL(8,2) NULL DEFAULT NULL AFTER `skill_description`;
+ALTER TABLE `gaz_XXXstaff_worked_hours`	ADD COLUMN `id_tes` INT(9) NULL DEFAULT NULL COMMENT 'può essere usato per link con tesbro al fine di aver un documento/resoconto del lavoro eseguito' AFTER `id_orderman`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
