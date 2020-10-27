8d79d08429e40de61b1bfab53cf2b39d48efe27b
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-10-27 14:06:59 +0000

    Aggiungo indice a tesdoc, serve in particolare su acquis/report_docacq.php
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4074 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.35.php
+++ setup/install/update_to_7.35.php
@@ -58,7 +58,7 @@ $result = gaz_dbi_dyn_query("*", $table_prefix.'_aziend', 1);
 while ($row = gaz_dbi_fetch_array($result)) {
 	$aziend_codice = sprintf("%03s", $row["codice"]);
 	// inizio controlli presenza di indici altrimenti li creo 
-	$idx=array(0=>array('work_day'=>'staff_worked_hours','id_orderman'=>'staff_worked_hours','id_tes'=>'staff_worked_hours')); 
+	$idx=array(0=>array('work_day'=>'staff_worked_hours','id_orderman'=>'staff_worked_hours','id_tes'=>'staff_worked_hours', 'datreg'=>'tesdoc')); 
 	foreach($idx as $vi){
 		foreach($vi as $k=>$v){
 			$rk=gaz_dbi_query("SHOW KEYS FROM ". $table_prefix . "_" . $aziend_codice.$v." WHERE 1");
