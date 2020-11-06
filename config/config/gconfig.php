1edae7f382c4680dec52fd63fba7708e1ce8f32d
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-11-06 16:39:42 +0000

    GAzie 7.35 ready
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4108 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- config/config/gconfig.php
+++ config/config/gconfig.php
@@ -45,7 +45,7 @@ DELLA CARTELLA "gazie" COMPRESO IL PRESENTE "gconfig.php"
     exit('Accesso diretto non consentito');
 }
 //versione software
-define('GAZIE_VERSION', '7.34');
+define('GAZIE_VERSION', '7.35');
 
 $fileConfPers=dirname(__FILE__) . '/gconfig.myconf.php';
 if (file_exists($fileConfPers)) {
