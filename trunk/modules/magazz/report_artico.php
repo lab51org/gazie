8eae9f11b072d5ade47843c87dc3a10f8b68f212
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2020-11-10 21:52:44 +0000

    Cambio il report degli articoli di magazzino per uniformarlo con gli altri

--- trunk/modules/magazz/report_artico.php
+++ trunk/modules/magazz/report_artico.php
@@ -1,150 +1,92 @@
 <?php
-/*
-  --------------------------------------------------------------------------
-  GAzie - Gestione Azienda
-  Copyright (C) 2004-2020 - Antonio De Vincentiis Montesilvano (PE)
-  (http://www.devincentiis.it)
-  <http://gazie.sourceforge.net>
-  --------------------------------------------------------------------------
-  Questo programma e` free software;   e` lecito redistribuirlo  e/o
-  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
-  come e` pubblicata dalla Free Software Foundation; o la versione 2
-  della licenza o (a propria scelta) una versione successiva.
-  Questo programma  e` distribuito nella speranza  che sia utile, ma
-  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
-  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
-  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
-  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
-  Generica GNU insieme a   questo programma; in caso  contrario,  si
-  scriva   alla   Free  Software Foundation, 51 Franklin Street,
-  Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
-  --------------------------------------------------------------------------
+ /*
+ --------------------------------------------------------------------------
+                            GAzie - Gestione Azienda
+    Copyright (C) - Antonio De Vincentiis Montesilvano (PE)
+         (http://www.devincentiis.it)
+ --------------------------------------------------------------------------
  */
 require("../../library/include/datlib.inc.php");
-
-checkCustoms($_SERVER['REQUEST_URI']);
-
- function selectFrom($table, $name, $key, $val, $order, $bridge = '', $key2, $class = 'FacetSelect') { // Antonio Germani - crea un input select
-        global $gTables;
-		$acc='';
-        $refresh = '';
-        $query = 'SELECT * FROM `' . $gTables[$table] . '` '. ' ORDER BY `' . $order . '`';
-		$acc .= "\t <select id=\"$name\" name=\"$name\" class=\"$class\" onchange=\"this.form.submit();\" > <option value=\"\"></option>\n";
-        $result = gaz_dbi_query($query);
-        while ($r = gaz_dbi_fetch_array($result)) {
-            $selected = '';
-            if ($r[$key] == $val) {
-                $selected = "selected";
-            }
-            $acc .= "\t\t <option value=\"" . $r[$key] . "\" $selected >";
-            $acc .= substr($r[$key], 0, 28) . $bridge . substr($r[$key2], 0, 35) . "</option>\n";
-        }       
-        $acc .= "\t </select>\n";
-		echo $acc;
-    }
-
-$admin_aziend = checkAdmin();
-
-if (isset($_POST['erase'])) {
-	unset($_POST);
-}
-
-if (isset($_POST['catmer'])){
- $form['catmer']=$_POST['catmer'];
- $cat = filter_input(INPUT_POST, 'catmer');
-} else {
-	$form['catmer']="";
-	$cat="";
-}
-
-
-if (isset($_POST['order_by'])) { // controllo se vengo da una richiesta di ordinamento
-    $rn = 0;
-    $ob = filter_input(INPUT_POST, 'order_by');
-    $so = filter_input(INPUT_POST, 'sort');
-    $cs = filter_input(INPUT_POST, 'cosear');
-} else {
-    $rn = '0';
-    $ob = 'last_modified';
-    $so = 'DESC';
-    $cs = '';
-}
+$admin_aziend=checkAdmin();
 require("../../library/include/header.php");
+$script_transl = HeadMain();
+// campi ammissibili per la ricerca
+$search_fields = [
+    'sea_codice' => "{$gTables['artico']}.codice LIKE '%%%s%%'",
+	'des_artico' => "{$gTables['artico']}.descri LIKE '%%%s%%'",
+	'unimis' => "{$gTables['artico']}.unimis LIKE '%%%s%%'",
+    'asset' => "id_assets = %d",
+    'codcat' => "{$gTables['catmer']}.codice = %d",
+];
+
+// creo l'array (header => campi) per l'ordinamento dei record
+$sortable_headers = array  (
+            "Codice" => 'codice',
+            "Descrizione"=>'descri',
+            "Merce<br/>Servizio" => 'good_or_service',
+            "Categoria" => 'catmer',
+            'U.M.' => 'unimis',
+            'Prezzo vendita<br/>listino 1' => 'preve1',
+            'Fornitore' => '',
+            'Prezzo acquisto' => '',
+            'Giacenza' => '',
+            '% IVA' => 'aliiva',
+            'Lotti' => '',
+            'Duplica' => '',
+            'Elimina' => ''
+);
+
+$tablejoin = $gTables['artico']. " LEFT JOIN " . $gTables['catmer'] . " ON " . $gTables['artico'] . ".catmer = " . $gTables['catmer'] . ".codice";
+
+$ts = new TableSorter(
+    $tablejoin, 
+    $passo, 
+    ['last_modified'=>'desc'],
+    ['asset' => 0]);
 ?>
-<script type="text/javascript">
-    $(window).scroll(function ()
-    {
-        if ($(document).height() <= $(window).scrollTop() + $(window).height() +20 ) {
-            loadmore();
-        }
-    });
-    $(window).load(function () {
-        loadmore();
-    });
-                                   
-    function loadmore()
-    {
-        var rn = $("#row_no").val();
-        var ob = $("#order_by").val();
-        var so = $("#sort").val();
-        var ca = '<?php echo $cs ?>';
-        var cat = '<?php echo $cat ?>';
-        $.ajax({
-            type: 'post',
-            url: 'report_artico_scroll.php',
-            data: {
-                rowno: rn,
-                orderby: ob,
-                sort: so,
-                codart: ca,
-				catmer: cat,
-            },
-            beforeSend: function () {
-                $('#loader-icon').show();
-            },
-            complete: function () {
-                $('#loader-icon').hide();
-            },
-            success: function (response) {
-                $("#all_rows").append(response); //append received data into the element
-                $("#row_no").val(Number(rn) + <?php echo PER_PAGE; ?>);
-                $('.gazie-tooltip').tooltip(
-                        {html: true,
-                            placement: 'auto bottom',
-                            delay: {show: 50},
-                            title: function () {
-                                return '<span>' + this.getAttribute('data-label') + '</span><img src="../root/view.php?table=artico&value=' + this.getAttribute('data-id') + '" onerror="this.src=\'../../library/images/link_break.png\'" alt="' + this.getAttribute('data-label') + '"  style="max-height:150px;" />';
-                            }
-                        });
-            }
-        });
-    }
-
-    $(function () {
-        $('.orby').click(function () {
-            var v = $(this).attr('data-order');
-            var actual_ob = $("#order_by").val();
-            var actual_so = $("#sort").val();
-            if (v === actual_ob) { // è la stessa colonna inverto l'ordine
-                if (actual_so === 'ASC') {
-                    $("#sort").val('DESC');
-                } else {
-                    $("#sort").val('ASC');
-                }
-            } else { // una colonna diversa la cambio 
-                $("#order_by").val(v);
-            }
-            $("#row_no").val(0); // quando richiedo un nuovo ordinamento devo necessariamente ricominciare da zero
-            $("#form").submit();
-        });
-    });
-
+<script>
+$(function() {
+	$("#dialog_delete").dialog({ autoOpen: false });
+	$('.dialog_delete').click(function() {
+		$("p#idcodice").html($(this).attr("ref"));
+		$("p#iddescri").html($(this).attr("artico"));
+		var id = $(this).attr('ref');
+		$( "#dialog_delete" ).dialog({
+			minHeight: 1,
+			width: "auto",
+			modal: "true",
+			show: "blind",
+			hide: "explode",
+			buttons: {
+				delete:{ 
+					text:'Elimina', 
+					'class':'btn btn-danger delete-button',
+					click:function (event, ui) {
+					$.ajax({
+						data: {'type':'artico',ref:id},
+						type: 'POST',
+						url: '../magazz/delete.php',
+						success: function(output){
+		                    //alert(output);
+							window.location.replace("./report_artico.php");
+						}
+					});
+				}},
+				"Non eliminare": function() {
+					$(this).dialog("close");
+				}
+			}
+		});
+		$("#dialog_delete" ).dialog( "open" );  
+	});
+});
 </script>
+<div class="text-center"><h3><?php echo $script_transl['title'];?></h3></div>
 <?php
-$script_transl = HeadMain(0, array('custom/autocomplete'));
-$gForm = new magazzForm();
+$ts->output_navbar();
+
 ?>
-<form method="POST" name="myform" id="form">
+<form method="GET">
 	<div style="display:none" id="dialog_delete" title="Conferma eliminazione">
         <p><b>articolo:</b></p>
         <p>codice:</p>
@@ -152,103 +94,118 @@ $gForm = new magazzForm();
         <p>Descrizione</p>
         <p class="ui-state-highlight" id="iddescri"></p>
 	</div>
-    <div class="text-center"><b><?php echo $script_transl['title']; ?></b></div>
-    <div class="panel panel-info col-lg-8">
-        <div class="container-fluid">
-        <label for="codice" class="col-lg-3 control-label"><?php echo $script_transl['codice'].'-'.$script_transl['descri']; ?></label>
-		<?php
-		echo ' <input type="text" class="col-lg-3" name="cosear" id="search_cosear" value="' .substr($cs, 0, 20) . '"  maxlength="16" />';
-		if (strlen($cs)<1){// Antonio Germani - Se non ? stata richiesta la ricerca per singolo articolo creo un select categoria merceologica
-			echo '<label class="col-lg-2 control-label">'.$script_transl['catmer'].'</label>';
-			selectFrom('catmer', 'catmer','codice', $form['catmer'], 'codice', ' - ','descri','FacetSelect');
-		}
-		echo '<button type="submit" name="erase" title="Reset" class="btn btn-default"  style="border-radius= 85px; "> <i class="glyphicon glyphicon-remove-circle"></i></button>'
+	<div class="table-responsive">
+	<table class="Tlarge table table-striped table-bordered table-condensed">
+	<tr>
+		<td class="FacetFieldCaptionTD">
+			<input type="text" name="sea_codice" placeholder="codice" class="input-sm form-control" value="<?php echo (isset($sea_codice))? $sea_codice : ""; ?>" maxlength="15">
+		</td>
+		<td class="FacetFieldCaptionTD">
+			<input type="text" name="des_artico" placeholder="descrizione" class="input-sm form-control" value="<?php echo (isset($des_artico))? $des_artico : ""; ?>" maxlength="15">
+        </td>
+		<td class="FacetFieldCaptionTD">
+        <?php gaz_flt_disp_select("good_or_service", "good_or_service", $tablejoin, 1,'good_or_service ASC', $script_transl['good_or_service_value']); ?>
+        </td>
+		<td class="FacetFieldCaptionTD">
+        <?php gaz_flt_disp_select("codcat", $gTables['catmer'].".codice AS codcat, ". $gTables['catmer'].".descri AS descat", $tablejoin, 1,'codcat ASC','descat'); ?>
+        </td>
+		<td class="FacetFieldCaptionTD">
+			<input type="text" name="unimis" placeholder="U.M." class="input-sm form-control" value="<?php echo (isset($unimis))? $unimis : ""; ?>" maxlength="3">
+        </td>
+		<td class="FacetFieldCaptionTD"></td>
+		<td class="FacetFieldCaptionTD" colspan="7">
+			<input type="submit" class="btn btn-xs btn-default" name="search" value="<?php echo $script_transl['search'];?>" onClick="javascript:document.report.all.value=1;">
+			<a class="btn btn-xs btn-default" href="?">Reset</a>
+			<?php  $ts->output_order_form(); ?>
+		</td>
+	</tr>
 
-		?>
-		</div>
-    </div>
-    <div class="panel panel-default">
-        <div id="gaz-responsive-table"  class="container-fluid">
-            <table class="table table-responsive table-striped table-condensed cf">
-                <thead>
-                    <tr class="bg-success">              
-                        <th>
-                            <a href="#" class="orby" data-order="codice">
-                                <?php echo $script_transl["codice"]; ?>
-                            </a>
-                        </th>
-                        <th>
-                            <a href="#" class="orby" data-order="descri">
-                                <?php echo $script_transl["descri"]; ?>
-                            </a>
-                        </th>
-                        <th>
-                            <a href="#" class="orby" data-order="good_or_service">
-                                <?php echo $script_transl["good_or_service"]; ?>
-                            </a>
-                        </th>
-                        <th class="text-right">
-                            <a href="#" class="orby" data-order="catmer">
-                                <?php echo $script_transl["catmer"]; ?>
-                            </a>
-                        </th>
-                        <th class="text-right">
-                            <?php echo $script_transl["unimis"]; ?>
-                        </th>
-                        <th class="text-right">
-                            <a href="#" class="orby" data-order="preve1">
-                                <?php echo $script_transl["preve1"]; ?>
-                            </a>
-                        </th>
-                        <!--+ nuova colonna fornitore - DC - 02 feb 2018 -->
-						<th>
-							<a href="#" class="orby" data-order="clfoco">
-								<?php echo $script_transl["clfoco"]; ?>
-							</a>
-                        </th>
-                        <!--- nuova colonna fornitore -->
-                        <th class="text-right">
-                            <?php echo $script_transl["preacq"]; ?>
-                        </th>
-                        <th class="text-right">
-                            <?php echo $script_transl["stock"]; ?>
-                        </th>
-                        <th class="text-center">
-                            <a href="#" class="orby" data-order="aliiva">
-                                <?php echo $script_transl["aliiva"]; ?>
-                            </a>
-                        </th>
-						<th class="text-center">
-                            <?php echo $script_transl["retention_tax"]; ?>
-                        </th>
-                        <th class="text-center">
-                            <?php echo $script_transl["payroll_tax"]; ?>
-                        </th>
-                        <th class="text-center">
-                            <?php echo $script_transl["barcode"]; ?>
-                        </th>
-						 <th class="text-center">
-                            <?php echo $script_transl["lot"]; ?>
-                        </th>
-                        <th class="text-center">
-                            <?php echo $script_transl["clone"]; ?>
-                        </th>
-                        <th class="text-center">
-                            <?php echo $script_transl["delete"]; ?>
-                        </th>
-                    </tr>      
-                </thead>    
-                <tbody id="all_rows">
-                </tbody>     
-            </table>
-        </div>  
-    </div>
-    <input type="hidden" name="row_no" id="row_no" value="<?php echo $rn; ?>">
-    <input type="hidden" name="order_by" id="order_by" value="<?php echo $ob; ?>">
-    <input type="hidden" name="sort" id="sort" value="<?php echo $so; ?>">
+<?php
+$gForm = new magazzForm();
+
+$result = gaz_dbi_dyn_query ( $gTables['artico']. ".*, ".$gTables['catmer']. ".descri AS descat, ".$gTables['catmer']. ".codice AS codcat",$tablejoin, $ts->where, $ts->orderby, $ts->getOffset(), $ts->getLimit());
+
+echo '<tr>';
+$ts->output_headers();
+echo '</tr>';
+while ($r = gaz_dbi_fetch_array($result)) {
+    // da configurazione azienda
+	$show_artico_composit = gaz_dbi_get_row($gTables['company_config'], 'var', 'show_artico_composit');
+	$tipo_composti = gaz_dbi_get_row($gTables['company_config'], 'var', 'tipo_composti');
+    // fornitore
+    $desfor='';
+    if ($r['clfoco']>100000000){
+        $rsfor = gaz_dbi_get_row($gTables['clfoco'], "codice", $r['clfoco']);
+        if ($rsfor) { $desfor = $rsfor['descri']; }
+    }
+    // giacenza
+    $mv = $gForm->getStockValue(false, $r['codice']);
+    $magval = array_pop($mv);
+	 if (isset($magval['q_g']) && round($magval['q_g'],6) == "-0"){
+		 $magval['q_g']=0;
+	 }
+	$class = 'success';
+    if ($magval['q_g'] < 0) { // giacenza inferiore a 0
+        $class = 'danger';
+    } elseif ($magval['q_g'] > 0) { //
+		if ($magval['q_g']<=$row['scorta']){
+			$class = 'warning';
+		}
+    } else { // giacenza = 0
+        $class = 'danger';
+    }
+    // contabilizzazione magazzino
+    $com = '';
+    if ($admin_aziend['conmag'] > 0 && $r["good_or_service"] != 1 && $tipo_composti['val']=="STD") {
+        $com = '<a class="btn btn-xs btn-'.$class.'" href="../magazz/select_schart.php?di=0101' . date('Y') . '&df=' . date('dmY') . '&id=' . $r['codice'] . '" target="_blank">
+	  <i class="glyphicon glyphicon-check"></i><i class="glyphicon glyphicon-print"></i>
+	  </a>';
+    }
+    // IVA
+    $iva = gaz_dbi_get_row($gTables['aliiva'], 'codice', $r['aliiva']);
+    
+    echo "<tr>\n";
+    echo '<td>
+    <a class="btn btn-xs btn-'.$class.'" href="../magazz/admin_artico.php?Update&codice='.$r['codice'].'" ><i class="glyphicon glyphicon-edit"></i> '.$r['codice'].'</a>';
+    if ( $r["good_or_service"] == 2 ) {
+        echo '<a class="btn btn-xs btn-default" href="../magazz/admin_artico_compost.php?Update&codice='.$r['codice'].'" ><i class="glyphicon glyphicon-plus"></i></a>';
+    }
+    echo '</td>';
+    echo '<td><span class="gazie-tooltip" data-type="product-thumb" data-id="'. $r['codice'] .'" data-title="'. $r['annota'].'" >'.$r['descri'].'</span>';
+    echo "</td>\n";
+    echo '<td class="text-center">'.$script_transl['good_or_service_value'][$r['good_or_service']];
+    echo "</td>\n";
+    echo '<td class="text-center">'.$r['catmer'].'-'.$r['descat'];
+    echo "</td>\n";
+    echo '<td class="text-center">'.$r['unimis'];
+	echo "</td>\n";
+    echo '<td class="text-right">'.number_format($r['preve1'], $admin_aziend['decimal_price'], ',', '.');
+	echo "</td>\n";
+    echo '<td>'.$desfor;
+	echo "</td>\n";
+    echo '<td class="text-right">'.number_format($r['preacq'], $admin_aziend['decimal_price'], ',', '.');
+	echo "</td>\n";
+    echo '<td class="text-right">'.gaz_format_quantity(floatval($magval['q_g']),1,$admin_aziend['decimal_quantity']).' '.$com;
+	echo "</td>\n";
+    echo '<td class="text-center">'.floatval($iva['aliquo']);
+	echo "</td>\n";
+    echo '<td class="text-center">';
+	if (intval($r['lot_or_serial'])>0) {
+	?>
+	<a class="btn btn-info btn-md" href="javascript:;" onclick="window.open('<?php echo"../../modules/magazz/mostra_lotti.php?codice=".$r['codice'];?>', 'titolo', 'menubar=no, toolbar=no, width=800, height=400, left=80%, top=80%, resizable, status, scrollbars=1, location');"><span class="glyphicon glyphicon-tag"></span></a>
+	<?php 
+    }	
+    echo "</td>\n";
+    echo '<td class="text-center"><a class="btn btn-xs btn-default" href="clone_artico.php?codice='.$r["codice"].'"> <i class="glyphicon glyphicon-export"></i></a>';
+	echo "</td>\n";
+    echo '<td class="text-center"><a class="btn btn-xs btn-default btn-elimina dialog_delete" ref="'. $r['codice'].'" artico="'. $r['descri'].'"> <i class="glyphicon glyphicon-remove"></i></a>';
+	echo "</td>\n";
+    echo "</tr>\n";
+}
+?>
+     </table>
+	</div>
 </form>
-<div id="loader-icon"><img src="../../library/images/ui-anim_basic_16x16.gif" />
-</div>  
 <?php
 require("../../library/include/footer.php");
 ?>
