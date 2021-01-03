20297f27ea94a6af993c4087d263af5d6ebc8954
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-01-03 10:55:47 +0000

    Cambiato anno sui riferimenti alla licensa
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4199 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- library/tcpdf/config/tcpdf_config.php
+++ library/tcpdf/config/tcpdf_config.php
@@ -8,7 +8,7 @@
 // Author      : Nicola Asuni - Tecnick.com LTD - www.tecnick.com - info@tecnick.com
 // License     : GNU-LGPL v3 (http://www.gnu.org/copyleft/lesser.html)
 // -------------------------------------------------------------------
-// Copyright (C) 2004-2014  Nicola Asuni - Tecnick.com LTD
+// Copyright (C) 2004-2021  Nicola Asuni - Tecnick.com LTD
 //
 // This file is part of TCPDF software library.
 //
