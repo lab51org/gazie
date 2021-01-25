abac831aeaed07e0f39ea5963928b211941d318f
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-01-25 12:29:21 +0000

    Pronta la funzione per generare xml-cbi tracciato bonifico relativamente all'intestazione, mancano gli elementi all'interno del foreach per le singole transazioni

--- trunk/library/include/CBIPaymentRequest.inc.php
+++ trunk/library/include/CBIPaymentRequest.inc.php
@@ -27,21 +27,20 @@
 class CBIPaymentXMLvars {
     function setPaymentsVars($gTables,$bank) {
 		// qui setto tutti i valori per l'intestazione 
-        $this->gTables = $gTables;
-        $admin_aziend = gaz_dbi_get_row($gTables['aziend'], 'codice', $_SESSION['company_id']);
-        $this->azienda = $admin_aziend;
-        $anagrafica = new Anagrafica();
-        //$this->Nm = $anagrafica->getPartner($this->pagame['id_bank']);
-        //$this->IBAN = $this->pagame['iban'];
-        $this->OthrId = (intval($admin_aziend['pariva'])>=100)?$admin_aziend['pariva']:$admin_aziend['codfis'];
+        $this->azienda = gaz_dbi_get_row($gTables['aziend'], 'codice', $_SESSION['company_id']);
+        $this->bank = gaz_dbi_get_row($gTables['clfoco'].' LEFT JOIN '.$gTables['banapp'].' ON '.$gTables['clfoco'].'.banapp = '.$gTables['banapp'].".codice", $gTables['clfoco'].'.codice',$bank);
+        $this->OthrId = (intval($this->azienda['pariva'])>=100)?$this->azienda['pariva']:$this->azienda['codfis'];
         $this->CtrlSum = 0.00;
         $this->NbOfTxs = 0;
-        $this->MsgId = time();
-		$this->CreDtTm = time();
+		$this->CreDtTm = date('Y-m-d\TH:i:s');
+        $this->MsgId = base64_encode($this->CreDtTm);
     }
 }
 
-function create_XML_CBIPayment($gTables,$paymov,$bank) {
+function create_XML_CBIPayment($gTables,$bank,$data) {
+	// in $data dovrò passare tutti i dati necessari per la creazione degli elementi <CdtTrfTxInf>
+	// le chiavi sono: EndToEndId (id univoco) ,InstdAmt (importo), Nm (descrizione creditore), IBAN (iban accredito), Ustrd (descrizione debito pagato) ognuno creerà il relativo elemento dentro <CdtTrfTxInf>
+	// EndToEndId non è indispensabile e se EndToEndId[0] non è valorizzato ne verrà creato uno random e non verrà considerata la presenza sugli eventuali altri indici in quanto ne verrà creato uno random
     $XMLvars = new CBIPaymentXMLvars();
     $domDoc = new DOMDocument;
 	$domDoc->preserveWhiteSpace = false;
@@ -49,6 +48,39 @@ function create_XML_CBIPayment($gTables,$paymov,$bank) {
     $XMLvars->setPaymentsVars($gTables,$bank);
 	$domDoc->load("../../library/include/template_CBIPaymentRequest.xml");
 	$xpath = new DOMXPath($domDoc);
+	$results = $xpath->query("//GrpHdr/MsgId")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->MsgId);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//GrpHdr/CreDtTm")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->CreDtTm);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//GrpHdr/InitgPty/Nm")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->bank['descri']);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//GrpHdr/InitgPty/Id/OrgId/Othr/Id")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->bank['cuc_code']);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//PmtInf/PmtInfId")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->MsgId);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//PmtInf/ReqdExctnDt")->item(0);
+	$attrVal = $domDoc->createTextNode(substr($XMLvars->CreDtTm,0,10));
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//PmtInf/Dbtr/Nm")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->bank['descri']);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//PmtInf/Dbtr/Id/OrgId/Othr/Id")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->OthrId);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//PmtInf/DbtrAcct/Id/IBAN")->item(0);
+	$attrVal = $domDoc->createTextNode($XMLvars->bank['iban']);
+	$results->appendChild($attrVal);
+	$results = $xpath->query("//PmtInf/DbtrAgt/FinInstnId/ClrSysMmbId/MmbId")->item(0);
+	$attrVal = $domDoc->createTextNode(str_pad($XMLvars->bank['codabi'],5,'0',STR_PAD_LEFT));
+	$results->appendChild($attrVal);
+	// creo gli elementi dei singoli bonifici
+	foreach($data as $v){
+	}
 	header("Content-type: text/plain");
 	header("Content-Disposition: attachment; filename=provaCBI.xml");
 	print $domDoc->saveXML();
