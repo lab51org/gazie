82ccc05f3f60273fb2442f92505838c4ad186449
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-01-25 21:17:54 +0000

    Aggiungo lo script per la selezione dei bonifici massivi sul menù

--- trunk/modules/acquis/menu.italian.php
+++ trunk/modules/acquis/menu.italian.php
@@ -55,7 +55,8 @@ $transl['acquis'] = array('name' => " Acquisti",
         17 => array("Pagamento RiBa fornitori", "Registra disposizione pagamenti"),
 		18 => array("Ordine automatico fornitore", "Ordine automatico fornitore"),
 		19 => array("Acquisisci fattura elettronica", "Acquisisci fattura elettronica"),
-        20 => array("Ricevuto DdT ritorno da lavorazione", "Ricevuto DdT ritorno da lavorazione")
+        20 => array("Ricevuto DdT ritorno da lavorazione", "Ricevuto DdT ritorno da lavorazione"),
+        21 => array("Genera XML-CBI bonifico", "Genera XML-CBI bonifico")
     )
 );
 ?>
