9ea13fa67a92eee735295323097802283f8f44d3
giovanniv <giovanniv@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-01-30 20:14:40 +0000

    - Qunado duplico un preventivo clono anche le righe tipo testo invece che lasciarle collegate all'originale come accadeva prima.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4329 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/vendit/duplicate_broven.php
+++ modules/vendit/duplicate_broven.php
@@ -31,24 +31,139 @@ if (!isset($_POST['ritorno'])) {
 }
 if (isset($_POST['Duplicate'])) {
 	//procedo alla duplicazione della testata e dei righi...
-	//duplico la testata
-	$tabella = $gTables['tesbro'];
-	$chiave = $_POST['id_tes'];
-	$numdoc = trovaNuovoNumero($gTables);  // numero nuovo documento
-	$today = gaz_today();
-	$sql = "INSERT INTO $tabella (`id_tes`, `seziva`, `tipdoc`, `template`, `print_total`, `delivery_time`, `day_of_validity`, `datemi`, `protoc`, `numdoc`, `numfat`, `datfat`, `clfoco`, `pagame`, `banapp`, `vettor`, `listin`, `destin`, `id_des`, `id_des_same_company`, `spediz`, `portos`, `imball`, `traspo`, `speban`, `spevar`, `round_stamp`, `cauven`, `caucon`, `caumag`, `id_agente`, `sconto`, `expense_vat`, `stamp`, `taxstamp`, `virtual_taxstamp`, `net_weight`, `gross_weight`, `units`, `volume`, `initra`, `geneff`, `id_contract`, `id_con`, `status`, `adminid`, `last_modified`) "
-	. "SELECT null, `seziva`, `tipdoc`, `template`, `print_total`, `delivery_time`, `day_of_validity`, '$today', `protoc`, $numdoc, '', '', `clfoco`, `pagame`, `banapp`, `vettor`, `listin`, `destin`, `id_des`, `id_des_same_company`, `spediz`, `portos`, `imball`, `traspo`, `speban`, `spevar`, `round_stamp`, `cauven`, `caucon`, `caumag`, `id_agente`, `sconto`, `expense_vat`, `stamp`, `taxstamp`, `virtual_taxstamp`, `net_weight`, `gross_weight`, `units`, `volume`,  '$today', `geneff`, `id_contract`, `id_con`, `status`, `adminid`, CURRENT_TIMESTAMP FROM $tabella WHERE id_tes = $chiave;";
-	mysqli_query($link, $sql);
-	$nuovaChiave = gaz_dbi_last_id();
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	$failure = false;
+	try {
+		$oldIdTes = $_POST['id_tes'];
+		$numdoc = trovaNuovoNumero($gTables);  // numero nuovo documento
+		// avvio una transazione sul DB
+		mysqli_begin_transaction($link);
 
-	//... e i righi
-	$tabella = $gTables['rigbro'];
-	$sql = "INSERT INTO $tabella (`id_rig`, `id_tes`, `tiprig`, `codart`, `descri`, `id_body_text`, `unimis`, `quanti`, `prelis`, `sconto`, `codvat`, `pervat`, `codric`, `provvigione`, `ritenuta`, `delivery_date`, `id_doc`, `id_mag`, `status`) "
-	. "SELECT null, $nuovaChiave, `tiprig`, `codart`, `descri`, `id_body_text`, `unimis`, `quanti`, `prelis`, `sconto`, `codvat`, `pervat`, `codric`, `provvigione`, `ritenuta`, `delivery_date`, 0, 0, 'INSERT' FROM $tabella WHERE id_tes = $chiave;";
-	mysqli_query($link, $sql);
+		/*
+		  - per la copia del record di testata
+		  query param. 1: numero documento da attribuire al nuovo record
+		  query param. 2: id del record originale in tesbro
+		*/
+		$stmtInsTes = mysqli_prepare($link,
+			"INSERT INTO {$gTables['tesbro']} (
+				`id_tes`, `seziva`, `tipdoc`, `template`, `print_total`, `delivery_time`,
+				`day_of_validity`, `datemi`, `protoc`, `numdoc`, `numfat`, `datfat`,
+				`clfoco`, `pagame`, `banapp`, `vettor`, `listin`, `destin`,
+				`id_des`, `id_des_same_company`, `spediz`, `portos`, `imball`, `traspo`,
+				`speban`, `spevar`, `round_stamp`, `cauven`, `caucon`, `caumag`,
+				`id_agente`, `sconto`, `expense_vat`, `stamp`, `taxstamp`, `virtual_taxstamp`,
+				`net_weight`, `gross_weight`, `units`, `volume`, `initra`, `geneff`,
+				`id_contract`, `id_con`, `status`, `adminid`, `last_modified`
+			) (
+			SELECT null, `seziva`, `tipdoc`, `template`, `print_total`, `delivery_time`,
+				`day_of_validity`, DATE(CURRENT_TIMESTAMP), `protoc`, ?, '', '',
+				`clfoco`, `pagame`, `banapp`, `vettor`, `listin`, `destin`,
+				`id_des`, `id_des_same_company`, `spediz`, `portos`, `imball`, `traspo`,
+				`speban`, `spevar`, `round_stamp`, `cauven`, `caucon`, `caumag`,
+				`id_agente`, `sconto`, `expense_vat`, `stamp`, `taxstamp`, `virtual_taxstamp`,
+				`net_weight`, `gross_weight`, `units`, `volume`, DATE(CURRENT_TIMESTAMP), `geneff`,
+				`id_contract`, `id_con`, `status`, `adminid`, CURRENT_TIMESTAMP
+			FROM {$gTables['tesbro']}
+			WHERE id_tes = ?
+			)"
+		);
+		mysqli_stmt_bind_param($stmtInsTes, 'ii', $numdoc, $oldIdTes);
+		mysqli_stmt_execute($stmtInsTes);
+		mysqli_stmt_close($stmtInsTes);
+		$newIdTes = mysqli_insert_id($link);
+		// eccesso di scrupolo...
+		if ($newIdTes <= $oldIdTes) { throw new Exception('Non ho ottenuto una chiave valida per il nuovo documento!'); }
 
-	header("Location: " . $_POST['ritorno']);
-	exit;
+		/*
+		  - per la copia del record di riga documento collegati
+		  query param. 1: id/chiave del nuovo record di testata
+		  query param. 2: id del record originale in tesbro
+		*/
+		$stmtInsRows = mysqli_prepare($link,
+			"INSERT INTO {$gTables['rigbro']} (
+				`id_rig`, `id_tes`, `tiprig`, `codart`, `descri`, `id_body_text`,
+				`unimis`, `quanti`, `prelis`, `sconto`, `codvat`, `pervat`,
+				`codric`, `provvigione`, `ritenuta`, `delivery_date`, `id_doc`, `id_mag`,
+				`status`
+			) (
+			SELECT null, ?, `tiprig`, `codart`, `descri`, `id_body_text`,
+				`unimis`, `quanti`, `prelis`, `sconto`, `codvat`, `pervat`,
+				`codric`, `provvigione`, `ritenuta`, `delivery_date`, 0, 0,
+				'INSERT'
+			FROM {$gTables['rigbro']}
+			WHERE id_tes = ?
+			)"
+		);
+
+		mysqli_stmt_bind_param($stmtInsRows, 'ii', $newIdTes, $oldIdTes);
+		mysqli_stmt_execute($stmtInsRows);
+		mysqli_stmt_close($stmtInsRows);
+
+		/*
+		  individuo gli id delle righe in body_text che dovrò successivamente clonare
+		  query param. 1: id del nuovo record di testata documento
+		*/
+		$stmtSlct = mysqli_prepare($link,
+			"SELECT `id_rig`, `id_body_text`
+			FROM {$gTables['rigbro']}
+			WHERE `tiprig` = 6 AND `id_tes` = ?"
+		);
+		mysqli_stmt_bind_param($stmtSlct, 'i', $newIdTes);
+		mysqli_stmt_execute($stmtSlct);
+		$textRows = mysqli_stmt_get_result($stmtSlct);
+		mysqli_stmt_close($stmtSlct);
+
+		/*
+		  preparo le query che saranno eseguite nel ciclo successivo
+
+		  - per la copia dei record in body_text
+		  query param. 1: id nuova riga documento tipo 6 precedentemente inserita in rig_bro
+		  query param. 2: id del record originale in body_text
+		*/
+		$stmtInsRow = mysqli_prepare($link,
+			"INSERT INTO {$gTables['body_text']} (`table_name_ref`, `id_ref`, `body_text`, `lang_id`) (
+				SELECT `table_name_ref`, ?, `body_text`, `lang_id`
+				FROM {$gTables['body_text']} WHERE `id_body` = ?
+			)"
+		);
+
+		/*
+		  - per la modifica dei riferimenti incrociati tra rigbro e body_text
+		  query param. 1: id del nuovo record inserito in body_text
+		  query param. 2: id nuova riga documento tipo 6 precedentemente inserita in rig_bro
+		  query param. 3: id del nuovo record di testata documento
+		*/
+		$stmtUpdRow = mysqli_prepare($link,
+			"UPDATE {$gTables['rigbro']}
+			SET `id_body_text` = ?
+			WHERE `id_rig` = ? AND `id_tes` = ?"
+		);
+
+		while ($tbl = mysqli_fetch_array($textRows, MYSQLI_ASSOC)) {
+			// copio il record collegato in body_text modificandone i riferimenti
+			mysqli_stmt_bind_param($stmtInsRow, 'ii', $tbl['id_rig'], $tbl['id_body_text']);
+			mysqli_stmt_execute($stmtInsRow);
+			$newIdText = mysqli_insert_id($link);
+			// modifico il riferimento a body_text nella riga documento
+			mysqli_stmt_bind_param($stmtUpdRow, 'iii', $newIdText, $tbl['id_rig'], $newIdTes);
+			mysqli_stmt_execute($stmtUpdRow);
+		}
+		mysqli_stmt_close($stmtInsRow);
+		mysqli_stmt_close($stmtUpdRow);
+	}
+
+	catch (Exception $ex) {
+		$failure = true;
+		mysqli_rollback($link);
+		error_log($ex->__toString());
+	}
+	finally {
+		if (!$failure) { mysqli_commit($link); }
+		// disattivo il report degli errori msqli, che per default è off, onde evitare di influenzare altri script
+		mysqli_report(MYSQLI_REPORT_OFF);
+		header("Location: " . $_POST['ritorno']);
+		exit;
+	}
 }
 
 if (isset($_POST['Return'])) {
