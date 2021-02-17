4b1493010dd62f990a6ab9b501437278876b5a3c
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-02-17 11:15:32 +0000

    Comincio spostamento dati vettori su anagrafiche comuni, gaz_vettor conterrà gli altri dati e permetterà una granularità a livello delle singlole aziende. Una (anagrafica) a molti (mezzi, targhe, conducenti, ecc) della stessa azienda di trasporti, oltra a poter essere cliente/fornitore
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4396 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ setup/install/update_to_7.42.sql
@@ -0,0 +1,7 @@
+UPDATE `gaz_config` SET `cvalue` = '137' WHERE `id` =2;
+-- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+ALTER TABLE `gaz_XXXvettor`	ADD COLUMN `id_anagra` INT(9) NOT NULL DEFAULT '0' AFTER `codice`,	ADD INDEX `id_anagra` (`id_anagra`);
+UPDATE `gaz_XXXvettor` t1 JOIN `gaz_anagra` t2 ON t1.partita_iva = t2.pariva SET t1.id_anagra = t2.id WHERE t1.partita_iva > 0;
+INSERT INTO `gaz_anagra` (ragso1, indspe, capspe, citspe, prospe, country,id_currency,id_language,telefo,pariva,codfis) SELECT ragione_sociale, indirizzo, cap, citta,provincia,'IT',1,1,telefo,partita_iva,codice_fiscale FROM `gaz_XXXvettor` WHERE `gaz_XXXvettor`.`id_anagra` = 0 AND `gaz_XXXvettor`.`partita_iva` > 0;
+UPDATE `gaz_XXXvettor` t1 JOIN `gaz_anagra` t2 ON t1.partita_iva = t2.pariva SET t1.id_anagra = t2.id WHERE t1.partita_iva > 0;
+-- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
