174518965b5454d1c3a86df22173faaa5837b738
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-03-02 19:23:45 +0000

    Corretto mancato invio email conferma ordini dei clienti.

--- trunk/modules/vendit/stampa_ordcli.php
+++ trunk/modules/vendit/stampa_ordcli.php
@@ -39,6 +39,7 @@ if ($tesbro['tipdoc']=='VOR' || $tesbro['tipdoc']=='VOG') {
 	$type=false;
 	$template='OrdineCliente';
     if (isset($_GET['dest'])&& $_GET['dest']=='E' ){ // se l'utente vuole inviare una mail
+		$type='E';
     } 
 	if (isset($_GET['lh'])){ // se l'utente vuole che venga stampata su una carta intestata
 		$type='H';
