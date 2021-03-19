44df52a9b210b0f5cbd8cae073b53c39ec8be9d4
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-03-19 16:49:42 +0000

    Maledizione!!! Ci sta chi mette 8 cifre decimali sui prezzi delle fatture elettroniche, cambiare unità di misura no!?!?

---
+++ trunk/setup/install/update_to_7.43.sql
@@ -0,0 +1,4 @@
+UPDATE `gaz_config` SET `cvalue` = '137' WHERE `id` =2;
+-- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+ALTER TABLE `gaz_XXXrigdoc`	CHANGE COLUMN `prelis` `prelis` DECIMAL(17,8) NULL DEFAULT '0' AFTER `quanti`;
+-- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
