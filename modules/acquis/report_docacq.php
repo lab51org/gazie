64a86afa791cdbee16c7097299c8d40fac5a51be
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-04-16 17:39:25 +0000

    Contabilizzazione fatture acquisto: ho constatato che su alcuni server si soffre la mancanza di transazioni in acquis/accounting_document.php, in particolare allo script viene passato l'id_tes che però non è stato inserito e pertanto rimane orfano. Riparo da codice eliminando il riferimento alla testata del movimento contabile quando questo in realtà non esiste.
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4453 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/acquis/report_docacq.php
+++ modules/acquis/report_docacq.php
@@ -268,9 +268,14 @@ while ($row = gaz_dbi_fetch_array($result)) {
 // Colonna movimenti (info)
     echo "<td align=\"center\">";
     if ($row["id_con"] > 0) {
+      // non usando le transazioni devo aggiunger un controllo di effettiva esistenza della testata di movimento contabile, se qualcosa non è andato per il verso giusto elimini il riferimento
+      $existtesmov = gaz_dbi_get_row($gTables['tesmov'], 'id_tes', $row['id_con']);
+      if ($existtesmov){
         echo " <a class=\"btn btn-xs btn-".$paymov_status['style']."\" style=\"font-size:10px;\" title=\"Modifica il movimento contabile " . $row["id_con"] . " generato da questo documento\" href=\"../contab/admin_movcon.php?id_tes=" . $row["id_con"] . "&Update\"> <i class=\"glyphicon glyphicon-euro\"></i> " . $importo["import"] . "</a> ";
-		
-       // echo "<a class=\"btn btn-xs btn-default btn-default\" href=\"../contab/admin_movcon.php?id_tes=" . $row["id_con"] . "&Update\">Cont. n." . $row["id_con"] . "</a>";
+      } else {
+        gaz_dbi_query("UPDATE ".$gTables['tesdoc']." SET id_con = 0 WHERE id_tes = ".$row['id_tes']);
+        echo "<a class=\"btn btn-xs btn-default btn-cont\" href=\"accounting_documents.php?type=A&last=" . $row["protoc"] . "\">Contabilizza</a>";					
+      }
     } else {
         echo "<a class=\"btn btn-xs btn-default btn-cont\" href=\"accounting_documents.php?type=A&last=" . $row["protoc"] . "\">Contabilizza</a>";					
     }
