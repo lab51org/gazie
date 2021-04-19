2954416c7ce35789f1c39c5e0a2bf754c6d25cbd
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-04-19 17:31:17 +0000

    Impedisco la cancellazione di aliquote IVA utilizzate
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4462 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/config/delete_aliiva.php
+++ modules/config/delete_aliiva.php
@@ -26,24 +26,41 @@ $titolo="Eliminazione aliquota I.V.A.";
 $admin_aziend=checkAdmin();
 $message = "Sei sicuro di voler rimuovere ?";
 if (isset($_POST['Delete'])) {
-        $result = gaz_dbi_del_row($gTables['aliiva'], "codice", $_POST['codice']);
-        header("Location: report_aliiva.php");
-        exit;
+  // eseguo i controlli di integrità, evito la cancellazione nel caso in cui l'aliquota IVA sia stata utilizzata sui rigmoi (codiva), rigdoc e rigbro (codvat)
+  $nt=0;
+  $c=intval($_GET['codice']);
+  $rs=gaz_dbi_query("SELECT COUNT(*) AS nu FROM ".$gTables['rigmoi']." WHERE codiva=".$c);
+  $r=gaz_dbi_fetch_array($rs);
+  $nt += ($r['nu'] >= 1)? $r['nu'] : 0;
+  $rs=gaz_dbi_query("SELECT COUNT(*) AS nu FROM ".$gTables['rigdoc']." WHERE codvat=".$c);
+  $r=gaz_dbi_fetch_array($rs);
+  $nt += ($r['nu'] >= 1)? $r['nu'] : 0;
+  $rs=gaz_dbi_query("SELECT COUNT(*) AS nu FROM ".$gTables['rigbro']." WHERE codvat=".$c);
+  $r=gaz_dbi_fetch_array($rs);
+  $nt += ($r['nu'] >= 1)? $r['nu'] : 0;
+  if ($nt >= 1) {
+    $message= "Errore: stai tentando di eliminare una aliquota usata ".$nt." volte su una o più tabelle Movimenti IVA, Righi documenti fiscali, righi ordini/preventivi.";  
+    $form = gaz_dbi_get_row($gTables['aliiva'], "codice", $c);
+  }else{
+    $result = gaz_dbi_del_row($gTables['aliiva'], "codice", $c);
+    header("Location: report_aliiva.php");
+    exit;
+  }
+} else {
+  $form = gaz_dbi_get_row($gTables['aliiva'], "codice", intval($_GET['codice']));
 }
 if (isset($_POST['Return'])){
         header("Location: report_aliiva.php");
         exit;
 }
-if (!isset($_POST['Delete'])){
-    $form = gaz_dbi_get_row($gTables['aliiva'], "codice", intval($_GET['codice']));
-}
 require("../../library/include/header.php");
 $script_transl=HeadMain('','','admin_aliiva');
 ?>
 <form method="POST">
-<input type="hidden" name="codice" value="<?php print intval($_GET['codice'])?>">
-<div align="center" class="FacetFormHeaderFont"><?php echo $script_transl['warning'].'!!! '.$script_transl['delete'].$script_transl[0].' n.'.intval($_GET['codice']); ?> </div>
-<table class="GazFormDeleteTable">
+<div class="FacetFormHeaderFont text-center"><?php echo $script_transl['warning'].'!!! '.$script_transl['delete'].$script_transl[0].' ID '.intval($_GET['codice']); ?> </div>
+<div class="text-center"><b class="text-danger"><?php echo $message; ?></b> </div>
+<div class="table-responsive">
+<table class="GazFormDeleteTable" style="margin: auto; width: 400px !important;">
   <tr>
     <td class="FacetFieldCaptionTD"><?php echo $script_transl[3]; ?></td>
     <td class="FacetDataTD"><?php print $form["aliquo"] ?>&nbsp;</td>
@@ -56,6 +73,10 @@ $script_transl=HeadMain('','','admin_aliiva');
     <td class="FacetFieldCaptionTD"><?php echo $script_transl[9]; ?></td>
     <td class="FacetDataTD"><?php print  $script_transl['tipiva'][$form["tipiva"]] ?></td>
   </tr>
+ <tr>
+    <td class="FacetFieldCaptionTD">Codice esenzione</td>
+    <td class="FacetDataTD"><?php print $form["fae_natura"] ?></td>
+  </tr>
 <tr>
     <td align="right">
 <?php
@@ -65,7 +86,7 @@ echo ' <input type="submit" accesskey="d" name="Delete" value="'.$script_transl[
 ?>
 </td>
 </tr>
-</table>
+</table></div>
 </form>
 <?php
 require("../../library/include/footer.php");
