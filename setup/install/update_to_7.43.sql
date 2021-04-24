43f7340d3f3de05435c5eeb76664fc54a4207c06
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-04-24 13:47:05 +0000

    GAzie 7.43 ready
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4490 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.43.sql
+++ setup/install/update_to_7.43.sql
@@ -1,9 +1,9 @@
-UPDATE `gaz_config` SET `cvalue` = '137' WHERE `id` =2;
-ALTER TABLE `gaz_menu_script`	ADD INDEX `id_menu` (`id_menu`);
-ALTER TABLE `gaz_menu_script`	ADD INDEX `link` (`link`);
-ALTER TABLE `gaz_menu_module`	ADD INDEX `id_module` (`id_module`), ADD INDEX `link` (`link`);
-ALTER TABLE `gaz_module` ADD INDEX `name` (`name`),	ADD INDEX `link` (`link`);
+UPDATE `gaz_config` SET `cvalue` = '138' WHERE `id` =2;
+ALTER TABLE `gaz_menu_script`	ADD INDEX (`id_menu`);
+ALTER TABLE `gaz_menu_script`	ADD INDEX (`link`);
+ALTER TABLE `gaz_menu_module`	ADD INDEX (`id_module`), ADD INDEX (`link`);
+ALTER TABLE `gaz_module` ADD INDEX (`name`),	ADD INDEX (`link`);
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXrigdoc`	CHANGE COLUMN `prelis` `prelis` DECIMAL(17,8) NULL DEFAULT '0' AFTER `quanti`;
-DROP IF EXISTS TABLE `gaz_XXXfornitore_magazzino`;
+DROP TABLE IF EXISTS `gaz_XXXfornitore_magazzino`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
