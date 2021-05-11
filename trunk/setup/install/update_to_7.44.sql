c16236f46d60648f3419c48443e460a4917613dc
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-05-11 16:54:52 +0000

    brexit

--- trunk/setup/install/update_to_7.44.sql
+++ trunk/setup/install/update_to_7.44.sql
@@ -1,4 +1,5 @@
 UPDATE `gaz_config` SET `cvalue` = '138' WHERE `id` =2;
+UPDATE `gaz_country` SET `istat_area`='12' WHERE  `iso`='GB';
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXfiles` ADD INDEX (`item_ref`);
 ALTER TABLE `gaz_XXXfiles`	CHANGE COLUMN `item_ref` `item_ref` VARCHAR(100) NOT NULL COMMENT 'a secondo dell\'utilizzo che se ne fà può contenere il codice articolo, il filename o altre referenze' AFTER `id_ref`;
