d2ed9322ae413da9dd82dad1aa91a56311edc078
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-05-13 05:59:52 +0000

    Risolto un notice generato da prepare_ref_doc_movmag.php

--- trunk/modules/magazz/prepare_ref_doc_movmag.php
+++ trunk/modules/magazz/prepare_ref_doc_movmag.php
@@ -4,7 +4,7 @@ function magazz_prepare_ref_doc($tipdoc,$id_rif){
     $acc=[];
     switch ($tipdoc){
         default:
-        $acc['link']=($r)?"admin_movmag.php?Update&id_mov=".$id_rif:'';
+        $acc['link']=($id_rif)?"admin_movmag.php?Update&id_mov=".$id_rif:'';
         break;
     }
     return $acc;
