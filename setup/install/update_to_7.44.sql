005650a585a530851b9a86241b3d6347d1ff2ad8
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-06-01 07:20:58 +0000

    descrizione estesa categorie merceologiche
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4552 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.44.sql
+++ setup/install/update_to_7.44.sql
@@ -21,4 +21,5 @@ CREATE TABLE IF NOT EXISTS `gaz_XXXartico_group` (
   PRIMARY KEY (`id_artico_group`),
   KEY `ref_ecommerce_id_main_product` (`ref_ecommerce_id_main_product`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+ALTER TABLE `gaz_XXXcatmer`	ADD COLUMN `large_descri` TEXT NOT NULL AFTER `descri`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
