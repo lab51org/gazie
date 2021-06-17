e3c1fd4eefd1209ff0f44ed981b57cf7f6a306ac
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-06-17 15:44:59 +0000

    Questo mi permette di visualizzare ed eventualmente fare il restore degli xml corrotti dalla 7.44

--- trunk/config/config/gconfig.php
+++ trunk/config/config/gconfig.php
@@ -45,7 +45,7 @@ DELLA CARTELLA "gazie" COMPRESO IL PRESENTE "gconfig.php"
     exit('Accesso diretto non consentito');
 }
 //versione software
-define('GAZIE_VERSION', '7.44');
+define('GAZIE_VERSION', '7.45');
 
 $fileConfPers=dirname(__FILE__) . '/gconfig.myconf.php';
 if (file_exists($fileConfPers)) {
