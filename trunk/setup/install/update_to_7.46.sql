83cecefabb2908ebb084291a04fac7ecfe4a8e9b
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-07-19 11:28:44 +0000

    Aggiungo alcuni indici mancanti ed una colonna per poter contabilizzare (in futuro) anche i documenti non facenti parte dei cicli attivi/passivi di fatturazione

--- trunk/setup/install/update_to_7.46.sql
+++ trunk/setup/install/update_to_7.46.sql
@@ -4,4 +4,7 @@ INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT MIN(id) FROM `gaz_menu_m
 ALTER TABLE `gaz_XXXartico`	ADD COLUMN `durability` INT(4) NOT NULL DEFAULT '0' COMMENT 'Durabilità anche se solo presunta espressa in unità di misura specificata nel campo successivo' AFTER `maintenance_period`,
 	ADD COLUMN `durability_mu` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Unità di misura della durabilità di cui al campo precedente (H=ore,D=giorni,M=mesi) nel caso di durabilità alimenti (<=minore di,>=maggiore di)' AFTER `durability`,
 	ADD COLUMN `warranty_days` INT(4) NOT NULL DEFAULT '0' COMMENT 'Durata della garanzia in giorni' AFTER `durability_mu`;
+ALTER TABLE `gaz_XXXrigbro`
+	ADD COLUMN `id_rigmoc` INT(9) NULL DEFAULT NULL COMMENT 'Riferimento ad id_rig della tabella gaz_NNNrigmoc' AFTER `id_orderman`,
+	ADD INDEX `id_rigmoc` (`id_rigmoc`);    
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
