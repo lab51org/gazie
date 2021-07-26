9f90d1e5ec0a3e6d32f6cce8f0188f145fa6ca4a
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-07-26 08:59:35 +0000

    Elimino un po' di notice dai moduli magazzino e produzione prima di procedere alla valorizzazione dei movimenti di magazzino derivanti da produzioni
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4668 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/orderman/prepare_ref_doc_movmag.php
+++ modules/orderman/prepare_ref_doc_movmag.php
@@ -1,10 +1,10 @@
 <?php
-function orderman_prepare_ref_doc($tipdoc,$id_rif){
+function orderman_prepare_ref_doc($tipdoc,$id_rif=false){
     global $gTables;
     $acc=[];
     switch ($tipdoc){
         default:
-        $acc['link']=($r)?"../orderman/admin_orderman.php?Update&codice=".$id_rif:'';
+        $acc['link']=($id_rif)?"../orderman/admin_orderman.php?Update&codice=".$id_rif:'';
         break;
     }
     return $acc;
