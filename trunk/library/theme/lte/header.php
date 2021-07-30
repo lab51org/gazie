e7eec781dc928e025f20414b3fdadd7a50b4a112
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-07-30 12:29:50 +0000

    Bottone di alert dei moduli sincronizzati: faccio lo strip dei tag html dal title del bottone, tag che invece possono essere usati dentro al dialog per facilitarne la lettura

--- trunk/library/theme/lte/header.php
+++ trunk/library/theme/lte/header.php
@@ -145,7 +145,7 @@ function menu_alerts_check(mod,title,button,label,link,style){
 	// questa funzione attiva l'alert sulla barra del menù e viene richiamata sia dalla funzione menu_check_from_modules() dal browser tramite setInterval che alla fine della pagina (lato server) quando il controllo fatto dal php tramite $_SESSION['menu_alerts_lastcheck'] è scaduto
     // faccio append solo se già non esiste
     if (style.length >= 2) { // solo se style è valorizzato faccio l'alert sul menu 
-        $("li.blink").html( '<a mod="'+mod+'" class="btn btn-'+style+' dialog_menu_alerts" title="'+title+'" >'+button+'</a>').click(function() {
+        $("li.blink").html( '<a mod="'+mod+'" class="btn btn-'+style+' dialog_menu_alerts" title="'+title.replace(/(<([^>]+)>)/ig,"")+'" >'+button+'</a>').click(function() {
 			$("p#diatitle").html(title);
 			$( "#dialog_menu_alerts" ).dialog({
                 title: button ,
