1817544c52bd7620fa32d8e3198483ff6956d892
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-08-05 07:46:26 +0000

    Aggiunto su DB il numero di autorizzazione fitofarmaci
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4692 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.46.sql
+++ setup/install/update_to_7.46.sql
@@ -1,5 +1,6 @@
 UPDATE `gaz_config` SET `cvalue` = '141' WHERE `id` =2;
 INSERT INTO `gaz_menu_script` SELECT MAX(id)+1, (SELECT MIN(id) FROM `gaz_menu_module` WHERE `link`='report_artico.php'), 'admin_group.php?Insert', '', '', 14, '', 2  FROM `gaz_menu_script`;
+ALTER TABLE `gaz_camp_uso_fitofarmaci` ADD `numero_registrazione` INT(6) NOT NULL COMMENT 'Riferito alla tabella camp_fitofarmaci' AFTER `tempo_sosp`;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXartico`	ADD COLUMN `durability` INT(4) NOT NULL DEFAULT '0' COMMENT 'Durabilità anche se solo presunta espressa in unità di misura specificata nel campo successivo' AFTER `maintenance_period`,
 	ADD COLUMN `durability_mu` VARCHAR(1) NULL DEFAULT NULL COMMENT 'Unità di misura della durabilità di cui al campo precedente (H=ore,D=giorni,M=mesi) nel caso di durabilità alimenti (<=minore di,>=maggiore di)' AFTER `durability`,
