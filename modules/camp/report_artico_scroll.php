0f1642266a376e498780b25cc85e712509ee128a
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-08-09 07:10:03 +0000

    Ripristinato questo file che non andava modificato
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4708 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/report_artico_scroll.php
+++ modules/camp/report_artico_scroll.php
@@ -53,18 +53,18 @@ if (isset($_POST['rowno'])) { //	Evitiamo errori se lo script viene chiamato dir
     $so = filter_input(INPUT_POST, 'sort');
     $ca = filter_input(INPUT_POST, 'codart');
 	$mt = filter_input(INPUT_POST, 'mostra'); 
-	$where = "";
-	$addwhere = "";
-    if (!empty($ca)) {   
+	
+    if (empty($ca)) {
+        $where = '1';
+    } else {
         $where = "codice = '" . $ca . "'";
         $no = '0';
-		$addwhere=" AND ";
     }
 	if ($mt==0){
-		$where=$where.$addwhere."mostra_qdc = 1"; // Antonio Germani seleziona quali prodotti mostrare nell'elenco
+		$where=$where." AND mostra_qdc = '1' "; // Antonio Germani seleziona quali prodotti mostrare nell'elenco
 	}
     $gForm = new magazzForm();
-    $result = gaz_dbi_dyn_query('*', $gTables['artico'], $where, $ob . ' ' . $so, $no);
+    $result = gaz_dbi_dyn_query('*', $gTables['artico'], $where, $ob . ' ' . $so, $no, PER_PAGE);
     while ($row = gaz_dbi_fetch_array($result)) {
 		unset ($magval);unset($mv);
 		$lastdoc = getLastDoc($row["codice"]);
