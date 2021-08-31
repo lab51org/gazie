f9528e7bd8d3abdca522327d563814ae8b980434
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-08-31 08:22:51 +0000

    Per allineare alcuni db carenti
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@4801 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_7.46.php
+++ setup/install/update_to_7.46.php
@@ -66,7 +66,7 @@ $result = gaz_dbi_dyn_query("*", $table_prefix.'_aziend', 1);
 while ($row = gaz_dbi_fetch_array($result)) {
 	$aziend_codice = sprintf("%03s", $row["codice"]);
 	// inizio controlli presenza di indici altrimenti li creo 
-	$idx=array(0=>array('codart'=>'rigbro','tiprig'=>'rigbro','id_doc'=>'rigbro','id_mag'=>'rigbro','id_contract'=>'tesbro','id_con'=>'tesbro','id_orderman'=>'tesbro','filename_ori'=>'fae_flux','filename_zip_package'=>'fae_flux','id_tes_ref'=>'fae_flux','filename_son'=>'fae_flux','id_SDI'=>'fae_flux','flux_status'=>'fae_flux','mail_id'=>'fae_flux','exec_date'=>'fae_flux','received_date'=>'fae_flux','delivery_date'=>'fae_flux','operat'=>'movmag','id_lotmag'=>'movmag','id_assets'=>'movmag')); 
+	$idx=array(0=>array('codart'=>'rigbro','tiprig'=>'rigbro','id_doc'=>'rigbro','id_mag'=>'rigbro','id_contract'=>'tesbro','id_con'=>'tesbro','id_orderman'=>'tesbro','filename_ori'=>'fae_flux','filename_zip_package'=>'fae_flux','id_tes_ref'=>'fae_flux','filename_son'=>'fae_flux','id_SDI'=>'fae_flux','flux_status'=>'fae_flux','mail_id'=>'fae_flux','exec_date'=>'fae_flux','received_date'=>'fae_flux','delivery_date'=>'fae_flux','operat'=>'movmag','id_lotmag'=>'movmag','id_assets'=>'movmag','numfat'=>'tesdoc','datfat'=>'tesdoc')); 
 	foreach($idx as $vi){
 		foreach($vi as $k=>$v){
 			$rk=gaz_dbi_query("SHOW KEYS FROM ". $table_prefix . "_" . $aziend_codice.$v." WHERE 1");
