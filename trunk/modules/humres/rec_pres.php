c167c3efad7eb1dd30a2fb081ddf06b36c83fbb8
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-09-23 15:29:32 +0000

    Impedito accesso diretto ai file con chiamata ajax

--- trunk/modules/humres/rec_pres.php
+++ trunk/modules/humres/rec_pres.php
@@ -20,6 +20,13 @@
   Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
   --------------------------------------------------------------------------
  */
+ // prevent direct access
+$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND
+        strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+if (!$isAjax) {
+    $user_error = 'Access denied - not an AJAX request...';
+    trigger_error($user_error, E_USER_ERROR);
+}
 require("../../library/include/datlib.inc.php");
 $admin_aziend = checkAdmin();
 //echo "<pre>",print_r ($_POST);
