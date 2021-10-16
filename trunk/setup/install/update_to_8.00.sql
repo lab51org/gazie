775c445629479ed1d23e8bb004393fd35802ec2c
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-10-16 14:10:06 +0000

    Upgrade db lo farò solo al rilascio versione, per adesso lo bypasso

--- trunk/setup/install/update_to_8.00.sql
+++ trunk/setup/install/update_to_8.00.sql
@@ -1,4 +1,4 @@
-UPDATE `gaz_config` SET `cvalue` = '143' WHERE `id` =2;
+UPDATE `gaz_config` SET `cvalue` = '100' WHERE `id` =2;
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXcamp_mov_sian` CHANGE `varieta` `varieta` VARCHAR(250) NOT NULL COMMENT 'Campo descrittivo della varietà da utilizzare per il campo note nel registro telematico oli SIAN';
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
