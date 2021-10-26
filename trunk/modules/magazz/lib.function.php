bd4fc4dc55a691996202debc5559bc778ec9e5fc
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-10-26 14:49:37 +0000

    Warning

--- trunk/modules/magazz/lib.function.php
+++ trunk/modules/magazz/lib.function.php
@@ -747,6 +747,9 @@ class magazzForm extends GAzieForm {
     function uploadMag($id_rigo_doc = '0', $tipdoc='', $numdoc=0, $seziva='', $datdoc='', $clfoco=0, $sconto_chiusura=0, $caumag='', $codart='', $quantita=0, $prezzo=0, $sconto_rigo=0, $id_movmag = 0, $stock_eval_method = null, $data_from_admin_mov = false, $protoc = '',$id_lotmag=0,$id_orderman=0,$campo_impianto=0,$custom_field='') {  // su id_rigo_doc 0 per inserire 1 o + per fare l'upload 'DEL' per eliminare il movimento
         // in $data_from_admin_mov  ci sono i dati in più provenienti da admin_movmag (desdoc,operat, datreg)
         global $gTables, $admin_aziend;
+		$synccommerce=explode(',',$admin_aziend['gazSynchro'])[0];		
+        $admin_aziend['synccommerce_classname'] = preg_replace("/[^a-zA-Z]/", "",$synccommerce)."gazSynchro";
+		
         $docOperat = $this->getOperators();
         if ($tipdoc == 'FAD') {  // per il magazzino una fattura differita è come dire DDT
             $tipdoc = 'DDT';
@@ -789,7 +792,7 @@ class magazzForm extends GAzieForm {
             $datreg = $data_from_admin_mov['datreg']; // prendo la descrizione e l'operatore da questo
             $operat = $data_from_admin_mov['operat'];
             $desdoc = $data_from_admin_mov['desdoc'];
-		}
+		}	
         $row_movmag = array('caumag' => $caumag,
             'operat' => $operat,
             'datreg' => $datreg,
