16f9de3b80456a936a90c3bbe687c73b018328ad
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-11-11 09:46:25 +0000

    Produzioni: apertura pdf riepilogo in iframe
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@5088 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/orderman/orderman_report.php
+++ modules/orderman/orderman_report.php
@@ -139,6 +139,16 @@ $(function() {
 	});
     
 });
+function printPdf(urlPrintDoc){
+	$(function(){			
+		$('#framePdf').attr('src',urlPrintDoc);
+		$('#framePdf').css({'height': '100%'});
+		$('.framePdf').css({'display': 'block','width': '90%', 'height': '80%', 'z-index':'2000'});
+		$('#closePdf').on( "click", function() {
+			$('.framePdf').css({'display': 'none'});
+		});	
+	});	
+};
 </script>
 <div align="center" class="FacetFormHeaderFont">Elenco produzioni</div>
 <?php
@@ -146,6 +156,13 @@ $ts->output_navbar();
 
 ?>
 <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<div class="framePdf panel panel-success" style="display: none; position: absolute; left: 5%; top: 100px">
+		<div class="col-lg-12">
+			<div class="col-xs-11"><h4><?php echo $script_transl['print'];; ?></h4></div>
+			<div class="col-xs-1"><h4><button type="button" id="closePdf"><i class="glyphicon glyphicon-remove"></i></button></h4></div>
+		</div>
+		<iframe id="framePdf"  style="height: 100%; width: 100%" src=""></iframe>
+	</div>
 	<div style="display:none" id="dialog_delete" title="Conferma eliminazione">
         <p><b>produzione:</b></p>
         <p>codice:</p>
@@ -256,13 +273,7 @@ while ($r = gaz_dbi_fetch_array($result)) {
 			<!-- Colonna stampa distinta -->
 			<?php
 			if ($r['order_type']=="IND" or $r['order_type']=="ART"){
-			?>
-			<td align="center" title="Stampa riepilogo della produzione">
-				<a class="btn btn-xs btn-info" href="stampa_produzione.php?id_orderman=<?php echo $r['id']; ?>">
-					<i class="glyphicon glyphicon-list-alt"></i>
-				</a>
-			</td>
-			<?php
+				echo "<td align=\"center\"><a class=\"btn btn-xs btn-info\" style=\"cursor:pointer;\" onclick=\"printPdf('stampa_produzione.php?id_orderman=".$r['id']."')\"><i class=\"glyphicon glyphicon-list-alt\" title=\"Stampa riepilogo produzione PDF\"></i></a></td>";
 			} else  {
 				echo '<td></td>';
 			}
