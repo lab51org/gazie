91b9b1b828229596e5dd786ebe7ae1c32ed49f2d
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2021-12-19 16:29:29 +0000

    shop-synchronize: migliorata la gestione dell'esportazione delle immagini da GAzie all'e-commerce
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@5214 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/shop-synchronize/export_articoli.php
+++ modules/shop-synchronize/export_articoli.php
@@ -6,28 +6,28 @@
 	  (http://www.devincentiis.it)
 	  <http://gazie.sourceforge.net>
 	  --------------------------------------------------------------------------
-	  SHOP SYNCHRONIZE è un modulo creato per GAzie da Antonio Germani, Massignano AP 
+	  SHOP SYNCHRONIZE è un modulo creato per GAzie da Antonio Germani, Massignano AP
 	  Copyright (C) 2018-2021 - Antonio Germani, Massignano (AP)
-	  https://www.lacasettabio.it 
+	  https://www.lacasettabio.it
 	  https://www.programmisitiweb.lacasettabio.it
 	  --------------------------------------------------------------------------
 	  Questo programma e` free software;   e` lecito redistribuirlo  e/o
 	  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
 	  come e` pubblicata dalla Free Software Foundation; o la versione 2
 	  della licenza o (a propria scelta) una versione successiva.
-	
+
 	  Questo programma  e` distribuito nella speranza  che sia utile, ma
 	  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
 	  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
 	  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
-	
+
 	  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
 	  Generica GNU insieme a   questo programma; in caso  contrario,  si
 	  scriva   alla   Free  Software Foundation,  Inc.,   59
 	  Temple Place, Suite 330, Boston, MA 02111-1307 USA Stati Uniti.
-	  --------------------------------------------------------------------------	 
+	  --------------------------------------------------------------------------
 	  # free to use, Author name and references must be left untouched  #
-	  --------------------------------------------------------------------------	  
+	  --------------------------------------------------------------------------
 */
 /* Antonio Germani - ESPORTAZIONE MANUALE (update) DEGLI ARTICOLI DA GAZIE ALL'E-COMMERCE -  GLI ARTICOLI DEVONO GIà ESISTERE NELL'E-COMMERCE ALTRIMENTI NON VERRANNO CONSIDERATI */
 
@@ -65,11 +65,11 @@ use phpseclib3\Net\SFTP;
 if (!isset($_POST['ritorno'])) {
     $_POST['ritorno'] = $_SERVER['HTTP_REFERER'];
 }
-if (isset($_POST['Return'])) { 
+if (isset($_POST['Return'])) {
         header("Location: " . $_POST['ritorno']);
         exit;
     }
- 
+
 if (isset($_POST['conferma'])) { // se confermato
 	if (gaz_dbi_get_row($gTables['company_config'], 'var', 'Sftp')['val']=="SI"){
 
@@ -79,7 +79,7 @@ if (isset($_POST['conferma'])) { // se confermato
 
 		if (gaz_dbi_get_row($gTables['company_config'], "var", "keypass")['val']=="key"){ // SFTP log-in con KEY
 			$key = PublicKeyLoader::load(file_get_contents('../../data/files/'.$admin_aziend['codice'].'/secret_key/'. $ftp_key .''),$ftp_pass);
-			
+
 			$sftp = new SFTP($ftp_host, $ftp_port);
 			if (!$sftp->login($ftp_user, $key)) {
 				// non si connette: key LOG-IN FALSE
@@ -99,7 +99,7 @@ if (isset($_POST['conferma'])) { // se confermato
 				<?php
 			}
 		} else { // SFTP log-in con password
-		
+
 			$sftp = new SFTP($ftp_host, $ftp_port);
 			if (!$sftp->login($ftp_user, $ftp_pass)) {
 				// non si connette: password LOG-IN FALSE
@@ -116,16 +116,16 @@ if (isset($_POST['conferma'])) { // se confermato
 				</div>
 				<?php
 			}
-		}				
+		}
 	} else {
 		// imposto la connessione al server
 		$conn_id = ftp_connect($ftp_host);
-		
+
 		// effettuo login con user e pass
 		$mylogin = ftp_login($conn_id, $ftp_user, $ftp_pass);
-		
+
 		// controllo se la connessione è OK...
-		if ((!$conn_id) or (!$mylogin)){			
+		if ((!$conn_id) or (!$mylogin)){
 			?>
 			<script>
 			alert("<?php echo "Errore: connessione FTP a " . $ftp_host . " non riuscita!"; ?>");
@@ -135,32 +135,32 @@ if (isset($_POST['conferma'])) { // se confermato
 		}
 		//FTP turn passive mode on
 		ftp_pasv($conn_id, true);
-	}	
+	}
 	if ($_GET['img']=="updimg"){ // se si devono aggiornare le immagini
-		if (!@ftp_mkdir($conn_id, $ftp_path_upload."images")){ // se non c'è la cartella images la creo				
-			// get contents of the current directory	
+		if (!@ftp_mkdir($conn_id, $ftp_path_upload."images")){ // se non c'è la cartella images la creo
+			// get contents of the current directory
 			$files = ftp_nlist($conn_id, $ftp_path_upload."images");
+
 			foreach ($files as $file){ // se c'era, cancello i files del precedente aggiornamento
-				if (@ftp_delete($conn_id, $ftp_path_upload."images/".$file)){
-					
+				if (@ftp_delete($conn_id, $file)){
 				} else {
 					header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=6");
 					exit;
 				}
 			}
 		}
-	}	
-	
+	}
+
 		// creo il file xml
 	$xml_output = '<?xml version="1.0" encoding="UTF-8"?>
 	<GAzieDocuments AppVersion="1" Creator="Antonio Germani Copyright" CreatorUrl="https://www.lacasettabio.it">';
 	$xml_output .= "\n<Products>\n";
 	for ($ord=0 ; $ord<=$_POST['num_products']; $ord++){// ciclo gli articoli e creo il file xml
-		if (isset($_POST['download'.$ord])){ // se selezionato	
+		if (isset($_POST['download'.$ord])){ // se selezionato
 			if ((isset ($_POST['barcode'.$ord]) AND intval($_POST['barcode'.$ord])==0) OR !isset ($_POST['barcode'.$ord])) {
 				$_POST['barcode'.$ord]="NULL";
-			} 
-			
+			}
+
 			$xml_output .= "\t<Product>\n";
 			$xml_output .= "\t<Id>".$_POST['ref_ecommerce_id_product'.$ord]."</Id>\n";
 			$xml_output .= "\t<IdMain>".$_POST['ref_ecommerce_id_main_product'.$ord]."</IdMain>\n";
@@ -199,31 +199,32 @@ if (isset($_POST['conferma'])) { // se confermato
 				$xml_output .= "\t<Description>".preg_replace('/[\x00-\x1f]/','',htmlspecialchars($_POST['body_text'.$ord]))."</Description>\n";
 			}
 			$xml_output .= "\t<WebPublish>".$_POST['web_public'.$ord]."</WebPublish>\n";// 1=attivo su web; 2=attivo e prestabilito; 3=attivo e pubblicato in home; 4=attivo, in home e prestabilito; 5=disattivato su web"
-
 			if ($_GET['img']=="updimg" AND strlen($_POST['imgurl'.$ord])>0){ // se è da aggiornare e c'è un'immagine HQ
-				if (ftp_put($conn_id, $ftp_path_upload."images/".$_POST['imgname'.$ord], $_POST['imgurl'.$ord],  FTP_BINARY)){					
-					// scrivo l'immagine web HQ nella cartella e-commerce
+				if (ftp_put($conn_id, $ftp_path_upload."images/".$_POST['imgname'.$ord], $_POST['imgurl'.$ord],  FTP_BINARY)){
+					// ho scritto l'immagine web HQ nella cartella e-commerce
+          ftp_chmod($conn_id, 0664, $ftp_path_upload."images/".$_POST['imgname'.$ord]);// fornisco i permessi necessari all'immagine
 					$xml_output .= "\t<ImgUrl>".$web_site_path."images/".$_POST['imgname'.$ord]."</ImgUrl>\n"; // ne scrivo l'url nel file xml
 				} else {
-					// ERRORE chiudo la connessione FTP 
-					ftp_quit($conn_id);
-					header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=5");
-					exit;
-				}				
-			}
-			if ($_GET['img']=="updimg" AND strlen($_POST['imgblob'.$ord])>0){// se è da aggiornare e c'è un'immagine blob			  
-				file_put_contents("../../data/files/tmp/img.jpg", base64_decode($_POST['imgblob'.$ord])); // salvo immagine nella cartella temporanea
-				if (ftp_put($conn_id, $ftp_path_upload."images/".$_POST['codice'.$ord].".jpg", "../../data/files/tmp/img.jpg",  FTP_BINARY)){					
-					// scrivo l'immagine web blob nella cartella images dell'e-commerce
-					$xml_output .= "\t<ImgUrl>".$web_site_path."images/".$_POST['codice'.$ord].".jpg</ImgUrl>\n"; // ne scrivo l'url nel file xml
-				} else {
-					// ERRORE chiudo la connessione FTP 
+					// ERRORE chiudo la connessione FTP
 					ftp_quit($conn_id);
 					header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=5");
 					exit;
 				}
 			}
-			
+			if ($_GET['img']=="updimg" AND strlen($_POST['imgblob'.$ord])>0){// se è da aggiornare e c'è un'immagine blob
+				file_put_contents("../../data/files/tmp/img.jpg", base64_decode($_POST['imgblob'.$ord])); // salvo immagine nella cartella temporanea
+				if (ftp_put($conn_id, $ftp_path_upload."images/".$_POST['codice'.$ord].".jpg", "../../data/files/tmp/img.jpg",  FTP_BINARY)){
+					// scrivo l'immagine web blob nella cartella images dell'e-commerce
+          ftp_chmod($conn_id, 0664, $ftp_path_upload."images/".$_POST['codice'.$ord].".jpg");// fornisco i permessi necessari all'immagine
+					$xml_output .= "\t<ImgUrl>".$web_site_path."images/".$_POST['codice'.$ord].".jpg</ImgUrl>\n"; // ne scrivo l'url nel file xml
+				} else {
+					// ERRORE chiudo la connessione FTP
+					ftp_quit($conn_id);
+					header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=5");
+					exit;
+				}
+			}
+
 			$xml_output .= "\t</Product>\n";
 		}
 	}
@@ -232,9 +233,9 @@ if (isset($_POST['conferma'])) { // se confermato
 	$xmlHandle = fopen($xmlFile, "w");
 	fwrite($xmlHandle, $xml_output);
 	fclose($xmlHandle);
-	
+
 	if (gaz_dbi_get_row($gTables['company_config'], 'var', 'Sftp')['val']=="SI"){
-		
+
 		if ($sftp->put($ftp_path_upload."prodotti.xml", $xmlFile, SFTP::SOURCE_LOCAL_FILE)){
 			$sftp->disconnect();
 			?>
@@ -243,24 +244,24 @@ if (isset($_POST['conferma'])) { // se confermato
 			<strong>ok</strong> il file xml è stato trasferito al sito web tramite SFTP.
 			</div>
 			-->
-			<?php			
+			<?php
 		}else {
-			// chiudo la connessione FTP 
+			// chiudo la connessione FTP
 			$sftp->disconnect();
 			?>
 			<script>
 			alert("<?php echo "Errore di upload del file xml tramite SFTP"; ?>");
 			location.replace("<?php echo $_POST['ritorno']; ?>");
 			</script>
-			<?php			
-		}	
+			<?php
+		}
 	} else { // FTP semplice
 		// upload file xml
 		if (ftp_put($conn_id, $ftp_path_upload."prodotti.xml", $xmlFile, FTP_ASCII)){
 			// è OK
 			//echo "xml trasferito";
 		} else{
-			// ERRORE chiudo la connessione FTP 
+			// ERRORE chiudo la connessione FTP
 			ftp_quit($conn_id);
 			header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=4");
 			exit;
@@ -270,29 +271,29 @@ if (isset($_POST['conferma'])) { // se confermato
 
 	// avvio il file di interfaccia presente nel sito web remoto
 	$headers = get_headers ($urlinterf.'?access='.$access);
-	
+
 	if ( intval(substr($headers[0], 9, 3))==200){ // controllo se il file esiste o mi dà accesso
-		
+
 		$file = fopen ($urlinterf.'?access='.$access, "r");
 		if (!$file) {
-			// chiudo la connessione FTP 
+			// chiudo la connessione FTP
 			ftp_quit($conn_id);
 			header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=3");
 			exit;
-			
+
 		} else {
-			// chiudo la connessione FTP 
+			// chiudo la connessione FTP
 			ftp_quit($conn_id);
-						
+
 		}
 	} else { // IL FILE INTERFACCIA NON ESISTE > ESCO
-		// chiudo la connessione FTP 
+		// chiudo la connessione FTP
 		ftp_quit($conn_id);
 		header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=2");
 		exit;
 	}
 
-	// chiudo la connessione FTP 
+	// chiudo la connessione FTP
 	ftp_quit($conn_id);
 	header("Location: " . "../../modules/shop-synchronize/export_articoli.php?success=1");
     exit;
@@ -301,7 +302,7 @@ if (isset($_POST['conferma'])) { // se confermato
 	$script_transl = HeadMain();
 }
 
-if (!isset($_GET['success'])){	
+if (!isset($_GET['success'])){
 // Apro il form per la selezione degli articoli
 		?>
 		<script>
@@ -313,15 +314,15 @@ if (!isset($_GET['success'])){
             if (input.getAttribute('type') == 'checkbox'){
                 checkboxes.push(input);
             }
-        } 
+        }
         return checkboxes;
-    }    
+    }
     function check(checks){
       var checkboxes = selectCheckbox();
       for(var i=0; i < checkboxes.length; i++){
         checkboxes[i].checked = checks.checked;
       }
-    }    
+    }
 </script>
 		<form method="POST" name="download" enctype="multipart/form-data">
 			<input type="hidden" name="ritorno" value="<?php echo $_POST['ritorno'];?>" >
@@ -348,7 +349,7 @@ if (!isset($_GET['success'])){
 						<!-- Trigger the modal with a button -->
 						<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#downloader">Aggiorna i prodotti nell'e-commerce</button>
 						<!-- Modal content-->
-						<div id="downloader" class="modal fade" role="dialog">    
+						<div id="downloader" class="modal fade" role="dialog">
 							<div class="modal-dialog modal-content">
 								<div class="modal-header" align="left">
 									<button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -363,7 +364,7 @@ if (!isset($_GET['success'])){
 								</div>
 							</div>
 						</div>
-					</div>						
+					</div>
 				</div>
 				<div class="row bg-info" style="border-bottom: 1px solid;">
 					<div class="col-sm-2">
@@ -372,13 +373,13 @@ if (!isset($_GET['success'])){
 					<div class="col-sm-2">
 						<h4>Codice</h4>
 					</div>
-					<div class="col-sm-5">	
+					<div class="col-sm-5">
 						<h4>Nome</h4>
 					</div>
-					<div class="col-sm-2" align="right">	
+					<div class="col-sm-2" align="right">
 						<h4>Seleziona</h4>
 					</div>
-					<div class="col-sm-1">	
+					<div class="col-sm-1">
 						TUTTI <input type="checkbox" onClick="check(this)">
 					</div>
 				</div>
@@ -396,7 +397,7 @@ if (!isset($_GET['success'])){
 					$avqty = 0;
 					$ordinatic = $gForm->get_magazz_ordinati($item['codice'], "VOR");
 					$mv = $gForm->getStockValue(false, $item['codice']);
-					
+
 						$magval = array_pop($mv);
 						if ($magval){
 							$avqty=$magval['q_g']-$ordinatic;
@@ -404,7 +405,7 @@ if (!isset($_GET['success'])){
 						if ($avqty<0 or $avqty==""){
 							$avqty="0";
 						}
-					
+
 					?>
 					<div class="row bg-success" style="border-bottom: 1px solid;">
 							<div class="col-sm-2">
@@ -421,7 +422,7 @@ if (!isset($_GET['success'])){
 								?>
 							</div>
 							<div class="col-sm-1">
-								<?php 
+								<?php
 								if ($_GET['descri']=="upddes"){ // se devo aggiornare il body_text
 									$body = gaz_dbi_get_row($gTables['body_text'], "table_name_ref", "artico_". $item['codice']);
 									echo '<input type="hidden" name="body_text'. $n .'" value="'. preg_replace('/[\x00-\x1f]/','',htmlspecialchars($body['body_text'])) . '">';
@@ -434,15 +435,16 @@ if (!isset($_GET['success'])){
 								echo '<input type="hidden" name="ref_ecommerce_id_product'. $n .'" value="'. $item['ref_ecommerce_id_product'] .'">';
 								echo '<input type="hidden" name="ecomm_option_attribute'. $n .'" value="'. htmlspecialchars($item['ecomm_option_attribute']) .'">';
 								if ($_GET['img']=="updimg"){ // se devo aggiornare l'immagine ne trovo l'url di GAzie
-									$imgres = gaz_dbi_get_row($gTables['files'], "table_name_ref", "artico", "AND id_ref ='1' AND item_ref = '". $item['codice']."'");
+									unset ($imgres);
+                  $imgres = gaz_dbi_get_row($gTables['files'], "table_name_ref", "artico", "AND id_ref ='1' AND item_ref = '". $item['codice']."'");
 									if (isset($imgres['id_doc']) AND $imgres['id_doc']>0){ // se c'è un'immagine
 										$imgurl=DATA_DIR."files/".$admin_aziend['company_id']."/images/". $imgres['id_doc'] . "." . $imgres['extension'];
-										$imgblob="";
+										$imgblob="";echo "Img HQ";
 									} else {
 										$imgurl="";
 										$imgres['id_doc']="";
 										$imgres['extension']="";
-										$imgblob=$item['image'];
+										$imgblob=$item['image'];echo "Img blob";
 									}
 									echo '<input type="hidden" name="imgurl'. $n .'" value="'. $imgurl .'">';
 									echo '<input type="hidden" name="imgname'. $n .'" value="'. $imgres['id_doc'] . "." . $imgres['extension'] .'">';
@@ -454,18 +456,18 @@ if (!isset($_GET['success'])){
 								<input type="checkbox" name="download<?php echo $n; ?>" value="download">
 								<input type="hidden" name="num_products" value="<?php echo $n; ?>">
 							</div>
-			
+
 					</div>
 				<?php
 				$n++;
 				}
-				
-				
+
+
 				// carico in $parent i gruppi che sono presenti in GAzie
 				$parent = gaz_dbi_query ('SELECT * FROM '.$gTables['artico_group'].' WHERE web_public = \'1\' ORDER BY id_artico_group');
-				
-				while ($item = gaz_dbi_fetch_array($parent)){ // li ciclo					
-					
+
+				while ($item = gaz_dbi_fetch_array($parent)){ // ciclo i PARENT / GRUPPI
+
 					?>
 					<div class="row bg-warning" style="border-bottom: 1px solid;">
 							<div class="col-sm-2">
@@ -482,33 +484,37 @@ if (!isset($_GET['success'])){
 								?>
 							</div>
 							<div class="col-sm-1">
-								<?php 
-								
+								<?php
+
 								echo '<input type="hidden" name="body_text'. $n .'" value="'. preg_replace('/[\x00-\x1f]/','',htmlspecialchars($item['large_descri'])) . '">';
-								
+
 								echo '<input type="hidden" name="web_public'. $n .'" value="'. $item['web_public'] . '">';
 								echo '<input type="hidden" name="quanti'. $n .'" value="">';
 								echo '<input type="hidden" name="aliiva'. $n .'" value="">';
 								echo '<input type="hidden" name="web_price'. $n .'" value="">';
 								echo '<input type="hidden" name="ref_ecommerce_id_main_product'. $n .'" value="'. $item['ref_ecommerce_id_main_product'] .'">';
 								echo '<input type="hidden" name="ref_ecommerce_id_product'. $n .'" value="">';
-								
+
 								if ($_GET['img']=="updimg"){ // se devo aggiornare l'immagine cerco l'url di quella HQ High Quality in GAzie
+                  unset ($imgres);
+                  // NB: al momento i gruppi/parent non gestiscono le immagini HQ
+                  /*  quando verrà gestitata bata decommentare la riga seguente e tutto funziona già
 									$imgres = gaz_dbi_get_row($gTables['files'], "table_name_ref", "artico_group", "AND id_ref ='1' AND item_ref = '". $item['id_artico_group']."'");
-									// Si preferisce l'immagine HQ, in mancanza si invia la blob
+									*/
+                  // Si preferisce l'immagine HQ, in mancanza si invia la blob
 									if (isset($imgres['id_doc']) AND $imgres['id_doc']>0){ // se c'è un'immagine High Quality
 										$imgurl=DATA_DIR."files/".$admin_aziend['company_id']."/images/". $imgres['id_doc'] . "." . $imgres['extension'];
-										$imgblob="";
+										$imgblob="";echo "Img HQ";
 									} else {
 										$imgurl="";
 										$imgres['id_doc']="";
 										$imgres['extension']="";
-										$imgblob=$item['image'];
-										
+										$imgblob=$item['image'];echo "img blob";
+
 									}
 									echo '<input type="hidden" name="imgurl'. $n .'" value="'. $imgurl .'">';
 									echo '<input type="hidden" name="imgname'. $n .'" value="'. $imgres['id_doc'] . "." . $imgres['extension'] .'">';
-									echo '<input type="hidden" name="imgblob'. $n .'" value="'. $imgblob .'">';
+									echo '<input type="hidden" name="imgblob'. $n .'" value="'. base64_encode($imgblob) .'">';
 								}
 								?>
 							</div>
@@ -516,12 +522,12 @@ if (!isset($_GET['success'])){
 								<input type="checkbox" name="download<?php echo $n; ?>" value="download">
 								<input type="hidden" name="num_products" value="<?php echo $n; ?>">
 							</div>
-			
+
 					</div>
 				<?php
 				$n++;
-				}				
-				
+				}
+
 				?>
 				<div class="row bg-info">
 					<div class="col-sm-4">
@@ -533,11 +539,11 @@ if (!isset($_GET['success'])){
 					<div class="col-sm-4" align="right">
 						<!-- Trigger the modal with a button -->
 						<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#downloader">Aggiorna i prodotti nell'e-commerce</button>
-												
-					</div>						
+
+					</div>
 				</div>
-				
-				
+
+
 			</div> <!-- container fluid -->
 		</form>
 <?php
