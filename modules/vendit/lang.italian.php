0d8cb2d125d1b1891c4c8086a6f8fde4be2f96ee
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2022-01-11 18:51:06 +0000

    Mancava il 'dialog_fae_content_PA' nel file lingua. Da completarne il significato...
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@5276 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/vendit/lang.italian.php
+++ modules/vendit/lang.italian.php
@@ -127,6 +127,7 @@ $strScript = array("admin_client.php" =>
         'mail_alert2' => 'con allegato il seguente documento di vendita:',
         'dialog_fae_title' => 'File XML della fattura: ',
         'dialog_fae_filename' => 'Scarica il file: ',
+		'dialog_fae_content_PA' => 'Il file è ..... ',
         'dialog_fae_content_DI' => 'Il file è da inviare. Puoi scegliere di farlo singolarmente oppure impacchettandolo assieme ad eventuali altri precedenti non ancora inviati ',
         'dialog_fae_content_ZI' => 'Il file è da inviare ma fa parte di un pacchetto ZIP, invialo assieme agli altri',
         'dialog_fae_content_RE' => 'Solo qualora si voglia procedere ad un reinvio <b class="bg-warning">A CAUSA DI UNO SCARTO</b>, dopo aver corretto gli errori che lo hanno causato, cliccare su questo link per generare un file con nuovo nome:' ,
