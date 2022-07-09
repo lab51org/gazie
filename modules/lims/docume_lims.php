9f3f60aa2b052d597142d4860962843671848295
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2022-07-09 13:01:52 +0000

    new module (embrional)
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@5540 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/lims/docume_lims.php
@@ -0,0 +1,34 @@
+<?php
+/*
+  --------------------------------------------------------------------------
+  GAzie - Gestione Azienda
+  Copyright (C) 2004-2022 - Antonio De Vincentiis Montesilvano (PE)
+  (http://www.devincentiis.it)
+  <http://gazie.sourceforge.net>
+  --------------------------------------------------------------------------
+  Questo programma e` free software;   e` lecito redistribuirlo  e/o
+  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
+  come e` pubblicata dalla Free Software Foundation; o la versione 2
+  della licenza o (a propria scelta) una versione successiva.
+
+  Questo programma  e` distribuito nella speranza  che sia utile, ma
+  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
+  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
+  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
+
+  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
+  Generica GNU insieme a   questo programma; in caso  contrario,  si
+  scriva   alla   Free  Software Foundation, 51 Franklin Street,
+  Fifth Floor Boston, MA 02110-1335 USA Stati Uniti.
+  --------------------------------------------------------------------------
+*/
+require("../../library/include/datlib.inc.php");
+$admin_aziend=checkAdmin();
+?>
+<div class="help">
+<div class="panel panel-info text-center"><h2>
+<p>Il modulo "LIMS" per GAZIE (Gestione AZIEnda) è in fase di sviluppo, per qualsiasi chiarimento puoi contattare l'autore:</p>
+<p>Antonio De Vincentiis Montesilvano (PE) https://www.devincentiis.it - telefono +39 338 31 21 161</p>
+</h2></div>
+</div>
+</div>
