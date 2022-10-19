4f59be547045f2ea1cb151c5276c0aa1481bcfe7
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2022-10-19 09:59:18 +0000

    per chiudere il bug [#346]
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@5832 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_9.01.sql
+++ setup/install/update_to_9.01.sql
@@ -1,4 +1,5 @@
 UPDATE `gaz_config` SET `cvalue` = '151' WHERE `id` =2;
+ALTER TABLE `gaz_anagraes` COLLATE='utf8_general_ci';
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 UPDATE gaz_XXXtesdoc SET data_ordine = datemi WHERE id_contract >= 1 AND data_ordine < 2020-01-01;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
