0a0f86ca86497fa3b1fd63ce5a1aba92618b4b99
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2022-11-23 09:05:59 +0000

    aggiungo colonna di riferimento all'azienda per consentire le configurazioni personalizzate dell'utente per ogni specifica azienda
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@5899 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_9.01.sql
+++ setup/install/update_to_9.01.sql
@@ -1,6 +1,7 @@
 UPDATE `gaz_config` SET `cvalue` = '151' WHERE `id` =2;
 ALTER TABLE `gaz_anagraes` COLLATE='utf8_general_ci';
 ALTER TABLE `gaz_anagra` ADD COLUMN `fiscal_reg` VARCHAR(4) NULL AFTER `pariva`;
+ALTER TABLE `gaz_admin_config` ADD COLUMN `company_id` INT(3) NULL DEFAULT 0 AFTER `adminid`,	ADD INDEX `company_id` (`company_id`);
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 UPDATE gaz_XXXtesdoc SET data_ordine = datemi WHERE id_contract >= 1 AND data_ordine < 2020-01-01;
 ALTER TABLE `gaz_XXXfae_flux` CHANGE `filename_ori` `filename_ori` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
