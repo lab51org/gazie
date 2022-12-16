641da37f3e81e2f0f50958a2be81e5ac8d6f81f2
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2022-12-16 08:27:52 +0000

    warning php 8.1
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@5977 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/report_artico_scroll.php
+++ modules/camp/report_artico_scroll.php
@@ -79,11 +79,16 @@ if (isset($_POST['rowno'])) { //	Evitiamo errori se lo script viene chiamato dir
 			$mv = $gForm->getStockValue(false, $row['codice']);
 			$magval = array_pop($mv);
 			$decimal_quantity=$admin_aziend['decimal_quantity'];
-			
+
+			if ( filter_var($magval, FILTER_VALIDATE_INT) === false ) {
+      }else{// is an integer
+        unset($magval);
+        $magval['q_g']=0;
+      }
 			if (number_format($magval['q_g'],8)<0){
 				$decimal_quantity=8;
 			}
-			
+
 		} 	else {
 			$magval['q_g'] = 0;
 		}
@@ -162,7 +167,7 @@ if (isset($_POST['rowno'])) { //	Evitiamo errori se lo script viene chiamato dir
 
             <td data-title="<?php echo $script_transl["stock"]; ?>" title="Visualizza scheda prodotto" class="text-center <?php echo $class; ?>">
                <?php $print_magval=str_replace(",","",$magval['q_g']);echo gaz_format_quantity($print_magval,1,$decimal_quantity); echo '<p style="float:right;">'.$com.'</p></td><td title="Visualizza lotti">';
-			  
+
 			   if (intval($row['lot_or_serial'])>0) {
 			   ?>
 			   <a  class="btn btn-info btn-md" href="javascript:;" onclick="window.open('<?php echo"../../modules/magazz/mostra_lotti.php?codice=".$row['codice'];?>', 'titolo', 'menubar=no, toolbar=no, width=800, height=400, left=80%, top=80%, resizable, status, scrollbars=1, location');">
