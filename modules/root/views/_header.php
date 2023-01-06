60bae4aff40d1a9c42daaa5e7184f8a6f3c4edcf
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-01-06 19:15:31 +0000

    Chiedo ai crawler di non indicizzare la pagina di login
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6056 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/root/views/_header.php
+++ modules/root/views/_header.php
@@ -2,8 +2,9 @@
 <html lang="en">
     <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="Antonio De Vincentiis http://www.devincentiis.it">
+    <meta name="author" content="Antonio De Vincentiis https://www.devincentiis.it">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <?php
