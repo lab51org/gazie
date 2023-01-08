58a14d50740eba82f577ce7bc6ec48cad1e57034
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-01-08 14:55:19 +0000

    ho riscontrato che in alcune vecchie installazioni sono rimasti i widget messi tutti sulla root, li tolgo
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6061 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_9.03.php
+++ setup/install/update_to_9.03.php
@@ -59,4 +59,6 @@ while ($row = gaz_dbi_fetch_array($result)) {
 	gaz_dbi_query("UPDATE ". $table_prefix . "_aziend SET descrifae_vat = ( SELECT codice FROM ". $table_prefix . "_" . $aziend_codice."aliiva WHERE tipiva = 'I' AND aliquo = 22.0 LIMIT 1) WHERE codice =".$row["codice"]);
   echo "<p>Azienda n. ".$row["codice"]." aggiunta aliquota per righi descrittivi delle fatture elettroniche</p>";
 }
+// ho riscontrato che in alcune vecchie installazioni sono rimasti i widget messi tutti sulla root, li tolgo
+array_map('unlink', glob('../../modules/root/dash_*.php'));
 ?>
