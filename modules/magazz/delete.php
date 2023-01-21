3c7e6e5c37d170991655cdd72593ac3dee578852
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-01-21 06:52:55 +0000

    Piccola correzione per prevenire sql injection (anche se siamo in una chiamata ajax)
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6120 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/magazz/delete.php
+++ modules/magazz/delete.php
@@ -86,7 +86,7 @@ if (isset($_POST['type'])&&isset($_POST['ref'])) {
       gaz_dbi_del_row($gTables['artico_position'], "id_position", $i);
     break;
     case "blob":
-			$i=$_POST['ref'];
+			$i=substr($_POST['ref'],0,32);
       $res=gaz_dbi_put_query($gTables['artico'], "codice = '".$i."'", 'image', '');
     break;
 	}
