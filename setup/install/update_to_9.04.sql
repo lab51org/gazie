7985ff6a29f52a432473096c5f6c95c1e7d33153
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-02-11 13:22:16 +0000

    qualche indice
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6182 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ setup/install/update_to_9.04.sql
@@ -0,0 +1,5 @@
+UPDATE `gaz_config` SET `cvalue` = '152' WHERE `id` =2;
+-- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
+ALTER TABLE `gaz_XXXrigbro`	CHANGE COLUMN `codice_fornitore` `codice_fornitore` VARCHAR(50) NOT NULL COMMENT 'Mappatura di codart con il codice usato dal fornitore in caso di acquisto, potrebbe essere usato anche per mappare i codici clienti ad es. nella acquisizione ordini da clienti' AFTER `codart`, ADD INDEX `codice_fornitore` (`codice_fornitore`);
+ALTER TABLE `gaz_XXXtesbro`	ADD INDEX `tipdoc` (`tipdoc`);
+-- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
