0be575b9c77c86003ef9acf49ecd36aeb8f25736
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-02-17 11:30:28 +0000

    La documentazione del modulo camp non caricava correttamente l'immagine icona
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6197 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/camp/docume_camp.php
+++ modules/camp/docume_camp.php
@@ -6,42 +6,40 @@
 	  (http://www.devincentiis.it)
 	  <http://gazie.sourceforge.net>
 	  --------------------------------------------------------------------------
-	  REGISTRO DI CAMPAGNA è un modulo creato per GAzie da Antonio Germani, Massignano AP 
-	  Copyright (C) 2018-2021 - Antonio Germani, Massignano (AP)
-	  https://www.lacasettabio.it 
+	  REGISTRO DI CAMPAGNA è un modulo creato per GAzie da Antonio Germani, Massignano AP
+	  Copyright (C) 2018-2023 - Antonio Germani, Massignano (AP)
+	  https://www.lacasettabio.it
 	  https://www.programmisitiweb.lacasettabio.it
 	  --------------------------------------------------------------------------
 	  Questo programma e` free software;   e` lecito redistribuirlo  e/o
 	  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
 	  come e` pubblicata dalla Free Software Foundation; o la versione 2
 	  della licenza o (a propria scelta) una versione successiva.
-	
+
 	  Questo programma  e` distribuito nella speranza  che sia utile, ma
 	  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
 	  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
 	  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
-	
+
 	  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
 	  Generica GNU insieme a   questo programma; in caso  contrario,  si
 	  scriva   alla   Free  Software Foundation,  Inc.,   59
 	  Temple Place, Suite 330, Boston, MA 02111-1307 USA Stati Uniti.
-	  --------------------------------------------------------------------------	 
+	  --------------------------------------------------------------------------
 	  # free to use, Author name and references must be left untouched  #
-	  --------------------------------------------------------------------------	  
+	  --------------------------------------------------------------------------
 */
 require("../../library/include/datlib.inc.php");
 
 $admin_aziend=checkAdmin();
 
-//require("../../library/include/header.php");
-//$script_transl=HeadMain();
-//
-// Se esiste, viene incluso il file "help/italian/docume_magazz_help.php",
+
+// Se esiste, viene incluso il file "help/italian/docume_camp_help.php",
 // o l'equivalente di un altro linguaggio.
 //
 echo '<div class="help">';
-if (file_exists("help/".$admin_aziend['lang']."/docume_magazz_help.php")) {
-    include("help/".$admin_aziend['lang']."/docume_magazz_help.php");
+if (file_exists("help/".$admin_aziend['lang']."/docume_camp_help.php")) {
+    include("help/".$admin_aziend['lang']."/docume_camp_help.php");
 }
 ?>
 </div>
