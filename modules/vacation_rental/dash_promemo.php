72b39e456972f9861b296e955cfd4734272f059b
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-02-26 08:35:30 +0000

    Vacation rental: predisposto widget promemoria
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6256 0ed6177e-08db-47c9-9b37-4a62b3198fc7

---
+++ modules/vacation_rental/dash_promemo.php
@@ -0,0 +1,48 @@
+<?php
+/*
+	  --------------------------------------------------------------------------
+	  GAzie - Gestione Azienda
+	  Copyright (C) 2004-2023 - Antonio De Vincentiis Montesilvano (PE)
+	  (http://www.devincentiis.it)
+	  <http://gazie.sourceforge.net>
+	  --------------------------------------------------------------------------
+	 VACATION RENTAL è un modulo creato per GAzie da Antonio Germani, Massignano AP
+	  Copyright (C) 2022-2023 - Antonio Germani, Massignano (AP)
+	  https://www.lacasettabio.it
+	  https://www.programmisitiweb.lacasettabio.it
+	  --------------------------------------------------------------------------
+	  Questo programma e` free software;   e` lecito redistribuirlo  e/o
+	  modificarlo secondo i  termini della Licenza Pubblica Generica GNU
+	  come e` pubblicata dalla Free Software Foundation; o la versione 2
+	  della licenza o (a propria scelta) una versione successiva.
+
+	  Questo programma  e` distribuito nella speranza  che sia utile, ma
+	  SENZA   ALCUNA GARANZIA; senza  neppure  la  garanzia implicita di
+	  NEGOZIABILITA` o di  APPLICABILITA` PER UN  PARTICOLARE SCOPO.  Si
+	  veda la Licenza Pubblica Generica GNU per avere maggiori dettagli.
+
+	  Ognuno dovrebbe avere   ricevuto una copia  della Licenza Pubblica
+	  Generica GNU insieme a   questo programma; in caso  contrario,  si
+	  scriva   alla   Free  Software Foundation,  Inc.,   59
+	  Temple Place, Suite 330, Boston, MA 02111-1307 USA Stati Uniti.
+	  --------------------------------------------------------------------------
+	  # free to use, Author name and references must be left untouched  #
+	  --------------------------------------------------------------------------
+*/
+
+?>
+<div class="panel panel-info col-sm-12">
+<?php
+
+?>
+<div class="box-header bg-info">
+	<h4 class="box-title"><i class="glyphicon glyphicon-transfer"></i> Promemoria prossimi 7 giorni</h4>
+	<a class="pull-right dialog_grid" id_bread="<?php echo $grr['id_bread']; ?>" style="cursor:pointer;"><i class="glyphicon glyphicon-cog"></i></a>
+</div>
+<div class="box-body">
+	<h4> Da sviluppare </h4>
+</div>
+<?php
+
+?>
+</div>
