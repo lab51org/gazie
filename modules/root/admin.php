3b33138b1a6c2d4f3808dcc7821199edc2c8bcee
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-03-04 07:44:46 +0000

    A seguire per il commit precedente
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6300 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/root/admin.php
+++ modules/root/admin.php
@@ -30,8 +30,6 @@ $rescomp=gaz_dbi_get_row($gTables['config'], 'variable', 'users_noadmin_all_comp
 $company_choice = ($rescomp)?$rescomp['cvalue']:'';
 require( "../../modules/vendit/lib.function.php");
 $lm = new lotmag;
-$schedule_view = gaz_dbi_get_row($gTables['company_config'], 'var', 'schedule_view');
-
 
 if (!isset($_POST['hidden_req'])) {
     $form['hidden_req'] = '';
