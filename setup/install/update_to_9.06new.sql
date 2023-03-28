78e2c88f6f9f8ebe24d01808087207c73bc53976
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-03-28 20:44:18 +0000

    precisazione dovuta
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6411 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- setup/install/update_to_9.06new.sql
+++ setup/install/update_to_9.06new.sql
@@ -5,4 +5,5 @@ UPDATE `gaz_config` SET `cvalue` = JSON_MERGE_PRESERVE(cvalue, JSON_OBJECT('VOL'
 -- START_WHILE ( questo e' un tag che serve per istruire install.php ad INIZIARE ad eseguire le query seguenti su tutte le aziende dell'installazione)
 ALTER TABLE `gaz_XXXclfoco`	ADD INDEX (`descri`);
 ALTER TABLE `gaz_XXXrigbro`	CHANGE COLUMN `tiprig` `tiprig` INT(2) NOT NULL DEFAULT '0' AFTER `id_tes`;
+ALTER TABLE `gaz_XXXrigbro`	CHANGE COLUMN `id_doc` `id_doc` INT(9) NOT NULL DEFAULT '0' COMMENT 'può essere usato come riferimento ad un figlio anche se in rigdoc (es. se questo è il rigo di un ordine evaso) o ad un genitore (es. se questo è un task di un diagramma di Gantt anch\'esso in rigbro)' AFTER `delivery_date`;
 -- STOP_WHILE ( questo e' un tag che serve per istruire install.php a SMETTERE di eseguire le query su tutte le aziende dell'installazione )
