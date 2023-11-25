13a828339775ad92a22b4bb6977225f83e73e811
devincen <devincen@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2023-11-25 09:28:10 +0000

    La richiesta di preventivo a fornitore veniva inviata sempre all'indirizzo email dell'anagrafica e non su quella eventualmente diverso scelto o scritto sul dialog
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6617 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/acquis/stampa_prefor.php
+++ modules/acquis/stampa_prefor.php
@@ -32,14 +32,36 @@ if ($tesbro['tipdoc'] <> 'APR') {
     header("Location: report_broacq.php?flt_tipo='APR'");
     exit;
 }
+function get_template_lang($clfoco) {
+  global $gTables;
+  $lang = false;
+	$rs_customer_language = gaz_dbi_dyn_query("sef",
+	$gTables['clfoco']." LEFT JOIN ".$gTables['anagra']." ON ".$gTables['clfoco'].".id_anagra = ".$gTables['anagra'].".id
+	LEFT JOIN ".$gTables['languages']." ON ".$gTables['anagra'].".id_language = ".$gTables['languages'].".lang_id",$gTables['clfoco'].".codice = ".$clfoco);
+    if ($rs_customer_language->num_rows > 0) {
+        $customer_language = gaz_dbi_fetch_array($rs_customer_language)['sef'];
+		if (!empty($customer_language)) {
+			switch ($customer_language) {
+				case 'en':
+					$lang = 'english';
+					break;
+				case 'es':
+					$lang = 'espanol';
+					break;
+			}
+		}
+	}
+  return $lang;
+}
+$lang = get_template_lang($tesbro['clfoco']);
 if (isset($_GET['dest'])){
   if ($_GET['dest']=='E'){ //  invio  mail all'indirizzo in testata o in alternativa se sta sul fornitore
   } else { // in dest ho l'indirizzo email quindi lo setto in testata e poi procedo all'invio
 	$tesbro['email']=filter_var($_GET['dest'], FILTER_VALIDATE_EMAIL);
   	$r=gaz_dbi_put_row($gTables['tesbro'], 'id_tes', $testat, 'email',$tesbro['email']);
-  }	
-  createDocument($tesbro, 'PreventivoFornitore',$gTables,'rigbro','E');
+  }
+  createDocument($tesbro, 'PreventivoFornitore',$gTables,'rigbro',$tesbro['email'], $lang, false);
 } else {
-  createDocument($tesbro, 'PreventivoFornitore',$gTables,'rigbro');
+  createDocument($tesbro, 'PreventivoFornitore',$gTables,'rigbro',false, $lang);
 }
 ?>
