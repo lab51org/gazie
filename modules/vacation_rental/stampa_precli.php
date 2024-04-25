4f71a10b4ef290bbcec4d9378a23484f739d6723
tonybio <tonybio@0ed6177e-08db-47c9-9b37-4a62b3198fc7>
2024-04-25 10:17:13 +0000

    Corretto anno copyright
    git-svn-id: https://svn.code.sf.net/p/gazie/code/trunk@6778 0ed6177e-08db-47c9-9b37-4a62b3198fc7

--- modules/vacation_rental/stampa_precli.php
+++ modules/vacation_rental/stampa_precli.php
@@ -2,13 +2,13 @@
 /*
   --------------------------------------------------------------------------
   GAzie - MODULO 'VACATION RENTAL'
-  Copyright (C) 2022-2023 - Antonio Germani, Massignano (AP)
+  Copyright (C) 2022-2024 - Antonio Germani, Massignano (AP)
   (http://www.programmisitiweb.lacasettabio.it)
 
   --------------------------------------------------------------------------
  --------------------------------------------------------------------------
                             GAzie - Gestione Azienda
-    Copyright (C) 2004-2023 - Antonio De Vincentiis Montesilvano (PE)
+    Copyright (C) 2004-2024 - Antonio De Vincentiis Montesilvano (PE)
          (http://www.devincentiis.it)
            <http://gazie.sourceforge.net>
  --------------------------------------------------------------------------
